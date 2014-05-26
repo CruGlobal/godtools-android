120248491d41cd336508fe62ef472671186255eb
John Perez <john.perez@pointwest.com.ph>
2014-05-26 15:00:07 +0800

    Added crittercism crash reporting

--- src/org/keynote/godtools/android/Splash.java
+++ src/org/keynote/godtools/android/Splash.java
@@ -1,16 +1,12 @@
 package org.keynote.godtools.android;
 
-import org.keynote.godtools.android.R;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
-import android.view.Window;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.LinearLayout;
+
+import com.crittercism.app.Crittercism;
 
 public class Splash extends Activity {
     protected boolean _active = true;
@@ -21,7 +17,10 @@ public class Splash extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-        
+
+        // Enable crash reporting
+        Crittercism.initialize(getApplicationContext(), getString(R.string.key_crittercism));
+
         //The following was tried to help in 3.0 but didnt do the trick:
         //LinearLayout layout = (LinearLayout)findViewById(R.id.splashscreen_layout);        
         //LayoutParams lp = layout.getLayoutParams();
