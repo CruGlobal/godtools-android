dc3741b47bb6d480c227e61f2b6bfeb94911e3ef
John Perez <john.perez@pointwest.com.ph>
2014-06-19 20:36:01 +0800

    Main.java - revert to initial version, this will only serve as a reference for creating the new main activity
    MainPW.java
    - this will be the new main activity
    - currently fetch the package list from the db and then display it on a list
    - checks language setting difference every time this activity is resumed
    
    Splash.java
    - copies the english resources from assets to internal directory on first launch
    - downloads new language resources if needed
    - save the package information on db
    
    Created dialog that asks the user if he wants to change the default language
    Created a list fragment, used in the main activity to display package list

--- src/org/keynote/godtools/android/Main.java
+++ src/org/keynote/godtools/android/Main.java
@@ -25,24 +25,8 @@ import android.widget.AbsoluteLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import org.keynote.godtools.android.business.GTPackage;
-import org.keynote.godtools.android.http.GodToolsApiClient;
-import org.keynote.godtools.android.http.HttpTask;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-
-public class Main extends Activity implements HttpTask.HttpTaskHandler {
+public class Main extends Activity {
 	private static final String TAG = "Main";
 
 	private static final String PREFS_NAME = "MAIN";
@@ -55,18 +39,18 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
 	private int					mPageWidth;
 	private int					mPageHeight;
 	private boolean				mSetupNeeded;
-
+	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(
-        		WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        		WindowManager.LayoutParams.FLAG_FULLSCREEN, 
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
-
+        
         setContentView(R.layout.main);
-
+        
         // Make the Settings button highlight when pressed (without defining a separate image)
         ImageButton button = (ImageButton) findViewById(R.id.homescreen_settings_button);
         button.setOnTouchListener(new OnTouchListener() {
@@ -74,8 +58,8 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
         	public boolean onTouch(View arg0, MotionEvent me) {
         		ImageButton button = (ImageButton)arg0;
         		Drawable d = button.getBackground();
-        		PorterDuffColorFilter grayFilter =
-        			    new PorterDuffColorFilter(Color.LTGRAY, PorterDuff.Mode.SRC_ATOP);
+        		PorterDuffColorFilter grayFilter =  
+        			    new PorterDuffColorFilter(Color.LTGRAY, PorterDuff.Mode.SRC_ATOP); 
 
         		if (me.getAction() == MotionEvent.ACTION_DOWN) {
         			d.setColorFilter(grayFilter);
@@ -89,9 +73,9 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
         		}
         		else
         			return false;
-        	}
-        });
-
+        	}         
+        });        
+        
 		// restore state from prefs
 		//SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
 		//E.g.:
@@ -99,7 +83,7 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
 		//setLanguage        (settings.getString("currLanguageCode", getLanguageDefault()));
 		mSetupNeeded = true;
     }
-
+  
 
     @Override
     public void onStart()
@@ -107,7 +91,7 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
        super.onStart();
        Log.d(TAG, "onStart");
     }
-
+    
     @Override
     public void onStop()
     {
@@ -128,14 +112,14 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
 	@Override
 	protected void onResume() {
 		super.onResume();
-		// Setup is only done on first resume,
+		// Setup is only done on first resume, 
 		// else we resize the screen elements smaller and smaller each time.
 		if (mSetupNeeded) {
-			doSetup(1000); 	// 1 second delay required to make sure activity fully created
+			doSetup(1000); 	// 1 second delay required to make sure activity fully created 
 							// - is there something we can test for that is better than a fixed timeout?
 		}
 	}
-
+	
 	private void doSetup(int delay) {
 		new Handler().postDelayed(new Runnable() {
 			public void run() {
@@ -144,16 +128,16 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
 				showTheHomeScreen();
 				mSetupNeeded = false;
 			}
-		}, delay);  // delay can be required to make sure activity fully created - is there something we can test for that is better than a fixed timeout?
-
+		}, delay);  // delay can be required to make sure activity fully created - is there something we can test for that is better than a fixed timeout?      		
+		
 	}
-
+	
 	private void resizeTheApp() {
 		Rect r = new Rect();
 	    Window w = getWindow();
 	    w.getDecorView().getWindowVisibleDisplayFrame(r);
-	    // we want to ignore the status bar
-	    // since we have asked for it not to be displayed
+	    // we want to ignore the status bar 
+	    // since we have asked for it not to be displayed 
 	    // but getWindowVisibleDisplayFrame is still reporting as if it was there
 	    r.top = 0;
 	    int width  = r.width();
@@ -165,13 +149,13 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
 	    double aspectRatio       = (double)r.width()                   / (double)r.height();
 	    if (aspectRatio > aspectRatioTarget) {
 	    	height = r.height();
-	    	width  = (int)Math.round(height * aspectRatioTarget);
+	    	width  = (int)Math.round(height * aspectRatioTarget);	    	
 	    }
 	    else {
 	    	width  = r.width();
-	    	height = (int)Math.round(width  / aspectRatioTarget);
+	    	height = (int)Math.round(width  / aspectRatioTarget);	    		    	
 	    }
-
+	    
     	// Update layout to set the size we have decided to use instead of FILL_PARENT
         View container = findViewById(R.id.homescreen_container);
         AbsoluteLayout.LayoutParams lp = (AbsoluteLayout.LayoutParams)container.getLayoutParams();
@@ -179,19 +163,19 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
         lp.height = height;
         // and also center it
         left = r.left + (r.width () - width )/2;
-        top  = 0      + (r.height() - height)/2; // r is from top of screen , lp is from bottom of status bar
+        top  = 0      + (r.height() - height)/2; // r is from top of screen , lp is from bottom of status bar 
         // (also note that iphone status bar is 20, Android in small layout is still 25. So we lose 5 pixels, 23 at top, 2 at bottom!)
-
+        
         lp.x = left;
         lp.y = top;
         container.setLayoutParams(lp);
-
+	        
 	    mPageLeft	= left;
 	    mPageTop	= top;
 	    mPageWidth	= width;
 	    mPageHeight	= height;
 	}
-
+	
 	private void createTheHomeScreen() {
 		Context	context = getApplicationContext();
 // 		Bitmap bm = context.getResources().getDrawable(R.drawable.homescreen_godtools_logo); //   getBitmapFromAssetOrFile(context, backgroundImage);
@@ -221,19 +205,19 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
             }
         }
 	}
-
+	
 	private void showTheHomeScreen() {
 		// Now that it is resized - show it
 		// TODO: Can we fade it in?
 		ViewGroup container = (ViewGroup) findViewById(R.id.homescreen_container);
 		container.setVisibility(View.VISIBLE);
-
+		
 	}
-
-
+	
+	
 	private float getScaledTextSize(float textSize) {
 		final float scale = getApplicationContext().getResources().getDisplayMetrics().density;
-		return (textSize * (float)mPageHeight / (float)REFERENCE_DEVICE_HEIGHT )/scale;
+		return (textSize * (float)mPageHeight / (float)REFERENCE_DEVICE_HEIGHT )/scale;		
 	}
 
 	private int getScaledXValue(int x) {
@@ -242,7 +226,7 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
 	private int getScaledYValue(int y) {
 		return (int)Math.round((double)(y * mPageHeight) / (double)REFERENCE_DEVICE_HEIGHT);
 	}
-
+	
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -256,48 +240,48 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
             case R.id.CMD_SETTINGS:
             	onCmd_settings(null);
                 return true;
-
+               
             case R.id.CMD_QUIT:
             	quit();
             	return true;
         }
-
+        
         return false;
     }
-
+ 	
 
 	private void addPageFrameToIntent (Intent intent) {
 		// TODO: Ought this to be replaced by some sort of app-wide storage
        	intent.putExtra("PageLeft"  , mPageLeft);
     	intent.putExtra("PageTop"   , mPageTop);
     	intent.putExtra("PageWidth" , mPageWidth);
-    	intent.putExtra("PageHeight", mPageHeight);
+    	intent.putExtra("PageHeight", mPageHeight);		
 	}
-
+	
 	public void onCmd_kgp(View view)
 	{
     	Intent intent = new Intent(this,LanguagesActivity.class);
     	intent.putExtra("PackageName", "kgp");
     	addPageFrameToIntent(intent);
-     	startActivity(intent);
+     	startActivity(intent);		
 	}
-
+    
 	public void onCmd_satisfied(View view)
 	{
 		Intent intent = new Intent(this,LanguagesActivity.class);
     	intent.putExtra("PackageName", "satisfied");
     	addPageFrameToIntent(intent);
-    	startActivity(intent);
+    	startActivity(intent);		
 	}
-
+    
 	public void onCmd_fourlaws(View view)
 	{
 		Intent intent = new Intent(this,LanguagesActivity.class);
     	intent.putExtra("PackageName", "fourlaws");
     	addPageFrameToIntent(intent);
-    	startActivity(intent);
+    	startActivity(intent);		
 	}
-
+    
 	public void onCmd_everystudent(View view)
 	{
     	Intent intent = new Intent(this,LanguagesActivity.class);
@@ -305,79 +289,16 @@ public class Main extends Activity implements HttpTask.HttpTaskHandler {
     	addPageFrameToIntent(intent);
     	startActivity(intent);
 	}
-
+	
 	public void onCmd_settings(View view) {
 	   	Intent intent = new Intent(this,About.class);
     	startActivity(intent);
 	}
-
+ 
     private void quit()
     {
        super.onDestroy();
        this.finish();
     }
-
-    private List<GTPackage> getPackages(String langCode){
-        return GTPackage.getPackageByLanguage(getApplicationContext(), langCode);
-    }
-
-    private void getPackagesFromServer(String langCode){
-        GodToolsApiClient.getListOfPackages(langCode, "tag", this);
-    }
-
-    @Override
-    public void httpTaskComplete(String url, InputStream is, int statusCode, String tag) {
-        List<GTPackage> packageList = processResponse(is);
-    }
-
-    @Override
-    public void httpTaskFailure(String url, InputStream is, int statusCode, String tag) {
-
-    }
-
-    private List<GTPackage> processResponse(InputStream is){
-
-        List<GTPackage> packageList = new ArrayList<GTPackage>();
-
-        Document xmlDoc;
-        try {
-            xmlDoc = DocumentBuilderFactory
-                    .newInstance()
-                    .newDocumentBuilder()
-                    .parse(is);
-            xmlDoc.normalize();
-
-            NodeList nlLanguage = xmlDoc.getElementsByTagName("language");
-            String language = ((Element)nlLanguage.item(0)).getAttribute("code");
-
-            NodeList nlPackages = xmlDoc.getElementsByTagName("package");
-            int numPackages = nlPackages.getLength();
-
-            for (int i=0; i<numPackages; i++) {
-                Element element = (Element)nlPackages.item(i);
-
-                String name = element.getAttribute("name");
-                String code = element.getAttribute("code");
-                int version = Integer.valueOf(element.getAttribute("version"));
-
-                GTPackage gtp = new GTPackage();
-                gtp.setCode(code);
-                gtp.setName(name);
-                gtp.setVersion(version);
-                gtp.setLanguage(language);
-
-                packageList.add(gtp);
-            }
-
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-
-
-        return packageList;
-    }
+	
 }
--- src/org/keynote/godtools/android/Splash.java
+++ src/org/keynote/godtools/android/Splash.java
@@ -1,70 +1,236 @@
 package org.keynote.godtools.android;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import com.crittercism.app.Crittercism;
+import org.keynote.godtools.android.business.GTPackage;
+import org.keynote.godtools.android.business.GTPackageReader;
+import org.keynote.godtools.android.dao.DBAdapter;
+import org.keynote.godtools.android.http.DownloadTask;
+import org.keynote.godtools.android.http.GodToolsApiClient;
+import org.keynote.godtools.android.snuffy.SnuffyApplication;
+import org.keynote.godtools.android.utils.Device;
 
-public class Splash extends Activity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+
+public class Splash extends Activity implements DownloadTask.DownloadTaskHandler {
     protected boolean _active = true;
     protected int _splashTime = 2000;
-    
-    /** Called when the activity is first created. */
+    private static final String PREFS_NAME = "GodTools";
+
+    private String languagePhone;
+
+    TextView tvTask;
+    ProgressBar progressBar;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
+        setContentView(R.layout.splash_pw);
+        tvTask = (TextView) findViewById(R.id.tvTask);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         // Enable crash reporting
-        Crittercism.initialize(getApplicationContext(), getString(R.string.key_crittercism));
+        //Crittercism.initialize(getApplicationContext(), getString(R.string.key_crittercism));
 
-        //The following was tried to help in 3.0 but didnt do the trick:
-        //LinearLayout layout = (LinearLayout)findViewById(R.id.splashscreen_layout);        
-        //LayoutParams lp = layout.getLayoutParams();
-		//Rect r = new Rect();
-	    //Window w = getWindow();
-	    //w.getDecorView().getWindowVisibleDisplayFrame(r);
-        //lp.height = r.height();
-        //layout.setLayoutParams(lp);
-        
-        final Intent intent = new Intent(this,Main.class);
-        
-        // thread for displaying the SplashScreen
-        Thread splashThread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    int waited = 0;
-                    while(_active && (waited < _splashTime)) {
-                        sleep(100);
-                        if(_active) {
-                            waited += 100;
+        if (isFirstLaunch()) {
+
+            new PrepareInitialContentTask((SnuffyApplication) getApplication()).execute((Void) null);
+
+        } else {
+            // thread for displaying the SplashScreen
+            Thread splashThread = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        int waited = 0;
+                        while (_active && (waited < _splashTime)) {
+                            sleep(100);
+                            if (_active) {
+                                waited += 100;
+                            }
                         }
+                    } catch (InterruptedException e) {
+                        // do nothing
+                    } finally {
+
+                        goToMainActivity();
+
                     }
-                } catch(InterruptedException e) {
-                    // do nothing
-                } finally {
-                    finish();
-                    startActivity(intent);
                 }
-            }
-        };
-        splashThread.start();
+            };
+            splashThread.start();
+        }
     }
-    
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         _active = false;
         return true;
     }
-    
+
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent msg)
-    {
-    	_active = false;
-    	return true;
+    public boolean onKeyDown(int keyCode, KeyEvent msg) {
+        _active = false;
+        return true;
+    }
+
+    private boolean isFirstLaunch() {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean isFirst = settings.getBoolean("firstLaunch", true);
+        if (isFirst) {
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("firstLaunch", false);
+            editor.commit();
+        }
+        return isFirst;
+    }
+
+    private void showLoading() {
+        tvTask.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideLoading() {
+        tvTask.setVisibility(View.GONE);
+        progressBar.setVisibility(View.GONE);
+    }
+
+    private boolean shouldUpdateLanguageSettings() {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        String langPrimary = settings.getString("languagePrimary", "en");
+        String langPhone = Device.getDefaultLanguage();
+
+        return !langPrimary.equalsIgnoreCase(langPhone);
+    }
+
+    /**
+     * Copies the english resources from assets to internal storage,
+     * then saves package information on the database.
+     */
+    private class PrepareInitialContentTask extends AsyncTask<Void, Void, Void> {
+
+        Context mContext;
+        SnuffyApplication mApp;
+        File documentsDir;
+        DBAdapter adapter;
+
+        public PrepareInitialContentTask(SnuffyApplication app) {
+            mContext = app.getApplicationContext();
+            documentsDir = app.getDocumentsDir();
+            adapter = DBAdapter.getInstance(mContext);
+            mApp = app;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            tvTask.setText("Copying files from assets...");
+            showLoading();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            AssetManager manager = mContext.getAssets();
+
+            try {
+                String[] files = manager.list("english");
+                for (String fileName : files) {
+                    InputStream is = manager.open("english/" + fileName);
+                    File outFile = new File(documentsDir, fileName);
+                    OutputStream os = new FileOutputStream(outFile);
+
+                    copyFile(is, os);
+                    is.close();
+                    is = null;
+                    os.flush();
+                    os.close();
+                    os = null;
+                }
+
+                InputStream contentsStream = manager.open("contents.xml");
+                List<GTPackage> packageList = GTPackageReader.processContentFile(contentsStream);
+
+                adapter.open();
+                for (GTPackage gtp : packageList) {
+                    adapter.insertGTPackage(gtp);
+                }
+                adapter.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+
+            if (Device.isConnected(mContext) && shouldUpdateLanguageSettings()) {
+
+                languagePhone = Device.getDefaultLanguage();
+                tvTask.setText(String.format("Downloading %s package...", languagePhone));
+                GodToolsApiClient.downloadLanguagePack(mApp, languagePhone, "", Splash.this);
+
+            } else {
+
+                goToMainActivity();
+
+            }
+        }
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+
+    @Override
+    public void downloadTaskComplete(String url, String filePath, String tag) {
+
+        hideLoading();
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("languagePrimary", languagePhone);
+        editor.commit();
+
+        goToMainActivity();
+
+    }
+
+    @Override
+    public void downloadTaskFailure(String url, String filePath, String tag) {
+        // TODO: show dialog to inform the user that the download failed
+        finish();
+    }
+
+    private void goToMainActivity(){
+        Intent intent = new Intent(this, MainPW.class);
+        finish();
+        startActivity(intent);
     }
 }
