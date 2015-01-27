27b6731bc655198f544278334b539511111a89ba
ryancarlson <ryan.t.carlson@cru.org>
2015-01-27 14:40:55 -0500

    compiler error, static reference

--- src/org/keynote/godtools/android/Splash.java
+++ src/org/keynote/godtools/android/Splash.java
@@ -38,7 +38,7 @@ import java.util.Locale;
 
 public class Splash extends Activity implements DownloadTask.DownloadTaskHandler, MetaTask.MetaTaskHandler
 {
-	private static final String LOG_TAG = Splash.getClass().getSimpleName();
+	private static final String LOG_TAG = Splash.class.getSimpleName();
 
 	protected boolean _active = true;
 	protected int _splashTime = 2000;
