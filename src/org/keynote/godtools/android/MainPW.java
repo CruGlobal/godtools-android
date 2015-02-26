33d0fb74381003f4b2e366ff270c017f7de3f880
Matthew Frederick <matthew.frederick@cru.org>
2015-02-26 16:24:57 -0500

    further updates for Anne - add information dialog for alternate languages not available

--- src/org/keynote/godtools/android/MainPW.java
+++ src/org/keynote/godtools/android/MainPW.java
@@ -22,6 +22,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
--- src/org/keynote/godtools/android/SnuffyPWActivity.java
+++ src/org/keynote/godtools/android/SnuffyPWActivity.java
@@ -1,8 +1,10 @@
 package org.keynote.godtools.android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
@@ -555,22 +557,39 @@ public class SnuffyPWActivity extends Activity
 
     private void switchLanguage()
     {
-
-        if (isUsingPrimaryLanguage)
+        if (isParallelLanguageSet && mParallelPackage != null)
         {
-            mConfigFileName = mConfigParallel;
-            isUsingPrimaryLanguage = false;
+            if (isUsingPrimaryLanguage)
+            {
+                mConfigFileName = mConfigParallel;
+                isUsingPrimaryLanguage = false;
 
+            }
+            else
+            {
+                mConfigFileName = mConfigPrimary;
+                isUsingPrimaryLanguage = true;
+            }
+
+            mPager.setAdapter(null);
+            doSetup(0);
         }
         else
         {
-            mConfigFileName = mConfigPrimary;
-            isUsingPrimaryLanguage = true;
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.alternate_language)
+                    .setMessage(R.string.alternate_language_message)
+                    .setNeutralButton(R.string.ok, new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i)
+                        {
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .create();
+            builder.show();
         }
-
-
-        mPager.setAdapter(null);
-        doSetup(0);
 
     }
 
@@ -604,10 +623,7 @@ public class SnuffyPWActivity extends Activity
         // enable this feature if the the parallel language is set
         // and a translation is available for this package
         MenuItem switchItem = menu.findItem(R.id.CMD_SWITCH_LANGUAGE);
-        if (isParallelLanguageSet && mParallelPackage != null)
-            switchItem.setVisible(true);
-        else
-            switchItem.setVisible(false);
+        switchItem.setVisible(true);
 
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
 
