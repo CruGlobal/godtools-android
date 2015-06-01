d508988a0aabbcd86b0a7ebd252ca60412f7a656
Matthew Frederick <matthew.frederick@cru.org>
2015-06-01 16:02:48 -0400

    remove unused methods.

--- src/org/keynote/godtools/android/utils/Strings.java
+++ src/org/keynote/godtools/android/utils/Strings.java
@@ -10,15 +10,6 @@ public final class Strings
     private Strings() {
     }
 
-    public static String nullToEmpty(@Nullable String string) {
-        return string == null?"":string;
-    }
-
-    @Nullable
-    public static String emptyToNull(@Nullable String string) {
-        return isNullOrEmpty(string)?null:string;
-    }
-
     public static boolean isNullOrEmpty(@Nullable String string) {
         return string == null || string.length() == 0;
     }
