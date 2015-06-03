16fb0f0e90f606c9231d32ec4c1d30e17a4a686b
Matthew Frederick <matthew.frederick@cru.org>
2015-06-03 14:26:22 -0400

    ok so these were not duplicates.

--- src/org/keynote/godtools/android/snuffy/PackageReader.java
+++ src/org/keynote/godtools/android/snuffy/PackageReader.java
@@ -1319,12 +1319,12 @@ public class PackageReader
             int diSize = buttonHeight;
             if (bExpanded)
             {
-                iv.setImageResource(R.drawable.expander_ic_maximized);
+                iv.setImageResource(R.drawable.disclosure_indicator_maximized);
                 iv.setLayoutParams(new SnuffyLayoutParams(diSize, diSize, containerWidth - diSize - getScaledXValue(0), lp.y + 2));
             }
             else
             {
-                iv.setImageResource(R.drawable.expander_ic_minimized);
+                iv.setImageResource(R.drawable.disclosure_indicator_minimized);
                 iv.setLayoutParams(new SnuffyLayoutParams(diSize, diSize, containerWidth - diSize + getScaledXValue(5), lp.y + 2));
             }
             theContainer.addView(iv);
