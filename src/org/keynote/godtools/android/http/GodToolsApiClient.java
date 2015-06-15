8948faeb48c7a6911886ee66ab62c13965e64161
Matthew Frederick <matthew.frederick@cru.org>
2015-06-15 10:48:00 -0400

    go back to using the prod database.

--- src/org/keynote/godtools/android/http/GodToolsApiClient.java
+++ src/org/keynote/godtools/android/http/GodToolsApiClient.java
@@ -11,8 +11,8 @@ import java.util.UUID;
 
 public class GodToolsApiClient {
 
-    private static final String BASE_URL = "http://GodToolsAPI-Stage-1291189452.us-east-1.elb.amazonaws.com/godtools-api/rest/";
-    // private static final String BASE_URL = "https://api.godtoolsapp.com/godtools-api/rest/";
+    // private static final String BASE_URL = "http://GodToolsAPI-Stage-1291189452.us-east-1.elb.amazonaws.com/godtools-api/rest/";
+    private static final String BASE_URL = "https://api.godtoolsapp.com/godtools-api/rest/";
     private static final String ENDPOINT_META = "meta/";
     private static final String ENDPOINT_PACKAGES = "packages/";
     private static final String ENDPOINT_TRANSLATIONS = "translations/";
