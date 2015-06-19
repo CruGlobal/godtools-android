866d5b3388a4aecc8d75b97a1e7e777ff9f382dc
Matthew Frederick <matthew.frederick@cru.org>
2015-06-19 11:36:05 -0400

    a little more reformating

--- src/org/keynote/godtools/android/utils/Device.java
+++ src/org/keynote/godtools/android/utils/Device.java
@@ -6,17 +6,20 @@ import android.net.NetworkInfo;
 
 import org.keynote.godtools.android.snuffy.SnuffyApplication;
 
-public class Device {
+public class Device
+{
 
 
-
-    public static boolean isConnected(Context context) {
+    public static boolean isConnected(Context context)
+    {
 
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo[] allNetworks = cm.getAllNetworkInfo();
 
-        for (NetworkInfo networkInfo : allNetworks) {
-            if (networkInfo.isAvailable() && networkInfo.isConnected()) {
+        for (NetworkInfo networkInfo : allNetworks)
+        {
+            if (networkInfo.isAvailable() && networkInfo.isConnected())
+            {
                 return true;
             }
 
@@ -25,7 +28,8 @@ public class Device {
         return false;
     }
 
-    public static String getDefaultLanguage(SnuffyApplication application){
+    public static String getDefaultLanguage(SnuffyApplication application)
+    {
         return application.getDeviceLocale().getLanguage();
     }
 }
