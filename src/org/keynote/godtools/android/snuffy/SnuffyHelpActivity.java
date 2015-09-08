b8b984df99f88f35acce5c648789a893126210bc
Matthew Frederick <matthew.frederick@cru.org>
2015-09-08 12:01:14 -0400

    clean SnuffyHelpActivity

--- src/org/keynote/godtools/android/snuffy/SnuffyHelpActivity.java
+++ src/org/keynote/godtools/android/snuffy/SnuffyHelpActivity.java
@@ -1,16 +1,11 @@
 package org.keynote.godtools.android.snuffy;
 
-//import org.keynote.godtools.android.utils.FlurryAPI;
-
-import org.keynote.godtools.android.R;
-//import com.flurry.android.FlurryAgent;
-
 import android.app.Activity;
 import android.os.Bundle;
- 
+
+import org.keynote.godtools.android.R;
+
 public class SnuffyHelpActivity extends Activity {
- 
-	public static final String LOGTAG = "SnuffyHelp";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +16,6 @@ public class SnuffyHelpActivity extends Activity {
 		String packageTitle = getIntent().getStringExtra("PackageTitle");
 		String windowTitle = getString(R.string.snuffy_help_title);
 		windowTitle = windowTitle.replace("%1", packageTitle);
-		setTitle(windowTitle);		
-		
-		//FlurryAgent.onEvent(FlurryAPI.FlurryPrefix + LOGTAG);
-		//FlurryAgent.onPageView();
+		setTitle(windowTitle);
 	}
-	
-    @Override
-    public void onStart()
-    {
-       super.onStart();
-       //FlurryAPI.onStartSession(this);
-    }
-    
-    @Override
-    public void onStop()
-    {
-       super.onStop();
-       //FlurryAgent.onEndSession(this);
-    }
 }
