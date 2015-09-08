ae0b8b8a3f6b355e104f72ec7bcdc87e26a1f07a
Matthew Frederick <matthew.frederick@cru.org>
2015-09-08 12:01:30 -0400

    refactor SnuffyHelpActivity

--- src/org/keynote/godtools/android/snuffy/SnuffyHelpActivity.java
+++ src/org/keynote/godtools/android/snuffy/SnuffyHelpActivity.java
@@ -5,17 +5,19 @@ import android.os.Bundle;
 
 import org.keynote.godtools.android.R;
 
-public class SnuffyHelpActivity extends Activity {
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// Initialize the layout
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.snuffy_help);
-		
-		String packageTitle = getIntent().getStringExtra("PackageTitle");
-		String windowTitle = getString(R.string.snuffy_help_title);
-		windowTitle = windowTitle.replace("%1", packageTitle);
-		setTitle(windowTitle);
-	}
+public class SnuffyHelpActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        // Initialize the layout
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.snuffy_help);
+
+        String packageTitle = getIntent().getStringExtra("PackageTitle");
+        String windowTitle = getString(R.string.snuffy_help_title);
+        windowTitle = windowTitle.replace("%1", packageTitle);
+        setTitle(windowTitle);
+    }
 }
