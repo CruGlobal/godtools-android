b80cd1650b7dc57aca058a4f458b7d5d084c6970
Daniel Frett <daniel.frett@ccci.org>
2016-02-09 18:55:59 -0500

    add on additional default queries for GTPackage

--- src/org/keynote/godtools/android/business/GTPackage.java
+++ src/org/keynote/godtools/android/business/GTPackage.java
@@ -1,17 +1,29 @@
 package org.keynote.godtools.android.business;
 
 import android.content.Context;
+import static org.ccci.gto.android.common.db.Expression.bind;
+import static org.keynote.godtools.android.dao.DBContract.GTPackageTable.FIELD_LANGUAGE;
+import static org.keynote.godtools.android.dao.DBContract.GTPackageTable.FIELD_STATUS;
+
 import android.support.annotation.Nullable;
 
 import com.google.common.base.Function;
 
 import org.keynote.godtools.android.dao.DBAdapter;
+import org.ccci.gto.android.common.db.Expression;
 import org.keynote.godtools.android.model.HomescreenLayout;
 
 import java.util.List;
 
 public class GTPackage {
     public static final String EVERYSTUDENT_PACKAGE_CODE = "everystudent";
+
+    public static final String STATUS_LIVE = "live";
+    public static final String STATUS_DRAFT = "draft";
+
+    // common SQL queries
+    public static final Expression SQL_WHERE_DRAFT_BY_LANGUAGE =
+            FIELD_LANGUAGE.eq(bind()).and(FIELD_STATUS.eq(STATUS_DRAFT));
 
     public static final Function<GTPackage, String> FUNCTION_CODE = new Function<GTPackage, String>() {
         @Nullable
