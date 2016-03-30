fc96583b1bee05763287eceddd627ae80a0b3023
dsgoers <daniel.goers@cru.org>
2016-03-30 15:03:29 -0400

    Add a class for subscribers.

---
+++ src/org/keynote/godtools/android/api/Subscriber.java
@@ -0,0 +1,54 @@
+package org.keynote.godtools.android.api;
+
+/**
+ * Created by dsgoers on 3/29/16.
+ */
+public class Subscriber {
+
+    private String routeId;
+    private String languageCode;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public String getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(String routeId) {
+        this.routeId = routeId;
+    }
+
+    public String getLanguageCode() {
+        return languageCode;
+    }
+
+    public void setLanguageCode(String languageCode) {
+        this.languageCode = languageCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
