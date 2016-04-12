f70c709bea6682235c0c17f09dd8e6401482b95b
Daniel Frett <daniel.frett@ccci.org>
2016-04-12 13:39:11 -0400

    add missing import

--- src/main/java/org/keynote/godtools/android/snuffy/model/GtPage.java
+++ src/main/java/org/keynote/godtools/android/snuffy/model/GtPage.java
@@ -1,6 +1,7 @@
 package org.keynote.godtools.android.snuffy.model;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 
 import com.google.common.collect.ImmutableSet;
