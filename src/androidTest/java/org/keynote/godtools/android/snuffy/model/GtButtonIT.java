2e8dade9d84964e8e2b7871a20b49ce75f1d2b26
dsgoers <daniel.goers@cru.org>
2016-04-13 17:59:05 -0400

    Fix another test.

--- src/androidTest/java/org/keynote/godtools/android/snuffy/model/GtButtonIT.java
+++ src/androidTest/java/org/keynote/godtools/android/snuffy/model/GtButtonIT.java
@@ -2,9 +2,11 @@ package org.keynote.godtools.android.snuffy.model;
 
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.keynote.godtools.android.snuffy.model.GtButton.Mode;
+import org.keynote.godtools.android.utils.EventID;
 
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
@@ -15,12 +17,24 @@ import static org.keynote.godtools.android.snuffy.TestParserUtils.getParserForTe
 @RunWith(AndroidJUnit4.class)
 public class GtButtonIT {
     private static final String TEXT = "Button Text";
-    private static final String EVENT1 = "event1";
-    private static final String EVENT2 = "ns:event2";
+    private static final EventID EVENT1 = new EventID();
+    private static final EventID EVENT2 = new EventID();
+    private static final String NAMESPACE = "ns";
+    private static final GtPage PAGE = new GtPage(new GtManifest(NAMESPACE));
+
+    @Before
+    public void before()
+    {
+        EVENT1.setId("event1");
+        EVENT1.setNamespace(NAMESPACE);
+
+        EVENT2.setId("event2");
+        EVENT2.setNamespace(NAMESPACE);
+    }
 
     @Test
     public void verifyButtonLinkElement() throws Exception {
-        final GtButton button = GtButton.fromXml(getParserForTestAsset("button-link-element.xml"));
+        final GtButton button = GtButton.fromXml(PAGE, getParserForTestAsset("button-link-element.xml"));
         assertNotNull(button);
         assertThat(button.getMode(), is(Mode.LINK));
         assertThat(button.getTapEvents(), hasItems(EVENT1, EVENT2));
@@ -29,7 +43,7 @@ public class GtButtonIT {
 
     @Test
     public void verifyButtonLinkMode() throws Exception {
-        final GtButton button = GtButton.fromXml(getParserForTestAsset("button-link-mode.xml"));
+        final GtButton button = GtButton.fromXml(PAGE, getParserForTestAsset("button-link-mode.xml"));
         assertNotNull(button);
         assertThat(button.getMode(), is(Mode.LINK));
         assertThat(button.getTapEvents(), hasItems(EVENT1, EVENT2));
@@ -38,7 +52,7 @@ public class GtButtonIT {
 
     @Test
     public void verifyButtonPanel() throws Exception {
-        final GtButton button = GtButton.fromXml(getParserForTestAsset("button-panel-simple.xml"));
+        final GtButton button = GtButton.fromXml(PAGE, getParserForTestAsset("button-panel-simple.xml"));
         assertNotNull(button);
         assertThat(button.getMode(), is(Mode.PANEL));
         assertThat(button.getText(), is(TEXT));
@@ -46,7 +60,7 @@ public class GtButtonIT {
 
     @Test
     public void verifyButtonUnknown() throws Exception {
-        final GtButton button = GtButton.fromXml(getParserForTestAsset("button-unknown-simple.xml"));
+        final GtButton button = GtButton.fromXml(PAGE, getParserForTestAsset("button-unknown-simple.xml"));
         assertNotNull(button);
         assertThat(button.getMode(), is(Mode.UNKNOWN));
     }
--- src/main/java/org/keynote/godtools/android/utils/EventID.java
+++ src/main/java/org/keynote/godtools/android/utils/EventID.java
@@ -1,11 +1,24 @@
 package org.keynote.godtools.android.utils;
 
+import com.google.common.base.Objects;
+
 /**
  * Created by dsgoers on 4/13/16.
  */
 public class EventID {
     private String namespace;
     private String id;
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof EventID && namespace.equals(((EventID) obj).getNamespace()) && id.equals(((EventID)
+                obj).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(namespace, id);
+    }
 
     public String getNamespace() {
         return namespace;
