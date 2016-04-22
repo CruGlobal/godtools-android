34550c2f5fa989a25305252d01b45f7f31e4c727
Daniel Frett <daniel.frett@ccci.org>
2016-04-22 15:03:41 -0400

    add support for rendering the Followup Modal

--- src/main/java/org/keynote/godtools/android/snuffy/model/GtFollowupModal.java
+++ src/main/java/org/keynote/godtools/android/snuffy/model/GtFollowupModal.java
@@ -1,45 +1,59 @@
 package org.keynote.godtools.android.snuffy.model;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
 import org.ccci.gto.android.common.util.XmlPullParserUtils;
+import org.keynote.godtools.android.R;
+import org.keynote.godtools.android.event.GodToolsEvent.EventID;
 import org.keynote.godtools.android.model.Followup;
+import org.keynote.godtools.android.snuffy.ParserUtils;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-public class GtFollowupModal {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class GtFollowupModal extends GtModel {
     static final String XML_FOLLOWUP_MODAL = "followup-modal";
     private static final String XML_FALLBACK = "fallback";
     private static final String XML_TITLE = "followup-title";
     private static final String XML_BODY = "followup-body";
 
     private static final String XML_ATTR_FOLLOWUP_ID = "followup-id";
+    private static final String XML_ATTR_LISTENERS = "listeners";
 
     @NonNull
-    private final GtPage mPage;
     private String mId = "";
 
     private long mFollowupId = Followup.INVALID_ID;
-    private String mTitle;
-    private String mBody;
+    @NonNull
+    private Set<EventID> mListeners = ImmutableSet.of();
+    String mTitle;
+    String mBody;
     private final List<GtInputField> mInputFields = new ArrayList<>();
-    private GtButtonPair mButtonPair;
+    @Nullable
+    GtButtonPair mButtonPair;
 
-    private GtFollowupModal(@NonNull final GtPage page) {
-        mPage = page;
+    private GtFollowupModal(@NonNull final GtModel parent) {
+        super(parent);
     }
 
     @NonNull
-    public GtPage getPage() {
-        return mPage;
-    }
-
     public String getId() {
         return mId;
     }
@@ -50,6 +64,19 @@ public class GtFollowupModal {
 
     public long getFollowupId() {
         return mFollowupId;
+    }
+
+    @NonNull
+    public Set<EventID> getListeners() {
+        return mListeners;
+    }
+
+    public Integer getColor() {
+        final GtPage page = getPage();
+        if (page != null) {
+            return page.getColor();
+        }
+        return null;
     }
 
     public String getTitle() {
@@ -64,14 +91,30 @@ public class GtFollowupModal {
         return ImmutableList.copyOf(mInputFields);
     }
 
+    @Nullable
     public GtButtonPair getButtonPair() {
         return mButtonPair;
     }
 
+    @Nullable
+    @Override
+    public ViewHolder render(@NonNull final Context context, @Nullable final ViewGroup parent,
+                             final boolean attachToRoot) {
+        final LayoutInflater inflater = LayoutInflater.from(context);
+
+        // inflate the raw view
+        final ViewHolder holder = new ViewHolder(inflater.inflate(R.layout.gt_followup_modal, parent, false));
+        if (parent != null && attachToRoot) {
+            parent.addView(holder.mRoot);
+        }
+
+        return holder;
+    }
+
     @NonNull
-    static GtFollowupModal fromXml(@NonNull final GtPage page, @NonNull final XmlPullParser parser)
+    public static GtFollowupModal fromXml(@NonNull final GtModel parent, @NonNull final XmlPullParser parser)
             throws IOException, XmlPullParserException {
-        final GtFollowupModal followup = new GtFollowupModal(page);
+        final GtFollowupModal followup = new GtFollowupModal(parent);
         followup.parse(parser);
         return followup;
     }
@@ -84,6 +127,8 @@ public class GtFollowupModal {
         } catch (final Exception suppressed) {
             mFollowupId = Followup.INVALID_ID;
         }
+        mListeners = ParserUtils
+                .parseEvents(parser.getAttributeValue(null, XML_ATTR_LISTENERS), getManifest().getPackageCode());
 
         // loop until we reach the matching end tag for this element
         while (parser.next() != XmlPullParser.END_TAG) {
@@ -126,11 +171,67 @@ public class GtFollowupModal {
                     mInputFields.add(GtInputField.fromXml(parser));
                     break;
                 case GtButtonPair.XML_BUTTON_PAIR:
-                    mButtonPair = GtButtonPair.fromXml(mPage, parser);
+                    mButtonPair = GtButtonPair.fromXml(this, parser);
                     break;
                 default:
                     // skip unrecognized nodes
                     XmlPullParserUtils.skipTag(parser);
+            }
+        }
+    }
+
+    public class ViewHolder extends GtModel.ViewHolder {
+        @Nullable
+        @Bind(R.id.title)
+        TextView mTitleText;
+        @Nullable
+        TextView mBodyText;
+
+        @Nullable
+        @Bind(R.id.buttons)
+        ViewGroup mButtons;
+
+        ViewHolder(@NonNull final View root) {
+            super(root);
+            ButterKnife.bind(this, mRoot);
+
+            updateBackground();
+            updateTitle();
+            updateBody();
+            attachButtonPair();
+        }
+
+        private void updateBackground() {
+            // update the background color & watermark
+            final Integer color = getColor();
+            mRoot.setBackgroundColor(color != null ? color : Color.TRANSPARENT);
+        }
+
+        private void updateTitle() {
+            if (mTitleText != null) {
+                mTitleText.setVisibility(mTitle != null ? View.VISIBLE : View.GONE);
+                if (mTitle != null) {
+                    mTitleText.setText(mTitle);
+                }
+            }
+        }
+
+        private void updateBody() {
+            if (mBodyText != null) {
+                mBodyText.setVisibility(mBody != null ? View.VISIBLE : View.GONE);
+                if (mBody != null) {
+                    mBodyText.setText(mBody);
+                }
+            }
+        }
+
+        private void attachButtonPair() {
+            if (mButtons != null) {
+                mButtons.setVisibility(mButtonPair != null ? View.VISIBLE : View.GONE);
+                if (mButtonPair != null) {
+                    final GtModel.ViewHolder holder = mButtonPair.render(mButtons.getContext(), mButtons, true);
+                    holder.setParentHolder(this);
+                }
             }
         }
     }
