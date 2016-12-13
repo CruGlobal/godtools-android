87bff4543189b4dc8e623241747f17da537319e5
Ryan Mattison <rmattison1@gmail.com>
2016-12-13 05:31:09 -0600

    Added in big button layout refactor

--- CRUReader/app/src/main/java/com/example/rmatt/crureader/bo/GPage/GQuestion.java
+++ CRUReader/app/src/main/java/com/example/rmatt/crureader/bo/GPage/GQuestion.java
@@ -5,7 +5,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.rmatt.crureader.R;
+import com.example.rmatt.crureader.bo.GPage.Compat.RenderViewCompat;
 import com.example.rmatt.crureader.bo.GPage.Views.AutoScaleTextView;
+import com.github.captain_miao.optroundcardview.OptRoundCardView;
 
 import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Root;
@@ -27,15 +29,18 @@ public class GQuestion extends GBaseTextAttributes {
     @Override
     public int render(LayoutInflater inflater, ViewGroup viewGroup, int position) {
         View gQuestion;
+        OptRoundCardView layout = null;
         if (mode
                 != null && mode.equalsIgnoreCase("straight")) {
             gQuestion = inflater.inflate(R.layout.g_question_straight, viewGroup);
+            layout = (OptRoundCardView)gQuestion.findViewById(R.id.g_question_straight_outerlayout_optroundcardview);
+            layout.setId(RenderViewCompat.generateViewId());
             defaultColor(position);
         } else {
             gQuestion = inflater.inflate(R.layout.g_question_default, viewGroup);
         }
         AutoScaleTextView gQuestionTextView = (AutoScaleTextView) gQuestion.findViewById(R.id.g_question_textview);
         updateBaseAttributes(gQuestionTextView);
-        return gQuestionTextView.getId();
+        return layout == null ? gQuestionTextView.getId() : layout.getId();
     }
 }
