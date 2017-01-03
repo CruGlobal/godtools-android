c5cf8b959b61065afeeeb49151572002e7c44f1c
Ryan Mattison <rmattison1@gmail.com>
2017-01-02 21:11:31 -0600

    Text Size tweaks for headers and titles

--- godtools-renderer/src/main/java/org/keynote/godtools/renderer/crureader/bo/GPage/Compat/RenderViewCompat.java
+++ godtools-renderer/src/main/java/org/keynote/godtools/renderer/crureader/bo/GPage/Compat/RenderViewCompat.java
@@ -26,7 +26,8 @@ public class RenderViewCompat {
             final int result = sNextGeneratedId.get();
             // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
             int newValue = result + 1;
-            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (newValue > 0x00FFFFFF)
+                newValue = 1; // Roll over to 1, not 0.
             if (sNextGeneratedId.compareAndSet(result, newValue)) {
                 return result;
             }
@@ -34,8 +35,11 @@ public class RenderViewCompat {
     }
 
     public static void textViewAlign(TextView textView, String textAlign) {
-        textView.setGravity(RenderConstants.getGravityFromAlign(textAlign));
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            textView.setTextAlignment(RenderConstants.getTextAlign(textAlign));
+        } else {
+            textView.setGravity(RenderConstants.getGravityFromAlign(textAlign));
+        }
 
     }
 
