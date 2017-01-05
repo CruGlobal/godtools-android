b1629d1fc57846c63f24d83e898fbc5f91e67946
Ryan Mattison <rmattison1@gmail.com>
2017-01-04 18:21:59 -0600

    (No screenshot) - cleaned up test methods

--- godtools-renderer/src/main/java/org/keynote/godtools/renderer/crureader/XMLUtil.java
+++ godtools-renderer/src/main/java/org/keynote/godtools/renderer/crureader/XMLUtil.java
@@ -1,7 +1,5 @@
 package org.keynote.godtools.renderer.crureader;
 
-import android.content.Context;
-
 import org.keynote.godtools.renderer.crureader.bo.GDocument.GDocument;
 import org.keynote.godtools.renderer.crureader.bo.GPage.GButton;
 import org.keynote.godtools.renderer.crureader.bo.GPage.GPage;
@@ -12,7 +10,6 @@ import org.simpleframework.xml.core.Persister;
 import org.simpleframework.xml.strategy.Strategy;
 
 import java.io.File;
-import java.io.InputStream;
 
 /**
  * Created by rmatt on 10/24/2016.
@@ -20,25 +17,8 @@ import java.io.InputStream;
 
 public class XMLUtil {
 
-    public static GPage parseGPage(Context context, String xmlFileName) throws Exception {
-        InputStream testIS = context.getResources().getAssets().open(xmlFileName);
-        Registry registry = new Registry();
-        Strategy strategy = new RegistryStrategy(registry);
-        Serializer serializer = new Persister(strategy);
-        registry.bind(GButton.class, ComplexConverter.class);
-        GPage GPage = serializer.read(GPage.class, testIS);
-        return GPage;
-    }
 
-    public static GDocument parseGDocument(Context appContext, String baseXml) throws Exception {
-
-        InputStream testIS = appContext.getResources().getAssets().open(baseXml);
-        Serializer serializer = new Persister();
-        GDocument GDocument = serializer.read(GDocument.class, testIS);
-        return GDocument;
-    }
-
-    public static GPage parseGPage(Context context, File file) throws Exception {
+    public static GPage parseGPage(File file) throws Exception {
 
         Registry registry = new Registry();
         Strategy strategy = new RegistryStrategy(registry);
@@ -47,7 +27,7 @@ public class XMLUtil {
         return serializer.read(GPage.class, file);
     }
 
-    public static GDocument parseGDocument(Context context, File file) throws Exception {
+    public static GDocument parseGDocument(File file) throws Exception {
         Serializer serializer = new Persister();
         return serializer.read(GDocument.class, file);
     }
