8ad6cce45456c16b319cefbd9ef251d926ee66f6
Daniel Frett <daniel.frett@ccci.org>
2017-01-24 16:38:10 -0500

    remove a couple more old tests

--- src/testShared/java/org/keynote/godtools/android/snuffy/model/Constants.java
+++
@@ -1,8 +0,0 @@
-package org.keynote.godtools.android.snuffy.model;
-
-public class Constants {
-    public static final String NAMESPACE = "kgp";
-    public static final GtManifest MANIFEST = new GtManifest(NAMESPACE, "en");
-    public static final GtPage PAGE = new GtPage(MANIFEST, "test");
-    public static final GtFollowupModal FOLLOWUP_MODAL = new GtFollowupModal(PAGE, "test");
-}
