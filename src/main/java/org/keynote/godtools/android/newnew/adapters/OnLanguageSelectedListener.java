8d7f586527868f73bf648480594cffe8fa5c0aac
Ryan Mattison <rmattison1@gmail.com>
2017-03-14 10:11:05 -0500

    Updated listview statuses download, delete

--- src/main/java/org/keynote/godtools/android/newnew/adapters/OnLanguageSelectedListener.java
+++ src/main/java/org/keynote/godtools/android/newnew/adapters/OnLanguageSelectedListener.java
@@ -2,10 +2,6 @@ package org.keynote.godtools.android.newnew.adapters;
 
 import org.keynote.godtools.android.business.GTLanguage;
 
-/**
- * Created by rmatt on 3/14/2017.
- */
-
 public interface OnLanguageSelectedListener {
     void onLanguageSelected(GTLanguage gtLanguage);
 }
