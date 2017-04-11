f3f3dc9a3c006d88d8b32ef62ddf9308823d4f90
Daniel Frett <daniel.frett@ccci.org>
2017-04-11 15:50:20 -0400

    re-enable MultiDex for debug builds only

--- src/debug/java/org/keynote/godtools/android/DebugGodToolsApplication.java
+++ src/debug/java/org/keynote/godtools/android/DebugGodToolsApplication.java
@@ -1,5 +1,9 @@
 package org.keynote.godtools.android;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.multidex.MultiDex;
+
 import com.facebook.stetho.Stetho;
 
 import org.keynote.godtools.android.snuffy.SnuffyApplication;
@@ -9,5 +13,11 @@ public class DebugGodToolsApplication extends SnuffyApplication {
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
+    }
+
+    @Override
+    protected void attachBaseContext(@NonNull final Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
