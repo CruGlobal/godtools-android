e42b590bd504e75d1d86c51a4d1881440e6280d1
Daniel Frett <daniel.frett@ccci.org>
2017-04-20 16:46:47 -0400

    enable last sync database tracking

--- src/main/java/org/keynote/godtools/android/db/GodToolsDatabase.java
+++ src/main/java/org/keynote/godtools/android/db/GodToolsDatabase.java
@@ -11,6 +11,7 @@ import com.crashlytics.android.Crashlytics;
 import com.google.common.base.Throwables;
 
 import org.ccci.gto.android.common.app.ApplicationUtils;
+import org.ccci.gto.android.common.db.CommonTables.LastSyncTable;
 import org.ccci.gto.android.common.db.WalSQLiteOpenHelper;
 import org.keynote.godtools.android.dao.DBContract.GSSubscriberTable;
 import org.keynote.godtools.android.dao.DBContract.GTLanguageTable;
@@ -22,7 +23,7 @@ import io.fabric.sdk.android.Fabric;
 
 public final class GodToolsDatabase extends WalSQLiteOpenHelper {
     private static final String DATABASE_NAME = "resource.db";
-    private static final int DATABASE_VERSION = 7;
+    private static final int DATABASE_VERSION = 8;
 
     /*
      * Version history
@@ -37,6 +38,7 @@ public final class GodToolsDatabase extends WalSQLiteOpenHelper {
      * 6: 2016-04-05
      * v4.2.0 - v4.3.2
      * 7: 2017-04-19
+     * 8: 2017-04-19
      */
 
     @NonNull
@@ -67,6 +69,7 @@ public final class GodToolsDatabase extends WalSQLiteOpenHelper {
 
             db.execSQL(GTPackageTable.SQL_CREATE_TABLE);
             db.execSQL(GTLanguageTable.SQL_CREATE_TABLE);
+            db.execSQL(LastSyncTable.SQL_CREATE_TABLE);
             db.execSQL(GSSubscriberTable.SQL_CREATE_TABLE);
             db.execSQL(FollowupTable.SQL_CREATE_TABLE);
             db.execSQL(LanguageTable.SQL_CREATE_TABLE);
@@ -137,6 +140,9 @@ public final class GodToolsDatabase extends WalSQLiteOpenHelper {
                     case 7:
                         db.execSQL(LanguageTable.SQL_CREATE_TABLE);
                         break;
+                    case 8:
+                        db.execSQL(LastSyncTable.SQL_CREATE_TABLE);
+                        break;
                     default:
                         // unrecognized version
                         throw new SQLiteException("Unrecognized database version");
@@ -176,6 +182,7 @@ public final class GodToolsDatabase extends WalSQLiteOpenHelper {
             db.execSQL(GSSubscriberTable.SQL_DELETE_TABLE);
             db.execSQL(FollowupTable.SQL_DELETE_TABLE);
             db.execSQL(LanguageTable.SQL_DELETE_TABLE);
+            db.execSQL(LastSyncTable.SQL_DELETE_TABLE);
 
             onCreate(db);
 
