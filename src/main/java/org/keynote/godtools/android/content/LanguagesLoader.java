df9dde41a9ed94893bce292bf23da2b5e5df7d8c
Daniel Frett <daniel.frett@ccci.org>
2017-04-25 12:06:41 -0400

    create a LanguagesLoader and ActiveLocaleLoader

---
+++ src/main/java/org/keynote/godtools/android/content/LanguagesLoader.java
@@ -0,0 +1,30 @@
+package org.keynote.godtools.android.content;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import org.ccci.gto.android.common.db.Query;
+import org.ccci.gto.android.common.eventbus.content.CachingAsyncTaskEventBusLoader;
+import org.keynote.godtools.android.db.GodToolsDao;
+import org.keynote.godtools.android.model.Language;
+
+import java.util.List;
+
+public final class LanguagesLoader extends CachingAsyncTaskEventBusLoader<List<Language>> {
+    private static final Query<Language> QUERY_LANGUAGES = Query.select(Language.class);
+
+    @NonNull
+    private final GodToolsDao mDao;
+
+    public LanguagesLoader(@NonNull final Context context) {
+        super(context);
+        mDao = GodToolsDao.getInstance(context);
+
+        addEventBusSubscriber(new LanguageEventBusSubscriber(this));
+    }
+
+    @Override
+    public List<Language> loadInBackground() {
+        return mDao.get(QUERY_LANGUAGES);
+    }
+}
