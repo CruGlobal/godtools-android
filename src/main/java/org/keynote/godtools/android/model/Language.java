7603a333efa53f1967fcec8ce0bb3c4595d2f016
Daniel Frett <daniel.frett@ccci.org>
2017-04-25 12:27:53 -0400

    track an added flag for languages
    This flag is used to indicate when a language has been added to the device

--- src/main/java/org/keynote/godtools/android/model/Language.java
+++ src/main/java/org/keynote/godtools/android/model/Language.java
@@ -1,8 +1,10 @@
 package org.keynote.godtools.android.model;
 
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import org.ccci.gto.android.common.jsonapi.annotation.JsonApiAttribute;
+import org.ccci.gto.android.common.jsonapi.annotation.JsonApiIgnore;
 import org.ccci.gto.android.common.jsonapi.annotation.JsonApiType;
 
 import java.util.Locale;
@@ -18,11 +20,27 @@ public class Language extends Base {
     @JsonApiAttribute(name = JSON_LOCALE)
     private Locale mLocale;
 
+    @JsonApiIgnore
+    private boolean mAdded = false;
+
     public Locale getLocale() {
         return mLocale;
     }
 
     public void setLocale(@Nullable final Locale locale) {
         mLocale = locale;
+    }
+
+    public boolean isAdded() {
+        return mAdded;
+    }
+
+    public void setAdded(final boolean state) {
+        mAdded = state;
+    }
+
+    @NonNull
+    public String getDisplayName() {
+        return mLocale != null ? mLocale.getDisplayName() : "";
     }
 }
