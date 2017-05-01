485ca580b1aa89dd06049c8ae299dc7f90e77956
Daniel Frett <daniel.frett@ccci.org>
2017-05-01 16:02:02 -0400

    use the GTO support library DaoCursorEventBusLoader

--- src/main/java/org/keynote/godtools/android/content/ResourcesCursorLoader.java
+++ src/main/java/org/keynote/godtools/android/content/ResourcesCursorLoader.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import org.ccci.gto.android.common.eventbus.content.DaoCursorEventBusLoader;
+import org.keynote.godtools.android.db.GodToolsDao;
 import org.keynote.godtools.android.model.Resource;
 
-public final class ResourcesCursorLoader extends DaoEventBusCursorLoader<Resource> {
+public final class ResourcesCursorLoader extends DaoCursorEventBusLoader<Resource> {
     public ResourcesCursorLoader(@NonNull final Context context, @Nullable final Bundle args) {
-        super(context, Resource.class, args);
+        super(context, GodToolsDao.getInstance(context), Resource.class, args);
         addEventBusSubscriber(new ResourceEventBusSubscriber(this));
     }
 }
