af9eccf93078a6668a5daf6518ddc37247fc967e
Daniel Frett <daniel.frett@ccci.org>
2017-05-04 15:30:56 -0400

    add latest translation content loader

---
+++ src/main/java/org/keynote/godtools/android/content/LatestTranslationLoader.java
@@ -0,0 +1,48 @@
+package org.keynote.godtools.android.content;
+
+import android.content.Context;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+
+import org.ccci.gto.android.common.db.Query;
+import org.ccci.gto.android.common.eventbus.content.CachingAsyncTaskEventBusLoader;
+import org.keynote.godtools.android.db.Contract.TranslationTable;
+import org.keynote.godtools.android.db.GodToolsDao;
+import org.keynote.godtools.android.model.Translation;
+
+import java.util.List;
+import java.util.Locale;
+
+public final class LatestTranslationLoader extends CachingAsyncTaskEventBusLoader<Translation> {
+    private static final Query<Translation> QUERY = Query.select(Translation.class)
+            .orderBy(TranslationTable.COLUMN_VERSION + " DESC")
+            .limit(1);
+
+    @NonNull
+    private final GodToolsDao mDao;
+    private final long mResourceId;
+    @NonNull
+    private Locale mLocale;
+
+    public LatestTranslationLoader(@NonNull final Context context, final long resourceId,
+                                   @NonNull final Locale locale) {
+        super(context);
+        mDao = GodToolsDao.getInstance(context);
+        mResourceId = resourceId;
+        mLocale = locale;
+        addEventBusSubscriber(new TranslationEventBusSubscriber(this));
+    }
+
+    @MainThread
+    public void setLocale(@NonNull final Locale locale) {
+        mLocale = locale;
+        onContentChanged();
+    }
+
+    @Override
+    public Translation loadInBackground() {
+        final List<Translation> translations =
+                mDao.get(QUERY.where(TranslationTable.SQL_WHERE_RESOURCE_LANGUAGE.args(mResourceId, mLocale)));
+        return translations.isEmpty() ? null : translations.get(0);
+    }
+}
--- src/main/java/org/keynote/godtools/android/db/Contract.java
+++ src/main/java/org/keynote/godtools/android/db/Contract.java
@@ -82,6 +82,7 @@ public final class Contract extends BaseContract {
 
         private static final Field FIELD_ID = TABLE.field(COLUMN_ID);
         public static final Field FIELD_RESOURCE = TABLE.field(COLUMN_RESOURCE);
+        private static final Field FIELD_LANGUAGE = TABLE.field(COLUMN_LANGUAGE);
 
         static final String[] PROJECTION_ALL =
                 {COLUMN_ID, COLUMN_RESOURCE, COLUMN_LANGUAGE, COLUMN_VERSION, COLUMN_NAME, COLUMN_DESCRIPTION,
@@ -96,6 +97,8 @@ public final class Contract extends BaseContract {
         private static final String SQL_COLUMN_DOWNLOADED = COLUMN_DOWNLOADED + " INTEGER";
 
         static final Expression SQL_WHERE_PRIMARY_KEY = FIELD_ID.eq(bind());
+        public static final Expression SQL_WHERE_RESOURCE_LANGUAGE =
+                FIELD_RESOURCE.eq(bind()).and(FIELD_LANGUAGE.eq(bind()));
 
         static final String SQL_CREATE_TABLE =
                 create(TABLE_NAME, SQL_COLUMN_ID, SQL_COLUMN_RESOURCE, SQL_COLUMN_LANGUAGE, SQL_COLUMN_VERSION,
