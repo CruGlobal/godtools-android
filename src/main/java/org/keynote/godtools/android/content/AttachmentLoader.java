8be1cda84ea6af59d4a3d963b60dfd0cfb8fed13
Daniel Frett <daniel.frett@ccci.org>
2017-05-16 15:02:20 -0400

    support the banner image in the tool detail UI

---
+++ src/main/java/org/keynote/godtools/android/content/AttachmentLoader.java
@@ -0,0 +1,35 @@
+package org.keynote.godtools.android.content;
+
+import android.content.Context;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+
+import org.ccci.gto.android.common.eventbus.content.CachingAsyncTaskEventBusLoader;
+import org.keynote.godtools.android.db.GodToolsDao;
+import org.keynote.godtools.android.model.Attachment;
+
+public final class AttachmentLoader extends CachingAsyncTaskEventBusLoader<Attachment> {
+    @NonNull
+    private final GodToolsDao mDao;
+    private long mId = Attachment.INVALID_ID;
+
+    public AttachmentLoader(@NonNull final Context context) {
+        super(context);
+        mDao = GodToolsDao.getInstance(context);
+        addEventBusSubscriber(new AttachmentEventBusSubscriber(this));
+    }
+
+    @MainThread
+    public void setId(final long id) {
+        final long oldId = mId;
+        mId = id;
+        if (oldId != mId) {
+            onContentChanged();
+        }
+    }
+
+    @Override
+    public Attachment loadInBackground() {
+        return mId != Attachment.INVALID_ID ? mDao.find(Attachment.class, mId) : null;
+    }
+}
