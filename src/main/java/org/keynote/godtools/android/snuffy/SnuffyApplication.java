58b3b39646d0e6b6f5157ee20509307c54ddd81b
Daniel Frett <daniel.frett@ccci.org>
2017-05-16 14:15:33 -0400

    configure Picasso

--- src/main/java/org/keynote/godtools/android/snuffy/SnuffyApplication.java
+++ src/main/java/org/keynote/godtools/android/snuffy/SnuffyApplication.java
@@ -1,10 +1,14 @@
 package org.keynote.godtools.android.snuffy;
 
 import android.os.Environment;
+import android.support.annotation.NonNull;
 
 import com.crashlytics.android.Crashlytics;
+import com.jakewharton.picasso.OkHttp3Downloader;
 import com.newrelic.agent.android.NewRelic;
+import com.squareup.picasso.Picasso;
 
+import org.ccci.gto.android.common.api.okhttp3.util.OkHttpClientUtil;
 import org.keynote.godtools.android.BuildConfig;
 import org.keynote.godtools.android.RenderAppConfig;
 import org.keynote.godtools.android.service.GodToolsToolManager;
@@ -14,6 +18,7 @@ import org.keynote.godtools.renderer.crureader.RenderApp;
 import java.io.File;
 
 import io.fabric.sdk.android.Fabric;
+import okhttp3.OkHttpClient;
 
 public class SnuffyApplication extends RenderApp {
 
@@ -24,6 +29,9 @@ public class SnuffyApplication extends RenderApp {
         // Enable crash reporting
         Fabric.with(this, new Crashlytics());
         NewRelic.withApplicationToken(BuildConfig.NEW_RELIC_API_KEY).start(this);
+
+        // initialize several support libraries
+        Picasso.setSingletonInstance(picassoBuilder().build());
 
         // Initialize tool manager
         GodToolsToolManager.getInstance(this);
@@ -53,5 +61,18 @@ public class SnuffyApplication extends RenderApp {
             documentsDir = getFilesDir();
         }
         return documentsDir;
+    }
+
+    @NonNull
+    protected Picasso.Builder picassoBuilder() {
+        final Picasso.Builder builder = new Picasso.Builder(this);
+
+        // use OkHttp3 for the downloader
+        final OkHttpClient okhttp = OkHttpClientUtil.attachGlobalInterceptors(new OkHttpClient.Builder())
+                .cache(OkHttp3Downloader.createDefaultCache(this))
+                .build();
+        builder.downloader(new OkHttp3Downloader(okhttp));
+
+        return builder;
     }
 }
