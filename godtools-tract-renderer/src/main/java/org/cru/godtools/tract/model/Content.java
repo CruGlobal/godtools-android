27d5ec959a2324d11fdf9c91316b2064081b0a52
Daniel Frett <daniel.frett@ccci.org>
2017-05-25 13:58:01 -0400

    add paragraph rendering

--- godtools-tract-renderer/src/main/java/org/cru/godtools/tract/model/Content.java
+++ godtools-tract-renderer/src/main/java/org/cru/godtools/tract/model/Content.java
@@ -11,6 +11,8 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 
 import static org.cru.godtools.tract.Constants.XMLNS_CONTENT;
+import static org.cru.godtools.tract.Constants.XMLNS_TRACT;
+import static org.cru.godtools.tract.model.Paragraph.XML_PARAGRAPH;
 import static org.cru.godtools.tract.model.Text.XML_TEXT;
 
 public abstract class Content extends Base {
@@ -24,6 +26,12 @@ public abstract class Content extends Base {
         parser.require(XmlPullParser.START_TAG, null, null);
 
         switch (parser.getNamespace()) {
+            case XMLNS_TRACT:
+                switch (parser.getName()) {
+                    case XML_PARAGRAPH:
+                        return Paragraph.fromXml(parent, parser);
+                }
+                break;
             case XMLNS_CONTENT:
                 switch (parser.getName()) {
                     case XML_TEXT:
