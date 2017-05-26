789b5f97dedc8d7522023210400537ae41811de5
Daniel Frett <daniel.frett@ccci.org>
2017-05-26 10:52:38 -0400

    add a couple missing constants

--- src/main/java/org/keynote/godtools/android/Constants.java
+++ src/main/java/org/keynote/godtools/android/Constants.java
@@ -10,6 +10,8 @@ public final class Constants {
 
     // common extras
     public static final String EXTRA_TOOL = "tool";
+    public static final String EXTRA_PRIMARY_LANGUAGE = "language_primary";
+    public static final String EXTRA_PARALLEL_LANGUAGE = "language_parallel";
 
     // SharedPreferences for this app
     public static final String PREFS_SETTINGS = "GodTools";
