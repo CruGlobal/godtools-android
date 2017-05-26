adf9b722147ebef4f862bd27d8da47002c4cc1ae
Daniel Frett <daniel.frett@ccci.org>
2017-05-26 10:32:57 -0400

    add support for rendering the hero

--- godtools-tract-renderer/src/main/java/org/cru/godtools/tract/model/Hero.java
+++ godtools-tract-renderer/src/main/java/org/cru/godtools/tract/model/Hero.java
@@ -2,14 +2,22 @@ package org.cru.godtools.tract.model;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.annimon.stream.Stream;
 
 import org.ccci.gto.android.common.util.XmlPullParserUtils;
+import org.cru.godtools.tract.R;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.ButterKnife;
 
 import static org.cru.godtools.tract.Constants.XMLNS_TRACT;
 
@@ -23,7 +31,7 @@ public final class Hero extends Base {
     private Text mHeading;
 
     @NonNull
-    private List<Content> mContent = new ArrayList<>();
+    private final List<Content> mContent = new ArrayList<>();
 
     private Hero(@NonNull final Base parent) {
         super(parent);
@@ -68,5 +76,41 @@ public final class Hero extends Base {
         }
 
         return this;
+    }
+
+    public static void bind(@Nullable final Hero hero, @Nullable final View view) {
+        if (view != null) {
+            final Text heading;
+            if (hero != null) {
+                view.setVisibility(View.VISIBLE);
+                heading = hero.mHeading;
+            } else {
+                view.setVisibility(View.GONE);
+                heading = null;
+            }
+            bindHeading(heading, ButterKnife.findById(view, R.id.hero_heading));
+            bindContent(hero, ButterKnife.findById(view, R.id.hero_content));
+        }
+    }
+
+    private static void bindHeading(@Nullable final Text heading, @Nullable final TextView view) {
+        if (view != null) {
+            if (heading != null) {
+                view.setVisibility(View.VISIBLE);
+                final float textSize = view.getResources().getDimension(R.dimen.text_size_hero);
+                Text.bind(heading, view, heading.getPage().getPrimaryColor(), textSize, DEFAULT_HEADING_TEXT_SCALE);
+            } else {
+                view.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    private static void bindContent(@Nullable final Hero hero, @Nullable final LinearLayout container) {
+        if (container != null) {
+            container.removeAllViews();
+            if (hero != null) {
+                Stream.of(hero.mContent).map(c -> c.render(container)).forEach(container::addView);
+            }
+        }
     }
 }
