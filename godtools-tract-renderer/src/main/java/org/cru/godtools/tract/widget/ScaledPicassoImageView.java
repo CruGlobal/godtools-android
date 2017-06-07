62d76dd3761faff4f8158bb7dcca4fe31dc52d22
Daniel Frett <daniel.frett@ccci.org>
2017-06-07 17:29:00 -0400

    create a ScaledPicassoImageView that supports custom scale types and gravities

---
+++ godtools-tract-renderer/src/main/java/org/cru/godtools/tract/widget/ScaledPicassoImageView.java
@@ -0,0 +1,81 @@
+package org.cru.godtools.tract.widget;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.squareup.picasso.RequestCreator;
+
+import org.ccci.gto.android.common.base.model.Dimension;
+import org.ccci.gto.android.common.picasso.transformation.ScaleTransformation;
+import org.ccci.gto.android.common.picasso.view.PicassoImageView;
+
+import jp.wasabeef.picasso.transformations.CropTransformation;
+import jp.wasabeef.picasso.transformations.CropTransformation.GravityHorizontal;
+import jp.wasabeef.picasso.transformations.CropTransformation.GravityVertical;
+
+import static org.cru.godtools.tract.widget.ScaledPicassoImageView.ScaleType.FILL_X;
+import static org.cru.godtools.tract.widget.ScaledPicassoImageView.ScaleType.FILL_Y;
+
+public interface ScaledPicassoImageView extends PicassoImageView {
+    enum ScaleType {FIT, FILL, FILL_X, FILL_Y}
+
+    class ScaleHelper extends Helper {
+        @NonNull
+        private ScaleType mScaleType = ScaleType.FIT;
+        @NonNull
+        private GravityHorizontal mGravityHorizontal = GravityHorizontal.CENTER;
+        @NonNull
+        private GravityVertical mGravityVertical = GravityVertical.CENTER;
+
+        ScaleHelper(@NonNull final ImageView view, @Nullable final AttributeSet attrs, final int defStyleAttr,
+                    final int defStyleRes) {
+            super(view, attrs, defStyleAttr, defStyleRes);
+        }
+
+        public final void setScaleType(@NonNull final ScaleType type) {
+            mScaleType = type;
+        }
+
+        public final void setGravityHorizontal(@NonNull final GravityHorizontal gravity) {
+            mGravityHorizontal = gravity;
+        }
+
+        public final void setGravityVertical(@NonNull final GravityVertical gravity) {
+            mGravityVertical = gravity;
+        }
+
+        @Override
+        protected void onSetUpdateScale(@NonNull final RequestCreator update, final Dimension size) {
+            switch (mScaleType) {
+                case FILL:
+                case FILL_X:
+                case FILL_Y:
+                    if (size.width > 0 && mScaleType == FILL_X) {
+                        update.resize(size.width, 0);
+                    } else if (size.height > 0 && mScaleType == FILL_Y) {
+                        update.resize(0, size.height);
+                    } else {
+                        update.transform(new ScaleTransformation(size.width, size.height));
+                    }
+
+                    // crop with gravity
+                    update.transform(
+                            new CropTransformation(size.width, size.height, mGravityHorizontal, mGravityVertical));
+                    break;
+                case FIT:
+                    update.resize(size.width, size.height);
+                    update.onlyScaleDown();
+                    update.centerInside();
+                    break;
+            }
+        }
+    }
+
+    void setScaleType(@NonNull ScaleType type);
+
+    void setGravityHorizontal(@NonNull GravityHorizontal gravity);
+
+    void setGravityVertical(@NonNull GravityVertical gravity);
+}
---
+++ godtools-tract-renderer/src/main/java/org/cru/godtools/tract/widget/TractPicassoImageView.java
@@ -0,0 +1,58 @@
+package org.cru.godtools.tract.widget;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+import org.ccci.gto.android.common.picasso.view.SimplePicassoImageView;
+
+import jp.wasabeef.picasso.transformations.CropTransformation.GravityHorizontal;
+import jp.wasabeef.picasso.transformations.CropTransformation.GravityVertical;
+
+public class TractPicassoImageView extends SimplePicassoImageView implements ScaledPicassoImageView {
+    public TractPicassoImageView(@NonNull final Context context) {
+        this(context, null);
+    }
+
+    public TractPicassoImageView(@NonNull final Context context, @Nullable final AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TractPicassoImageView(@NonNull final Context context, @Nullable final AttributeSet attrs,
+                                 final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public TractPicassoImageView(@NonNull final Context context, @Nullable final AttributeSet attrs,
+                                 final int defStyleAttr, final int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @NonNull
+    @Override
+    protected Helper createHelper(@Nullable final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
+        return new ScaleHelper(this, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void setScaleType(@NonNull final ScaledPicassoImageView.ScaleType type) {
+        if (mHelper instanceof ScaleHelper) {
+            ((ScaleHelper) mHelper).setScaleType(type);
+        }
+    }
+
+    @Override
+    public void setGravityHorizontal(@NonNull final GravityHorizontal gravity) {
+        if (mHelper instanceof ScaleHelper) {
+            ((ScaleHelper) mHelper).setGravityHorizontal(gravity);
+        }
+    }
+
+    @Override
+    public void setGravityVertical(@NonNull final GravityVertical gravity) {
+        if (mHelper instanceof ScaleHelper) {
+            ((ScaleHelper) mHelper).setGravityVertical(gravity);
+        }
+    }
+}
