8d364167aef04bf576398a41687bd510a804d019
Daniel Frett <daniel.frett@ccci.org>
2017-06-14 16:01:50 -0400

    simplify a couple tract loading utilities

--- src/main/java/org/keynote/godtools/android/util/TractUtil.java
+++ src/main/java/org/keynote/godtools/android/util/TractUtil.java
@@ -1,15 +1,10 @@
 package org.keynote.godtools.android.util;
 
 import android.content.Context;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import org.ccci.gto.android.common.db.Query;
 import org.cru.godtools.tract.service.TractManager;
-import org.keynote.godtools.android.db.Contract.TranslationTable;
-import org.keynote.godtools.android.db.GodToolsDao;
-import org.keynote.godtools.android.model.Translation;
 
 import java.util.Locale;
 
@@ -18,15 +13,7 @@ public final class TractUtil {
                                                    @Nullable final Locale locale) {
         if (locale != null) {
             final TractManager tractManager = TractManager.getInstance(context);
-            final GodToolsDao dao = GodToolsDao.getInstance(context);
-            AsyncTask.THREAD_POOL_EXECUTOR.execute(() -> dao.streamCompat(
-                    Query.select(Translation.class)
-                            .where(TranslationTable.SQL_WHERE_TOOL_LANGUAGE.args(toolId, locale)
-                                           .and(TranslationTable.SQL_WHERE_PUBLISHED)
-                                           .and(TranslationTable.SQL_WHERE_DOWNLOADED))
-                            .orderBy(TranslationTable.COLUMN_VERSION + " DESC")
-                            .limit(1))
-                    .forEach(tractManager::getManifest));
+            tractManager.getLatestPublishedManifest(toolId, locale);
         }
     }
 }
