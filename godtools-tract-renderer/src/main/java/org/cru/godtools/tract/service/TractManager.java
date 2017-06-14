28225a6be271e1e5e0f225bb650836c87639d6e1
Daniel Frett <daniel.frett@ccci.org>
2017-06-14 10:37:10 -0400

    utilize our own executor to make parsing independent from any other background tasks

--- godtools-tract-renderer/src/main/java/org/cru/godtools/tract/service/TractManager.java
+++ godtools-tract-renderer/src/main/java/org/cru/godtools/tract/service/TractManager.java
@@ -2,7 +2,6 @@ package org.cru.godtools.tract.service;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.util.LruCache;
@@ -15,6 +14,7 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 
+import org.ccci.gto.android.common.concurrent.NamedThreadFactory;
 import org.ccci.gto.android.common.support.v4.util.WeakLruCache;
 import org.cru.godtools.base.util.FileUtils;
 import org.cru.godtools.tract.model.Manifest;
@@ -25,14 +25,20 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 
 public class TractManager {
+    private static final int PARSING_CONCURRENCY = 6;
+
     private final LruCache<String, ListenableFuture<Manifest>> mCache = new WeakLruCache<>(1);
 
     @NonNull
     private final Context mContext;
+    private final ThreadPoolExecutor mExecutor;
 
     @Nullable
     @SuppressLint("StaticFieldLeak")
@@ -47,6 +53,8 @@ public class TractManager {
 
     private TractManager(@NonNull final Context context) {
         mContext = context;
+        mExecutor = new ThreadPoolExecutor(0, PARSING_CONCURRENCY, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<>(),
+                                           new NamedThreadFactory(TractManager.class.getSimpleName()));
     }
 
     @NonNull
@@ -76,7 +84,7 @@ public class TractManager {
     private ListenableFuture<Manifest> loadManifest(@NonNull final String manifestName) {
         // load the manifest
         final SettableFuture<Manifest> manifestTask = SettableFuture.create();
-        AsyncTask.THREAD_POOL_EXECUTOR.execute(() -> {
+        mExecutor.execute(() -> {
             try {
                 // find the file on disk
                 final File file = FileUtils.getFile(mContext, manifestName);
@@ -129,7 +137,7 @@ public class TractManager {
     private ListenableFuture<Page> parsePage(@NonNull final Page page) {
         // parse the page
         final SettableFuture<Page> result = SettableFuture.create();
-        AsyncTask.THREAD_POOL_EXECUTOR.execute(() -> {
+        mExecutor.execute(() -> {
             try {
                 // find the file on disk
                 final File file = FileUtils.getFile(mContext, page.getLocalFileName());
