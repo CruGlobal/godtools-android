ec1a3a1daeaac2eab4c33f5f03b2e36f7e4b19ed
Daniel Frett <daniel.frett@ccci.org>
2017-07-06 14:15:28 -0400

    remove some unused renderer config methods

--- src/main/java/org/keynote/godtools/android/snuffy/SnuffyApplication.java
+++ src/main/java/org/keynote/godtools/android/snuffy/SnuffyApplication.java
@@ -1,6 +1,5 @@
 package org.keynote.godtools.android.snuffy;
 
-import android.os.Environment;
 import android.support.annotation.NonNull;
 
 import com.crashlytics.android.Crashlytics;
@@ -15,8 +14,6 @@ import org.keynote.godtools.android.BuildConfig;
 import org.keynote.godtools.android.RenderAppConfig;
 import org.keynote.godtools.renderer.crureader.BaseAppConfig;
 import org.keynote.godtools.renderer.crureader.RenderApp;
-
-import java.io.File;
 
 import io.fabric.sdk.android.Fabric;
 import okhttp3.OkHttpClient;
@@ -42,27 +39,6 @@ public class SnuffyApplication extends RenderApp {
     @Override
     public BaseAppConfig getBaseAppConfig() {
         return new RenderAppConfig();
-    }
-
-    public File getDocumentsDir() {
-        File documentsDir = null;
-        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            documentsDir = getExternalFilesDir(null);
-            if (documentsDir != null) {
-                Crashlytics.log("documentsDir: " + documentsDir.getPath());
-                if (!documentsDir.isDirectory()) {
-                    Crashlytics.log("documentsDir doesn't exist");
-                    if (!documentsDir.mkdirs()) {
-                        Crashlytics.log("unable to create documents directory, falling back to internal directory");
-                        documentsDir = null;
-                    }
-                }
-            }
-        }
-        if (documentsDir == null) {
-            documentsDir = getFilesDir();
-        }
-        return documentsDir;
     }
 
     @NonNull
