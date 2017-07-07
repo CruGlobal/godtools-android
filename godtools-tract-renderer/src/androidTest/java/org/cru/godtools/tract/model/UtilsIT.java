0cc7d23145c17f383746ea72c088c5641df715bb
Daniel Frett <daniel.frett@ccci.org>
2017-07-07 14:12:33 -0400

    add parseUrl method that will assume http if there isn't a scheme on the URL

--- godtools-tract-renderer/src/androidTest/java/org/cru/godtools/tract/model/UtilsIT.java
+++ godtools-tract-renderer/src/androidTest/java/org/cru/godtools/tract/model/UtilsIT.java
@@ -1,12 +1,18 @@
 package org.cru.godtools.tract.model;
 
 import android.graphics.Color;
+import android.net.Uri;
 import android.support.test.runner.AndroidJUnit4;
+
+import com.google.common.collect.ImmutableMap;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Map;
+
 import static org.cru.godtools.tract.model.Utils.parseColor;
+import static org.cru.godtools.tract.model.Utils.parseUrl;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -22,5 +28,17 @@ public final class UtilsIT {
         // default parse behavior
         assertThat(parseColor(null, Color.RED), is(Color.RED));
         assertThat(parseColor("akjsdf", Color.RED), is(Color.RED));
+    }
+
+    private static final Map<String, Uri> TEST_URLS = ImmutableMap.<String, Uri>builder()
+            .put("https://www.example.com/path", Uri.parse("https://www.example.com/path"))
+            .put("www.example.com/path", Uri.parse("http://www.example.com/path"))
+            .put("mailto:someone@example.com", Uri.parse("mailto:someone@example.com"))
+            .build();
+    @Test
+    public void verifyParseUrl() throws Exception {
+        for (final Map.Entry<String, Uri> url : TEST_URLS.entrySet()) {
+            assertThat(parseUrl(url.getKey(), null), is(url.getValue()));
+        }
     }
 }
