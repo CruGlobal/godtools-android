303ff61d9979bf0c5aea304c59adbb204241aab5
Daniel Frett <daniel.frett@ccci.org>
2017-07-07 18:06:28 -0400

    remove temporary tool_id extra name

--- godtools-base/src/main/java/org/cru/godtools/base/Constants.java
+++ godtools-base/src/main/java/org/cru/godtools/base/Constants.java
@@ -6,8 +6,4 @@ public final class Constants {
     public static final String EXTRA_LANGUAGE = "language";
     public static final String EXTRA_PRIMARY_LANGUAGE = "language_primary";
     public static final String EXTRA_PARALLEL_LANGUAGE = "language_parallel";
-
-    // XXX: extra used for transitioning to tool code
-    @Deprecated
-    public static final String EXTRA_TOOL_ID = "tool_id";
 }
