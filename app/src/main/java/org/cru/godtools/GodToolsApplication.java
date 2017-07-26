dadf39afa26f2fb7c3e55e542fb4d96f83c04cf5
Daniel Frett <daniel.frett@ccci.org>
2017-07-26 19:53:14 -0400

    Picasso is only being used to load images off disk, so no need for an http client

--- app/src/main/java/org/cru/godtools/GodToolsApplication.java
+++ app/src/main/java/org/cru/godtools/GodToolsApplication.java
@@ -1,15 +1,11 @@
 package org.cru.godtools;
 
 import android.app.Application;
-import android.support.annotation.NonNull;
 
 import com.crashlytics.android.Crashlytics;
 import com.evernote.android.job.JobManager;
-import com.jakewharton.picasso.OkHttp3Downloader;
 import com.newrelic.agent.android.NewRelic;
-import com.squareup.picasso.Picasso;
 
-import org.ccci.gto.android.common.api.okhttp3.util.OkHttpClientUtil;
 import org.cru.godtools.analytics.AnalyticsEventBusIndex;
 import org.cru.godtools.download.manager.DownloadManagerEventBusIndex;
 import org.cru.godtools.download.manager.GodToolsDownloadManager;
@@ -21,7 +17,6 @@ import org.cru.godtools.tract.service.FollowupService;
 import org.greenrobot.eventbus.EventBus;
 
 import io.fabric.sdk.android.Fabric;
-import okhttp3.OkHttpClient;
 
 import static org.keynote.godtools.android.BuildConfig.NEW_RELIC_API_KEY;
 
@@ -37,28 +32,12 @@ public class GodToolsApplication extends Application {
         // configure eventbus
         configureEventbus();
 
-        // initialize several support libraries
-        Picasso.setSingletonInstance(picassoBuilder().build());
-
         // start the Job Manager
         JobManager.create(this).addJobCreator(new SyncJobCreator());
 
         // Initialize tool manager
         GodToolsDownloadManager.getInstance(this);
         FollowupService.start(this);
-    }
-
-    @NonNull
-    protected Picasso.Builder picassoBuilder() {
-        final Picasso.Builder builder = new Picasso.Builder(this);
-
-        // use OkHttp3 for the downloader
-        final OkHttpClient okhttp = OkHttpClientUtil.attachGlobalInterceptors(new OkHttpClient.Builder())
-                .cache(OkHttp3Downloader.createDefaultCache(this))
-                .build();
-        builder.downloader(new OkHttp3Downloader(okhttp));
-
-        return builder;
     }
 
     private void configureEventbus() {
