f49a539680611a6ce80e961f597f5d3adcdbe74f
Daniel Frett <daniel.frett@ccci.org>
2017-07-31 13:18:53 -0400

    if the user doesn't have an app to handle mailto links, show the web contact form

--- app/src/main/java/org/keynote/godtools/android/Constants.java
+++ app/src/main/java/org/keynote/godtools/android/Constants.java
@@ -13,6 +13,7 @@ public final class Constants {
 
     // Common base URI's
     public static final Uri MAILTO_SUPPORT = Uri.parse("mailto:support@godtoolsapp.com");
+    public static final Uri URI_SUPPORT = Uri.parse("http://www.godtoolsapp.com/#contact");
     public static final Uri URI_HELP = Uri.parse("http://www.godtoolsapp.com/faq");
     public static final Uri URI_PRIVACY = Uri.parse("https://www.cru.org/about/privacy.html");
     public static final Uri URI_TERMS_OF_USE = Uri.parse("http://www.godtoolsapp.com/terms");
