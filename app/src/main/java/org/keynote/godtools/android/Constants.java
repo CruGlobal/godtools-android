52b7f676dc476214c0d8aeb8e5455c02a4ea5217
Daniel Frett <daniel.frett@ccci.org>
2017-08-01 18:39:43 -0400

    remove a couple unused constants

--- app/src/main/java/org/keynote/godtools/android/Constants.java
+++ app/src/main/java/org/keynote/godtools/android/Constants.java
@@ -3,11 +3,7 @@ package org.keynote.godtools.android;
 import android.net.Uri;
 
 public final class Constants {
-    // common extras
-    public static final String EXTRA_TOOL = org.cru.godtools.base.Constants.EXTRA_TOOL;
-
     // SharedPreferences for this app
-    public static final String PREFS_SETTINGS = "GodTools";
     public static final String PREF_PRIMARY_LANGUAGE = "languagePrimary";
     public static final String PREF_PARALLEL_LANGUAGE = "languageParallel";
 
