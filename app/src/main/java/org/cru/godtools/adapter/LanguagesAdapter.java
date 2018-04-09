434126a70d352e7b2fefeefd50c322767106e2fb
Daniel Frett <daniel.frett@ccci.org>
2018-04-09 10:57:58 -0400

    fix a checkstyle violation

--- app/src/main/java/org/cru/godtools/adapter/LanguagesAdapter.java
+++ app/src/main/java/org/cru/godtools/adapter/LanguagesAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.UiThread;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
