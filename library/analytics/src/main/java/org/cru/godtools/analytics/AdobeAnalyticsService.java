a2af73056cc7b9a289f0ae9ae699441e03150da5
Daniel Frett <daniel.frett@ccci.org>
2018-04-10 19:12:55 -0400

    s/trackEvent/trackState

--- library/analytics/src/main/java/org/cru/godtools/analytics/AdobeAnalyticsService.java
+++ library/analytics/src/main/java/org/cru/godtools/analytics/AdobeAnalyticsService.java
@@ -71,18 +71,18 @@ class AdobeAnalyticsService implements AnalyticsService {
 
     @Override
     public void onTrackScreen(@NonNull final String screen, @Nullable final Locale locale) {
-        trackEvent(screen, locale);
+        trackState(screen, locale);
     }
 
     @Override
     public void onTrackTractPage(@NonNull final String tract, @NonNull final Locale locale, final int page,
                                  @Nullable final Integer card) {
-        trackEvent(tractPageToScreenName(tract, page, card), locale);
+        trackState(tractPageToScreenName(tract, page, card), locale);
     }
 
     /* END tracking methods */
 
-    private void trackEvent(@NonNull final String screen, @Nullable final Locale contentLocale) {
+    private void trackState(@NonNull final String screen, @Nullable final Locale contentLocale) {
         final Activity activity = mActiveActivity.get();
         if (activity != null) {
             mAnalyticsExecutor.execute(() -> {
