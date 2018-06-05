2710d5325cbbb9e65850b6b5785986d7129e3e59
Daniel Frett <daniel.frett@ccci.org>
2018-06-05 14:49:30 -0400

    drop the www from several fixed nav links

--- app/src/main/java/org/keynote/godtools/android/Constants.java
+++ app/src/main/java/org/keynote/godtools/android/Constants.java
@@ -9,9 +9,9 @@ public final class Constants {
 
     // Common base URI's
     public static final Uri MAILTO_SUPPORT = Uri.parse("mailto:support@godtoolsapp.com");
-    public static final Uri URI_SUPPORT = Uri.parse("https://www.godtoolsapp.com/#contact");
-    public static final Uri URI_HELP = Uri.parse("https://www.godtoolsapp.com/faq/");
+    public static final Uri URI_SUPPORT = Uri.parse("https://godtoolsapp.com/#contact");
+    public static final Uri URI_HELP = Uri.parse("https://godtoolsapp.com/faq/");
     public static final Uri URI_PRIVACY = Uri.parse("https://www.cru.org/about/privacy.html");
-    public static final Uri URI_TERMS_OF_USE = Uri.parse("https://www.godtoolsapp.com/user-agreement/");
-    public static final Uri URI_COPYRIGHT = Uri.parse("https://www.godtoolsapp.com/copyright/");
+    public static final Uri URI_TERMS_OF_USE = Uri.parse("https://godtoolsapp.com/user-agreement/");
+    public static final Uri URI_COPYRIGHT = Uri.parse("https://godtoolsapp.com/copyright/");
 }
