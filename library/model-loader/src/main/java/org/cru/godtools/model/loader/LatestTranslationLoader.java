fcd8a7c72531576836def35d3712f5a6d52f3752
Daniel Frett <daniel.frett@ccci.org>
2018-06-12 14:38:23 -0400

    utilize convenience method on DAO

--- library/model-loader/src/main/java/org/cru/godtools/model/loader/LatestTranslationLoader.java
+++ library/model-loader/src/main/java/org/cru/godtools/model/loader/LatestTranslationLoader.java
@@ -4,20 +4,13 @@ import android.content.Context;
 import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 
-import org.ccci.gto.android.common.db.Query;
 import org.ccci.gto.android.common.eventbus.content.CachingAsyncTaskEventBusLoader;
 import org.cru.godtools.model.Translation;
-import org.keynote.godtools.android.db.Contract.TranslationTable;
 import org.keynote.godtools.android.db.GodToolsDao;
 
-import java.util.List;
 import java.util.Locale;
 
 public final class LatestTranslationLoader extends CachingAsyncTaskEventBusLoader<Translation> {
-    private static final Query<Translation> QUERY = Query.select(Translation.class)
-            .orderBy(TranslationTable.COLUMN_VERSION + " DESC")
-            .limit(1);
-
     @NonNull
     private final GodToolsDao mDao;
     @NonNull
@@ -47,8 +40,6 @@ public final class LatestTranslationLoader extends CachingAsyncTaskEventBusLoade
 
     @Override
     public Translation loadInBackground() {
-        final List<Translation> translations =
-                mDao.get(QUERY.where(TranslationTable.SQL_WHERE_TOOL_LANGUAGE.args(mTool, mLocale)));
-        return translations.isEmpty() ? null : translations.get(0);
+        return mDao.getLatestTranslation(mTool, mLocale).orElse(null);
     }
 }
