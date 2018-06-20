e0952ed567019ce65676474beaeefd22292bcd42
Daniel Frett <daniel.frett@ccci.org>
2018-06-20 14:02:36 -0400

    move Snowplow analytics to background thread

--- library/analytics/src/main/java/org/cru/godtools/analytics/SnowplowAnalyticsService.java
+++ library/analytics/src/main/java/org/cru/godtools/analytics/SnowplowAnalyticsService.java
@@ -5,13 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.snowplowanalytics.snowplow.tracker.Emitter;
-import com.snowplowanalytics.snowplow.tracker.Emitter.EmitterBuilder;
 import com.snowplowanalytics.snowplow.tracker.Tracker;
-import com.snowplowanalytics.snowplow.tracker.Tracker.TrackerBuilder;
 import com.snowplowanalytics.snowplow.tracker.events.ScreenView;
 import com.snowplowanalytics.snowplow.tracker.events.Structured;
 
 import java.util.Locale;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import static org.cru.godtools.analytics.BuildConfig.SNOWPLOW_ENDPOINT;
 
@@ -20,18 +20,24 @@ class SnowplowAnalyticsService implements AnalyticsService {
     private static final String SNOWPLOW_APP_ID = "GodTools";
     private static final String SNOWPLOW_NAMESPACE = "GodToolsSnowPlowAndroidTracker";
 
-    @NonNull
-    private final Tracker mSnowPlowTracker;
+    /**
+     * Single thread executor to serialize events on a background thread.
+     */
+    private final Executor mAnalyticsExecutor = Executors.newSingleThreadExecutor();
+    private Tracker mSnowPlowTracker;
 
     private SnowplowAnalyticsService(@NonNull final Context context) {
-        Tracker.close();
-        final Emitter emitter = new EmitterBuilder(SNOWPLOW_ENDPOINT, context).build();
-        mSnowPlowTracker = new TrackerBuilder(emitter, SNOWPLOW_NAMESPACE, SNOWPLOW_APP_ID, context)
-//                .level(LogLevel.DEBUG)
-                .base64(false)
-                .mobileContext(true)
-                .lifecycleEvents(true)
-                .build();
+        mAnalyticsExecutor.execute(() -> {
+            Tracker.close();
+            // XXX: creating an Emitter will initialize the event store database on whichever thread the emitter is
+            // XXX: created on. Because of this we serialize all Snowplow interactions on a background thread.
+            final Emitter emitter = new Emitter.EmitterBuilder(SNOWPLOW_ENDPOINT, context).build();
+            mSnowPlowTracker = new Tracker.TrackerBuilder(emitter, SNOWPLOW_NAMESPACE, SNOWPLOW_APP_ID, context)
+                    .base64(false)
+                    .mobileContext(true)
+                    .lifecycleEvents(true)
+                    .build();
+        });
     }
 
     @Nullable
@@ -49,16 +55,16 @@ class SnowplowAnalyticsService implements AnalyticsService {
 
     @Override
     public void onTrackScreen(@NonNull final String screen, @Nullable final Locale locale) {
-        mSnowPlowTracker.track(ScreenView.builder().name(screen).build());
+        mAnalyticsExecutor.execute(() -> mSnowPlowTracker.track(ScreenView.builder().name(screen).build()));
     }
 
     @Override
     public void onTrackEveryStudentSearch(@NonNull final String query) {
-        mSnowPlowTracker.track(Structured.builder()
-                                       .category(CATEGORY_EVERYSTUDENT_SEARCH)
-                                       .action(ACTION_EVERYSTUDENT_SEARCH)
-                                       .label(query)
-                                       .build());
+        mAnalyticsExecutor.execute(() -> mSnowPlowTracker.track(Structured.builder()
+                                                                        .category(CATEGORY_EVERYSTUDENT_SEARCH)
+                                                                        .action(ACTION_EVERYSTUDENT_SEARCH)
+                                                                        .label(query)
+                                                                        .build()));
     }
 
     /* END tracking methods */
