d77345ffa957f1c3365e829a55d52d89d2578e81
Gyasi Story <gyasi.story@cru.org>
2018-08-13 17:43:07 -0400

    Errors
    Had a few Typos and unnecessary commented code.

--- gradlew.bat
+++ gradlew.bat
@@ -1,4 +1,4 @@
-gra@if "%DEBUG%" == "" @echo off
+@if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
