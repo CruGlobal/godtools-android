cb110508873eebc20c91dbe679b01fe2a77f3be2
Gyasi Story <gyasi.story@cru.org>
2018-08-21 16:34:50 -0400

    There are a few issues in some of the queries. That I still need to work out. But the Models are done for now.

--- ui/articles-aem-renderer/src/main/java/org/godtools/uiarticles_aem_renderer/model/Article.java
+++ ui/articles-aem-renderer/src/main/java/org/godtools/uiarticles_aem_renderer/model/Article.java
@@ -12,7 +12,7 @@ import java.sql.Date;
  *
  * @author Gyasi Story
  */
-@Entity(tableName = "article_table", indices = )
+@Entity(tableName = "article_table")
 public class Article {
     /**
      * Unique Identifier for the article table
