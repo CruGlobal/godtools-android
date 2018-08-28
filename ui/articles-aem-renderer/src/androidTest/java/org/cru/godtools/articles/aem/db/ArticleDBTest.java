76f35181dc8aa5e68b306d1164f4f22fb20be019
Daniel Frett <daniel.frett@ccci.org>
2018-08-28 15:43:25 -0400

    let any ParseException throw instead of trying to handle it gracefully

--- ui/articles-aem-renderer/src/androidTest/java/org/cru/godtools/articles/aem/db/ArticleDBTest.java
+++ ui/articles-aem-renderer/src/androidTest/java/org/cru/godtools/articles/aem/db/ArticleDBTest.java
@@ -4,7 +4,6 @@ import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
 
 import org.cru.godtools.articles.aem.model.Article;
 import org.cru.godtools.articles.aem.model.Attachment;
@@ -15,7 +14,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -39,7 +37,7 @@ public class ArticleDBTest {
     private List<Article> mSavedArticles = new ArrayList<>();
 
     @Before
-    public void createDb() {
+    public void createDb() throws Exception {
         Context context = InstrumentationRegistry.getTargetContext();
         db = Room.inMemoryDatabaseBuilder(context,
                 ArticleRoomDatabase.class).allowMainThreadQueries().build();
@@ -52,15 +50,10 @@ public class ArticleDBTest {
             Article article = new Article();
 
             article.mContent = "<p> The Body </>";
-            try {
-                article.mDateCreated = new SimpleDateFormat("E MMM dd yyyy HH:mm:ss zz")
-                        .parse("Fri Jun 08 2018 18:55:00 GMT+0000").getTime();
-                article.mDateUpdated = new SimpleDateFormat("E MMM dd yyyy HH:mm:ss zz")
-                        .parse("Sat May 19 2018 00:23:39 GMT+0000").getTime();
-            } catch (ParseException e) {
-                Log.e(TAG, "createDb: ", e);
-                assert (false);
-            }
+            article.mDateCreated = new SimpleDateFormat("E MMM dd yyyy HH:mm:ss zz")
+                    .parse("Fri Jun 08 2018 18:55:00 GMT+0000").getTime();
+            article.mDateUpdated = new SimpleDateFormat("E MMM dd yyyy HH:mm:ss zz")
+                    .parse("Sat May 19 2018 00:23:39 GMT+0000").getTime();
             Calendar calendar = Calendar.getInstance();
             calendar.getTime();
             calendar.setTimeInMillis(article.mDateCreated);
@@ -75,7 +68,6 @@ public class ArticleDBTest {
         }
 
         mSavedArticles = mArticleDao.getTestableAllArticles();
-
 
         for (int i = 0; i < mSavedArticles.size(); i++) {
             ManifestAssociation association = new ManifestAssociation();
