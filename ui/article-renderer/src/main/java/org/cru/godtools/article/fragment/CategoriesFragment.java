d151a9896e3ef43241c80471085a432acd665890
Daniel Frett <daniel.frett@ccci.org>
2018-09-07 13:48:04 -0400

    create CategoriesFragment that hosts the categories recyclerview

---
+++ ui/article-renderer/src/main/java/org/cru/godtools/article/fragment/CategoriesFragment.java
@@ -0,0 +1,105 @@
+package org.cru.godtools.article.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.CallSuper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.ccci.gto.android.common.recyclerview.decorator.VerticalSpaceItemDecoration;
+import org.cru.godtools.article.R;
+import org.cru.godtools.article.R2;
+import org.cru.godtools.article.adapter.CategoriesAdapter;
+import org.cru.godtools.base.tool.fragment.BaseToolFragment;
+import org.cru.godtools.xml.model.Category;
+
+import java.util.Locale;
+
+import butterknife.BindView;
+import timber.log.Timber;
+
+public class CategoriesFragment extends BaseToolFragment implements CategoriesAdapter.Callbacks {
+    @Nullable
+    @BindView(R2.id.categories)
+    RecyclerView mCategoriesView;
+    @Nullable
+    CategoriesAdapter mCategoriesAdapter;
+
+    public static Fragment newInstance(@NonNull final String code, @NonNull final Locale locale) {
+        final CategoriesFragment fragment = new CategoriesFragment();
+        final Bundle args = new Bundle(2);
+        populateArgs(args, code, locale);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    // region Lifecycle Events
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container,
+                             @Nullable final Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_categories, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupCategoriesView();
+    }
+
+    @Override
+    @CallSuper
+    protected void onManifestUpdated() {
+        super.onManifestUpdated();
+        updateCategoriesView();
+    }
+
+    @Override
+    public void onCategorySelected(@Nullable final Category category) {
+        Timber.d(category != null ? category.getId() : null);
+    }
+
+    @Override
+    public void onDestroyView() {
+        cleanupCategoriesView();
+        super.onDestroyView();
+    }
+
+    // endregion Lifecycle Events
+
+    // region Categories View
+
+    private void setupCategoriesView() {
+        if (mCategoriesView != null) {
+            mCategoriesView.setLayoutManager(new LinearLayoutManager(requireContext()));
+            mCategoriesView.setHasFixedSize(true);
+            mCategoriesView.addItemDecoration(new VerticalSpaceItemDecoration(R.dimen.categories_list_gap));
+
+            mCategoriesAdapter = new CategoriesAdapter();
+            mCategoriesAdapter.setCallbacks(this);
+            mCategoriesView.setAdapter(mCategoriesAdapter);
+            updateCategoriesView();
+        }
+    }
+
+    private void updateCategoriesView() {
+        if (mCategoriesAdapter != null) {
+            mCategoriesAdapter.setCategories(mManifest != null ? mManifest.getCategories() : null);
+        }
+    }
+
+    private void cleanupCategoriesView() {
+        if (mCategoriesAdapter != null) {
+            mCategoriesAdapter.setCallbacks(null);
+        }
+        mCategoriesAdapter = null;
+    }
+
+    // endregion Categories View
+}
