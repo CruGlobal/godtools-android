665123298587aca82a9bbc037a83e34de22f97b3
Daniel Frett <daniel.frett@ccci.org>
2018-09-14 11:31:18 -0400

    extract PriorityRunnable in order to reuse it

--- library/download-manager/src/main/java/org/cru/godtools/download/manager/GodToolsDownloadManager.java
+++ library/download-manager/src/main/java/org/cru/godtools/download/manager/GodToolsDownloadManager.java
@@ -22,7 +22,6 @@ import com.annimon.stream.Stream;
 import com.google.common.base.Objects;
 import com.google.common.io.Closer;
 import com.google.common.io.CountingInputStream;
-import com.google.common.primitives.Ints;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import org.ccci.gto.android.common.concurrent.NamedThreadFactory;
@@ -35,6 +34,7 @@ import org.ccci.gto.android.common.util.IOUtils.ProgressCallback;
 import org.cru.godtools.api.GodToolsApi;
 import org.cru.godtools.base.Settings;
 import org.cru.godtools.base.util.FileUtils;
+import org.cru.godtools.base.util.PriorityRunnable;
 import org.cru.godtools.model.Attachment;
 import org.cru.godtools.model.Language;
 import org.cru.godtools.model.LocalFile;
@@ -871,22 +871,13 @@ public final class GodToolsDownloadManager {
         void onDownloadProgressUpdated(@Nullable DownloadProgress progress);
     }
 
-    abstract static class PriorityRunnable implements Comparable<PriorityRunnable>, Runnable {
-        static final int PRIORITY_PRIMARY = -10;
-        static final int PRIORITY_ATTACHMENT = 0;
-        static final int PRIORITY_PARALLEL = 10;
-        static final int PRIORITY_OTHER = 20;
-        static final int PRIMARY_PRUNE_FILESYSTEM = Integer.MAX_VALUE;
+    // region Task PriorityRunnables
 
-        protected int getPriority() {
-            return Integer.MAX_VALUE;
-        }
-
-        @Override
-        public final int compareTo(@NonNull final PriorityRunnable o) {
-            return Ints.compare(getPriority(), o.getPriority());
-        }
-    }
+    private static final int PRIORITY_PRIMARY = -10;
+    private static final int PRIORITY_ATTACHMENT = 0;
+    private static final int PRIORITY_PARALLEL = 10;
+    private static final int PRIORITY_OTHER = 20;
+    private static final int PRIMARY_PRUNE_FILESYSTEM = Integer.MAX_VALUE;
 
     final class DownloadTranslationRunnable extends PriorityRunnable {
         @NonNull
@@ -946,4 +937,6 @@ public final class GodToolsDownloadManager {
             scheduleNextCleanFilesystem();
         }
     }
+
+    // endregion Task PriorityRunnables
 }
