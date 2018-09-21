970f3c1d91670eed5f53c9b1ba84027d9bf997a0
Daniel Frett <daniel.frett@ccci.org>
2018-09-21 13:54:27 -0400

    we already have the FragmentManager, so no need to get it again

--- ui/article-renderer/src/main/java/org/cru/godtools/article/activity/CategoriesActivity.java
+++ ui/article-renderer/src/main/java/org/cru/godtools/article/activity/CategoriesActivity.java
@@ -65,7 +65,7 @@ public class CategoriesActivity extends BaseSingleToolActivity implements Catego
 
         // update the displayed fragment
         assert mTool != null;
-        getSupportFragmentManager().beginTransaction()
+        fm.beginTransaction()
                 .replace(R.id.frame, CategoriesFragment.newInstance(mTool, mLocale), TAG_MAIN_FRAGMENT)
                 .commit();
     }
