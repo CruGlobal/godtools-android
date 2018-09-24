5e2ac5bc1dd994ecec082bd66c01217fb4f8d00b
Daniel Frett <daniel.frett@ccci.org>
2018-09-24 13:40:44 -0400

    create an AEM Retrofit API client

---
+++ ui/articles-aem-renderer/src/main/java/org/cru/godtools/articles/aem/api/AemApi.java
@@ -0,0 +1,41 @@
+package org.cru.godtools.articles.aem.api;
+
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import org.ccci.gto.android.common.api.okhttp3.util.OkHttpClientUtil;
+import org.ccci.gto.android.common.api.retrofit2.converter.JSONObjectConverterFactory;
+import org.json.JSONObject;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
+
+public interface AemApi {
+    @GET
+    Call<JSONObject> getJson(@Url Uri uri);
+
+    @GET
+    @Streaming
+    Call<ResponseBody> downloadAttachment(@Url Uri uri);
+
+    static AemApi buildInstance(@NonNull final String uri) {
+        // create OkHttp client
+        final OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS);
+        final OkHttpClient okHttp = OkHttpClientUtil.attachGlobalInterceptors(builder).build();
+
+        // create RetroFit API
+        return new Retrofit.Builder().baseUrl(uri)
+                .addConverterFactory(new JSONObjectConverterFactory())
+                .callFactory(okHttp)
+                .build().create(AemApi.class);
+    }
+}
