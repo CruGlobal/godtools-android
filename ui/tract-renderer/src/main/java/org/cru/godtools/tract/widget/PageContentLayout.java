eabfb6a9c01f0b79e172ba2ebdfd582fbe2c28d0
Daniel Frett <daniel.frett@ccci.org>
2018-09-27 10:56:10 -0400

    stop any PCL animation currently running if we are changing the active card without animating.

--- ui/tract-renderer/src/main/java/org/cru/godtools/tract/widget/PageContentLayout.java
+++ ui/tract-renderer/src/main/java/org/cru/godtools/tract/widget/PageContentLayout.java
@@ -269,6 +269,7 @@ public class PageContentLayout extends FrameLayout implements NestedScrollingPar
         changeActiveCard(getChildAt(mCardPositionOffset + cardPosition), animate);
     }
 
+    @UiThread
     public void changeActiveCard(@Nullable final View view, final boolean animate) {
         if (view != null && view.getParent() != this) {
             throw new IllegalArgumentException("can't change the active view to a view that isn't a child");
@@ -294,10 +295,17 @@ public class PageContentLayout extends FrameLayout implements NestedScrollingPar
                     oldAnimation.cancel();
                 }
                 mAnimation.start();
-            }
+            } else {
+                // stop any running animation
+                final Animator oldAnimation = mAnimation;
+                mAnimation = null;
+                if (oldAnimation != null) {
+                    oldAnimation.cancel();
+                }
 
-            updateChildrenOffsetsAndAlpha();
-            dispatchActiveCardChanged();
+                updateChildrenOffsetsAndAlpha();
+                dispatchActiveCardChanged();
+            }
         } else {
             updateActiveCardPosition(true);
         }
@@ -630,6 +638,7 @@ public class PageContentLayout extends FrameLayout implements NestedScrollingPar
         return 1;
     }
 
+    @UiThread
     void updateChildrenOffsetsAndAlpha() {
         // update the child position if we aren't animating
         if (mAnimation == null) {
