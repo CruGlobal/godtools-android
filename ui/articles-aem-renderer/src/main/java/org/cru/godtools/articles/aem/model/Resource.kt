d29a2d2ef3aa273a4561afaa4b3c3c324b68a26a
Daniel Frett <daniel.frett@ccci.org>
2018-10-10 14:16:37 -0400

    s/Attachment/Resource
    Also convert it to Kotlin in the process

---
+++ ui/articles-aem-renderer/src/main/java/org/cru/godtools/articles/aem/model/Resource.kt
@@ -0,0 +1,19 @@
+package org.cru.godtools.articles.aem.model
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+import android.net.Uri
+import java.util.*
+
+@Entity(tableName = "resources")
+class Resource(@field:PrimaryKey val uri: Uri) {
+    var localFileName: String? = null
+    var dateDownloaded: Date? = null
+
+    /**
+     * @return true if this resource needs to be downloaded
+     */
+    fun needsDownload(): Boolean {
+        return localFileName == null || dateDownloaded == null
+    }
+}
