b6b91daf1aa1a810c043253de31900f9b0c2e85f
Daniel Frett <daniel.frett@ccci.org>
2018-10-16 15:34:15 -0400

    parse <article:aem-tag id="{tag}"/> for categories

--- library/xml-model/src/main/java/org/cru/godtools/xml/model/Category.java
+++ library/xml-model/src/main/java/org/cru/godtools/xml/model/Category.java
@@ -4,16 +4,22 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 
+import com.google.common.collect.ImmutableList;
+
 import org.ccci.gto.android.common.util.XmlPullParserUtils;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.cru.godtools.xml.Constants.XMLNS_ARTICLE;
 import static org.cru.godtools.xml.Constants.XMLNS_MANIFEST;
 
 public class Category extends Base {
     static final String XML_CATEGORY = "category";
+    private static final String XML_AEM_TAG = "aem-tag";
     private static final String XML_ID = "id";
     private static final String XML_LABEL = "label";
     private static final String XML_BANNER = "banner";
@@ -24,6 +30,9 @@ public class Category extends Base {
     private Text mLabel;
     @Nullable
     private String mBanner;
+
+    @NonNull
+    private List<String> mAemTags = ImmutableList.of();
 
     @NonNull
     @WorkerThread
@@ -51,6 +60,11 @@ public class Category extends Base {
         return getResource(mBanner);
     }
 
+    @NonNull
+    public List<String> getAemTags() {
+        return mAemTags;
+    }
+
     @WorkerThread
     private Category parse(@NonNull final XmlPullParser parser) throws IOException, XmlPullParserException {
         parser.require(XmlPullParser.START_TAG, XMLNS_MANIFEST, XML_CATEGORY);
@@ -58,6 +72,7 @@ public class Category extends Base {
         mId = parser.getAttributeValue(null, XML_ID);
         mBanner = parser.getAttributeValue(null, XML_BANNER);
 
+        final List<String> aemTags = new ArrayList<>();
         while (parser.next() != XmlPullParser.END_TAG) {
             if (parser.getEventType() != XmlPullParser.START_TAG) {
                 continue;
@@ -72,7 +87,15 @@ public class Category extends Base {
                             continue;
                     }
                     break;
+                case XMLNS_ARTICLE:
+                    switch (parser.getName()) {
+                        case XML_AEM_TAG:
+                            aemTags.add(parser.getAttributeValue(null, XML_ID));
+                            break;
+                    }
+                    break;
             }
+            mAemTags = ImmutableList.copyOf(aemTags);
 
             // skip unrecognized nodes
             XmlPullParserUtils.skipTag(parser);
