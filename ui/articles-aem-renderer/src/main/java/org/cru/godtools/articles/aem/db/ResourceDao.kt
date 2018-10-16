fb25ce514b894593af0ec7f45e5623a0a542e738
Gyasi Story <gyasi.story@cru.org>
2018-10-16 16:34:42 -0400

    Remove unnecessary method

--- ui/articles-aem-renderer/src/main/java/org/cru/godtools/articles/aem/db/ResourceDao.kt
+++ ui/articles-aem-renderer/src/main/java/org/cru/godtools/articles/aem/db/ResourceDao.kt
@@ -1,6 +1,5 @@
 package org.cru.godtools.articles.aem.db
 
-import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
@@ -8,7 +7,7 @@ import android.arch.persistence.room.Query
 import android.net.Uri
 import android.support.annotation.WorkerThread
 import org.cru.godtools.articles.aem.model.Resource
-import java.util.Date
+import java.util.*
 
 @Dao
 interface ResourceDao {
@@ -33,11 +32,4 @@ interface ResourceDao {
         WHERE a.articleUri = :uri
         """)
     fun getAllForArticle(uri: Uri): List<Resource>
-
-    @Query("""
-        SELECT r.*
-        FROM resources AS r JOIN articleResources AS a ON a.resourceUri = r.uri
-        WHERE a.articleUri = :uri
-        """)
-    fun getAllLiveForArticle(uri: Uri): LiveData<List<Resource>>
 }
