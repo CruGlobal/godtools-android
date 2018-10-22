22c8f9429cc89d202ff76d0a0e47fbdfd3836439
Gyasi Story <gyasi.story@cru.org>
2018-10-22 11:17:39 -0400

    Move logic to its Own Class to prepare for move to GTO-Support

--- ui/articles-aem-renderer/src/main/java/org/cru/godtools/articles/aem/api/AemApi.java
+++ ui/articles-aem-renderer/src/main/java/org/cru/godtools/articles/aem/api/AemApi.java
@@ -1,31 +1,14 @@
 package org.cru.godtools.articles.aem.api;
 
 import android.net.Uri;
-import android.os.Build;
 import android.support.annotation.NonNull;
 
-import com.google.common.collect.ImmutableList;
-
 import org.ccci.gto.android.common.api.retrofit2.converter.JSONObjectConverterFactory;
-import org.ccci.gto.android.common.okhttp3.util.OkHttpClientUtil;
+import org.cru.godtools.articles.aem.service.OkHttpClientProvider;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.TimeUnit;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
-
-import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
-import okhttp3.TlsVersion;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -33,7 +16,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 import retrofit2.http.Streaming;
 import retrofit2.http.Url;
-import timber.log.Timber;
 
 public interface AemApi {
     @GET
@@ -48,26 +30,7 @@ public interface AemApi {
 
     static AemApi buildInstance(@NonNull final String uri) {
 
-            // create OkHttp client
-            final OkHttpClient.Builder builder = new OkHttpClient.Builder()
-                    .connectTimeout(60, TimeUnit.SECONDS)
-                    .readTimeout(60, TimeUnit.SECONDS);
-
-
-            if (Build.VERSION.SDK_INT < 22) {
-                try {
-                    SSLContext sc = SSLContext.getInstance("TLS");
-                    sc.init(null, null, null);
-                    builder.sslSocketFactory(new Tls12SocketFactory(sc.getSocketFactory()));
-                    ConnectionSpec connectionSpec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-                            .tlsVersions(TlsVersion.TLS_1_2)
-                            .build();
-                    builder.connectionSpecs(ImmutableList.of(connectionSpec));
-                } catch (NoSuchAlgorithmException | KeyManagementException e) {
-                    Timber.e(e);
-                }
-            }
-        final OkHttpClient okHttp = OkHttpClientUtil.attachGlobalInterceptors(builder).build();
+        final OkHttpClient okHttp = OkHttpClientProvider.createBuilder().build();
             // create RetroFit API
             return new Retrofit.Builder().baseUrl(uri)
                     .addConverterFactory(ScalarsConverterFactory.create())
@@ -77,55 +40,5 @@ public interface AemApi {
 
     }
 
-    class Tls12SocketFactory extends SSLSocketFactory {
-        private static final String[] TLS_V12_ONLY = {"TLS"};
 
-        final SSLSocketFactory delegate;
-
-        public Tls12SocketFactory(SSLSocketFactory base) {
-            this.delegate = base;
-        }
-
-        @Override
-        public String[] getDefaultCipherSuites() {
-            return delegate.getDefaultCipherSuites();
-        }
-
-        @Override
-        public String[] getSupportedCipherSuites() {
-            return delegate.getSupportedCipherSuites();
-        }
-
-        @Override
-        public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
-            return patch(delegate.createSocket(s, host, port, autoClose));
-        }
-
-        @Override
-        public Socket createSocket(String host, int port) throws IOException, UnknownHostException {
-            return patch(delegate.createSocket(host, port));
-        }
-
-        @Override
-        public Socket createSocket(String host, int port, InetAddress localHost, int localPort) throws IOException, UnknownHostException {
-            return patch(delegate.createSocket(host, port, localHost, localPort));
-        }
-
-        @Override
-        public Socket createSocket(InetAddress host, int port) throws IOException {
-            return patch(delegate.createSocket(host, port));
-        }
-
-        @Override
-        public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort) throws IOException {
-            return patch(delegate.createSocket(address, port, localAddress, localPort));
-        }
-
-        private Socket patch(Socket s) {
-            if (s instanceof SSLSocket) {
-                ((SSLSocket) s).setEnabledProtocols(TLS_V12_ONLY);
-            }
-            return s;
-        }
-    }
 }
---
+++ ui/articles-aem-renderer/src/main/java/org/cru/godtools/articles/aem/service/OkHttpClientProvider.java
@@ -0,0 +1,117 @@
+package org.cru.godtools.articles.aem.service;
+
+import android.os.Build;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Collections;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.ConnectionSpec;
+import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
+import timber.log.Timber;
+
+public class OkHttpClientProvider {
+
+    public static OkHttpClient.Builder createBuilder () {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        return getEnabledTLSSupportedBuilder(builder);
+    }
+
+    private static OkHttpClient.Builder getEnabledTLSSupportedBuilder(OkHttpClient.Builder builder) {
+        if (Build.VERSION.SDK_INT >= 21) {
+            return builder;
+        }
+
+        try {
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+                    TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init((KeyStore) null);
+            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+            if (trustManagers.length != 1 || !(trustManagers[0]instanceof X509TrustManager)) {
+                throw new IllegalStateException("Unexpected default trust managers: " + Arrays.toString(trustManagers));
+            }
+            X509TrustManager trustManager = (X509TrustManager) trustManagers[0];
+
+            builder.sslSocketFactory(new Tls12SocketFactory(), trustManager);
+
+            ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS).tlsVersions(TlsVersion.TLS_1_2).build();
+
+            builder.connectionSpecs(Collections.singletonList(cs));
+        } catch (NoSuchAlgorithmException | IllegalStateException | KeyStoreException | KeyManagementException e) {
+            Timber.e(e);
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+        return builder;
+    }
+
+
+    private static class Tls12SocketFactory extends SSLSocketFactory {
+
+        final SSLSocketFactory delegate;
+
+        public Tls12SocketFactory() throws NoSuchAlgorithmException, KeyManagementException {
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, null, null);
+            this.delegate = context.getSocketFactory();
+        }
+
+        @Override
+        public String[] getDefaultCipherSuites() {
+            return delegate.getDefaultCipherSuites();
+        }
+
+        @Override
+        public String[] getSupportedCipherSuites() {
+            return delegate.getSupportedCipherSuites();
+        }
+
+        @Override
+        public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
+            return enableTLSOnSocket(delegate.createSocket(s, host, port, autoClose));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port) throws IOException {
+            return enableTLSOnSocket(delegate.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port, InetAddress localHost, int localPort) throws IOException {
+            return enableTLSOnSocket(delegate.createSocket(host, port, localHost, localPort));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress host, int port) throws IOException {
+            return enableTLSOnSocket(delegate.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort) throws IOException {
+            return enableTLSOnSocket(delegate.createSocket(address, port, localAddress, localPort));
+        }
+
+        private Socket enableTLSOnSocket(Socket socket) {
+            if (socket instanceof SSLSocket) {
+                ((SSLSocket) socket).setEnabledProtocols(
+                        new String[] {"TLSv1", "TLSv1.1", "TLSv1.2"}
+                );
+            }
+            return socket;
+        }
+    }
+}
