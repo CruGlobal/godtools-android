8a351585f6506a00a4f2c053c47ceecddb4d54cc
Daniel Frett <daniel.frett@ccci.org>
2018-10-24 14:26:47 -0400

    create a Builder for DynamicSSLSocketFactory

--- ui/articles-aem-renderer/src/main/java/org/cru/godtools/articles/aem/api/AemApi.java
+++ ui/articles-aem-renderer/src/main/java/org/cru/godtools/articles/aem/api/AemApi.java
@@ -1,14 +1,13 @@
 package org.cru.godtools.articles.aem.api;
 
 import android.net.Uri;
-import android.os.Build;
 import android.support.annotation.NonNull;
 
 import org.ccci.gto.android.common.api.retrofit2.converter.JSONObjectConverterFactory;
+import org.ccci.gto.android.common.okhttp3.util.OkHttpClientUtil;
 import org.cru.godtools.articles.aem.service.DynamicSSLSocketFactory;
 import org.json.JSONObject;
 
-import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +15,7 @@ import javax.net.ssl.SSLSocketFactory;
 
 import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
+import okhttp3.TlsVersion;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -37,26 +37,28 @@ public interface AemApi {
     Call<ResponseBody> downloadResource(@Url Uri uri);
 
     static AemApi buildInstance(@NonNull final String uri) {
-
-        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+        // create OkHttp client
+        final OkHttpClient.Builder builder = new OkHttpClient.Builder()
                 .connectTimeout(60, TimeUnit.SECONDS)
                 .readTimeout(60, TimeUnit.SECONDS);
-
-        if (Build.VERSION.SDK_INT <= 21) {
-            try {
-                SSLSocketFactory sslSocketFactory = new DynamicSSLSocketFactory();
-                builder.sslSocketFactory(sslSocketFactory);
-            } catch (NoSuchAlgorithmException | KeyManagementException e) {
-                Timber.e(e);
-            }
+        try {
+            // customize the SSLSocketFactory for this OkHttpClient
+            final SSLSocketFactory factory = DynamicSSLSocketFactory.create()
+                    // enable TLS 1.1 and TLS 1.2 for older versions of android that support it but don't enable it
+                    .addEnabledProtocols(TlsVersion.TLS_1_1.javaName(), TlsVersion.TLS_1_2.javaName())
+                    .build();
+            builder.sslSocketFactory(factory);
+        } catch (final NoSuchAlgorithmException e) {
+            Timber.tag("AemApi")
+                    .d(e, "Error creating the DynamicSSLSocketFactory");
         }
+        final OkHttpClient okHttp = OkHttpClientUtil.attachGlobalInterceptors(builder).build();
 
-        final OkHttpClient okHttp = builder.build();
-            // create RetroFit API
-            return new Retrofit.Builder().baseUrl(uri)
-                    .addConverterFactory(ScalarsConverterFactory.create())
-                    .addConverterFactory(new JSONObjectConverterFactory())
-                    .callFactory(okHttp)
-                    .build().create(AemApi.class);
+        // create RetroFit API
+        return new Retrofit.Builder().baseUrl(uri)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(new JSONObjectConverterFactory())
+                .callFactory(okHttp)
+                .build().create(AemApi.class);
     }
 }
--- ui/articles-aem-renderer/src/main/java/org/cru/godtools/articles/aem/service/DynamicSSLSocketFactory.java
+++ ui/articles-aem-renderer/src/main/java/org/cru/godtools/articles/aem/service/DynamicSSLSocketFactory.java
@@ -1,77 +1,118 @@
 package org.cru.godtools.articles.aem.service;
 
-import com.annimon.stream.Stream;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
 public class DynamicSSLSocketFactory extends SSLSocketFactory {
+    private final SSLSocketFactory mDelegate;
+    private final Set<String> mEnabledProtocolsToAdd = new HashSet<>();
 
-    final SSLSocketFactory delegate;
+    // region Initialization
 
-    public DynamicSSLSocketFactory() throws NoSuchAlgorithmException, KeyManagementException {
-        SSLContext context = SSLContext.getInstance("TLS");
-        context.init(null, null, null);
-        this.delegate = context.getSocketFactory();
+    DynamicSSLSocketFactory(@NonNull final Builder builder) {
+        mDelegate = builder.mFactory;
+        mEnabledProtocolsToAdd.addAll(builder.mEnabledProtocolsToAdd);
     }
+
+    public static Builder create() throws NoSuchAlgorithmException {
+        return new Builder(SSLContext.getDefault().getSocketFactory());
+    }
+
+    public static Builder wrap(@NonNull final SSLSocketFactory factory) {
+        return new Builder(factory);
+    }
+
+    // endregion Initialization
+
+    // region SSLSocketFactory Overrides
 
     @Override
     public String[] getDefaultCipherSuites() {
-        return delegate.getDefaultCipherSuites();
+        return mDelegate.getDefaultCipherSuites();
     }
 
     @Override
     public String[] getSupportedCipherSuites() {
-        return delegate.getSupportedCipherSuites();
+        return mDelegate.getSupportedCipherSuites();
     }
 
     @Override
-    public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
-        return enableTLSOnSocket(delegate.createSocket(s, host, port, autoClose));
+    public Socket createSocket(final Socket s, final String host, final int port, final boolean autoClose)
+            throws IOException {
+        return adjustSocket(mDelegate.createSocket(s, host, port, autoClose));
     }
 
     @Override
-    public Socket createSocket(String host, int port) throws IOException {
-        return enableTLSOnSocket(delegate.createSocket(host, port));
+    public Socket createSocket(final String host, final int port) throws IOException {
+        return adjustSocket(mDelegate.createSocket(host, port));
     }
 
     @Override
-    public Socket createSocket(String host, int port, InetAddress localHost, int localPort) throws IOException {
-        return enableTLSOnSocket(delegate.createSocket(host, port, localHost, localPort));
+    public Socket createSocket(final String host, final int port, final InetAddress localHost, final int localPort)
+            throws IOException {
+        return adjustSocket(mDelegate.createSocket(host, port, localHost, localPort));
     }
 
     @Override
-    public Socket createSocket(InetAddress host, int port) throws IOException {
-        return enableTLSOnSocket(delegate.createSocket(host, port));
+    public Socket createSocket(final InetAddress host, final int port) throws IOException {
+        return adjustSocket(mDelegate.createSocket(host, port));
     }
 
     @Override
-    public Socket createSocket(InetAddress address, int port, InetAddress localAddress,
-                               int localPort) throws IOException {
-        return enableTLSOnSocket(delegate.createSocket(address, port, localAddress, localPort));
+    public Socket createSocket(final InetAddress address, final int port, final InetAddress localAddress,
+                               final int localPort) throws IOException {
+        return adjustSocket(mDelegate.createSocket(address, port, localAddress, localPort));
     }
 
-    private Socket enableTLSOnSocket(Socket socket) {
+    // endregion SSLSocketFactory Overrides
+
+    private Socket adjustSocket(@Nullable final Socket socket) {
         if (socket instanceof SSLSocket) {
-            SSLSocket sslSocket = (SSLSocket) socket;
-            String[] supportedProtocols = sslSocket.getSupportedProtocols();
-            ArrayList<String> enabledProtocols = new ArrayList<>(Arrays.asList(sslSocket.getEnabledProtocols()));
-            Stream.of(supportedProtocols).forEach(ep -> {
-                if (!enabledProtocols.contains(ep)) {
-                    enabledProtocols.add(ep);
+            final SSLSocket sslSocket = (SSLSocket) socket;
+
+            // update the enabled protocols if required
+            if (!mEnabledProtocolsToAdd.isEmpty()) {
+                final Set<String> enabledProtocols = new HashSet<>();
+                Collections.addAll(enabledProtocols, sslSocket.getEnabledProtocols());
+                for (final String protocol : sslSocket.getSupportedProtocols()) {
+                    if (mEnabledProtocolsToAdd.contains(protocol)) {
+                        enabledProtocols.add(protocol);
+                    }
                 }
-            });
-            sslSocket.setEnabledProtocols(enabledProtocols.toArray(new String[0]));
+                sslSocket.setEnabledProtocols(enabledProtocols.toArray(new String[0]));
+            }
         }
         return socket;
+    }
+
+    public static final class Builder {
+        @NonNull
+        final SSLSocketFactory mFactory;
+        final Set<String> mEnabledProtocolsToAdd = new HashSet<>();
+
+        Builder(@NonNull final SSLSocketFactory factory) {
+            mFactory = factory;
+        }
+
+        public Builder addEnabledProtocols(@NonNull final String... protocols) {
+            Collections.addAll(mEnabledProtocolsToAdd, protocols);
+            return this;
+        }
+
+        public DynamicSSLSocketFactory build() {
+            return new DynamicSSLSocketFactory(this);
+        }
     }
 }
