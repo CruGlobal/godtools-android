356b787073e9b9802a2b078ab62baf57bbc12539
Daniel Frett <daniel.frett@ccci.org>
2018-10-25 14:13:01 -0400

    update ui/base-tool module for AndroidX

--- ui/base-tool/src/main/java/org/cru/godtools/base/tool/model/view/TextViewUtils.java
+++ ui/base-tool/src/main/java/org/cru/godtools/base/tool/model/view/TextViewUtils.java
@@ -2,14 +2,15 @@ package org.cru.godtools.base.tool.model.view;
 
 import android.content.Context;
 import android.graphics.Typeface;
-import android.support.annotation.ColorInt;
-import android.support.annotation.DimenRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.Gravity;
 import android.widget.TextView;
 
 import org.cru.godtools.xml.model.Text;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.DimenRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import static android.util.TypedValue.COMPLEX_UNIT_PX;
 
