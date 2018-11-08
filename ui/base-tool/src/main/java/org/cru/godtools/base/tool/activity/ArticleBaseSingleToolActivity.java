5dbfeb2857f88c4da56267ddaae7f758aaa15f72
Gyasi Story <gyasi.story@cru.org>
2018-11-08 15:54:32 -0500

    Verify that the article is an article before loading.

---
+++ ui/base-tool/src/main/java/org/cru/godtools/base/tool/activity/ArticleBaseSingleToolActivity.java
@@ -0,0 +1,22 @@
+package org.cru.godtools.base.tool.activity;
+
+import org.cru.godtools.xml.model.Manifest;
+
+public class ArticleBaseSingleToolActivity extends BaseSingleToolActivity {
+
+    public ArticleBaseSingleToolActivity(final boolean immersive) {
+        super(immersive);
+    }
+
+    @Override
+    protected int determineActiveToolState() {
+        int state = super.determineActiveToolState();
+        if (state == STATE_LOADED) {
+            if (mManifest == null) {
+                return STATE_NOT_FOUND;
+            }
+            return Manifest.Type.ARTICLE == mManifest.getType() ? state : STATE_NOT_FOUND;
+        }
+        return state;
+    }
+}
