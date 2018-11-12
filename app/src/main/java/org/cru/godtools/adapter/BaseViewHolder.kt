c0f1fc779ffa61b9ff50c1a23230fd28a472e659
Daniel Frett <daniel.frett@ccci.org>
2018-11-12 16:58:16 -0500

    convert BaseViewHolder to Kotlin

---
+++ app/src/main/java/org/cru/godtools/adapter/BaseViewHolder.kt
@@ -0,0 +1,13 @@
+package org.cru.godtools.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import butterknife.ButterKnife
+
+internal abstract class BaseViewHolder(rootView: View) : RecyclerView.ViewHolder(rootView) {
+    init {
+        ButterKnife.bind(this, rootView)
+    }
+
+    protected open fun bind(position: Int) {}
+}
