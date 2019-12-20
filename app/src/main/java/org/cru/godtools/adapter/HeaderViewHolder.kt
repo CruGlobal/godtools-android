bec84b2fc74329f9b82fbacec9836cf8ec36f3f1
Daniel Frett <daniel.frett@ccci.org>
2019-12-20 14:49:49 -0500

    remove a couple unused classes

--- app/src/main/java/org/cru/godtools/adapter/HeaderViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package org.cru.godtools.adapter
-
-import org.ccci.gto.android.common.recyclerview.adapter.DataBindingViewHolder
-import org.cru.godtools.databinding.ToolsTutorialBannerBinding
-
-class HeaderViewHolder(binding: ToolsTutorialBannerBinding) :
-    DataBindingViewHolder<ToolsTutorialBannerBinding>(binding)
