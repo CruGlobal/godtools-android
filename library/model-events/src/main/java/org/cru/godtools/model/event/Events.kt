679eb6c8de5ca20bac5b6139aeae24a7e836bc8c
Daniel Frett <daniel.frett@ccci.org>
2020-01-10 17:01:51 -0500

    convert ToolUsedEvent to Kotlin

--- library/model-events/src/main/java/org/cru/godtools/model/event/Events.kt
+++ library/model-events/src/main/java/org/cru/godtools/model/event/Events.kt
@@ -4,3 +4,8 @@ object AttachmentUpdateEvent
 object LanguageUpdateEvent
 object ToolUpdateEvent
 object TranslationUpdateEvent
+
+/**
+ * This event is fired when a tool is actually opened.
+ */
+class ToolUsedEvent(val toolCode: String)
