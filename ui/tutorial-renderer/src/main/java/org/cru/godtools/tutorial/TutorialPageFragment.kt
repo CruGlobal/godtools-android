cb85761fdd0f84fab1026922c9bbbe6703c6c3f7
Gyasi Story <gyasi.story@cru.org>
2020-01-13 15:33:18 -0500

    Implement Events

--- ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/TutorialPageFragment.kt
+++ ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/TutorialPageFragment.kt
@@ -8,9 +8,12 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import org.ccci.gto.android.common.util.findListener
+import org.cru.godtools.tutorial.analytics.model.TutorialAnalyticsActionEvent
+import org.cru.godtools.tutorial.analytics.model.TutorialAnalyticsScreenEvent
 import org.cru.godtools.tutorial.animation.animateViews
 import org.cru.godtools.tutorial.databinding.TutorialOnboardingWelcomeBinding
 import org.cru.godtools.tutorial.util.TutorialCallbacks
+import org.greenrobot.eventbus.EventBus
 import splitties.fragmentargs.arg
 
 internal class TutorialPageFragment() : Fragment(), TutorialCallbacks {
@@ -21,6 +24,7 @@ internal class TutorialPageFragment() : Fragment(), TutorialCallbacks {
     private var page by arg<Page>()
 
     private var binding: ViewDataBinding? = null
+    private val eventBus by lazy { EventBus.getDefault() }
 
     // region Lifecycle
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
@@ -36,6 +40,11 @@ internal class TutorialPageFragment() : Fragment(), TutorialCallbacks {
         super.onViewCreated(view, savedInstanceState)
         binding?.setVariable(BR.callbacks, this)
         binding?.startAnimations()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        eventBus.post(TutorialAnalyticsScreenEvent(page.screenName))
     }
 
     override fun onDestroyView() {
@@ -59,14 +68,17 @@ internal class TutorialPageFragment() : Fragment(), TutorialCallbacks {
 
     // region TutorialCallbacks
     override fun nextPage() {
+        eventBus.post(TutorialAnalyticsScreenEvent(page.screenName))
         findListener<TutorialCallbacks>()?.nextPage()
     }
 
     override fun launchTraining() {
+        eventBus.post(TutorialAnalyticsActionEvent(TutorialAnalyticsActionEvent.TUTORIAL_MORE_ACTION))
         findListener<TutorialCallbacks>()?.launchTraining()
     }
 
     override fun finishTutorial() {
+        eventBus.post(TutorialAnalyticsActionEvent(TutorialAnalyticsActionEvent.TUTORIAL_START_ACTION))
         findListener<TutorialCallbacks>()?.finishTutorial()
     }
     // endregion TutorialCallbacks
