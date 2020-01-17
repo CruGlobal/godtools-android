221da6230d83bb3d92c5ae9f3cdf158c4aa5d92d
Gyasi Story <gyasi.story@cru.org>
2020-01-17 08:59:23 -0500

    Revert "break line after every language"
    This reverts commit ee33beab5ef10390a6b238cfa4e43e1a3981b02a.

--- app/src/main/java/org/cru/godtools/ui/tooldetails/ToolDetailsFragment.java
+++ app/src/main/java/org/cru/godtools/ui/tooldetails/ToolDetailsFragment.java
@@ -586,13 +586,10 @@ public class ToolDetailsFragment extends BasePlatformFragment
                                                                           mTool));
                     break;
                 case 1:
-                    StringBuilder langList = new StringBuilder();
-                    for (String lang : Stream.of(mLanguages)
-                            .map(l -> LocaleUtils.getDisplayName(l, container.getContext(), null, null)).withoutNulls()
-                            .sorted(String.CASE_INSENSITIVE_ORDER).toList()) {
-                        langList.append(lang).append("\n");
-                    }
-                    textView.setText(langList.toString());
+                    textView.setText(Stream.of(mLanguages).map(l -> LocaleUtils
+                            .getDisplayName(l, container.getContext(), null, null)
+                    ).withoutNulls().sorted(String.CASE_INSENSITIVE_ORDER)
+                                             .reduce((l1, l2) -> l1 + ", " + l2).orElse(""));
                     break;
             }
             container.addView(textView, position);
