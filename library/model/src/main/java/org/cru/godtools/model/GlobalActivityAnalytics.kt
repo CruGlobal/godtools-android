4a5cb4b0c14bf5ef57c10fb6a141ad615154d6f0
Daniel Frett <daniel.frett@ccci.org>
2020-01-24 17:03:17 -0500

    make the json api type constant private

--- library/model/src/main/java/org/cru/godtools/model/GlobalActivityAnalytics.kt
+++ library/model/src/main/java/org/cru/godtools/model/GlobalActivityAnalytics.kt
@@ -3,13 +3,14 @@ package org.cru.godtools.model
 import org.ccci.gto.android.common.jsonapi.annotation.JsonApiAttribute
 import org.ccci.gto.android.common.jsonapi.annotation.JsonApiType
 
-const val JSON_API_TYPE = "global-activity-analytics"
+private const val JSON_API_TYPE_GLOBAL_ANALYTICS = "global-activity-analytics"
+
 private const val JSON_USERS = "users"
 private const val JSON_COUNTRIES = "countries"
 private const val JSON_LAUNCHES = "launches"
 private const val JSON_GOSPEL_PRESENTATION = "gospel-presentations"
 
-@JsonApiType(JSON_API_TYPE)
+@JsonApiType(JSON_API_TYPE_GLOBAL_ANALYTICS)
 class GlobalActivityAnalytics : Base() {
     @JsonApiAttribute(JSON_USERS)
     var users = 0
