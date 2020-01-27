ef6720e21acb8149bc9bbbe6edadd44234a35b4d
Gyasi Story <gyasi.story@cru.org>
2020-01-27 14:11:15 -0500

    Update To use Key Valuse

--- app/src/main/java/org/cru/godtools/fragment/GlobalDashboardFragment.kt
+++ app/src/main/java/org/cru/godtools/fragment/GlobalDashboardFragment.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import me.thekey.android.TheKey
 import org.cru.godtools.databinding.FragmentGlobalDashboardBinding
+import org.cru.godtools.viewmodel.GlobalDashboardDataModel
 
 class GlobalDashboardFragment : BasePlatformFragment() {
     private var binding: FragmentGlobalDashboardBinding? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentGlobalDashboardBinding.inflate(inflater, container, false)
+        binding?.viewModel = GlobalDashboardDataModel()
         return binding?.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding?.viewModel?.theKey?.value = TheKey.getInstance(requireContext())
     }
 }
--- app/src/main/java/org/cru/godtools/viewmodel/GlobalDashboardDataModel.kt
+++ app/src/main/java/org/cru/godtools/viewmodel/GlobalDashboardDataModel.kt
@@ -1,15 +1,21 @@
 package org.cru.godtools.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import me.thekey.android.TheKey
 
 class GlobalDashboardDataModel : ViewModel() {
+
+    val theKey = MutableLiveData<TheKey>()
     // TODO:  Create temporary Data to be updated in later PR
 
     val uniqueUsers = MutableLiveData<Int>()
     val gospelPresentation = MutableLiveData<Int>()
     val sessions = MutableLiveData<Int>()
     val countries = MutableLiveData<Int>()
-    val accountName = MutableLiveData<String>()
+    val accountName = Transformations.map(theKey) { key ->
+        String.format("%s %s", key.cachedAttributes.firstName, key.cachedAttributes.lastName)
+    }
     val accountDate = MutableLiveData<String>()
 }
