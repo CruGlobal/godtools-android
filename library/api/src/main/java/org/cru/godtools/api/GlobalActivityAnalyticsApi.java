45fa51c10b4289f31de97a7a4505670636420956
Gyasi Story <gyasi.story@cru.org>
2020-01-27 09:49:36 -0500

    No need for Streaming

--- library/api/src/main/java/org/cru/godtools/api/GlobalActivityAnalyticsApi.java
+++ library/api/src/main/java/org/cru/godtools/api/GlobalActivityAnalyticsApi.java
@@ -5,12 +5,10 @@ import org.cru.godtools.model.GlobalActivityAnalytics;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Streaming;
 
 public interface GlobalActivityAnalyticsApi {
     String PATH_ANALYTICS_GLOBAL = "analytics/global";
 
-    @Streaming
     @GET(PATH_ANALYTICS_GLOBAL)
     Call<JsonApiObject<GlobalActivityAnalytics>> getAnalytics();
 }
