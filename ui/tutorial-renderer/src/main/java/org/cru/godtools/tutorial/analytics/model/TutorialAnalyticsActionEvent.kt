a6ac38f8226eb09d9b5b6a745794a17e7353f7d0
Gyasi Story <gyasi.story@cru.org>
2020-01-27 11:09:57 -0500

    add cru prefix

--- ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/analytics/model/TutorialAnalyticsActionEvent.kt
+++ ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/analytics/model/TutorialAnalyticsActionEvent.kt
@@ -6,8 +6,8 @@ import org.cru.godtools.analytics.model.AnalyticsSystem
 internal const val ACTION_TUTORIAL_ONBOARDING_TRAINING = "On-Boarding More"
 internal const val ACTION_TUTORIAL_ONBOARDING_FINISH = "On-Boarding Start"
 
-private const val ADOBE_TUTORIAL_ONBOARDING_TRAINING = "onboarding_more"
-private const val ADOBE_TUTORIAL_ONBOARDING_FINISH = "onboarding_start"
+private const val ADOBE_TUTORIAL_ONBOARDING_TRAINING = "cru.onboarding_more"
+private const val ADOBE_TUTORIAL_ONBOARDING_FINISH = "cru.onboarding_start"
 
 class TutorialAnalyticsActionEvent(action: String) : AnalyticsActionEvent(category = null, action = action) {
     override fun isForSystem(system: AnalyticsSystem) = system == AnalyticsSystem.ADOBE
