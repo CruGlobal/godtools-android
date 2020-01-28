43c55c325f775d6f75fc6066508555de4fe92855
Gyasi Story <gyasi.story@cru.org>
2020-01-28 15:52:40 -0500

    Clean up viewModel calls and add missing values

--- app/src/main/java/org/cru/godtools/fragment/GlobalDashboardFragment.kt
+++ app/src/main/java/org/cru/godtools/fragment/GlobalDashboardFragment.kt
@@ -4,24 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProviders
-import me.thekey.android.TheKey
+import androidx.fragment.app.viewModels
 import org.cru.godtools.databinding.FragmentGlobalDashboardBinding
 import org.cru.godtools.viewmodel.GlobalDashboardDataModel
+import java.util.Calendar
 
 class GlobalDashboardFragment : BasePlatformFragment() {
     private var binding: FragmentGlobalDashboardBinding? = null
+    private val viewModel: GlobalDashboardDataModel by viewModels { defaultViewModelProviderFactory }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentGlobalDashboardBinding.inflate(inflater, container, false)
-        binding?.viewModel = ViewModelProviders.of(this).get(GlobalDashboardDataModel::class.java)
+        binding?.viewModel = viewModel
+        binding?.year = "${Calendar.getInstance().get(Calendar.YEAR)}"
         return binding?.root
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        val key = TheKey.getInstance(requireContext())
-        binding?.viewModel?.firstName?.value = key.cachedAttributes.firstName
-        binding?.viewModel?.lastName?.value = key.cachedAttributes.lastName
     }
 }
--- app/src/main/java/org/cru/godtools/viewmodel/GlobalDashboardDataModel.kt
+++ app/src/main/java/org/cru/godtools/viewmodel/GlobalDashboardDataModel.kt
@@ -1,15 +1,23 @@
 package org.cru.godtools.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import me.thekey.android.TheKey
 import org.ccci.gto.android.common.lifecycle.combineWith
 
-class GlobalDashboardDataModel : ViewModel() {
+class GlobalDashboardDataModel(application: Application) : AndroidViewModel(application) {
+    init {
+        TheKey.getInstance(getApplication()).let {
+            firstName.value = it.cachedAttributes.firstName
+            lastName.value = it.cachedAttributes.lastName
+        }
+    }
 
-    val firstName = MutableLiveData<String>()
-    val lastName = MutableLiveData<String>()
+    private val firstName = MutableLiveData<String>()
+    private val lastName = MutableLiveData<String>()
+
     // TODO:  Create temporary Data to be updated in later PR
-
     val uniqueUsers = MutableLiveData<Int>()
     val gospelPresentation = MutableLiveData<Int>()
     val sessions = MutableLiveData<Int>()
