0ecc14e15f5db14c05b7cf35fceb5f449a46fa3d
Gyasi Story <gyasi.story@cru.org>
2020-01-29 14:08:27 -0500

    add logic for using correct sub site sections

--- ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/analytics/model/TutorialAnalyticsScreenEvent.kt
+++ ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/analytics/model/TutorialAnalyticsScreenEvent.kt
@@ -4,7 +4,10 @@ import org.cru.godtools.analytics.model.AnalyticsScreenEvent
 import org.cru.godtools.tutorial.PageSet
 import java.util.Locale
 
-class TutorialAnalyticsScreenEvent(tutorial: PageSet, page: Int) :
+class TutorialAnalyticsScreenEvent(val tutorial: PageSet, page: Int) :
     AnalyticsScreenEvent("${tutorial.analyticsBaseScreenName}-${page + 1}", Locale.getDefault()) {
-    override val adobeSiteSection get() = ADOBE_SITE_SECTION_TUTORIAL
+    override val adobeSiteSection get() = when(tutorial.analyticsBaseScreenName) {
+        BASE_SCREEN_NAME_ON_BOARDING -> ADOBE_SITE_SECTION_ON_BOARDING
+        else -> ADOBE_SITE_SECTION_TUTORIAL
+    }
 }
