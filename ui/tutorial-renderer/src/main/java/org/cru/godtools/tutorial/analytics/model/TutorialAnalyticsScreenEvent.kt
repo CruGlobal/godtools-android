645b98b9120498bc414c0fdefe413744c86c4520
Gyasi Story <gyasi.story@cru.org>
2020-01-29 14:14:48 -0500

    lint check

--- ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/analytics/model/TutorialAnalyticsScreenEvent.kt
+++ ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/analytics/model/TutorialAnalyticsScreenEvent.kt
@@ -6,8 +6,9 @@ import java.util.Locale
 
 class TutorialAnalyticsScreenEvent(val tutorial: PageSet, page: Int) :
     AnalyticsScreenEvent("${tutorial.analyticsBaseScreenName}-${page + 1}", Locale.getDefault()) {
-    override val adobeSiteSection get() = when(tutorial.analyticsBaseScreenName) {
-        BASE_SCREEN_NAME_ON_BOARDING -> ADOBE_SITE_SECTION_ON_BOARDING
-        else -> ADOBE_SITE_SECTION_TUTORIAL
-    }
+    override val adobeSiteSection
+        get() = when (tutorial.analyticsBaseScreenName) {
+            BASE_SCREEN_NAME_ON_BOARDING -> ADOBE_SITE_SECTION_ON_BOARDING
+            else -> ADOBE_SITE_SECTION_TUTORIAL
+        }
 }
