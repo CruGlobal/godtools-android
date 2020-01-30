d9ff0e22832364c76295280afbb5cc31fb6d9a3a
Daniel Frett <daniel.frett@ccci.org>
2020-01-30 16:43:20 -0500

    convert TranslationFileMapper to Kotlin

---
+++ library/db/src/main/java/org/keynote/godtools/android/db/TranslationFileMapper.kt
@@ -0,0 +1,27 @@
+package org.keynote.godtools.android.db
+
+import android.content.ContentValues
+import android.database.Cursor
+import org.ccci.gto.android.common.db.AbstractMapper
+import org.ccci.gto.android.common.util.database.getLong
+import org.ccci.gto.android.common.util.database.getString
+import org.cru.godtools.model.LocalFile
+import org.cru.godtools.model.Translation
+import org.cru.godtools.model.TranslationFile
+import org.keynote.godtools.android.db.Contract.TranslationFileTable
+
+internal object TranslationFileMapper : AbstractMapper<TranslationFile>() {
+    override fun mapField(values: ContentValues, field: String, file: TranslationFile) {
+        when (field) {
+            TranslationFileTable.COLUMN_TRANSLATION -> values.put(field, file.translationId)
+            TranslationFileTable.COLUMN_FILE -> values.put(field, file.fileName)
+            else -> super.mapField(values, field, file)
+        }
+    }
+
+    override fun newObject(c: Cursor) = TranslationFile()
+    override fun toObject(c: Cursor) = super.toObject(c).apply {
+        setTranslationId(c.getLong(TranslationFileTable.COLUMN_TRANSLATION, Translation.INVALID_ID))
+        fileName = c.getString(TranslationFileTable.COLUMN_FILE, LocalFile.INVALID_FILE_NAME)
+    }
+}
