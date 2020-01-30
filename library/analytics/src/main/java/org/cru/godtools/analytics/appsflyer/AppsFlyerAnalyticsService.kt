bc757f1f930089b7bc7a984bb65c48d4ed8e0d13
Daniel Frett <daniel.frett@ccci.org>
2020-01-30 16:10:24 -0500

    create an AppsFlyerAnalyticsService to capture all AppsFlyer specific logic

---
+++ library/analytics/src/main/java/org/cru/godtools/analytics/appsflyer/AppsFlyerAnalyticsService.kt
@@ -0,0 +1,41 @@
+package org.cru.godtools.analytics.appsflyer
+
+import android.app.Application
+import com.appsflyer.AppsFlyerConversionListener
+import com.appsflyer.AppsFlyerLib
+import org.cru.godtools.base.util.SingletonHolder
+import timber.log.Timber
+
+private const val TAG = "AppsFlyerAnalyticsSrvc"
+
+class AppsFlyerAnalyticsService private constructor(app: Application) {
+    companion object : SingletonHolder<AppsFlyerAnalyticsService, Application>(::AppsFlyerAnalyticsService)
+
+    init {
+        AppsFlyerLib.getInstance()
+            .init("", GodToolsAppsFlyerConversionListener, app.applicationContext)
+            .startTracking(app)
+    }
+}
+
+private object GodToolsAppsFlyerConversionListener : AppsFlyerConversionListener {
+    override fun onAppOpenAttribution(data: Map<String, String>?) {
+        data?.map { (key, value) ->
+            Timber.tag(TAG).d("AppsFlyer onAppOpenAttribution Attribute: %s = %s", key, value)
+        }
+    }
+
+    override fun onAttributionFailure(error: String?) {
+        Timber.tag(TAG).e("AppsFlyer onAttributionFailure: %s", error)
+    }
+
+    override fun onConversionDataSuccess(data: Map<String, Any>?) {
+        data?.map { (key, value) ->
+            Timber.tag(TAG).d("AppsFlyer onConversionDataSuccess Attribute: %s = %s", key, value)
+        }
+    }
+
+    override fun onConversionDataFail(error: String?) {
+        Timber.tag(TAG).e("AppsFlyer onConversionDataFail: %s", error)
+    }
+}
