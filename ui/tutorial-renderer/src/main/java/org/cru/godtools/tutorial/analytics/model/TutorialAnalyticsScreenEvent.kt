0afc8cb90522c69100489692ce004e99d2af9ac1
Daniel Frett <daniel.frett@ccci.org>
2020-01-30 10:12:10 -0500

    ktlint fixup

--- ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/analytics/model/TutorialAnalyticsScreenEvent.kt
+++ ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/analytics/model/TutorialAnalyticsScreenEvent.kt
@@ -6,7 +6,7 @@ import java.util.Locale
 
 class TutorialAnalyticsScreenEvent(private val tutorial: PageSet, page: Int) :
     AnalyticsScreenEvent("${tutorial.analyticsBaseScreenName}-${page + 1}", Locale.getDefault()) {
-    override val adobeSiteSection get() = when(tutorial) {
+    override val adobeSiteSection get() = when (tutorial) {
         PageSet.ONBOARDING -> ADOBE_SITE_SECTION_ONBOARDING
         PageSet.TRAINING -> ADOBE_SITE_SECTION_TUTORIAL
     }
