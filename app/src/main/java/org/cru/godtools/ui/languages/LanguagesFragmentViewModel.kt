c60b1f73d40d90193c01ad76e975ff2efe747bf7
Daniel Frett <daniel.frett@ccci.org>
2020-01-31 13:24:46 -0500

    create a ViewModel for LanguagesFragment

---
+++ app/src/main/java/org/cru/godtools/ui/languages/LanguagesFragmentViewModel.kt
@@ -0,0 +1,42 @@
+package org.cru.godtools.ui.languages
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.distinctUntilChanged
+import androidx.lifecycle.map
+import org.ccci.gto.android.common.db.Query
+import org.ccci.gto.android.common.db.getAsLiveData
+import org.ccci.gto.android.common.lifecycle.combineWith
+import org.cru.godtools.model.Language
+import org.keynote.godtools.android.db.Contract
+import org.keynote.godtools.android.db.GodToolsDao
+
+class LanguagesFragmentViewModel(application: Application) : AndroidViewModel(application) {
+    private val dao = GodToolsDao.getInstance(application)
+
+    val query = MutableLiveData<String?>(null)
+    val showNone = MutableLiveData<Boolean>(false)
+
+    private val rawLanguages = Query.select<Language>()
+        .join(Contract.LanguageTable.SQL_JOIN_TRANSLATION)
+        .where(Contract.TranslationTable.SQL_WHERE_PUBLISHED)
+        .getAsLiveData(dao)
+    private val sortedLanguages: LiveData<Map<String, Language>> = rawLanguages
+        .map { it.associateBy { lang -> lang.getDisplayName(application) }.toSortedMap(String.CASE_INSENSITIVE_ORDER) }
+    private val filteredLanguages = query.distinctUntilChanged().combineWith(sortedLanguages) { query, languages ->
+        when {
+            query.isNullOrEmpty() -> languages?.values?.toList()
+            else -> languages?.entries?.filter { it.key.contains(query, true) }?.map { it.value }?.toList()
+        }
+    }
+
+    // List of sorted filtered languages with none (null) prepended if requested
+    val languages = showNone.distinctUntilChanged().combineWith(filteredLanguages) { showNone, languages ->
+        when (showNone) {
+            true -> listOf<Language?>(null) + languages.orEmpty()
+            else -> languages.orEmpty()
+        }
+    }
+}
