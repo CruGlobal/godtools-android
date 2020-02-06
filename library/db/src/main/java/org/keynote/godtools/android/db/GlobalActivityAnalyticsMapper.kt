6ea545d1368e9ad2fcf2598015640e2eb8e22122
Gyasi Story <gyasi.story@cru.org>
2020-02-06 13:26:44 -0500

    Fix issue with matching Data

--- library/db/src/main/java/org/keynote/godtools/android/db/GlobalActivityAnalyticsMapper.kt
+++ library/db/src/main/java/org/keynote/godtools/android/db/GlobalActivityAnalyticsMapper.kt
@@ -20,7 +20,7 @@ internal object GlobalActivityAnalyticsMapper : BaseMapper<GlobalActivityAnalyti
     override fun newObject(c: Cursor) = GlobalActivityAnalytics()
     override fun toObject(c: Cursor) = super.toObject(c).apply {
         users = c.getInt(GlobalActivityAnalyticsTable.COLUMN_USERS, 0)
-        countries = c.getInt(GlobalActivityAnalyticsTable.COLUMN_LAUNCHES, 0)
+        countries = c.getInt(GlobalActivityAnalyticsTable.COLUMN_COUNTRIES, 0)
         launches = c.getInt(GlobalActivityAnalyticsTable.COLUMN_LAUNCHES, 0)
         gospelPresentation = c.getInt(GlobalActivityAnalyticsTable.COLUMN_GOSPEL_PRESENTATIONS, 0)
     }
