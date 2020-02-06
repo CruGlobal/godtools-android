20dbdc65b3f42b0140c7268662ee0fd9c5d11980
Daniel Frett <daniel.frett@ccci.org>
2020-02-06 10:13:29 -0500

    trigger edit buttons using data binding

--- app/src/main/java/org/cru/godtools/ui/languages/LanguageSettingsFragment.kt
+++ app/src/main/java/org/cru/godtools/ui/languages/LanguageSettingsFragment.kt
@@ -1,8 +1,6 @@
 package org.cru.godtools.ui.languages
 
 import android.os.Bundle
-import butterknife.OnClick
-import butterknife.Optional
 import org.cru.godtools.R
 import org.cru.godtools.activity.startLanguageSelectionActivity
 import org.cru.godtools.databinding.LanguageSettingsFragmentBinding
@@ -13,16 +11,12 @@ class LanguageSettingsFragment :
     // region Lifecycle
     override fun onBindingCreated(binding: LanguageSettingsFragmentBinding, savedInstanceState: Bundle?) {
         super.onBindingCreated(binding, savedInstanceState)
+        binding.callbacks = this
         binding.primaryLanguage = settings.primaryLanguageLiveData
         binding.parallelLanguage = settings.parallelLanguageLiveData
     }
     // endregion Lifecycle
 
-    @Optional
-    @OnClick(R.id.primary_language_button)
-    internal fun editPrimaryLanguage() = requireActivity().startLanguageSelectionActivity(true)
-
-    @Optional
-    @OnClick(R.id.parallel_language_button)
-    internal fun editParallelLanguage() = requireActivity().startLanguageSelectionActivity(false)
+    fun editPrimaryLanguage() = requireActivity().startLanguageSelectionActivity(true)
+    fun editParallelLanguage() = requireActivity().startLanguageSelectionActivity(false)
 }
