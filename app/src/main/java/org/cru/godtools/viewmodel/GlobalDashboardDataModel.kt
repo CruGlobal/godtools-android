d21b268f056415533dcaef5c7c950d1da4a55476
Gyasi Story <gyasi.story@cru.org>
2020-02-07 10:48:12 -0500

    Simplify Live Data Creation

--- app/src/main/java/org/cru/godtools/viewmodel/GlobalDashboardDataModel.kt
+++ app/src/main/java/org/cru/godtools/viewmodel/GlobalDashboardDataModel.kt
@@ -2,31 +2,18 @@ package org.cru.godtools.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
-import org.ccci.gto.android.common.db.Query
-import org.ccci.gto.android.common.db.getAsLiveData
 import org.cru.godtools.model.GlobalActivityAnalytics
 import org.keynote.godtools.android.db.GodToolsDao
 
 class GlobalDashboardDataModel(app: Application) : AndroidViewModel(app) {
 
-    private val globalAnalyticsQuery: LiveData<List<GlobalActivityAnalytics>>? by lazy {
-        Query.select(GlobalActivityAnalytics::class.java)
-            .getAsLiveData(GodToolsDao.getInstance(getApplication()))
-    }
-
     private val globalActivityAnalytics by lazy {
-        globalAnalyticsQuery?.map {
-            if (it.count() > 0) {
-                return@map it.first()
-            }
-            return@map null
-        }
+        GodToolsDao.getInstance(getApplication()).findLiveData(GlobalActivityAnalytics::class.java, 1)
     }
 
-    val uniqueUsers = globalActivityAnalytics?.map { "${it?.users ?: 0}" }
-    val gospelPresentation = globalActivityAnalytics?.map { "${it?.gospelPresentation ?: 0}" }
-    val sessions = globalActivityAnalytics?.map { "${it?.launches ?: 0}" }
-    val countries = globalActivityAnalytics?.map { "${it?.countries ?: 0}" }
+    val uniqueUsers = globalActivityAnalytics.map { "${it?.users ?: 0}" }
+    val gospelPresentation = globalActivityAnalytics.map { "${it?.gospelPresentation ?: 0}" }
+    val sessions = globalActivityAnalytics.map { "${it?.launches ?: 0}" }
+    val countries = globalActivityAnalytics.map { "${it?.countries ?: 0}" }
 }
