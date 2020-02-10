3c4dabd38573421d1f030c453b3142163afb95ae
Gyasi Story <gyasi.story@cru.org>
2020-02-10 11:36:12 -0500

    remove unused adobeSiteSubSection

--- app/src/main/java/org/cru/godtools/analytics/model/ToolDetailsScreenEvent.kt
+++ app/src/main/java/org/cru/godtools/analytics/model/ToolDetailsScreenEvent.kt
@@ -2,13 +2,8 @@ package org.cru.godtools.analytics.model
 
 import java.util.Locale
 
-private const val SITE_SUB_SECTION_ADD_TOOLS = "add tools"
-
 class ToolDetailsScreenEvent(toolCode: String, locale: Locale? = null) :
     AnalyticsScreenEvent("$toolCode-tool-info", locale) {
     override val adobeSiteSection: String?
         get() = SITE_SECTION_TOOLS
-
-    override val adobeSiteSubSection: String?
-        get() = SITE_SUB_SECTION_ADD_TOOLS
 }
