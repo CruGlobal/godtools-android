a45a5a373ea6d7363d2744ddcb5e21541e4df443
Daniel Frett <daniel.frett@ccci.org>
2020-02-17 14:05:53 -0500

    add a test for Header parsing

---
+++ library/xml-model/src/test/java/org/cru/godtools/xml/model/HeaderTest.kt
@@ -0,0 +1,29 @@
+package org.cru.godtools.xml.model
+
+import android.graphics.Color
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.cru.godtools.xml.util.getXmlParserForResource
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+private const val TOOL_CODE = "test"
+
+@RunWith(AndroidJUnit4::class)
+class HeaderTest {
+    private lateinit var manifest: Manifest
+
+    @Before
+    fun setup() {
+        manifest = Manifest(TOOL_CODE)
+    }
+
+    @Test
+    fun testParseHeader() {
+        val header = Header.fromXml(Page(manifest, 1), getXmlParserForResource("header.xml"))
+        assertEquals("5", header.number!!.mText)
+        assertEquals("title", header.title!!.mText)
+        assertEquals(Color.RED, header.backgroundColor)
+    }
+}
