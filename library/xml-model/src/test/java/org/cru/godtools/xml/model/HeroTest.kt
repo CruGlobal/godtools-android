d005e9128db552d7f9ec2862086fa6f7911d3410
Daniel Frett <daniel.frett@ccci.org>
2020-02-17 17:46:04 -0500

    add unit tests for Hero parsing

---
+++ library/xml-model/src/test/java/org/cru/godtools/xml/model/HeroTest.kt
@@ -0,0 +1,31 @@
+package org.cru.godtools.xml.model
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.cru.godtools.xml.util.getXmlParserForResource
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.contains
+import org.hamcrest.Matchers.hasSize
+import org.hamcrest.Matchers.instanceOf
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class HeroTest {
+    @Test
+    fun testParseHero() {
+        val hero = Hero.fromXml(Manifest(), getXmlParserForResource("hero.xml"))
+        assertThat(hero.analyticsEvents, hasSize(1))
+        assertEquals("Heading", hero.heading!!.mText)
+        assertThat(
+            hero.content,
+            contains(instanceOf(Image::class.java), instanceOf(Paragraph::class.java), instanceOf(Tabs::class.java))
+        )
+    }
+
+    @Test
+    fun testParseHeroIgnoredContent() {
+        val hero = Hero.fromXml(Manifest(), getXmlParserForResource("hero_ignored_content.xml"))
+        assertThat(hero.content, contains(instanceOf(Paragraph::class.java), instanceOf(Tabs::class.java)))
+    }
+}
