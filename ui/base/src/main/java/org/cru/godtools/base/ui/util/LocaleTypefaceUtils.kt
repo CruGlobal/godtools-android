70ff26e3d796bb251018a010c85869bdcb8b5281
Daniel Frett <daniel.frett@ccci.org>
2020-02-20 14:16:31 -0500

    make LocaleTypefaceUtils extension functions

--- ui/base/src/main/java/org/cru/godtools/base/ui/util/LocaleTypefaceUtils.kt
+++ ui/base/src/main/java/org/cru/godtools/base/ui/util/LocaleTypefaceUtils.kt
@@ -1,3 +1,5 @@
+@file:JvmName("LocaleTypefaceUtils")
+
 package org.cru.godtools.base.ui.util
 
 import android.content.Context
@@ -7,36 +9,32 @@ import uk.co.chrisjenx.calligraphy.CalligraphyUtils
 import uk.co.chrisjenx.calligraphy.TypefaceUtils
 import java.util.Locale
 
-object LocaleTypefaceUtils {
-    private val typefaces = mutableMapOf<Locale, String>().apply {
-        // Sinhala, only needed for Android pre-Marshmallow
-        //
-        // https://forum.xda-developers.com/android/general/font-sinhala-font-android-5-0-lollipop-t3150536
-        // http://www.xperiablog.net/2015/12/08/sony-confirms-sinhalese-language-support-in-android-6-0-update/
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            put(Locale("si"), "fonts/NotoSansSinhala-Regular.ttf")
-        }
-
-        // Tibetan, added in Marshmallow
-        //
-        // http://digitaltibetan.org/index.php/Tibetan_support_in_Android_6_Marshmallow
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            put(Locale("bo"), "fonts/NotoSansTibetan-Regular.ttf")
-        }
+private val typefaces = mutableMapOf<Locale, String>().apply {
+    // Sinhala, only needed for Android pre-Marshmallow
+    //
+    // https://forum.xda-developers.com/android/general/font-sinhala-font-android-5-0-lollipop-t3150536
+    // http://www.xperiablog.net/2015/12/08/sony-confirms-sinhalese-language-support-in-android-6-0-update/
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+        put(Locale("si"), "fonts/NotoSansSinhala-Regular.ttf")
     }
 
-    @JvmStatic
-    fun getTypeface(context: Context?, locale: Locale?) = when {
-        context != null && typefaces.containsKey(locale) -> TypefaceUtils.load(context.assets, typefaces[locale])
-        else -> null
+    // Tibetan, added in Marshmallow
+    //
+    // http://digitaltibetan.org/index.php/Tibetan_support_in_Android_6_Marshmallow
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+        put(Locale("bo"), "fonts/NotoSansTibetan-Regular.ttf")
     }
+}
 
-    /**
-     * This method works around a crash caused by setting a null Typeface span within Calligraphy.
-     */
-    @JvmStatic
-    fun safeApplyTypefaceSpan(s: CharSequence?, typeface: Typeface?) = when {
-        typeface != null -> CalligraphyUtils.applyTypefaceSpan(s, typeface)
-        else -> null
-    }
+fun Context?.getTypeface(locale: Locale?) = when {
+    this != null && typefaces.containsKey(locale) -> TypefaceUtils.load(assets, typefaces[locale])
+    else -> null
+}
+
+// TODO: make CharSequence receiver non-null
+@JvmName("safeApplyTypefaceSpan")
+fun CharSequence?.applyTypefaceSpan(typeface: Typeface?) = when {
+    // workaround a crash caused by setting a null Typeface span within Calligraphy.
+    typeface != null -> CalligraphyUtils.applyTypefaceSpan(this, typeface)
+    else -> null
 }
--- ui/base/src/main/java/org/cru/godtools/base/ui/util/ModelUtils.kt
+++ ui/base/src/main/java/org/cru/godtools/base/ui/util/ModelUtils.kt
@@ -3,16 +3,15 @@
 package org.cru.godtools.base.ui.util
 
 import android.content.Context
-import org.cru.godtools.base.ui.util.LocaleTypefaceUtils.safeApplyTypefaceSpan
 import org.cru.godtools.model.Tool
 import org.cru.godtools.model.Translation
 
 @JvmName("getTranslationName")
 fun Translation?.getName(tool: Tool?, context: Context?) =
-    this?.let { safeApplyTypefaceSpan(name, getTypeface(context)) } ?: tool?.name ?: ""
+    this?.let { name?.applyTypefaceSpan(getTypeface(context)) } ?: tool?.name ?: ""
 
 @JvmName("getTranslationDescription")
 fun Translation?.getDescription(tool: Tool?, context: Context?) =
-    this?.let { safeApplyTypefaceSpan(description, getTypeface(context)) } ?: tool?.description ?: ""
+    this?.let { description?.applyTypefaceSpan(getTypeface(context)) } ?: tool?.description ?: ""
 
-private fun Translation.getTypeface(context: Context?) = LocaleTypefaceUtils.getTypeface(context, languageCode)
+private fun Translation.getTypeface(context: Context?) = context.getTypeface(languageCode)
