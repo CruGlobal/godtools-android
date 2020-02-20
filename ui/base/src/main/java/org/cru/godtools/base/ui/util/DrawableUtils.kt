19f488c100cad00d4747f1ba8bf4b67a799cc662
Daniel Frett <daniel.frett@ccci.org>
2020-02-20 13:18:23 -0500

    convert DrawableUtils to Kotlin

---
+++ ui/base/src/main/java/org/cru/godtools/base/ui/util/DrawableUtils.kt
@@ -0,0 +1,10 @@
+@file:JvmName("DrawableUtils")
+
+package org.cru.godtools.base.ui.util
+
+import android.graphics.drawable.Drawable
+import androidx.annotation.ColorInt
+import androidx.core.graphics.drawable.DrawableCompat
+
+fun Drawable?.tint(@ColorInt color: Int) =
+    this?.let { DrawableCompat.wrap(it).mutate() }?.apply { DrawableCompat.setTint(this, color) }
