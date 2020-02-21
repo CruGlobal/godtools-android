ad496c8c464cf70b19885d14bf157475507ef5f2
Gyasi Story <gyasi.story@cru.org>
2020-02-21 14:57:54 -0500

    use Atrribute Live Data and clean up code

--- app/src/main/java/org/cru/godtools/ui/profile/ProfileActivity.kt
+++ app/src/main/java/org/cru/godtools/ui/profile/ProfileActivity.kt
@@ -4,9 +4,12 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.observe
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import me.thekey.android.TheKey
+import me.thekey.android.livedata.getAttributesLiveData
 import org.cru.godtools.R
 import org.cru.godtools.activity.BasePlatformActivity
 import org.cru.godtools.analytics.model.AnalyticsScreenEvent
@@ -39,15 +42,15 @@ class ProfileActivity : BasePlatformActivity() {
 
     private fun setBindingData() {
         val key = TheKey.getInstance(this)
-        binding.accountName = "${key.cachedAttributes.firstName} ${key.cachedAttributes.lastName}"
-        binding.myProfileViewpager?.adapter = ProfilePageAdapter(this)
-        binding.myProfileTabLayout?.let { tabLayout ->
-            binding.myProfileViewpager?.let { viewPager ->
-                TabLayoutMediator(tabLayout, viewPager) { tab, _ ->
-                    tab.text = getString(R.string.gt_gd_activity_text)
-                }.attach()
-            }
+        key.getAttributesLiveData().observe(this) {
+            binding.accountName = "${it.firstName} ${it.lastName}"
         }
+        binding.myProfileViewpager?.adapter = ProfilePageAdapter(this)
+        val tabLayout = binding.myProfileTabLayout ?: return
+        val viewPager = binding.myProfileViewpager ?: return
+        TabLayoutMediator(tabLayout, viewPager) { tab, _ ->
+            tab.text = getString(R.string.gt_gd_activity_text)
+        }.attach()
     }
     // endregion Data Binding
 }
