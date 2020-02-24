3c52f7132c5ac527c1092c1cecdd88c46ff022c1
Gyasi Story <gyasi.story@cru.org>
2020-02-24 16:03:10 -0500

    add scrolling for shorter devices

--- app/src/main/java/org/cru/godtools/ui/profile/ProfileActivity.kt
+++ app/src/main/java/org/cru/godtools/ui/profile/ProfileActivity.kt
@@ -1,13 +1,12 @@
 package org.cru.godtools.ui.profile
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.lifecycleScope
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.lifecycle.observe
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.google.android.material.tabs.TabLayoutMediator
 import me.thekey.android.TheKey
 import me.thekey.android.livedata.getAttributesLiveData
 import org.cru.godtools.R
@@ -45,17 +44,18 @@ class ProfileActivity : BasePlatformActivity() {
         key.getAttributesLiveData().observe(this) {
             binding.accountName = "${it.firstName} ${it.lastName}"
         }
-        binding.myProfileViewpager?.adapter = ProfilePageAdapter(this)
-        val tabLayout = binding.myProfileTabLayout ?: return
-        val viewPager = binding.myProfileViewpager ?: return
-        TabLayoutMediator(tabLayout, viewPager) { tab, _ ->
-            tab.text = getString(R.string.gt_gd_activity_text)
-        }.attach()
+        binding.myProfileViewpager?.adapter = ProfilePageAdapter(supportFragmentManager, this)
+        binding.myProfileViewpager.currentItem = 1
+        binding.myProfileTabLayout.setupWithViewPager(binding.myProfileViewpager)
     }
     // endregion Data Binding
 }
 
-private class ProfilePageAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
-    override fun getItemCount() = 1
-    override fun createFragment(position: Int) = GlobalActivityFragment()
+private class ProfilePageAdapter(
+    fm: FragmentManager,
+    val context: Context
+) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getItem(position: Int) = GlobalActivityFragment()
+    override fun getCount() = 1
+    override fun getPageTitle(position: Int) = context.getString(R.string.gt_gd_activity_text)
 }
