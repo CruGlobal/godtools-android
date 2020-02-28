c6255348b5e5614c7818f562ff00d14f2445f226
Daniel Frett <daniel.frett@ccci.org>
2020-02-28 11:18:49 -0500

    rename the global activity fragment layout

--- app/src/main/java/org/cru/godtools/ui/profile/GlobalActivityFragment.kt
+++ app/src/main/java/org/cru/godtools/ui/profile/GlobalActivityFragment.kt
@@ -6,18 +6,19 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.AndroidViewModel
 import org.ccci.gto.android.common.db.findLiveData
 import org.cru.godtools.R
-import org.cru.godtools.databinding.FragmentGlobalDashboardBinding
+import org.cru.godtools.databinding.ProfilePageGlobalActivityFragmentBinding
 import org.cru.godtools.fragment.BaseBindingPlatformFragment
 import org.cru.godtools.model.GlobalActivityAnalytics
 import org.cru.godtools.sync.syncGlobalAnalytics
 import org.keynote.godtools.android.db.GodToolsDao
 import java.util.Calendar
 
-class GlobalActivityFragment :
-    BaseBindingPlatformFragment<FragmentGlobalDashboardBinding>(R.layout.fragment_global_dashboard) {
+class GlobalActivityFragment : BaseBindingPlatformFragment<ProfilePageGlobalActivityFragmentBinding>(
+    R.layout.profile_page_global_activity_fragment
+) {
     private val viewModel: GlobalActivityFragmentViewModel by viewModels()
 
-    override fun onBindingCreated(binding: FragmentGlobalDashboardBinding, savedInstanceState: Bundle?) {
+    override fun onBindingCreated(binding: ProfilePageGlobalActivityFragmentBinding, savedInstanceState: Bundle?) {
         binding.globalActivity = viewModel.globalActivity
         binding.year = "${Calendar.getInstance().get(Calendar.YEAR)}"
     }
