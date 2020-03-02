6053a749f25d62eeacd003831481f3bd92a479db
Daniel Frett <daniel.frett@ccci.org>
2020-03-02 14:55:54 -0500

    switch to ViewPager2

--- app/src/main/java/org/cru/godtools/ui/profile/ProfileActivity.kt
+++ app/src/main/java/org/cru/godtools/ui/profile/ProfileActivity.kt
@@ -1,13 +1,15 @@
 package org.cru.godtools.ui.profile
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import me.thekey.android.TheKey
 import me.thekey.android.livedata.getAttributesLiveData
+import org.ccci.gto.android.common.androidx.viewpager2.widget.setHeightWrapContent
 import org.cru.godtools.R
 import org.cru.godtools.activity.BasePlatformActivity
 import org.cru.godtools.analytics.model.AnalyticsScreenEvent
@@ -51,18 +53,21 @@ class ProfileActivity : BasePlatformActivity() {
 
     // region Pages
     private fun setupPages() {
-        binding.pages.adapter = ProfilePageAdapter(supportFragmentManager, this)
-        binding.pages.currentItem = 1
-        binding.tabs.setupWithViewPager(binding.pages)
+        binding.pages.setHeightWrapContent()
+        binding.pages.adapter = ProfilePageAdapter(this)
+        TabLayoutMediator(binding.tabs, binding.pages) { tab: TabLayout.Tab, i: Int ->
+            when (i) {
+                0 -> tab.setText(R.string.gt_gd_activity_text)
+            }
+        }.attach()
     }
     // endregion Pages
 }
 
-private class ProfilePageAdapter(
-    fm: FragmentManager,
-    val context: Context
-) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-    override fun getItem(position: Int) = GlobalActivityFragment()
-    override fun getCount() = 1
-    override fun getPageTitle(position: Int) = context.getString(R.string.gt_gd_activity_text)
+private class ProfilePageAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount() = 1
+    override fun createFragment(position: Int) = when (position) {
+        0 -> GlobalActivityFragment()
+        else -> throw IllegalArgumentException("Invalid position")
+    }
 }
