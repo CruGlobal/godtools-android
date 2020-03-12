8ecd143ef2feecef7dc991ffe67ff6014f6a665d
Daniel Frett <daniel.frett@ccci.org>
2020-03-12 09:55:18 -0400

    convert ArticleRoomDatabaseTest to Kotlin

---
+++ ui/article-aem-renderer/src/test/java/org/cru/godtools/article/aem/db/ArticleRoomDatabaseTest.kt
@@ -0,0 +1,27 @@
+package org.cru.godtools.article.aem.db
+
+import android.net.Uri
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.cru.godtools.article.aem.model.Article
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.hasSize
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ArticleRoomDatabaseTest : BaseArticleRoomDatabaseIT() {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun verifyGetAllArticles() {
+        val articleDao = mDb.articleDao()
+        val articles = articleDao.allArticles
+        articles.observeForever { }
+        for (i in 0..11) articleDao.insertOrIgnore(Article(Uri.parse("test:" + i + "aaslf" + i)))
+
+        assertThat(articles.value, hasSize(12))
+    }
+}
