474da0b96aeb8d710c1a04ede5a28964f12774ea
Daniel Frett <daniel.frett@ccci.org>
2020-03-16 15:46:57 -0400

    create a mock manifest and page util method

--- library/xml-model/src/test/java/org/cru/godtools/xml/model/CallToActionTest.kt
+++ library/xml-model/src/test/java/org/cru/godtools/xml/model/CallToActionTest.kt
@@ -7,26 +7,18 @@ import org.cru.godtools.xml.util.getXmlParserForResource
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.containsInAnyOrder
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class CallToActionTest {
-    private lateinit var manifest: Manifest
-    private lateinit var page: Page
-
-    @Before
-    fun setup() {
-        manifest = Manifest(TOOL_CODE)
-        page = Page(manifest, 0)
-    }
-
     @Test
     fun testParseCallToAction() {
         val events = Event.Id.parse(TOOL_CODE, "event1 ns:event2")
+        val page = mockPage()
         page.parsePageXml(getXmlParserForResource("call_to_action.xml"))
         val callToAction = page.callToAction
+
         assertEquals(Color.RED, callToAction.controlColor)
         assertThat(callToAction.events, containsInAnyOrder(*events.toTypedArray()))
         assertEquals("Call To Action", callToAction.label!!.mText)
