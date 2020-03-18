05c6e69e7b0927294e7aa7f1265a96b0469faa74
Daniel Frett <daniel.frett@ccci.org>
2020-03-18 09:45:52 -0400

    style cleanup for LanguageMapper

--- library/db/src/main/java/org/keynote/godtools/android/db/LanguageMapper.kt
+++ library/db/src/main/java/org/keynote/godtools/android/db/LanguageMapper.kt
@@ -2,24 +2,27 @@ package org.keynote.godtools.android.db
 
 import android.content.ContentValues
 import android.database.Cursor
+import org.ccci.gto.android.common.util.database.getLocale
 import org.ccci.gto.android.common.util.database.getString
 import org.cru.godtools.model.Language
-import org.keynote.godtools.android.db.Contract.LanguageTable
+import org.keynote.godtools.android.db.Contract.LanguageTable.COLUMN_ADDED
+import org.keynote.godtools.android.db.Contract.LanguageTable.COLUMN_CODE
+import org.keynote.godtools.android.db.Contract.LanguageTable.COLUMN_NAME
 
 internal object LanguageMapper : BaseMapper<Language>() {
-    override fun mapField(values: ContentValues, field: String, language: Language) {
+    override fun mapField(values: ContentValues, field: String, obj: Language) {
         when (field) {
-            LanguageTable.COLUMN_CODE -> values.put(field, serialize(language.code))
-            LanguageTable.COLUMN_ADDED -> values.put(field, language.isAdded)
-            LanguageTable.COLUMN_NAME -> values.put(field, language.name)
-            else -> super.mapField(values, field, language)
+            COLUMN_CODE -> values.put(field, serialize(obj.code))
+            COLUMN_NAME -> values.put(field, obj.name)
+            COLUMN_ADDED -> values.put(field, obj.isAdded)
+            else -> super.mapField(values, field, obj)
         }
     }
 
     override fun newObject(c: Cursor) = Language()
     override fun toObject(c: Cursor) = super.toObject(c).apply {
-        code = getLocale(c, LanguageTable.COLUMN_CODE) ?: Language.INVALID_CODE
-        name = c.getString(LanguageTable.COLUMN_NAME)
-        isAdded = getBool(c, LanguageTable.COLUMN_ADDED, false)
+        code = c.getLocale(COLUMN_CODE, Language.INVALID_CODE)!!
+        name = c.getString(COLUMN_NAME)
+        isAdded = getBool(c, COLUMN_ADDED, false)
     }
 }
