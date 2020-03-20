94c6c0088c447f83c50b8f338007b1cf42ab218f
Daniel Frett <daniel.frett@ccci.org>
2020-03-20 11:03:46 -0400

    make it possible to pass a layout to be inflated to BaseFragment

--- ui/base/src/main/java/org/cru/godtools/base/ui/fragment/BaseFragment.kt
+++ ui/base/src/main/java/org/cru/godtools/base/ui/fragment/BaseFragment.kt
@@ -2,23 +2,34 @@ package org.cru.godtools.base.ui.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import butterknife.ButterKnife
 import butterknife.Unbinder
 
-abstract class BaseFragment : Fragment() {
-    private var butterKnife: Unbinder? = null
-
+abstract class BaseFragment @JvmOverloads constructor(@LayoutRes layoutId: Int? = null) : Fragment(layoutId ?: 0) {
     // region Lifecycle
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        butterKnife = ButterKnife.bind(this, view)
+        bindButterKnife(view)
     }
 
     override fun onDestroyView() {
-        butterKnife?.unbind()
-        butterKnife = null
+        unbindButterKnife()
         super.onDestroyView()
     }
     // endregion Lifecycle
+
+    // region ButterKnife
+    private var butterKnife: Unbinder? = null
+
+    private fun bindButterKnife(view: View) {
+        butterKnife = ButterKnife.bind(this, view)
+    }
+
+    private fun unbindButterKnife() {
+        butterKnife?.unbind()
+        butterKnife = null
+    }
+    // endregion ButterKnife
 }
