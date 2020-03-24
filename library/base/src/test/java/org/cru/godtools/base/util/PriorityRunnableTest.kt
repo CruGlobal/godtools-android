c53e48a45c42d100d19eb8f5dedb434c6432ca96
Daniel Frett <daniel.frett@ccci.org>
2020-03-24 15:47:18 -0400

    add a PriorityRunnable unit test

---
+++ library/base/src/test/java/org/cru/godtools/base/util/PriorityRunnableTest.kt
@@ -0,0 +1,36 @@
+package org.cru.godtools.base.util
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.contains
+import org.junit.Test
+
+class PriorityRunnableTest {
+    private val negative = TestPriorityRunnable(-1)
+    private val one = TestPriorityRunnable(1)
+    private val ten = TestPriorityRunnable(10)
+
+    private val runnable = Runnable { }
+
+    @Test
+    fun verifyComparableInterface() {
+        assertThat(listOf(one, ten, negative).sorted(), contains(negative, one, ten))
+    }
+
+    @Test
+    fun verifyPriorityRunnableComparator() {
+        assertThat(listOf(one, ten, negative).sortedWith(PriorityRunnable.COMPARATOR), contains(negative, one, ten))
+    }
+
+    @Test
+    fun verifyPriorityRunnableComparatorMixed() {
+        assertThat(
+            listOf(one, runnable, ten, negative).sortedWith(PriorityRunnable.COMPARATOR),
+            contains(negative, runnable, one, ten)
+        )
+    }
+
+    class TestPriorityRunnable(private val priority: Int) : PriorityRunnable {
+        override fun run() = Unit
+        override fun getPriority() = priority
+    }
+}
