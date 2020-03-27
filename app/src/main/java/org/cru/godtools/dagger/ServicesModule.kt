7dd4cf4a73f9350a22961a615302fc457e5d583b
Daniel Frett <daniel.frett@ccci.org>
2020-03-27 15:55:11 -0400

    dynamically wire the EventBusIndexes into EventBus

--- app/src/main/java/org/cru/godtools/dagger/ServicesModule.kt
+++ app/src/main/java/org/cru/godtools/dagger/ServicesModule.kt
@@ -4,25 +4,17 @@ import android.content.Context
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
-import dagger.multibindings.ElementsIntoSet
 import dagger.multibindings.IntoSet
 import me.thekey.android.TheKey
 import me.thekey.android.core.TheKeyImpl
 import me.thekey.android.eventbus.EventBusEventsManager
 import org.ccci.gto.android.common.dagger.eager.EagerSingleton
 import org.ccci.gto.android.common.dagger.eager.EagerSingleton.ThreadMode
-import org.ccci.gto.android.common.eventbus.TimberLogger
-import org.cru.godtools.AppEventBusIndex
 import org.cru.godtools.account.BuildConfig
-import org.cru.godtools.analytics.AnalyticsEventBusIndex
-import org.cru.godtools.download.manager.DownloadManagerEventBusIndex
-import org.cru.godtools.model.event.ModelEventEventBusIndex
-import org.cru.godtools.shortcuts.ShortcutsEventBusIndex
-import org.cru.godtools.tract.TractEventBusIndex
 import org.greenrobot.eventbus.EventBus
 import javax.inject.Singleton
 
-@Module
+@Module(includes = [EventBusModule::class])
 abstract class ServicesModule {
     // TODO: TheKey doesn't need to be Eager once TheKey is only accessed via Dagger
     @Binds
@@ -31,18 +23,6 @@ abstract class ServicesModule {
     abstract fun eagerTheKey(theKey: TheKey): Any
 
     companion object {
-        @Provides
-        @Singleton
-        fun eventBus(): EventBus = EventBus.builder()
-            .logger(TimberLogger())
-            .addIndex(AnalyticsEventBusIndex())
-            .addIndex(AppEventBusIndex())
-            .addIndex(DownloadManagerEventBusIndex())
-            .addIndex(ModelEventEventBusIndex())
-            .addIndex(ShortcutsEventBusIndex())
-            .addIndex(TractEventBusIndex())
-            .installDefaultEventBus()
-
         @Provides
         @Singleton
         fun theKey(context: Context, eventBus: EventBus): TheKey {
@@ -54,11 +34,5 @@ abstract class ServicesModule {
             )
             return TheKey.getInstance(context)
         }
-
-        // TODO: EventBus doesn't need to be Eager once EventBus is only accessed via Dagger
-        @Provides
-        @ElementsIntoSet
-        @EagerSingleton(ThreadMode.MAIN)
-        internal fun mainEagerSingletons(eventBus: EventBus) = setOf(eventBus)
     }
 }
--- library/analytics/src/main/java/org/cru/godtools/analytics/dagger/AnalyticsModule.kt
+++ library/analytics/src/main/java/org/cru/godtools/analytics/dagger/AnalyticsModule.kt
@@ -2,14 +2,23 @@ package org.cru.godtools.analytics.dagger
 
 import dagger.Module
 import dagger.Provides
+import dagger.Reusable
 import dagger.multibindings.ElementsIntoSet
+import dagger.multibindings.IntoSet
 import org.ccci.gto.android.common.dagger.eager.EagerSingleton
 import org.ccci.gto.android.common.dagger.eager.EagerSingleton.ThreadMode
+import org.cru.godtools.analytics.AnalyticsEventBusIndex
 import org.cru.godtools.analytics.adobe.AdobeAnalyticsService
+import org.greenrobot.eventbus.meta.SubscriberInfoIndex
 
 @Module
 abstract class AnalyticsModule {
     companion object {
+        @IntoSet
+        @Provides
+        @Reusable
+        internal fun analyticsEventBusIndex(): SubscriberInfoIndex = AnalyticsEventBusIndex()
+
         @Provides
         @ElementsIntoSet
         @EagerSingleton(ThreadMode.MAIN)
