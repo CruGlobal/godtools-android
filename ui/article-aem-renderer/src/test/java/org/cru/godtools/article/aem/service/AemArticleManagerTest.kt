0a6e76302c109dac7d5bddff079b1754c0373640
Daniel Frett <daniel.frett@ccci.org>
2020-04-01 18:09:38 -0400

    fix the name of AemArticleManager

--- ui/article-aem-renderer/src/test/java/org/cru/godtools/article/aem/service/AemArticleManagerTest.kt
+++ ui/article-aem-renderer/src/test/java/org/cru/godtools/article/aem/service/AemArticleManagerTest.kt
@@ -3,9 +3,9 @@ package org.cru.godtools.article.aem.service
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class AEMDownloadManagerTest {
+class AemArticleManagerTest {
     @Test
     fun testRoundTimestamp() {
-        assertEquals(15000, AemArticleManger.roundTimestamp(15234, 1000))
+        assertEquals(15000, AemArticleManager.roundTimestamp(15234, 1000))
     }
 }
