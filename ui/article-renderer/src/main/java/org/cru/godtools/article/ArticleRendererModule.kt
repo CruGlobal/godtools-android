bbcc72fc131f280ed6031f62de984ade6155521b
Daniel Frett <daniel.frett@ccci.org>
2020-04-03 11:36:32 -0400

    use Dagger to create ArticlesFragmentDataModel

--- ui/article-renderer/src/main/java/org/cru/godtools/article/ArticleRendererModule.kt
+++ ui/article-renderer/src/main/java/org/cru/godtools/article/ArticleRendererModule.kt
@@ -1,11 +1,16 @@
 package org.cru.godtools.article
 
+import androidx.lifecycle.ViewModel
+import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoMap
+import org.ccci.gto.android.common.dagger.viewmodel.ViewModelKey
 import org.cru.godtools.article.activity.ArticlesActivity
 import org.cru.godtools.article.activity.CategoriesActivity
 import org.cru.godtools.article.aem.AemArticleRendererModule
 import org.cru.godtools.article.fragment.ArticlesFragment
+import org.cru.godtools.article.fragment.ArticlesFragmentDataModel
 import org.cru.godtools.article.fragment.CategoriesFragment
 
 @Module(includes = [AemArticleRendererModule::class])
@@ -15,6 +20,11 @@ abstract class ArticleRendererModule {
 
     @ContributesAndroidInjector
     internal abstract fun articlesFragmentInjector(): ArticlesFragment
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ArticlesFragmentDataModel::class)
+    internal abstract fun articlesFragmentDataModel(dataModel: ArticlesFragmentDataModel): ViewModel
 
     @ContributesAndroidInjector
     internal abstract fun categoriesActivityInjector(): CategoriesActivity
