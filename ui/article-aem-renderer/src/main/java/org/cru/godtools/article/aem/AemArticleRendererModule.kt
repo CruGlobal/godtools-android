3ca1cb22a06c280ee732c24a194ae1da4b05a8cd
Daniel Frett <daniel.frett@ccci.org>
2020-04-07 15:24:02 -0400

    expose the ArticleRoomDatabase in the Dagger object graph

--- ui/article-aem-renderer/src/main/java/org/cru/godtools/article/aem/AemArticleRendererModule.kt
+++ ui/article-aem-renderer/src/main/java/org/cru/godtools/article/aem/AemArticleRendererModule.kt
@@ -1,8 +1,10 @@
 package org.cru.godtools.article.aem
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.android.ContributesAndroidInjector
 import dagger.multibindings.IntoMap
 import dagger.multibindings.IntoSet
@@ -10,9 +12,11 @@ import org.ccci.gto.android.common.dagger.eager.EagerSingleton
 import org.ccci.gto.android.common.dagger.viewmodel.ViewModelKey
 import org.cru.godtools.article.aem.activity.AemArticleActivity
 import org.cru.godtools.article.aem.activity.AemArticleActivityDataModel
+import org.cru.godtools.article.aem.db.ArticleRoomDatabase
 import org.cru.godtools.article.aem.fragment.AemArticleFragment
 import org.cru.godtools.article.aem.fragment.AemArticleViewModel
 import org.cru.godtools.article.aem.service.AemArticleManager
+import javax.inject.Singleton
 
 @Module
 abstract class AemArticleRendererModule {
@@ -36,4 +40,10 @@ abstract class AemArticleRendererModule {
     @IntoSet
     @EagerSingleton(threadMode = EagerSingleton.ThreadMode.BACKGROUND)
     internal abstract fun aemArticleMangerEagerSingleton(aemArticleManger: AemArticleManager): Any
+
+    companion object {
+        @Provides
+        @Singleton
+        fun articleRoomDatabase(context: Context) = ArticleRoomDatabase.getInstance(context)
+    }
 }
