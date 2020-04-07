5bca60f249a08629bb38590c00dbb8212547e61a
Daniel Frett <daniel.frett@ccci.org>
2020-04-07 13:24:53 -0400

    utilize androidx for loading custom fonts

--- ui/base/src/main/java/org/cru/godtools/base/ui/util/LocaleTypefaceUtils.kt
+++ ui/base/src/main/java/org/cru/godtools/base/ui/util/LocaleTypefaceUtils.kt
@@ -5,31 +5,30 @@ package org.cru.godtools.base.ui.util
 import android.content.Context
 import android.graphics.Typeface
 import android.os.Build
+import androidx.core.content.res.ResourcesCompat
+import org.cru.godtools.base.ui.R
 import uk.co.chrisjenx.calligraphy.CalligraphyUtils
-import uk.co.chrisjenx.calligraphy.TypefaceUtils
 import java.util.Locale
 
-private val typefaces = mutableMapOf<Locale, String>().apply {
+@OptIn(ExperimentalStdlibApi::class)
+private val typefaces = buildMap<Locale, Int> {
     // Sinhala, only needed for Android pre-Marshmallow
     //
     // https://forum.xda-developers.com/android/general/font-sinhala-font-android-5-0-lollipop-t3150536
     // http://www.xperiablog.net/2015/12/08/sony-confirms-sinhalese-language-support-in-android-6-0-update/
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-        put(Locale("si"), "fonts/NotoSansSinhala-Regular.ttf")
+        put(Locale("si"), R.font.noto_sans_sinhala_regular)
     }
 
     // Tibetan, added in Marshmallow
     //
     // http://digitaltibetan.org/index.php/Tibetan_support_in_Android_6_Marshmallow
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-        put(Locale("bo"), "fonts/NotoSansTibetan-Regular.ttf")
+        put(Locale("bo"), R.font.noto_sans_tibetan_regular)
     }
 }
 
-fun Context?.getTypeface(locale: Locale?) = when {
-    this != null && typefaces.containsKey(locale) -> TypefaceUtils.load(assets, typefaces[locale])
-    else -> null
-}
+fun Context.getTypeface(locale: Locale?) = typefaces[locale]?.let { ResourcesCompat.getFont(this, it) }
 
 // TODO: make CharSequence receiver non-null
 @JvmName("safeApplyTypefaceSpan")
