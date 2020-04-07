6187093b6dcf8346ea7649bcdb572902e02e14c0
Daniel Frett <daniel.frett@ccci.org>
2020-04-07 11:23:15 -0400

    tweak constant imports

--- library/db/src/main/java/org/keynote/godtools/android/db/BaseMapper.kt
+++ library/db/src/main/java/org/keynote/godtools/android/db/BaseMapper.kt
@@ -5,17 +5,17 @@ import android.database.Cursor
 import org.ccci.gto.android.common.db.AbstractMapper
 import org.ccci.gto.android.common.util.database.getLong
 import org.cru.godtools.model.Base
-import org.keynote.godtools.android.db.Contract.BaseTable
+import org.keynote.godtools.android.db.Contract.BaseTable.COLUMN_ID
 
 internal abstract class BaseMapper<T : Base> : AbstractMapper<T>() {
     override fun mapField(values: ContentValues, field: String, obj: T) {
         when (field) {
-            BaseTable.COLUMN_ID -> values.put(field, obj.id)
+            COLUMN_ID -> values.put(field, obj.id)
             else -> super.mapField(values, field, obj)
         }
     }
 
     override fun toObject(c: Cursor): T = super.toObject(c).apply {
-        setId(c.getLong(BaseTable.COLUMN_ID, Base.INVALID_ID))
+        setId(c.getLong(COLUMN_ID, Base.INVALID_ID))
     }
 }
