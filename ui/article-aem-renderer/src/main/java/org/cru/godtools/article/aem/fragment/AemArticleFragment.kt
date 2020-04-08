27065e6cdb15818b5c763320f6a557d72d467560
Daniel Frett <daniel.frett@ccci.org>
2020-04-08 15:09:57 -0400

    update AemArticleFragment to leverage ViewBinding

--- ui/article-aem-renderer/src/main/java/org/cru/godtools/article/aem/fragment/AemArticleFragment.kt
+++ ui/article-aem-renderer/src/main/java/org/cru/godtools/article/aem/fragment/AemArticleFragment.kt
@@ -1,45 +1,32 @@
 package org.cru.godtools.article.aem.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.activityViewModels
-import butterknife.BindView
 import org.cru.godtools.article.aem.R
-import org.cru.godtools.article.aem.R2
+import org.cru.godtools.article.aem.databinding.AemArticleFragmentBinding
 import org.cru.godtools.article.aem.ui.AemArticleViewModel
 import org.cru.godtools.base.ui.fragment.BaseFragment
 
-class AemArticleFragment : BaseFragment<ViewDataBinding>() {
+class AemArticleFragment : BaseFragment<AemArticleFragmentBinding>(R.layout.aem_article_fragment) {
     private val viewModel: AemArticleViewModel by activityViewModels()
 
-    override val hasDataBinding get() = false
+    override val View.viewBinding: AemArticleFragmentBinding get() = AemArticleFragmentBinding.bind(this)
 
     // region Lifecycle
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-        inflater.inflate(R.layout.fragment_aem_article, container, false)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setupWebView()
+    override fun onBindingCreated(binding: AemArticleFragmentBinding, savedInstanceState: Bundle?) {
+        super.onBindingCreated(binding, savedInstanceState)
+        binding.setupWebView()
     }
 
-    override fun onDestroyView() {
-        cleanupWebView()
-        super.onDestroyView()
+    override fun onDestroyBinding(binding: AemArticleFragmentBinding) {
+        binding.cleanupWebView()
+        super.onDestroyBinding(binding)
     }
     // endregion Lifecycle
 
     // region WebView content
-    @JvmField
-    @BindView(R2.id.frame)
-    internal var webViewContainer: FrameLayout? = null
-
-    private fun setupWebView() = webViewContainer?.addView(viewModel.getWebView(requireActivity()))
-
-    private fun cleanupWebView() = webViewContainer?.removeView(viewModel.getWebView(requireActivity()))
+    private fun AemArticleFragmentBinding.setupWebView() = frame.addView(viewModel.getWebView(requireActivity()))
+    private fun AemArticleFragmentBinding.cleanupWebView() = frame.removeView(viewModel.getWebView(requireActivity()))
     // endregion WebView content
 }
