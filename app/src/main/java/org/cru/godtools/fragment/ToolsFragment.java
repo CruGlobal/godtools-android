dbf2b2266fb8c17eef1037cbd53399cb13d91a44
Daniel Frett <daniel.frett@ccci.org>
2020-04-16 10:28:14 -0400

    switch ToolsFragment to use new ToolsAdapter

--- app/src/main/java/org/cru/godtools/fragment/ToolsFragment.java
+++ app/src/main/java/org/cru/godtools/fragment/ToolsFragment.java
@@ -1,8 +1,6 @@
 package org.cru.godtools.fragment;
 
 import android.app.Activity;
-import android.content.Context;
-import android.database.Cursor;
 import android.graphics.drawable.NinePatchDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -14,34 +12,23 @@ import com.h6ah4i.android.widget.advrecyclerview.animator.DraggableItemAnimator;
 import com.h6ah4i.android.widget.advrecyclerview.draggable.RecyclerViewDragDropManager;
 import com.h6ah4i.android.widget.advrecyclerview.utils.WrapperAdapterUtils;
 
-import org.ccci.gto.android.common.db.Expression;
-import org.ccci.gto.android.common.db.Expression.Field;
-import org.ccci.gto.android.common.db.Join;
-import org.ccci.gto.android.common.db.Table;
-import org.ccci.gto.android.common.support.v4.app.SimpleLoaderCallbacks;
 import org.ccci.gto.android.common.support.v4.util.FragmentUtils;
 import org.cru.godtools.R;
 import org.cru.godtools.adapter.BannerHeaderAdapter;
-import org.cru.godtools.adapter.ToolsAdapter;
-import org.cru.godtools.base.Settings;
 import org.cru.godtools.base.util.LocaleUtils;
-import org.cru.godtools.content.ToolsCursorLoader;
 import org.cru.godtools.databinding.ToolsFragmentBinding;
 import org.cru.godtools.download.manager.GodToolsDownloadManager;
-import org.cru.godtools.model.Language;
 import org.cru.godtools.model.Tool;
 import org.cru.godtools.model.event.ToolUpdateEvent;
-import org.cru.godtools.model.event.content.AttachmentEventBusSubscriber;
 import org.cru.godtools.sync.GodToolsSyncServiceKt;
 import org.cru.godtools.tutorial.PageSet;
 import org.cru.godtools.tutorial.activity.TutorialActivityKt;
 import org.cru.godtools.tutorial.analytics.model.TutorialAnalyticsActionEvent;
 import org.cru.godtools.tutorial.analytics.model.TutorialAnalyticsActionEventKt;
+import org.cru.godtools.ui.tools.ToolsAdapter;
 import org.cru.godtools.ui.tools.ToolsAdapterCallbacks;
 import org.cru.godtools.ui.tools.ToolsFragmentDataModel;
 import org.cru.godtools.widget.BannerType;
-import org.keynote.godtools.android.db.Contract.ToolTable;
-import org.keynote.godtools.android.db.Contract.TranslationTable;
 import org.keynote.godtools.android.db.GodToolsDao;
 
 import java.util.Locale;
@@ -54,7 +41,6 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 
@@ -74,14 +60,10 @@ public class ToolsFragment extends BasePlatformFragment implements ToolsAdapterC
     public static final int MODE_ADDED = 1;
     public static final int MODE_AVAILABLE = 2;
 
-    private static final int LOADER_TOOLS = 101;
-
     @Inject
     GodToolsDao mDao;
     @Inject
     GodToolsDownloadManager mDownloadManager;
-
-    private final CursorLoaderCallbacks mCursorLoaderCallbacks = new CursorLoaderCallbacks();
 
     // these properties should be treated as final and only set/modified in onCreate()
     /*final*/ int mMode = MODE_ADDED;
@@ -97,9 +79,6 @@ public class ToolsFragment extends BasePlatformFragment implements ToolsAdapterC
     private BannerHeaderAdapter mToolsHeaderAdapter;
     @Nullable
     private ToolsAdapter mToolsAdapter;
-
-    @Nullable
-    private Cursor mResources;
 
     public static Fragment newInstance(final int mode) {
         final Fragment fragment = new ToolsFragment();
@@ -142,23 +121,6 @@ public class ToolsFragment extends BasePlatformFragment implements ToolsAdapterC
     public void onStart() {
         super.onStart();
         updateVisibleBanner();
-    }
-
-    void onLoadResources(@Nullable final Cursor cursor) {
-        mResources = cursor;
-        updateToolsList();
-    }
-
-    @Override
-    protected void onUpdatePrimaryLanguage() {
-        super.onUpdatePrimaryLanguage();
-        restartToolsLoader();
-    }
-
-    @Override
-    protected void onUpdateParallelLanguage() {
-        super.onUpdateParallelLanguage();
-        restartToolsLoader();
     }
 
     @Override
@@ -225,10 +187,6 @@ public class ToolsFragment extends BasePlatformFragment implements ToolsAdapterC
     }
     // endregion Lifecycle
 
-    private boolean showDownloading() {
-        return mMode == MODE_AVAILABLE;
-    }
-
     // region Banners
     private void updateVisibleBanner() {
         if (mToolsHeaderAdapter != null) {
@@ -271,14 +229,6 @@ public class ToolsFragment extends BasePlatformFragment implements ToolsAdapterC
         getSyncHelper().sync(GodToolsSyncServiceKt.syncTools(requireContext(), force));
     }
 
-    private void startLoaders() {
-        restartToolsLoader();
-    }
-
-    private void restartToolsLoader() {
-        getLoaderManager().restartLoader(LOADER_TOOLS, null, mCursorLoaderCallbacks);
-    }
-
     private void setupDataBinding() {
         if (mBinding != null) {
             mBinding.setTools(mDataModel.getTools());
@@ -297,7 +247,8 @@ public class ToolsFragment extends BasePlatformFragment implements ToolsAdapterC
 
             // create base tools adapter
             mToolsAdapter = new ToolsAdapter(this, new ViewModelProvider(this));
-            mToolsAdapter.setCallbacks(this);
+            mToolsAdapter.getCallbacks().set(this);
+            mDataModel.getTools().observe(getViewLifecycleOwner(), mToolsAdapter);
             RecyclerView.Adapter adapter = mToolsAdapter;
 
             // configure the DragDrop RecyclerView components (Only for Added tools)
@@ -328,20 +279,10 @@ public class ToolsFragment extends BasePlatformFragment implements ToolsAdapterC
             }
 
             updateVisibleBanner();
-            updateToolsList();
-        }
-    }
-
-    private void updateToolsList() {
-        if (mToolsAdapter != null) {
-            mToolsAdapter.swapCursor(mResources);
         }
     }
 
     private void cleanupToolsList() {
-        if (mToolsAdapter != null) {
-            mToolsAdapter.setCallbacks(null);
-        }
         if (mToolsView != null) {
             mToolsView.setItemAnimator(null);
             mToolsView.setAdapter(null);
@@ -372,143 +313,4 @@ public class ToolsFragment extends BasePlatformFragment implements ToolsAdapterC
         }
     }
     // endregion Empty List UI
-
-    class CursorLoaderCallbacks extends SimpleLoaderCallbacks<Cursor> {
-        @Nullable
-        @Override
-        public Loader<Cursor> onCreateLoader(final int id, @Nullable final Bundle args) {
-            switch (id) {
-                case LOADER_TOOLS:
-                    return new LocalToolsCursorLoader(requireContext(), args, mMode, getPrimaryLanguage(),
-                                                      getParallelLanguage());
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public void onLoadFinished(@NonNull final Loader<Cursor> loader, @Nullable final Cursor cursor) {
-            switch (loader.getId()) {
-                case LOADER_TOOLS:
-                    onLoadResources(cursor);
-                    break;
-            }
-        }
-    }
-
-    static class LocalToolsCursorLoader extends ToolsCursorLoader {
-        // set repetitive TranslationTable objects for primary, parallel, and default
-        private static final int TRANS_COUNT = 3;
-        private static final int TRANS_PRIM = 0;
-        private static final int TRANS_PARA = 1;
-        private static final int TRANS_DEF = 2;
-        private static final String[] TRANS_ALIAS = new String[TRANS_COUNT];
-        private static final Table[] TRANS_TABLE = new Table[TRANS_COUNT];
-        private static final Join[] TRANS_JOIN = new Join[TRANS_COUNT];
-        private static final Field[] TRANS_FIELD_LANG = new Field[TRANS_COUNT];
-
-        static {
-            for (int i = 0; i < TRANS_COUNT; i++) {
-                TRANS_ALIAS[i] = "trans" + i;
-                TRANS_TABLE[i] = TranslationTable.TABLE.as(TRANS_ALIAS[i]);
-                TRANS_FIELD_LANG[i] = TRANS_TABLE[i].field(TranslationTable.COLUMN_LANGUAGE);
-                //noinspection unchecked
-                TRANS_JOIN[i] = Join.create(TRANS_TABLE[i]).type("LEFT")
-                        .on(TRANS_TABLE[i].field(TranslationTable.COLUMN_TOOL).eq(ToolTable.FIELD_CODE))
-                        .andOn(TRANS_TABLE[i].field(TranslationTable.COLUMN_PUBLISHED).eq(true));
-            }
-        }
-
-        private static final String[] TOOLS_PROJECTION = {
-                ToolTable.COLUMN_ID,
-                ToolTable.COLUMN_CODE,
-                ToolTable.COLUMN_TYPE,
-                // title
-                "coalesce(" +
-                        TRANS_ALIAS[TRANS_PRIM] + "." + TranslationTable.COLUMN_NAME + "," +
-                        TRANS_ALIAS[TRANS_DEF] + "." + TranslationTable.COLUMN_NAME + "," +
-                        ToolTable.TABLE_NAME + "." + ToolTable.COLUMN_NAME + ") " +
-                        "AS " + ToolsAdapter.COL_TITLE,
-                // title_lang
-                "CASE " +
-                        "WHEN " + TRANS_ALIAS[TRANS_PRIM] + "." + TranslationTable.COLUMN_NAME + " IS NOT NULL THEN " +
-                        TRANS_ALIAS[TRANS_PRIM] + "." + TranslationTable.COLUMN_LANGUAGE + " " +
-                        "WHEN " + TRANS_ALIAS[TRANS_DEF] + "." + TranslationTable.COLUMN_NAME + " IS NOT NULL THEN " +
-                        TRANS_ALIAS[TRANS_DEF] + "." + TranslationTable.COLUMN_LANGUAGE + " " +
-                        "ELSE NULL END AS " + ToolsAdapter.COL_TITLE_LANGUAGE,
-                // tagline
-                "coalesce(" +
-                        TRANS_ALIAS[TRANS_PRIM] + "." + TranslationTable.COLUMN_TAGLINE + "," +
-                        TRANS_ALIAS[TRANS_PRIM] + "." + TranslationTable.COLUMN_DESCRIPTION + "," +
-                        TRANS_ALIAS[TRANS_DEF] + "." + TranslationTable.COLUMN_TAGLINE + "," +
-                        TRANS_ALIAS[TRANS_DEF] + "." + TranslationTable.COLUMN_DESCRIPTION + "," +
-                        ToolTable.TABLE_NAME + "." + ToolTable.COLUMN_DESCRIPTION + ") " +
-                        "AS " + ToolsAdapter.COL_TAGLINE,
-                // tagline_lang
-                "CASE " +
-                        "WHEN " + TRANS_ALIAS[TRANS_PRIM] + "." + TranslationTable.COLUMN_TAGLINE +
-                        " IS NOT NULL THEN " + TRANS_ALIAS[TRANS_PRIM] + "." + TranslationTable.COLUMN_LANGUAGE + " " +
-                        "WHEN " + TRANS_ALIAS[TRANS_PRIM] + "." + TranslationTable.COLUMN_DESCRIPTION +
-                        " IS NOT NULL THEN " + TRANS_ALIAS[TRANS_PRIM] + "." + TranslationTable.COLUMN_LANGUAGE + " " +
-                        "WHEN " + TRANS_ALIAS[TRANS_DEF] + "." + TranslationTable.COLUMN_TAGLINE +
-                        " IS NOT NULL THEN " + TRANS_ALIAS[TRANS_DEF] + "." + TranslationTable.COLUMN_LANGUAGE + " " +
-                        "WHEN " + TRANS_ALIAS[TRANS_DEF] + "." + TranslationTable.COLUMN_DESCRIPTION +
-                        " IS NOT NULL THEN " + TRANS_ALIAS[TRANS_DEF] + "." + TranslationTable.COLUMN_LANGUAGE + " " +
-                        "ELSE NULL END AS " + ToolsAdapter.COL_TAGLINE_LANGUAGE,
-                ToolTable.COLUMN_SHARES,
-                ToolTable.COLUMN_PENDING_SHARES,
-                ToolTable.COLUMN_ADDED,
-                TRANS_ALIAS[TRANS_PRIM] + "." + TranslationTable.COLUMN_LANGUAGE + " AS " +
-                        ToolsAdapter.COL_PRIMARY_LANGUAGE,
-                TRANS_ALIAS[TRANS_PARA] + "." + TranslationTable.COLUMN_LANGUAGE + " AS " +
-                        ToolsAdapter.COL_PARALLEL_LANGUAGE,
-                TRANS_ALIAS[TRANS_DEF] + "." + TranslationTable.COLUMN_LANGUAGE + " AS " +
-                        ToolsAdapter.COL_DEFAULT_LANGUAGE
-        };
-
-        private final int mMode;
-
-        LocalToolsCursorLoader(@NonNull final Context context, @Nullable final Bundle args, final int mode,
-                               @NonNull final Locale primaryLanguage, @Nullable final Locale parallelLanguage) {
-            super(context, args);
-            addEventBusSubscriber(new AttachmentEventBusSubscriber(this));
-            setProjection(TOOLS_PROJECTION);
-            //noinspection unchecked
-            setJoins(
-                    TRANS_JOIN[TRANS_PRIM].andOn(TRANS_FIELD_LANG[TRANS_PRIM].eq(primaryLanguage)),
-                    TRANS_JOIN[TRANS_PARA].andOn(TRANS_FIELD_LANG[TRANS_PARA]
-                                                         .eq(parallelLanguage != null ? parallelLanguage :
-                                                                     Language.INVALID_CODE)),
-                    TRANS_JOIN[TRANS_DEF].andOn(TRANS_FIELD_LANG[TRANS_DEF].eq(Settings.getDefaultLanguage()))
-            );
-            setGroupBy(ToolTable.FIELD_CODE);
-
-            mMode = mode;
-        }
-
-        @Nullable
-        @Override
-        public Expression getWhere() {
-            final Expression where = ToolTable.FIELD_TYPE.ne(Tool.Type.UNKNOWN);
-            switch (mMode) {
-                case MODE_ADDED:
-                case MODE_AVAILABLE:
-                    return where.and(ToolTable.FIELD_ADDED.eq(mMode == MODE_ADDED));
-                default:
-                    return where;
-            }
-        }
-
-        @Nullable
-        @Override
-        public String getSortOrder() {
-            switch (mMode) {
-                case MODE_ADDED:
-                    return ToolTable.COLUMN_ORDER;
-                case MODE_AVAILABLE:
-                default:
-                    return null;
-            }
-        }
-    }
 }
