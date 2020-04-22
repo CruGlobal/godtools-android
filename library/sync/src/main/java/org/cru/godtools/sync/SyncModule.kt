6968162800fcfeb2fccc75139bf87ed2c278bca2
Daniel Frett <daniel.frett@ccci.org>
2020-04-22 08:44:15 -0400

    use dagger to inject SyncFollowupWorker

--- library/sync/src/main/java/org/cru/godtools/sync/SyncModule.kt
+++ library/sync/src/main/java/org/cru/godtools/sync/SyncModule.kt
@@ -1,12 +1,24 @@
 package org.cru.godtools.sync
 
+import com.squareup.inject.assisted.dagger2.AssistedModule
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 import dagger.multibindings.Multibinds
+import org.ccci.gto.android.common.dagger.workmanager.AssistedWorkerFactory
+import org.ccci.gto.android.common.dagger.workmanager.WorkerKey
 import org.cru.godtools.sync.task.BaseSyncTasks
 import org.cru.godtools.sync.task.SyncTaskModule
+import org.cru.godtools.sync.work.SyncFollowupWorker
 
-@Module(includes = [SyncTaskModule::class])
+@AssistedModule
+@Module(includes = [AssistedInject_SyncModule::class, SyncTaskModule::class])
 abstract class SyncModule {
     @Multibinds
     abstract fun syncTasks(): Map<Class<out BaseSyncTasks>, BaseSyncTasks>
+
+    @Binds
+    @IntoMap
+    @WorkerKey(SyncFollowupWorker::class)
+    abstract fun syncFollowupWorker(syncFollowupWorker: SyncFollowupWorker.Factory): AssistedWorkerFactory
 }
--- library/sync/src/main/java/org/cru/godtools/sync/work/SyncFollowupWorker.kt
+++ library/sync/src/main/java/org/cru/godtools/sync/work/SyncFollowupWorker.kt
@@ -5,6 +5,9 @@ import androidx.work.CoroutineWorker
 import androidx.work.ExistingWorkPolicy
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
+import org.ccci.gto.android.common.dagger.workmanager.AssistedWorkerFactory
 import org.cru.godtools.sync.task.FollowupSyncTasks
 
 private const val WORK_NAME = "SyncFollowup"
@@ -12,7 +15,13 @@ private const val WORK_NAME = "SyncFollowup"
 internal fun Context.scheduleSyncFollowupWork() = WorkManager.getInstance(this)
     .enqueueUniqueWork(WORK_NAME, ExistingWorkPolicy.REPLACE, SyncWorkRequestBuilder<SyncFollowupWorker>().build())
 
-class SyncFollowupWorker(context: Context, workerParams: WorkerParameters) : CoroutineWorker(context, workerParams) {
+class SyncFollowupWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParams: WorkerParameters
+) : CoroutineWorker(context, workerParams) {
+    @AssistedInject.Factory
+    interface Factory : AssistedWorkerFactory
+
     override suspend fun doWork() =
         if (FollowupSyncTasks.getInstance(applicationContext).syncFollowups()) Result.success() else Result.retry()
 }
