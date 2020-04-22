3879b5b353555dbf2b1ea2b0c6d1c9f00b0f5a0d
Daniel Frett <daniel.frett@ccci.org>
2020-04-22 08:22:53 -0400

    enable Dagger wiring for WorkManager

--- app/src/main/java/org/cru/godtools/dagger/ServicesModule.kt
+++ app/src/main/java/org/cru/godtools/dagger/ServicesModule.kt
@@ -1,6 +1,8 @@
 package org.cru.godtools.dagger
 
 import android.content.Context
+import androidx.work.Configuration
+import androidx.work.WorkManager
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -11,6 +13,8 @@ import me.thekey.android.eventbus.EventBusEventsManager
 import org.ccci.gto.android.common.dagger.eager.EagerModule
 import org.ccci.gto.android.common.dagger.eager.EagerSingleton
 import org.ccci.gto.android.common.dagger.viewmodel.ViewModelModule
+import org.ccci.gto.android.common.dagger.workmanager.DaggerWorkerFactory
+import org.ccci.gto.android.common.dagger.workmanager.WorkManagerModule
 import org.cru.godtools.account.BuildConfig
 import org.cru.godtools.analytics.AnalyticsModule
 import org.cru.godtools.download.manager.DownloadManagerModule
@@ -30,7 +34,8 @@ import javax.inject.Singleton
         EventBusModule::class,
         ShortcutModule::class,
         SyncModule::class,
-        ViewModelModule::class
+        ViewModelModule::class,
+        WorkManagerModule::class
     ]
 )
 abstract class ServicesModule {
@@ -45,6 +50,11 @@ abstract class ServicesModule {
     @EagerSingleton(threadMode = EagerSingleton.ThreadMode.ASYNC)
     abstract fun eagerAccountListRegistrationService(service: AccountListRegistrationService): Any
 
+    @Binds
+    @IntoSet
+    @EagerSingleton(threadMode = EagerSingleton.ThreadMode.MAIN)
+    abstract fun eagerWorkManager(workManager: WorkManager): Any
+
     companion object {
         @Provides
         @Singleton
@@ -56,6 +66,13 @@ abstract class ServicesModule {
                     .service(EventBusEventsManager(eventBus))
             )
             return TheKey.getInstance(context)
+        }
+
+        @Provides
+        @Singleton
+        fun workManager(context: Context, workerFactory: DaggerWorkerFactory): WorkManager {
+            WorkManager.initialize(context, Configuration.Builder().setWorkerFactory(workerFactory).build())
+            return WorkManager.getInstance(context)
         }
     }
 }
