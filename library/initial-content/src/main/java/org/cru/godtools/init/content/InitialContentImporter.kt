5099f4110194710d6bef87a8d0a7afcff8df5832
Daniel Frett <daniel.frett@ccci.org>
2020-04-23 13:06:38 -0400

    use coroutines to launch the initial content importer

--- library/initial-content/src/main/java/org/cru/godtools/init/content/InitialContentImporter.kt
+++ library/initial-content/src/main/java/org/cru/godtools/init/content/InitialContentImporter.kt
@@ -1,7 +1,9 @@
 package org.cru.godtools.init.content
 
 import android.content.Context
-import android.os.AsyncTask
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.cru.godtools.init.content.task.InitialContentTasks
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -9,6 +11,8 @@ import javax.inject.Singleton
 @Singleton
 class InitialContentImporter @Inject internal constructor(context: Context) {
     init {
-        AsyncTask.THREAD_POOL_EXECUTOR.execute(InitialContentTasks(context))
+        GlobalScope.launch(Dispatchers.IO) {
+            InitialContentTasks(context).run()
+        }
     }
 }
