7b18cad4930c99ed7316429bffa80da33b2895c0
Daniel Frett <daniel.frett@ccci.org>
2020-05-04 13:44:40 -0400

    test default order parsing

--- library/model/src/test/java/org/cru/godtools/model/ToolTest.kt
+++ library/model/src/test/java/org/cru/godtools/model/ToolTest.kt
@@ -25,6 +25,7 @@ class ToolTest {
         assertEquals(Tool.Type.TRACT, tool.type)
         assertEquals(1L, tool.bannerId)
         assertEquals(2, tool.detailsBannerId)
+        assertEquals(10, tool.defaultOrder)
         assertThat(tool.attachments, hasSize(3))
         assertThat(tool.latestTranslations, hasSize(2))
     }
