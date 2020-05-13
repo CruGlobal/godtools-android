23191f08ead0004f452c35104403ed7508f7812b
Daniel Frett <daniel.frett@ccci.org>
2020-05-13 14:25:16 -0400

    add a list of translations to the TractActivityDataModel

--- ui/tract-renderer/src/main/java/org/cru/godtools/tract/activity/TractActivityDataModel.kt
+++ ui/tract-renderer/src/main/java/org/cru/godtools/tract/activity/TractActivityDataModel.kt
@@ -1,5 +1,7 @@
 package org.cru.godtools.tract.activity
 
+import androidx.collection.LruCache
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
@@ -7,16 +9,21 @@ import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.switchMap
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
+import org.ccci.gto.android.common.androidx.lifecycle.ImmutableLiveData
 import org.ccci.gto.android.common.androidx.lifecycle.combineWith
 import org.ccci.gto.android.common.androidx.lifecycle.emptyLiveData
 import org.ccci.gto.android.common.androidx.lifecycle.switchFold
 import org.ccci.gto.android.common.androidx.lifecycle.withInitialValue
 import org.ccci.gto.android.common.dagger.viewmodel.AssistedSavedStateViewModelFactory
 import org.cru.godtools.base.tool.service.ManifestManager
+import org.cru.godtools.model.Translation
+import org.cru.godtools.model.TranslationKey
 import org.cru.godtools.xml.model.Manifest
+import org.keynote.godtools.android.db.GodToolsDao
 import java.util.Locale
 
 class TractActivityDataModel @AssistedInject constructor(
+    private val dao: GodToolsDao,
     private val manifestManager: ManifestManager,
     @Assisted private val savedState: SavedStateHandle
 ) : ViewModel() {
@@ -26,14 +33,33 @@ class TractActivityDataModel @AssistedInject constructor(
     val tool = MutableLiveData<String?>()
     val locales = MutableLiveData<List<Locale>>(emptyList())
 
-    val manifests =
-        locales.distinctUntilChanged().switchFold(MutableLiveData(emptyList<Manifest?>())) { manifests, locale ->
-            val manifest = tool.distinctUntilChanged().switchMap {
-                when {
-                    it != null -> manifestManager.getLatestPublishedManifestLiveData(it, locale).withInitialValue(null)
-                    else -> emptyLiveData()
-                }
-            }
-            manifests.distinctUntilChanged().combineWith(manifest.distinctUntilChanged()) { list, item -> list + item }
+    private val distinctTool = tool.distinctUntilChanged()
+    private val distinctLocales = locales.distinctUntilChanged()
+
+    val manifests: LiveData<List<Manifest?>> =
+        distinctLocales.switchFold(ImmutableLiveData(emptyList())) { acc, locale ->
+            val manifest =
+                distinctTool.switchMap { manifestCache.get(TranslationKey(it, locale))!!.withInitialValue(null) }
+                    .distinctUntilChanged()
+            acc.distinctUntilChanged().combineWith(manifest) { manifests, manifest -> manifests + manifest }
         }
+    val translations: LiveData<List<Translation?>> =
+        distinctLocales.switchFold(ImmutableLiveData(emptyList())) { acc, locale ->
+            val translation =
+                distinctTool.switchMap { translationCache.get(TranslationKey(it, locale))!!.withInitialValue(null) }
+                    .distinctUntilChanged()
+            acc.distinctUntilChanged().combineWith(translation) { translations, trans -> translations + trans }
+        }
+
+    private val manifestCache = object : LruCache<TranslationKey, LiveData<Manifest?>>(10) {
+        override fun create(key: TranslationKey): LiveData<Manifest?> {
+            val tool = key.tool ?: return emptyLiveData()
+            val locale = key.locale ?: return emptyLiveData()
+            return manifestManager.getLatestPublishedManifestLiveData(tool, locale).distinctUntilChanged()
+        }
+    }
+    private val translationCache = object : LruCache<TranslationKey, LiveData<Translation?>>(10) {
+        override fun create(key: TranslationKey) =
+            dao.getLatestTranslationLiveData(key.tool, key.locale, trackAccess = true).distinctUntilChanged()
+    }
 }
