bca9cc2c63d79bf6abb3489de5ace97cf2cccae2
Daniel Frett <daniel.frett@ccci.org>
2020-06-09 12:09:31 -0400

    move layout definition to concrete activities out of the abstract activity

--- ui/base-tool/src/main/java/org/cru/godtools/base/tool/activity/BaseArticleActivity.kt
+++ ui/base-tool/src/main/java/org/cru/godtools/base/tool/activity/BaseArticleActivity.kt
@@ -1,14 +1,10 @@
 package org.cru.godtools.base.tool.activity
 
-import org.cru.godtools.base.tool.R
-import org.cru.godtools.base.tool.databinding.ToolGenericFragmentActivityBinding
+import androidx.annotation.LayoutRes
+import androidx.databinding.ViewDataBinding
 import org.cru.godtools.xml.model.Manifest
 
-abstract class BaseArticleActivity(requireTool: Boolean = true) :
-    BaseSingleToolActivity<ToolGenericFragmentActivityBinding>(
-        false,
-        contentLayoutId = R.layout.tool_generic_fragment_activity,
-        requireTool = requireTool
-    ) {
+abstract class BaseArticleActivity<B : ViewDataBinding>(@LayoutRes contentLayoutId: Int, requireTool: Boolean = true) :
+    BaseSingleToolActivity<B>(false, contentLayoutId, requireTool) {
     override fun isSupportedType(type: Manifest.Type) = type == Manifest.Type.ARTICLE
 }
