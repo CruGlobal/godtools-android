c4788baa4e73e17a33f746744d89ce433cd6d304
Daniel Frett <daniel.frett@ccci.org>
2020-06-16 17:16:28 -0400

    add StringRes annotation to overridden shareLinkMessage val

--- ui/base-tool/src/main/java/org/cru/godtools/base/tool/activity/BaseArticleActivity.kt
+++ ui/base-tool/src/main/java/org/cru/godtools/base/tool/activity/BaseArticleActivity.kt
@@ -1,11 +1,13 @@
 package org.cru.godtools.base.tool.activity
 
 import androidx.annotation.LayoutRes
+import androidx.annotation.StringRes
 import androidx.databinding.ViewDataBinding
 import org.cru.godtools.base.tool.R
 import org.cru.godtools.xml.model.Manifest
 
 abstract class BaseArticleActivity<B : ViewDataBinding>(@LayoutRes contentLayoutId: Int, requireTool: Boolean = true) :
     BaseSingleToolActivity<B>(false, contentLayoutId, requireTool, Manifest.Type.ARTICLE) {
+    @get:StringRes
     override val shareLinkMessageRes get() = R.string.share_tool_message_article
 }
