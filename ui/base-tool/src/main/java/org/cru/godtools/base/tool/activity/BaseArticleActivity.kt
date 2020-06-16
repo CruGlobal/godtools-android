d22f3469a406b7f0ea3aa29b84fd2c09013dcbf4
Daniel Frett <daniel.frett@ccci.org>
2020-06-16 14:59:06 -0400

    add a different share message for articles

--- ui/base-tool/src/main/java/org/cru/godtools/base/tool/activity/BaseArticleActivity.kt
+++ ui/base-tool/src/main/java/org/cru/godtools/base/tool/activity/BaseArticleActivity.kt
@@ -2,7 +2,10 @@ package org.cru.godtools.base.tool.activity
 
 import androidx.annotation.LayoutRes
 import androidx.databinding.ViewDataBinding
+import org.cru.godtools.base.tool.R
 import org.cru.godtools.xml.model.Manifest
 
 abstract class BaseArticleActivity<B : ViewDataBinding>(@LayoutRes contentLayoutId: Int, requireTool: Boolean = true) :
-    BaseSingleToolActivity<B>(false, contentLayoutId, requireTool, Manifest.Type.ARTICLE)
+    BaseSingleToolActivity<B>(false, contentLayoutId, requireTool, Manifest.Type.ARTICLE) {
+    override val shareLinkMessageRes get() = R.string.share_tool_message_article
+}
