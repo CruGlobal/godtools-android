ccfa2a271f5769a0f1126d951501c28ea94e3170
Daniel Frett <daniel.frett@ccci.org>
2020-06-18 17:43:35 -0400

    create an internal helper method that handles configuring the XmlPullParser

--- library/xml-model/src/main/java/org/cru/godtools/xml/service/ManifestParser.kt
+++ library/xml-model/src/main/java/org/cru/godtools/xml/service/ManifestParser.kt
@@ -1,13 +1,14 @@
 package org.cru.godtools.xml.service
 
-import android.util.Xml
 import androidx.annotation.AnyThread
+import androidx.annotation.VisibleForTesting
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import org.ccci.gto.android.common.kotlin.coroutines.MutexMap
 import org.ccci.gto.android.common.kotlin.coroutines.withLock
 import org.ccci.gto.android.common.support.v4.util.WeakLruCache
+import org.ccci.gto.android.common.util.xmlpull.CloseableXmlPullParser
 import org.cru.godtools.base.FileManager
 import org.cru.godtools.xml.model.Manifest
 import org.cru.godtools.xml.model.Page
@@ -15,6 +16,7 @@ import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserException
 import java.io.FileNotFoundException
 import java.io.IOException
+import java.io.InputStream
 import java.util.Locale
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -29,12 +31,8 @@ class ManifestParser @Inject internal constructor(private val fileManager: FileM
         try {
             cache[manifestName] ?: withContext(Dispatchers.IO) {
                 val manifest = try {
-                    fileManager.getInputStream(manifestName)!!.buffered().use {
-                        Manifest(toolCode, locale, Xml.newPullParser().apply {
-                            setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, true)
-                            setInput(it, "UTF-8")
-                            nextTag()
-                        })
+                    fileManager.getInputStream(manifestName)!!.buffered().xmlPullParser().use {
+                        Manifest(toolCode, locale, it)
                     }
                 } catch (e: FileNotFoundException) {
                     return@withContext Result.Error.NotFound
@@ -57,18 +55,19 @@ class ManifestParser @Inject internal constructor(private val fileManager: FileM
     private fun Page.parse(): Result {
         val fileName = page.localFileName ?: return Result.Success
         return try {
-            fileManager.getInputStream(fileName)!!.buffered().use {
-                page.parsePageXml(Xml.newPullParser().apply {
-                    setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, true)
-                    setInput(it, "UTF-8")
-                    nextTag()
-                })
-            }
+            fileManager.getInputStream(fileName)!!.buffered().xmlPullParser().use { page.parsePageXml(it) }
             Result.Success
         } catch (e: FileNotFoundException) {
             Result.Error.NotFound
         }
     }
+}
+
+@VisibleForTesting
+internal fun InputStream.xmlPullParser() = CloseableXmlPullParser().also {
+    it.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, true)
+    it.setInput(this, "UTF-8")
+    it.nextTag()
 }
 
 sealed class Result {
