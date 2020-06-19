d759238246b3abe02db6f72a53f26f205082978d
Daniel Frett <daniel.frett@ccci.org>
2020-06-19 18:04:04 -0400

    utilize Data Binding for binding a Button

--- ui/tract-renderer/src/main/java/org/cru/godtools/tract/viewmodel/ButtonViewHolder.kt
+++ ui/tract-renderer/src/main/java/org/cru/godtools/tract/viewmodel/ButtonViewHolder.kt
@@ -1,38 +1,25 @@
 package org.cru.godtools.tract.viewmodel
 
-import android.content.res.ColorStateList
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.annotation.UiThread
-import androidx.core.view.ViewCompat
-import butterknife.BindView
-import butterknife.OnClick
 import org.cru.godtools.analytics.model.ExitLinkActionEvent
-import org.cru.godtools.base.tool.model.view.bindTo
 import org.cru.godtools.base.ui.util.openUrl
 import org.cru.godtools.tract.R
-import org.cru.godtools.tract.R2
+import org.cru.godtools.tract.databinding.TractContentButtonBinding
 import org.cru.godtools.xml.model.AnalyticsEvent.Trigger
 import org.cru.godtools.xml.model.Button
-import org.cru.godtools.xml.model.buttonColor
 import org.greenrobot.eventbus.EventBus
 
 @UiThread
 internal class ButtonViewHolder(parent: ViewGroup, parentViewHolder: BaseViewHolder<*>?) :
     BaseViewHolder<Button>(Button::class.java, parent, R.layout.tract_content_button, parentViewHolder) {
+    private val binding = TractContentButtonBinding.bind(mRoot).also { it.holder = this }
 
-    @BindView(R2.id.button)
-    internal lateinit var mButton: TextView
-
-    // region Lifecycle Events
     public override fun onBind() {
         super.onBind()
-        mModel?.text.bindTo(mButton)
-        ViewCompat.setBackgroundTintList(mButton, ColorStateList.valueOf(mModel.buttonColor))
+        binding.model = mModel
     }
-    // endregion Lifecycle Events
 
-    @OnClick(R2.id.button)
     fun click() {
         val model = mModel ?: return
 
