6ee785dcea4693e12ee8e0e481fa96cda1b7acb5
Daniel Frett <daniel.frett@ccci.org>
2020-06-23 16:47:54 -0400

    add tests for arrow click actions

--- ui/tract-renderer/src/test/java/org/cru/godtools/tract/databinding/TractPageCallToActionBindingTest.kt
+++ ui/tract-renderer/src/test/java/org/cru/godtools/tract/databinding/TractPageCallToActionBindingTest.kt
@@ -5,7 +5,11 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.ContextThemeWrapper
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.never
 import com.nhaarman.mockitokotlin2.spy
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import org.cru.godtools.base.model.Event
 import org.cru.godtools.tract.R
@@ -25,17 +29,16 @@ class TractPageCallToActionBindingTest {
     private lateinit var page: Page
 
     @Before
-    fun createBinding() {
+    fun setup() {
         val activity = Robolectric.buildActivity(AppCompatActivity::class.java).get()
         val context = ContextThemeWrapper(activity, R.style.Theme_AppCompat)
 
         binding = TractPageCallToActionBinding.inflate(LayoutInflater.from(context), null, false)
         binding.lifecycleOwner = activity
+        binding.callbacks = mock()
+        binding.controller = mock()
         binding.executePendingBindings()
-    }
 
-    @Before
-    fun createMocks() {
         page = spy(Page(Manifest(), 0))
     }
 
@@ -62,5 +65,34 @@ class TractPageCallToActionBindingTest {
         binding.page = page
         binding.executePendingBindings()
         assertEquals(View.VISIBLE, binding.callToActionArrow.visibility)
+    }
+
+    @Test
+    fun verifyArrowOnClickNoCallToAction() {
+        binding.executePendingBindings()
+
+        binding.callToActionArrow.performClick()
+        verify(binding.callbacks!!).goToNextPage()
+        verify(binding.controller!!, never()).sendEvents(any())
+    }
+
+    @Test
+    fun verifyArrowOnClickNoEvents() {
+        binding.callToAction = CallToAction(page)
+        binding.executePendingBindings()
+
+        binding.callToActionArrow.performClick()
+        verify(binding.callbacks!!).goToNextPage()
+        verify(binding.controller!!, never()).sendEvents(any())
+    }
+
+    @Test
+    fun verifyArrowOnClickEvents() {
+        binding.callToAction = CallToAction(page, events = setOf(Event.Id.FOLLOWUP_EVENT))
+        binding.executePendingBindings()
+
+        binding.callToActionArrow.performClick()
+        verify(binding.callbacks!!, never()).goToNextPage()
+        verify(binding.controller!!).sendEvents(any())
     }
 }
