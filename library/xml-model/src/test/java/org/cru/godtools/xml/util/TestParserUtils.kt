6956f8962cbe0ac62e34df10de89e902287026d9
Daniel Frett <daniel.frett@ccci.org>
2020-06-29 13:37:43 -0400

    add a manifest parsing test for parsing pages

--- library/xml-model/src/test/java/org/cru/godtools/xml/util/TestParserUtils.kt
+++ library/xml-model/src/test/java/org/cru/godtools/xml/util/TestParserUtils.kt
@@ -1,9 +1,8 @@
 package org.cru.godtools.xml.util
 
 import org.cru.godtools.xml.service.xmlPullParser
-import org.xmlpull.v1.XmlPullParser
 import java.io.InputStream
 
-fun Any.getXmlParserForResource(name: String): XmlPullParser = getInputStreamForResource(name).xmlPullParser()
+fun Any.getXmlParserForResource(name: String) = getInputStreamForResource(name).xmlPullParser()
 
 fun Any.getInputStreamForResource(name: String): InputStream = this::class.java.getResourceAsStream(name)!!
