d8de898ba97e1c435b87ebf251d09ecf9b24ee72
Daniel Frett <daniel.frett@ccci.org>
2020-07-08 14:19:02 -0400

    convert TabsController to Kotlin

---
+++ ui/tract-renderer/src/main/java/org/cru/godtools/tract/ui/controller/TabsController.kt
@@ -0,0 +1,106 @@
+package org.cru.godtools.tract.ui.controller
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.CallSuper
+import androidx.annotation.UiThread
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import org.ccci.gto.android.common.material.tabs.setBackgroundTint
+import org.cru.godtools.base.model.Event
+import org.cru.godtools.tract.databinding.TractContentTabsBinding
+import org.cru.godtools.tract.viewmodel.BaseViewHolder
+import org.cru.godtools.xml.model.Tab
+import org.cru.godtools.xml.model.Tabs
+import org.cru.godtools.xml.model.primaryColor
+
+class TabsController private constructor(
+    private val binding: TractContentTabsBinding,
+    parentViewHolder: BaseViewHolder<*>?
+) : BaseViewHolder<Tabs>(Tabs::class.java, binding.root, parentViewHolder), OnTabSelectedListener {
+    internal constructor(parent: ViewGroup, parentViewHolder: BaseViewHolder<*>?) :
+        this(TractContentTabsBinding.inflate(LayoutInflater.from(parent.context), parent, false), parentViewHolder)
+
+    init {
+        binding.tabs.addOnTabSelectedListener(this)
+    }
+
+    private val tabCache = UiControllerCache(binding.tab, this)
+
+    private var bindingTabs = false
+    private var tabControllers = emptyList<TabController>()
+
+    // region Lifecycle
+    @UiThread
+    override fun onBind() {
+        super.onBind()
+        binding.model = model
+        bindTabs()
+    }
+
+    @CallSuper
+    override fun onContentEvent(event: Event) {
+        super.onContentEvent(event)
+        checkForTabEvent(event)
+        propagateEventToChildren(event)
+    }
+
+    override fun onTabSelected(tab: TabLayout.Tab) {
+        val holder = showTabContent(tab.position) ?: return
+        if (!bindingTabs) holder.trackSelectedAnalyticsEvents()
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab) = Unit
+    override fun onTabReselected(tab: TabLayout.Tab) = Unit
+    // endregion Lifecycle
+
+    private fun bindTabs() {
+        bindingTabs = true
+
+        // update tab controllers
+        tabControllers.forEach {
+            binding.tab.removeView(it.mRoot)
+            it.bind(null)
+            it.releaseTo(tabCache)
+        }
+        tabControllers = model?.tabs?.map { bindTabContentViewHolder(it) }.orEmpty()
+
+        // update tabs for the TabLayout
+        val primaryColor = model?.stylesParent.primaryColor
+        binding.tabs.removeAllTabs()
+        model?.tabs?.forEach {
+            with(binding.tabs) {
+                addTab(newTab().apply {
+                    text = it.label?.text
+                    setBackgroundTint(primaryColor)
+                })
+            }
+        }
+
+        bindingTabs = false
+    }
+
+    private fun selectTab(tab: Tab) {
+        binding.tabs.getTabAt(tab.position)?.select()
+    }
+
+    private fun bindTabContentViewHolder(tab: Tab?) =
+        tabCache.acquire(Tab::class).also { it?.bind(tab) } as TabController
+
+    private fun showTabContent(position: Int) = tabControllers[position].apply {
+        if (mRoot.parent !== binding.tab) {
+            binding.tab.removeAllViews()
+            binding.tab.addView(mRoot)
+        }
+    }
+
+    private fun propagateEventToChildren(event: Event) {
+        val position = binding.tabs.selectedTabPosition
+        if (position > -1) tabControllers[position].onContentEvent(event)
+    }
+
+    private fun checkForTabEvent(event: Event) {
+        model?.tabs?.firstOrNull { it.listeners.contains(event.id) }
+            ?.let { selectTab(it) }
+    }
+}
--- ui/tract-renderer/src/main/java/org/cru/godtools/tract/ui/controller/UiControllerCache.kt
+++ ui/tract-renderer/src/main/java/org/cru/godtools/tract/ui/controller/UiControllerCache.kt
@@ -5,7 +5,6 @@ import androidx.core.util.Pools
 import org.ccci.gto.android.common.app.ApplicationUtils
 import org.cru.godtools.tract.viewmodel.BaseViewHolder
 import org.cru.godtools.tract.viewmodel.InputViewHolder
-import org.cru.godtools.tract.viewmodel.TabsViewHolder
 import org.cru.godtools.xml.model.Base
 import org.cru.godtools.xml.model.Button
 import org.cru.godtools.xml.model.Form
@@ -41,7 +40,7 @@ internal class UiControllerCache(private val parent: ViewGroup, private val pare
             Input::class -> InputViewHolder(parent, parentViewHolder)
             Link::class -> LinkController(parent, parentViewHolder)
             Paragraph::class -> ParagraphController(parent, parentViewHolder)
-            Tabs::class -> TabsViewHolder.create(parent, parentViewHolder)
+            Tabs::class -> TabsController(parent, parentViewHolder)
             Tab::class -> TabController(parent, parentViewHolder)
             Text::class -> TextController(parent, parentViewHolder)
             else -> {
