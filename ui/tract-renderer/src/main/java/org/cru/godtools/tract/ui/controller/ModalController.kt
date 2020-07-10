ff0840ec6b2784d9b0dbe20c0597d5b526502d6e
Daniel Frett <daniel.frett@ccci.org>
2020-07-10 13:42:09 -0400

    track the ModalController on the ModalBinding

--- ui/tract-renderer/src/main/java/org/cru/godtools/tract/ui/controller/ModalController.kt
+++ ui/tract-renderer/src/main/java/org/cru/godtools/tract/ui/controller/ModalController.kt
@@ -1,11 +1,14 @@
 package org.cru.godtools.tract.ui.controller
 
 import org.cru.godtools.tract.databinding.TractContentModalBinding
-import org.cru.godtools.tract.viewmodel.BaseViewHolder
 import org.cru.godtools.xml.model.Modal
 
 class ModalController internal constructor(private val binding: TractContentModalBinding) :
     ParentController<Modal>(Modal::class, binding.root, null) {
+    init {
+        binding.controller = this
+    }
+
     override val contentContainer get() = binding.content
 
     override fun onBind() {
@@ -14,6 +17,4 @@ class ModalController internal constructor(private val binding: TractContentModa
     }
 }
 
-// TODO: this may change once I figure out what code pattern I want to use to create/bind controllers
-internal fun TractContentModalBinding.bindController() =
-    BaseViewHolder.forView(root, ModalController::class.java) ?: ModalController(this)
+internal fun TractContentModalBinding.bindController() = controller ?: ModalController(this)
