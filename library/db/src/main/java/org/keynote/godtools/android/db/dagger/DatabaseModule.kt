ff85d06792bd167e9a74e776124002a9ddf1141f
Daniel Frett <daniel.frett@ccci.org>
2020-07-13 18:34:30 -0400

    make GodToolsDao an actual Singleton

--- library/db/src/main/java/org/keynote/godtools/android/db/dagger/DatabaseModule.kt
+++ library/db/src/main/java/org/keynote/godtools/android/db/dagger/DatabaseModule.kt
@@ -3,17 +3,12 @@ package org.keynote.godtools.android.db.dagger
 import android.content.Context
 import dagger.Module
 import dagger.Provides
-import org.keynote.godtools.android.db.GodToolsDao
 import org.keynote.godtools.android.db.GodToolsDatabase
 import javax.inject.Singleton
 
 @Module
 abstract class DatabaseModule {
     companion object {
-        @Provides
-        @Singleton
-        fun provideGodToolsDao(context: Context) = GodToolsDao.getInstance(context)
-
         @Provides
         @Singleton
         fun provideGodToolsDatabase(context: Context) = GodToolsDatabase.getInstance(context)
