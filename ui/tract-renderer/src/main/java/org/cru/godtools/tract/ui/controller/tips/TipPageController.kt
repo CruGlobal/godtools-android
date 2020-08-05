e17eb7f2ef8eda1781023dcc71a3dc5eb12700ee
Daniel Frett <daniel.frett@ccci.org>
2020-08-05 15:39:40 -0400

    add initial UI for a TipPage

---
+++ ui/tract-renderer/src/main/java/org/cru/godtools/tract/ui/controller/tips/TipPageController.kt
@@ -0,0 +1,42 @@
+package org.cru.godtools.tract.ui.controller.tips
+
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
+import org.cru.godtools.tract.databinding.TractTipPageBinding
+import org.cru.godtools.tract.ui.controller.ParentController
+import org.cru.godtools.xml.model.tips.TipPage
+import org.greenrobot.eventbus.EventBus
+
+class TipPageController @AssistedInject internal constructor(
+    @Assisted private val binding: TractTipPageBinding,
+    override val eventBus: EventBus
+) : ParentController<TipPage>(TipPage::class, binding.root, null) {
+    @AssistedInject.Factory
+    interface Factory {
+        fun create(binding: TractTipPageBinding): TipPageController
+    }
+
+    interface Callbacks {
+        fun goToNextPage()
+        fun closeTip()
+    }
+
+    init {
+        binding.controller = this
+    }
+
+    var callbacks: Callbacks?
+        get() = binding.callbacks
+        set(value) {
+            binding.callbacks = value
+        }
+
+    override val contentContainer get() = binding.content
+
+    override fun onBind() {
+        super.onBind()
+        binding.page = model
+    }
+}
+
+fun TractTipPageBinding.bindController(factory: TipPageController.Factory) = controller ?: factory.create(this)
