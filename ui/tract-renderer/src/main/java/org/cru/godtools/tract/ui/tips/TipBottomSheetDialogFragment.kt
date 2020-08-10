d4f0bb00e9c6474135d8b780cb789ade333769fa
Daniel Frett <daniel.frett@ccci.org>
2020-08-10 15:36:38 -0400

    force the tips UI to be in expanded mode, and be full screen for smaller screen sizes

--- ui/tract-renderer/src/main/java/org/cru/godtools/tract/ui/tips/TipBottomSheetDialogFragment.kt
+++ ui/tract-renderer/src/main/java/org/cru/godtools/tract/ui/tips/TipBottomSheetDialogFragment.kt
@@ -2,11 +2,14 @@ package org.cru.godtools.tract.ui.tips
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.observe
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import org.ccci.gto.android.common.androidx.lifecycle.combineWith
 import org.cru.godtools.base.tool.service.ManifestManager
 import org.cru.godtools.base.tool.viewmodel.LatestPublishedManifestDataModel
@@ -43,6 +46,16 @@ class TipBottomSheetDialogFragment() : BaseBottomSheetDialogFragment<TractTipBin
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
         inflater.inflate(R.layout.tract_tip, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        forceExpandedMode()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (context?.resources?.getBoolean(R.bool.tract_tips_show_full_height) == true) makeFullScreen()
+    }
+
     override fun onBindingCreated(binding: TractTipBinding, savedInstanceState: Bundle?) {
         dataModel.tip.observe(viewLifecycleOwner) { binding.tip = it }
         binding.setupPages()
@@ -73,6 +86,19 @@ class TipBottomSheetDialogFragment() : BaseBottomSheetDialogFragment<TractTipBin
         dismissAllowingStateLoss()
     }
     // endregion TipPageController.Callbacks
+
+    private fun forceExpandedMode() {
+        (dialog as? BottomSheetDialog)?.behavior?.apply {
+            skipCollapsed = true
+            state = BottomSheetBehavior.STATE_EXPANDED
+        }
+    }
+
+    private fun makeFullScreen() {
+        dialog?.findViewById<View>(R.id.design_bottom_sheet)?.apply {
+            layoutParams = layoutParams.apply { height = ViewGroup.LayoutParams.MATCH_PARENT }
+        }
+    }
 }
 
 internal class TipBottomSheetDialogFragmentDataModel @Inject constructor(manifestManager: ManifestManager) :
