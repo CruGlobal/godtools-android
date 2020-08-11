48154ec05d9cdde2cc70c6e27b294433c17247ac
Daniel Frett <daniel.frett@ccci.org>
2020-08-11 10:52:48 -0400

    get rid of the unnecessary sealed class

--- app/src/main/java/org/cru/godtools/ui/tools/analytics/model/ToolsAnalyticsActionEvent.kt
+++ app/src/main/java/org/cru/godtools/ui/tools/analytics/model/ToolsAnalyticsActionEvent.kt
@@ -3,10 +3,7 @@ package org.cru.godtools.ui.tools.analytics.model
 import org.cru.godtools.analytics.model.AnalyticsActionEvent
 import org.cru.godtools.analytics.model.AnalyticsSystem
 
-sealed class ToolsAnalyticsActionEvent(action: String) : AnalyticsActionEvent(action) {
-    override fun isForSystem(system: AnalyticsSystem) = system == AnalyticsSystem.ADOBE
-}
-
-object ToolOpenTapAnalyticsActionEvent : ToolsAnalyticsActionEvent("cru.tool_open_tap")
-object ToolOpenButtonAnalyticsActionEvent : ToolsAnalyticsActionEvent("cru.tool_open_button")
-object AboutToolButtonAnalyticsActionEvent : ToolsAnalyticsActionEvent("cru.about_tool_button")
+object ToolOpenTapAnalyticsActionEvent : AnalyticsActionEvent("cru.tool_open_tap", system = AnalyticsSystem.ADOBE)
+object ToolOpenButtonAnalyticsActionEvent : AnalyticsActionEvent("cru.tool_open_button", system = AnalyticsSystem.ADOBE)
+object AboutToolButtonAnalyticsActionEvent :
+    AnalyticsActionEvent("cru.about_tool_button", system = AnalyticsSystem.ADOBE)
--- library/analytics/src/main/java/org/cru/godtools/analytics/model/AnalyticsActionEvent.kt
+++ library/analytics/src/main/java/org/cru/godtools/analytics/model/AnalyticsActionEvent.kt
@@ -10,8 +10,12 @@ private const val SNOWPLOW_CONTENT_SCORING_URI_PATH_ACTION = "action"
 open class AnalyticsActionEvent(
     val action: String,
     val label: String? = null,
-    locale: Locale? = null
-) : AnalyticsBaseEvent(locale) {
+    locale: Locale? = null,
+    systems: Collection<AnalyticsSystem>? = null
+) : AnalyticsBaseEvent(locale, systems) {
+    constructor(action: String, label: String? = null, locale: Locale? = null, system: AnalyticsSystem) :
+        this(action, label, locale, setOf(system))
+
     open val adobeAttributes: Map<String, *>? get() = null
 
     open val firebaseEventName get() = action
