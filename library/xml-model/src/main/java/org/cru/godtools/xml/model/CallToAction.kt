568e422c651dfb94bb6509c142d409f192329663
Daniel Frett <daniel.frett@ccci.org>
2020-08-12 11:56:20 -0400

    parse training tips attached to call-to-action elements

--- library/xml-model/src/main/java/org/cru/godtools/xml/model/CallToAction.kt
+++ library/xml-model/src/main/java/org/cru/godtools/xml/model/CallToAction.kt
@@ -2,11 +2,14 @@ package org.cru.godtools.xml.model
 
 import androidx.annotation.ColorInt
 import androidx.annotation.RestrictTo
+import androidx.annotation.VisibleForTesting
 import org.cru.godtools.base.model.Event
 import org.cru.godtools.xml.XMLNS_TRACT
+import org.cru.godtools.xml.XMLNS_TRAINING
 import org.xmlpull.v1.XmlPullParser
 
 private const val XML_CONTROL_COLOR = "control-color"
+private const val XML_TIP = "tip"
 
 class CallToAction : BaseModel {
     companion object {
@@ -21,10 +24,15 @@ class CallToAction : BaseModel {
     @get:ColorInt
     val controlColor get() = _controlColor ?: page.primaryColor
 
+    @VisibleForTesting
+    internal val tipId: String?
+    val tip get() = manifest.findTip(tipId)
+
     internal constructor(parent: Base) : super(parent) {
         label = null
         events = emptySet()
         _controlColor = null
+        tipId = null
     }
 
     internal constructor(parent: Base, parser: XmlPullParser) : super(parent) {
@@ -32,6 +40,7 @@ class CallToAction : BaseModel {
 
         events = parseEvents(parser, XML_EVENTS)
         _controlColor = parser.getAttributeValueAsColorOrNull(XML_CONTROL_COLOR)
+        tipId = parser.getAttributeValue(XMLNS_TRAINING, XML_TIP)
 
         label = Text.fromNestedXml(this, parser, XMLNS_TRACT, XML_CALL_TO_ACTION)
     }
@@ -41,11 +50,13 @@ class CallToAction : BaseModel {
         parent: Base,
         label: ((CallToAction) -> Text?)? = null,
         events: Set<Event.Id> = emptySet(),
-        @ColorInt controlColor: Int? = null
+        @ColorInt controlColor: Int? = null,
+        tip: String? = null
     ) : super(parent) {
         this.label = label?.invoke(this)
         this.events = events
         _controlColor = controlColor
+        tipId = tip
     }
 }
 
