31a7e6b373fbd260f272ce586b471b8dfaea8e83
Gyasi Story <gyasi.story@cru.org>
2020-08-14 16:55:08 -0400

    simplify Manifest Creation

--- app/src/test/java/org/cru/godtools/databinding/ToolDetailsFragmentBindingTest.kt
+++ app/src/test/java/org/cru/godtools/databinding/ToolDetailsFragmentBindingTest.kt
@@ -5,10 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.mock
 import org.cru.godtools.ui.tooldetails.ToolDetailsActivity
 import org.cru.godtools.xml.model.Manifest
+import org.cru.godtools.xml.model.tips.Tip
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
@@ -20,10 +19,6 @@ import org.robolectric.annotation.Config
 @Config(application = Application::class)
 class ToolDetailsFragmentBindingTest {
     private lateinit var binding: ToolDetailsFragmentBinding
-    private val tipsManifest: Manifest = mock {
-        on(this.mock.tips) doReturn mapOf(Pair("test1", mock { }))
-    }
-    private val emptyManifest: Manifest = mock {}
 
     @Before
     fun createBinding() {
@@ -33,7 +28,8 @@ class ToolDetailsFragmentBindingTest {
 
     @Test
     fun verifyWithTips() {
-        binding.manifest = MutableLiveData<Manifest>(tipsManifest)
+        val manifest = Manifest(tips = { listOf(Tip(it, "test1")) })
+        binding.manifest = MutableLiveData<Manifest>(manifest)
         binding.executePendingBindings()
 
         assertEquals(View.VISIBLE, binding.actionToolTraining.visibility)
@@ -41,7 +37,8 @@ class ToolDetailsFragmentBindingTest {
 
     @Test
     fun verifyNoTips() {
-        binding.manifest = MutableLiveData<Manifest>(emptyManifest)
+        val manifest = Manifest()
+        binding.manifest = MutableLiveData<Manifest>(manifest)
         binding.executePendingBindings()
 
         assertEquals(View.GONE, binding.actionToolTraining.visibility)
