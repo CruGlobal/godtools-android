5ca98f79167f9ada2adff855e60e5daa27b8d3a1
Gyasi Story <gyasi.story@cru.org>
2020-08-14 15:43:04 -0400

    Fix tips check and add noTips and Empty check

--- app/src/test/java/org/cru/godtools/databinding/ToolDetailsFragmentBindingTest.kt
+++ app/src/test/java/org/cru/godtools/databinding/ToolDetailsFragmentBindingTest.kt
@@ -5,41 +5,53 @@ import android.view.LayoutInflater
 import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.cru.godtools.xml.model.Manifest
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
 import org.cru.godtools.ui.tooldetails.ToolDetailsActivity
-import org.cru.godtools.ui.tooldetails.ToolDetailsFragment
-import org.cru.godtools.xml.util.getXmlParserForResource
+import org.cru.godtools.xml.model.Manifest
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.Assert.assertEquals
 import org.robolectric.Robolectric
 import org.robolectric.annotation.Config
-import java.util.Locale
-
-private const val TOOL_CODE = "test"
 
 @RunWith(AndroidJUnit4::class)
 @Config(application = Application::class)
 class ToolDetailsFragmentBindingTest {
     private lateinit var binding: ToolDetailsFragmentBinding
+    private val tipsManifest: Manifest = mock {
+        on(this.mock.tips) doReturn mapOf(Pair("test1", mock { }))
+    }
+    private val emptyManifest: Manifest = mock {}
 
     @Before
-    fun createBinding(){
+    fun createBinding() {
         val activityController = Robolectric.buildActivity(ToolDetailsActivity::class.java)
         binding = ToolDetailsFragmentBinding.inflate(LayoutInflater.from(activityController.get()), null, false)
     }
 
     @Test
     fun verifyWithTips() {
-        val manifest = Manifest(TOOL_CODE, Locale.ENGLISH, getXmlParserForResource("manifest_tips_invalid.xml")){
-            getXmlParserForResource(it)
-        }
-        binding.manifest = MutableLiveData<Manifest>(manifest)
+        binding.manifest = MutableLiveData<Manifest>(tipsManifest)
         binding.executePendingBindings()
 
         assertEquals(View.VISIBLE, binding.actionToolTraining.visibility)
     }
 
+    @Test
+    fun verifyNoTips() {
+        binding.manifest = MutableLiveData<Manifest>(emptyManifest)
+        binding.executePendingBindings()
 
+        assertEquals(View.GONE, binding.actionToolTraining.visibility)
+    }
+
+    @Test
+    fun verifyEmptyManifest() {
+        binding.manifest = MutableLiveData<Manifest>()
+        binding.executePendingBindings()
+
+        assertEquals(View.GONE, binding.actionToolTraining.visibility)
+    }
 }
