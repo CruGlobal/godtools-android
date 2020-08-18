4b1e30305652828e6b5002bdbda7a9d22288dfb5
Daniel Frett <daniel.frett@ccci.org>
2020-08-18 15:31:19 -0400

    add support for parsing header training tips from tract xml

--- library/xml-model/src/test/java/org/cru/godtools/xml/model/HeaderTest.kt
+++ library/xml-model/src/test/java/org/cru/godtools/xml/model/HeaderTest.kt
@@ -4,24 +4,18 @@ import android.graphics.Color
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.cru.godtools.xml.util.getXmlParserForResource
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class HeaderTest {
-    private lateinit var manifest: Manifest
-
-    @Before
-    fun setup() {
-        manifest = Manifest(TOOL_CODE)
-    }
-
     @Test
     fun testParseHeader() {
-        val header = Header(Page(manifest, 1), getXmlParserForResource("header.xml"))
+        val page = Page(Manifest(TOOL_CODE), 0, null, getXmlParserForResource("header.xml"))
+        val header = page.header!!
         assertEquals("5", header.number!!.text)
         assertEquals("title", header.title!!.text)
         assertEquals(Color.RED, header.backgroundColor)
+        assertEquals("header-tip", header.tipId)
     }
 }
