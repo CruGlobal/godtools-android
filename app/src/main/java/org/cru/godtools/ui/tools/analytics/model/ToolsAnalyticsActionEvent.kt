4af6ba9b124c49e055fd850c02b2cbee77f39bed
Gyasi Story <gyasi.story@cru.org>
2020-08-19 16:24:29 -0400

    Change to User Friendly Names

--- app/src/main/java/org/cru/godtools/ui/tools/analytics/model/ToolsAnalyticsActionEvent.kt
+++ app/src/main/java/org/cru/godtools/ui/tools/analytics/model/ToolsAnalyticsActionEvent.kt
@@ -3,7 +3,7 @@ package org.cru.godtools.ui.tools.analytics.model
 import org.cru.godtools.analytics.model.AnalyticsActionEvent
 import org.cru.godtools.analytics.model.AnalyticsSystem
 
-object ToolOpenTapAnalyticsActionEvent : AnalyticsActionEvent("cru.tool_open_tap", system = AnalyticsSystem.ADOBE)
-object ToolOpenButtonAnalyticsActionEvent : AnalyticsActionEvent("cru.tool_open_button", system = AnalyticsSystem.ADOBE)
+object ToolOpenTapAnalyticsActionEvent : AnalyticsActionEvent("Tool Open Tap", system = AnalyticsSystem.ADOBE)
+object ToolOpenButtonAnalyticsActionEvent : AnalyticsActionEvent("Tool Open Button", system = AnalyticsSystem.ADOBE)
 object AboutToolButtonAnalyticsActionEvent :
-    AnalyticsActionEvent("cru.about_tool_button", system = AnalyticsSystem.ADOBE)
+    AnalyticsActionEvent("About Tool Open Button", system = AnalyticsSystem.ADOBE)
