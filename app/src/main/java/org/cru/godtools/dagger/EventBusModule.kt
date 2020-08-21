4ec088d4e1ed63d9faed2c779f2ae0caba20456e
Daniel Frett <daniel.frett@ccci.org>
2020-08-21 16:17:05 -0400

    eventbus doesn't need to be eager anymore

--- app/src/main/java/org/cru/godtools/dagger/EventBusModule.kt
+++ app/src/main/java/org/cru/godtools/dagger/EventBusModule.kt
@@ -1,6 +1,5 @@
 package org.cru.godtools.dagger
 
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
@@ -9,8 +8,6 @@ import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.IntoSet
 import dagger.multibindings.Multibinds
 import javax.inject.Singleton
-import org.ccci.gto.android.common.dagger.eager.EagerSingleton
-import org.ccci.gto.android.common.dagger.eager.EagerSingleton.ThreadMode
 import org.ccci.gto.android.common.eventbus.TimberLogger
 import org.cru.godtools.AppEventBusIndex
 import org.greenrobot.eventbus.EventBus
@@ -21,12 +18,6 @@ import org.greenrobot.eventbus.meta.SubscriberInfoIndex
 abstract class EventBusModule {
     @Multibinds
     abstract fun eventBusIndexes(): Set<SubscriberInfoIndex>
-
-    // TODO: EventBus doesn't need to be Eager once EventBus is only accessed via Dagger
-    @Binds
-    @IntoSet
-    @EagerSingleton(threadMode = ThreadMode.MAIN)
-    abstract fun eagerEventBus(eventBus: EventBus): Any
 
     companion object {
         @Provides
