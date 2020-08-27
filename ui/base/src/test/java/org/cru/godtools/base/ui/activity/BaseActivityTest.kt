844934334217cede8790f80d3b9641b200b000e7
Daniel Frett <daniel.frett@ccci.org>
2020-08-27 18:43:46 -0400

    simplify the BaseActivityTest

--- ui/base/src/test/java/org/cru/godtools/base/ui/activity/BaseActivityTest.kt
+++ ui/base/src/test/java/org/cru/godtools/base/ui/activity/BaseActivityTest.kt
@@ -9,7 +9,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.viewbinding.ViewBinding
 import com.nhaarman.mockitokotlin2.doAnswer
 import com.nhaarman.mockitokotlin2.mock
-import dagger.android.HasAndroidInjector
 import org.ccci.gto.android.common.base.TimeConstants.DAY_IN_MS
 import org.cru.godtools.base.ui.R
 import org.junit.Assert.assertFalse
@@ -17,7 +16,6 @@ import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.RETURNS_DEEP_STUBS
 import org.robolectric.Robolectric
 import org.robolectric.android.controller.ActivityController
 import org.robolectric.annotation.Config
@@ -51,10 +49,10 @@ class BaseActivityTest {
     }
 }
 
-class MockApplication : Application(), HasAndroidInjector by mock(defaultAnswer = RETURNS_DEEP_STUBS) {
+class MockApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        setTheme(R.style.Theme_MaterialComponents)
+        setTheme(R.style.Theme_AppCompat)
     }
 }
 
