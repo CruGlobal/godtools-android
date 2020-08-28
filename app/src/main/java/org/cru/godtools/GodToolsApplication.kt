019a3870ebd3f9fff3816dff75147dd52c012c96
Daniel Frett <daniel.frett@ccci.org>
2020-08-28 17:39:26 -0400

    remove dagger-android because it is no longer in use

--- app/src/main/java/org/cru/godtools/GodToolsApplication.kt
+++ app/src/main/java/org/cru/godtools/GodToolsApplication.kt
@@ -1,15 +1,10 @@
 package org.cru.godtools
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.instantapps.InstantApps
 import com.google.firebase.crashlytics.FirebaseCrashlytics
-import dagger.android.AndroidInjector
-import dagger.android.DaggerApplication
-import dagger.hilt.EntryPoint
-import dagger.hilt.EntryPoints
-import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.components.SingletonComponent
 import java.util.Locale
 import javax.inject.Inject
 import org.ccci.gto.android.common.compat.util.LocaleCompat.toLanguageTag
@@ -19,7 +14,10 @@ import org.ccci.gto.android.common.util.LocaleUtils
 import timber.log.Timber
 
 @HiltAndroidApp
-open class GodToolsApplication : DaggerApplication() {
+open class GodToolsApplication : Application() {
+    @Inject
+    internal lateinit var eagerInitializer: EagerSingletonInitializer
+
     override fun onCreate() {
         // Enable application monitoring
         initializeCrashlytics()
@@ -46,17 +44,4 @@ open class GodToolsApplication : DaggerApplication() {
         crashlytics.setCustomKey("SystemLanguage", toLanguageTag(Locale.getDefault()))
         Timber.plant(CrashlyticsTree())
     }
-
-    // region Dagger
-    @EntryPoint
-    @InstallIn(SingletonComponent::class)
-    interface ApplicationInjector : AndroidInjector<GodToolsApplication>
-
-    override fun applicationInjector(): AndroidInjector<GodToolsApplication> {
-        return EntryPoints.get(this, ApplicationInjector::class.java)
-    }
-
-    @Inject
-    internal lateinit var eagerInitializer: EagerSingletonInitializer
-    // endregion Dagger
 }
--- app/src/main/java/org/cru/godtools/dagger/ApplicationModule.kt
+++ app/src/main/java/org/cru/godtools/dagger/ApplicationModule.kt
@@ -3,12 +3,11 @@ package org.cru.godtools.dagger
 import android.content.Context
 import dagger.Binds
 import dagger.Module
-import dagger.android.AndroidInjectionModule
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
-@Module(includes = [AndroidInjectionModule::class])
+@Module
 @InstallIn(SingletonComponent::class)
 abstract class ApplicationModule {
     @Binds
