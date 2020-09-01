5b3d0452321f9108f557fdd135c6d7279560cf28
Daniel Frett <daniel.frett@ccci.org>
2020-09-01 14:42:06 -0400

    add some unit tests to test share menu visibility

--- ui/tract-renderer/src/test/java/org/cru/godtools/tract/HiltTestingModule.kt
+++ ui/tract-renderer/src/test/java/org/cru/godtools/tract/HiltTestingModule.kt
@@ -1,34 +1,50 @@
 package org.cru.godtools.tract
 
+import androidx.lifecycle.MutableLiveData
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doAnswer
 import com.nhaarman.mockitokotlin2.mock
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.ccci.gto.android.common.androidx.lifecycle.ImmutableLiveData
 import org.ccci.gto.android.common.scarlet.ReferenceLifecycle
 import org.cru.godtools.api.TractShareService
 import org.cru.godtools.base.Settings
+import org.cru.godtools.base.tool.service.ManifestManager
 import org.cru.godtools.download.manager.GodToolsDownloadManager
 import org.cru.godtools.sync.GodToolsSyncService
 import org.cru.godtools.sync.task.ToolSyncTasks
 import org.greenrobot.eventbus.EventBus
 import org.keynote.godtools.android.db.GodToolsDao
+import org.mockito.Mockito.RETURNS_DEEP_STUBS
 
 @Module
 @InstallIn(SingletonComponent::class)
 class ExternalSingletonsModule {
     @get:Provides
-    val dao by lazy { mock<GodToolsDao>() }
+    val dao by lazy {
+        mock<GodToolsDao> {
+            on { getLatestTranslationLiveData(any(), any(), any(), any(), any()) } doAnswer { MutableLiveData(null) }
+        }
+    }
     @get:Provides
     val downloadManager by lazy { mock<GodToolsDownloadManager>() }
     @get:Provides
     val eventBus by lazy { mock<EventBus>() }
     @get:Provides
+    val manifestManager by lazy {
+        mock<ManifestManager> {
+            on { getLatestPublishedManifestLiveData(any(), any()) } doAnswer { ImmutableLiveData(null) }
+        }
+    }
+    @get:Provides
     val referenceLifecycle by lazy { mock<ReferenceLifecycle>() }
     @get:Provides
     val settings by lazy { mock<Settings>() }
     @get:Provides
-    val syncService by lazy { mock<GodToolsSyncService>() }
+    val syncService by lazy { mock<GodToolsSyncService>(defaultAnswer = RETURNS_DEEP_STUBS) }
     @get:Provides
     val toolSyncTasks by lazy { mock<ToolSyncTasks>() }
     @get:Provides
---
+++ ui/tract-renderer/src/test/java/org/cru/godtools/tract/activity/TractActivityTest.kt
@@ -0,0 +1,184 @@
+package org.cru.godtools.tract.activity
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.widget.Toolbar
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.argThat
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.stub
+import com.nhaarman.mockitokotlin2.whenever
+import com.squareup.picasso.picassoSingleton
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
+import dagger.hilt.android.testing.UninstallModules
+import java.util.Locale
+import javax.inject.Inject
+import org.ccci.gto.android.common.androidx.lifecycle.ImmutableLiveData
+import org.ccci.gto.android.common.db.Query
+import org.cru.godtools.analytics.AnalyticsModule
+import org.cru.godtools.api.ApiModule
+import org.cru.godtools.base.tool.service.ManifestManager
+import org.cru.godtools.download.manager.DownloadManagerModule
+import org.cru.godtools.model.Language
+import org.cru.godtools.model.Translation
+import org.cru.godtools.sync.SyncModule
+import org.cru.godtools.sync.task.SyncTaskModule
+import org.cru.godtools.tract.Constants.PARAM_LIVE_SHARE_STREAM
+import org.cru.godtools.tract.R
+import org.cru.godtools.xml.model.Manifest
+import org.junit.After
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.keynote.godtools.android.db.GodToolsDao
+import org.mockito.Mockito.RETURNS_DEEP_STUBS
+import org.robolectric.annotation.Config
+
+@HiltAndroidTest
+@UninstallModules(
+    AnalyticsModule::class,
+    ApiModule::class,
+    DownloadManagerModule::class,
+    SyncModule::class,
+    SyncTaskModule::class
+)
+@RunWith(AndroidJUnit4::class)
+@Config(application = HiltTestApplication::class)
+class TractActivityTest {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val context: Context get() = getInstrumentation().context
+    @Inject
+    lateinit var dao: GodToolsDao
+    @Inject
+    lateinit var manifestManager: ManifestManager
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+        picassoSingleton = mock(defaultAnswer = RETURNS_DEEP_STUBS)
+        dao.stub {
+            on {
+                getLiveData(argThat<Query<Language>> { table.type == Language::class.java })
+            } doReturn ImmutableLiveData(emptyList())
+        }
+    }
+
+    @After
+    fun cleanup() {
+        picassoSingleton = null
+    }
+
+    // region Share Menu Tests
+    // region Visibility
+    @Test
+    fun verifyShareMenuVisible() {
+        whenGetTranslation().thenReturn(ImmutableLiveData(Translation()))
+        whenGetManifest().thenReturn(ImmutableLiveData(Manifest()))
+
+        ActivityScenario.launch<TractActivity>(context.createTractActivityIntent("test", Locale.ENGLISH)).use {
+            it.moveToState(Lifecycle.State.RESUMED)
+            it.onActivity {
+                with(it.findViewById<Toolbar>(R.id.appbar)!!.menu!!.findItem(R.id.action_share)!!) {
+                    assertTrue(isVisible)
+                    assertTrue(isEnabled)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun verifyShareMenuVisibleForDeepLink() {
+        whenGetTranslation().thenReturn(ImmutableLiveData(Translation()))
+        whenGetManifest().thenReturn(ImmutableLiveData(Manifest()))
+
+        val intent = Intent(context, TractActivity::class.java).apply {
+            action = Intent.ACTION_VIEW
+            data = Uri.parse("https://knowgod.com/en/kgp?primaryLanguage=en")
+        }
+        ActivityScenario.launch<TractActivity>(intent).use {
+            it.moveToState(Lifecycle.State.RESUMED)
+            it.onActivity {
+                with(it.findViewById<Toolbar>(R.id.appbar)!!.menu!!.findItem(R.id.action_share)!!) {
+                    assertTrue(isVisible)
+                    assertTrue(isEnabled)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun verifyShareMenuHiddenWhenNoManifest() {
+        whenGetTranslation().thenReturn(ImmutableLiveData(Translation()))
+        whenGetManifest().thenReturn(ImmutableLiveData(null))
+
+        ActivityScenario.launch<TractActivity>(context.createTractActivityIntent("test", Locale.ENGLISH)).use {
+            it.moveToState(Lifecycle.State.RESUMED)
+            it.onActivity {
+                with(it.findViewById<Toolbar>(R.id.appbar)!!.menu!!.findItem(R.id.action_share)!!) {
+                    assertFalse(isVisible)
+                    assertFalse(isEnabled)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun verifyShareMenuHiddenWhenShowingTips() {
+        whenGetTranslation().thenReturn(ImmutableLiveData(Translation()))
+        whenGetManifest().thenReturn(ImmutableLiveData(Manifest()))
+
+        val intent = context.createTractActivityIntent("test", Locale.ENGLISH, showTips = true)
+        ActivityScenario.launch<TractActivity>(intent).use {
+            it.moveToState(Lifecycle.State.RESUMED)
+            it.onActivity {
+                with(it.findViewById<Toolbar>(R.id.appbar)!!.menu!!.findItem(R.id.action_share)!!) {
+                    assertFalse(isVisible)
+                    assertFalse(isEnabled)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun verifyShareMenuHiddenWhenLiveShareSubscriber() {
+        whenGetTranslation().thenReturn(ImmutableLiveData(Translation()))
+        whenGetManifest().thenReturn(ImmutableLiveData(Manifest()))
+
+        val intent = Intent(context, TractActivity::class.java).apply {
+            action = Intent.ACTION_VIEW
+            data = Uri.parse("https://knowgod.com/en/kgp?primaryLanguage=en&$PARAM_LIVE_SHARE_STREAM=asdf")
+        }
+        ActivityScenario.launch<TractActivity>(intent).use {
+            it.moveToState(Lifecycle.State.RESUMED)
+            it.onActivity {
+                with(it.findViewById<Toolbar>(R.id.appbar)!!.menu!!.findItem(R.id.action_share)!!) {
+                    assertFalse(isVisible)
+                    assertFalse(isEnabled)
+                }
+            }
+        }
+    }
+    // endregion Visibility
+    // endregion Share Menu Tests
+
+    private fun whenGetTranslation(tool: String? = any(), locale: Locale? = any()) =
+        whenever(dao.getLatestTranslationLiveData(tool, locale, any(), any(), any()))
+    private fun whenGetManifest(tool: String = any(), locale: Locale = any()) =
+        whenever(manifestManager.getLatestPublishedManifestLiveData(tool, locale))
+}
