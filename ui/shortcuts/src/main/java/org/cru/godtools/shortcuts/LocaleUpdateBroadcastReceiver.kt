5c88b781b546aa0ad8bbbeeedc0257207336fcef
Daniel Frett <daniel.frett@ccci.org>
2020-09-18 10:44:48 -0400

    remove unnecessary HiltBroadcastReceiver class

--- ui/shortcuts/src/main/java/org/cru/godtools/shortcuts/LocaleUpdateBroadcastReceiver.kt
+++ ui/shortcuts/src/main/java/org/cru/godtools/shortcuts/LocaleUpdateBroadcastReceiver.kt
@@ -1,20 +1,18 @@
 package org.cru.godtools.shortcuts
 
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
-import org.cru.godtools.base.dagger.HiltBroadcastReceiver
 
 @AndroidEntryPoint
-class LocaleUpdateBroadcastReceiver : HiltBroadcastReceiver() {
+class LocaleUpdateBroadcastReceiver : BroadcastReceiver() {
     @Inject
     internal lateinit var shortcutManager: GodToolsShortcutManager
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action != Intent.ACTION_LOCALE_CHANGED) return
-
-        super.onReceive(context, intent)
         shortcutManager.onUpdateSystemLocale(goAsync())
     }
 }
