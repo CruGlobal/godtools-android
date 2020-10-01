a915004a93cb279fda7d75941dd1a719c87d8995
Daniel Frett <daniel.frett@ccci.org>
2020-10-01 10:28:10 -0400

    create a TractAppsFlyerDeepLinkResolver

--- ui/tract-renderer/src/main/java/org/cru/godtools/tract/TractRendererModule.kt
+++ ui/tract-renderer/src/main/java/org/cru/godtools/tract/TractRendererModule.kt
@@ -7,6 +7,8 @@ import dagger.Reusable
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.IntoSet
+import org.cru.godtools.analytics.appsflyer.AppsFlyerDeepLinkResolver
+import org.cru.godtools.tract.analytics.appsflyer.TractAppsFlyerDeepLinkResolver
 import org.greenrobot.eventbus.meta.SubscriberInfoIndex
 
 @Module
@@ -17,4 +19,9 @@ object TractRendererModule {
     @Provides
     @Reusable
     fun tractEventBusIndex(): SubscriberInfoIndex = TractEventBusIndex()
+
+    @IntoSet
+    @Provides
+    @Reusable
+    fun tractAppsFlyerDeepLinkResolver(): AppsFlyerDeepLinkResolver = TractAppsFlyerDeepLinkResolver
 }
