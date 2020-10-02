fe2d834f03341fee81b595001831d7490f1b91a2
Daniel Frett <daniel.frett@ccci.org>
2020-10-02 09:57:00 -0400

    add an ACTIVITY_CLASS_TRACT constant

--- ui/base-tool/src/main/java/org/cru/godtools/base/tool/Activities.kt
+++ ui/base-tool/src/main/java/org/cru/godtools/base/tool/Activities.kt
@@ -10,13 +10,15 @@ import org.cru.godtools.base.EXTRA_LANGUAGES
 import org.cru.godtools.base.EXTRA_TOOL
 
 // region TractActivity
+private const val ACTIVITY_CLASS_TRACT = "org.cru.godtools.tract.activity.TractActivity"
+
 const val EXTRA_SHOW_TIPS = "org.cru.godtools.tract.activity.TractActivity.SHOW_TIPS"
 
 fun Activity.startTractActivity(toolCode: String, vararg languages: Locale?, showTips: Boolean) =
     startActivity(createTractActivityIntent(toolCode, *languages, showTips = showTips))
 
 fun Context.createTractActivityIntent(toolCode: String, vararg languages: Locale?, showTips: Boolean = false) =
-    Intent().setClassName(this, "org.cru.godtools.tract.activity.TractActivity")
+    Intent().setClassName(this, ACTIVITY_CLASS_TRACT)
         .putExtra(EXTRA_TOOL, toolCode)
         .putLanguagesExtra(*languages)
         .putExtra(EXTRA_SHOW_TIPS, showTips)
