6e0864e6f635997fcb991ec5a833c85dde1f1887
Daniel Frett <daniel.frett@ccci.org>
2020-10-02 13:01:22 -0400

    update the list of languages that support onboarding and the tutorial

--- ui/tutorial-renderer/src/test/java/org/cru/godtools/tutorial/PageSetTest.kt
+++ ui/tutorial-renderer/src/test/java/org/cru/godtools/tutorial/PageSetTest.kt
@@ -21,12 +21,16 @@ class PageSetTest {
         assertTrue(PageSet.ONBOARDING.supportsLocale(Locale.forLanguageTag("es-419")))
         assertTrue(PageSet.ONBOARDING.supportsLocale(Locale.forLanguageTag("es-ES")))
         assertTrue(PageSet.ONBOARDING.supportsLocale(Locale.forLanguageTag("es-MX")))
+        assertTrue(PageSet.ONBOARDING.supportsLocale(Locale.forLanguageTag("fr-CA")))
+        assertTrue(PageSet.ONBOARDING.supportsLocale(Locale.forLanguageTag("id")))
+        assertTrue(PageSet.ONBOARDING.supportsLocale(Locale.forLanguageTag("in")))
+        assertTrue(PageSet.ONBOARDING.supportsLocale(Locale.forLanguageTag("ru-MD")))
         assertTrue(PageSet.ONBOARDING.supportsLocale(Locale.forLanguageTag("zh-Hans-CN")))
     }
 
     @Test
     fun testOnboardingUnsupportedLanguages() {
-        assertFalse(PageSet.ONBOARDING.supportsLocale(Locale.forLanguageTag("fr")))
+        assertFalse(PageSet.ONBOARDING.supportsLocale(Locale.forLanguageTag("de")))
 
         assumeThat(Build.VERSION.SDK_INT, greaterThanOrEqualTo(Build.VERSION_CODES.LOLLIPOP))
         assertFalse(PageSet.ONBOARDING.supportsLocale(Locale.forLanguageTag("zh-TW")))
@@ -41,12 +45,16 @@ class PageSetTest {
         assertTrue(PageSet.TRAINING.supportsLocale(Locale.forLanguageTag("es-419")))
         assertTrue(PageSet.TRAINING.supportsLocale(Locale.forLanguageTag("es-ES")))
         assertTrue(PageSet.TRAINING.supportsLocale(Locale.forLanguageTag("es-MX")))
+        assertTrue(PageSet.TRAINING.supportsLocale(Locale.forLanguageTag("fr-CA")))
+        assertTrue(PageSet.TRAINING.supportsLocale(Locale.forLanguageTag("id")))
+        assertTrue(PageSet.TRAINING.supportsLocale(Locale.forLanguageTag("in")))
+        assertTrue(PageSet.TRAINING.supportsLocale(Locale.forLanguageTag("ru-MD")))
         assertTrue(PageSet.TRAINING.supportsLocale(Locale.forLanguageTag("zh-Hans-CN")))
     }
 
     @Test
     fun testTrainingUnsupportedLanguages() {
-        assertFalse(PageSet.TRAINING.supportsLocale(Locale.forLanguageTag("fr")))
+        assertFalse(PageSet.TRAINING.supportsLocale(Locale.forLanguageTag("de")))
 
         assumeThat(Build.VERSION.SDK_INT, greaterThanOrEqualTo(Build.VERSION_CODES.LOLLIPOP))
         assertFalse(PageSet.TRAINING.supportsLocale(Locale.forLanguageTag("zh-TW")))
