24c67443b9ea90bdd8364a095d42f08400dbc973
Daniel Frett <daniel.frett@ccci.org>
2020-10-19 10:14:54 -0400

    remove unnecessary eventbus index

--- library/download-manager/src/main/java/org/cru/godtools/download/manager/DownloadManagerModule.kt
+++ library/download-manager/src/main/java/org/cru/godtools/download/manager/DownloadManagerModule.kt
@@ -2,13 +2,10 @@ package org.cru.godtools.download.manager
 
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
-import dagger.Reusable
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.IntoSet
 import org.ccci.gto.android.common.dagger.eager.EagerSingleton
-import org.greenrobot.eventbus.meta.SubscriberInfoIndex
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -17,11 +14,4 @@ abstract class DownloadManagerModule {
     @IntoSet
     @EagerSingleton(on = EagerSingleton.LifecycleEvent.ACTIVITY_CREATED, threadMode = EagerSingleton.ThreadMode.ASYNC)
     abstract fun downloadManagerEagerSingleton(downloadManager: GodToolsDownloadManager): Any
-
-    companion object {
-        @IntoSet
-        @Provides
-        @Reusable
-        fun downloadManagerEventBusIndex(): SubscriberInfoIndex = DownloadManagerEventBusIndex()
-    }
 }
