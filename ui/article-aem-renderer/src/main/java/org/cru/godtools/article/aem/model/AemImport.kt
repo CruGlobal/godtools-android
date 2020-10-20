bbf95f53dc455acecfea3031551eafa39fbf22ea
Daniel Frett <daniel.frett@ccci.org>
2020-10-20 10:42:44 -0400

    cleanup annotation formatting for AemImport

--- ui/article-aem-renderer/src/main/java/org/cru/godtools/article/aem/model/AemImport.kt
+++ ui/article-aem-renderer/src/main/java/org/cru/godtools/article/aem/model/AemImport.kt
@@ -19,16 +19,23 @@ class AemImport(@field:PrimaryKey val uri: Uri) {
         return lastProcessed.before(Date(System.currentTimeMillis() - STALE_AGE))
     }
 
-    @Entity(tableName = "aemImportArticles",
-            primaryKeys = ["aemImportUri", "articleUri"],
-            indices = [Index("articleUri")],
-            foreignKeys = [
-                ForeignKey(entity = AemImport::class,
-                        onUpdate = ForeignKey.RESTRICT, onDelete = ForeignKey.CASCADE,
-                        parentColumns = ["uri"], childColumns = ["aemImportUri"]),
-                ForeignKey(entity = Article::class,
-                        onUpdate = ForeignKey.RESTRICT, onDelete = ForeignKey.CASCADE,
-                        parentColumns = ["uri"], childColumns = ["articleUri"])])
+    @Entity(
+        tableName = "aemImportArticles",
+        primaryKeys = ["aemImportUri", "articleUri"],
+        indices = [Index("articleUri")],
+        foreignKeys = [
+            ForeignKey(
+                entity = AemImport::class,
+                onUpdate = ForeignKey.RESTRICT, onDelete = ForeignKey.CASCADE,
+                parentColumns = ["uri"], childColumns = ["aemImportUri"]
+            ),
+            ForeignKey(
+                entity = Article::class,
+                onUpdate = ForeignKey.RESTRICT, onDelete = ForeignKey.CASCADE,
+                parentColumns = ["uri"], childColumns = ["articleUri"]
+            )
+        ]
+    )
     class AemImportArticle(val aemImportUri: Uri, val articleUri: Uri) {
         constructor(aemImport: AemImport, article: Article) : this(aemImport.uri, article.uri)
     }
