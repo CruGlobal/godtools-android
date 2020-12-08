baa2aaac9cafedf54ecc410c598a3049049a5d02
Daniel Frett <daniel.frett@ccci.org>
2020-12-08 16:25:21 -0500

    remove unused createResorucesDir method

--- library/base/src/main/java/org/cru/godtools/base/util/FileUtils.kt
+++ library/base/src/main/java/org/cru/godtools/base/util/FileUtils.kt
@@ -3,12 +3,8 @@
 package org.cru.godtools.base.util
 
 import android.content.Context
-import androidx.annotation.WorkerThread
 import java.io.File
 
 // TODO: Context.filesDir accesses storage and is blocking.
 val Context.godToolsResourcesDir get() = File(filesDir, "resources")
 fun Context.getGodToolsFile(name: String?) = name?.let { File(godToolsResourcesDir, name) }
-
-@WorkerThread
-fun Context.createGodToolsResourcesDir() = godToolsResourcesDir.run { (exists() || mkdirs()) && isDirectory }
