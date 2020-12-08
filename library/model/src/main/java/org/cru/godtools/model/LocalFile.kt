e94ea5adc167f18aaf67f431b888ccba97446df0
Daniel Frett <daniel.frett@ccci.org>
2020-12-08 17:11:17 -0500

    remove unused getFile() method from LocalFile

--- library/model/src/main/java/org/cru/godtools/model/LocalFile.kt
+++ library/model/src/main/java/org/cru/godtools/model/LocalFile.kt
@@ -1,10 +1,7 @@
 package org.cru.godtools.model
 
-import android.content.Context
 import org.cru.godtools.base.FileManager
-import org.cru.godtools.base.util.getGodToolsFile
 
 data class LocalFile(val filename: String) {
-    fun getFile(context: Context) = context.getGodToolsFile(filename)
     fun getFile(fileManager: FileManager) = fileManager.getFile(filename)
 }
