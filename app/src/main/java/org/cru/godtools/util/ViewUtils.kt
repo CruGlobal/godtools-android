9f04c53333780922d00074cdef52ee78795971ef
Daniel Frett <daniel.frett@ccci.org>
2020-12-09 10:21:13 -0500

    get rid of deprecated FileUtils in favor of using FileManager

--- app/src/main/java/org/cru/godtools/util/ViewUtils.kt
+++ app/src/main/java/org/cru/godtools/util/ViewUtils.kt
@@ -1,10 +1,11 @@
 package org.cru.godtools.util
 
 import org.ccci.gto.android.common.picasso.view.PicassoImageView
-import org.cru.godtools.base.util.getGodToolsFile
+import org.cru.godtools.base.fileManager
 import org.cru.godtools.model.Attachment
 
 fun PicassoImageView?.bindLocalImage(attachment: Attachment?) =
     bindLocalImage(attachment?.takeIf { it.isDownloaded }?.localFilename)
 
-fun PicassoImageView?.bindLocalImage(filename: String?) = this?.setPicassoFile(context.getGodToolsFile(filename))
+fun PicassoImageView?.bindLocalImage(filename: String?) =
+    this?.setPicassoFile(filename?.let { context.fileManager.getFileBlocking(it) })
