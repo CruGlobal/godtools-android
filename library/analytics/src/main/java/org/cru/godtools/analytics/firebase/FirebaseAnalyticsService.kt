ca4928f7d75d6bb1a8fb0d7528f0eb85c469702d
TheNoodleMoose <cjh605@gmail.com>
2020-12-14 13:05:49 -0500

    Rework adding additional user properties

--- library/analytics/src/main/java/org/cru/godtools/analytics/firebase/FirebaseAnalyticsService.kt
+++ library/analytics/src/main/java/org/cru/godtools/analytics/firebase/FirebaseAnalyticsService.kt
@@ -4,25 +4,15 @@ import android.app.Application
 import android.os.Bundle
 import androidx.annotation.MainThread
 import androidx.annotation.VisibleForTesting
-import androidx.annotation.WorkerThread
-import com.adobe.mobile.Visitor
-import com.adobe.mobile.VisitorID
 import com.google.android.gms.common.wrappers.InstantApps
 import com.google.firebase.analytics.FirebaseAnalytics
-import com.okta.oidc.net.response.UserInfo
 import java.util.Locale
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.stateIn
 import org.ccci.gto.android.common.compat.util.LocaleCompat
 import org.ccci.gto.android.common.okta.oidc.OktaUserProfileProvider
 import org.ccci.gto.android.common.okta.oidc.net.response.grMasterPersonId
@@ -48,11 +38,10 @@ private const val USER_PROP_SSO_GUID = "cru_ssoguid"
 private const val USER_PROP_GR_MASTER_PERSON_ID = "cru_grmasterpersonid"
 private const val USER_PROP_CONTENT_LANGUAGE = "cru_contentlanguage"
 
-private const val VISITOR_ID_GUID = "ssoguid"
-private const val VISITOR_ID_MASTER_PERSON_ID = "grmpid"
+private const val SITE_SECTION = "cru_sitesection"
+private const val SITE_SUB_SECTION = "cru_sitesubsection"
 
 @Singleton
-@OptIn(ExperimentalStdlibApi::class)
 class FirebaseAnalyticsService @VisibleForTesting internal constructor(
     app: Application,
     eventBus: EventBus,
@@ -67,10 +56,6 @@ class FirebaseAnalyticsService @VisibleForTesting internal constructor(
         eventBus: EventBus,
         oktaUserProfileProvider: OktaUserProfileProvider
     ) : this(app, eventBus, oktaUserProfileProvider, FirebaseAnalytics.getInstance(app))
-
-    private val coroutineScope = CoroutineScope(Dispatchers.Default)
-
-    private val analyticsExecutor: Executor = Executors.newSingleThreadExecutor()
 
     // region Tracking Events
     init {
@@ -90,10 +75,10 @@ class FirebaseAnalyticsService @VisibleForTesting internal constructor(
     private fun handleScreenEvent(event: AnalyticsScreenEvent) {
         val bundle = Bundle().apply {
             putString(FirebaseAnalytics.Param.SCREEN_NAME, event.screen)
-            putString("cru_appname", VALUE_GODTOOLS)
-            event.locale?.let { putString("cru_contentlanguage", LocaleCompat.toLanguageTag(it)) }
-            event.adobeSiteSection?.let { putString("cru_sitesection", event.adobeSiteSection) }
-            event.adobeSiteSubSection?.let { putString("cru_sitesubsection", event.adobeSiteSubSection) }
+            putString(USER_PROP_APP_NAME, VALUE_GODTOOLS)
+            event.locale?.let { putString(USER_PROP_CONTENT_LANGUAGE, LocaleCompat.toLanguageTag(it)) }
+            event.adobeSiteSection?.let { putString(SITE_SECTION, event.adobeSiteSection) }
+            event.adobeSiteSubSection?.let { putString(SITE_SUB_SECTION, event.adobeSiteSubSection) }
         }
         firebase.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW, bundle)
     }
@@ -110,52 +95,15 @@ class FirebaseAnalyticsService @VisibleForTesting internal constructor(
         firebase.logEvent(event.firebaseEventName, bundle)
     }
 
-    // region Visitor ids
-    @WorkerThread
-    private fun MutableMap<String, Any?>.baseContextData(userProfile: UserInfo?, event: AnalyticsBaseEvent? = null) {
-        put(USER_PROP_APP_NAME, VALUE_GODTOOLS)
-        put(USER_PROP_CONTENT_LANGUAGE, LocaleCompat.toLanguageTag(Locale.getDefault()))
-        put(USER_PROP_LOGGED_IN_STATUS, "false")
-        if (userProfile != null) {
-            put(USER_PROP_LOGGED_IN_STATUS, "true")
-            put(USER_PROP_SSO_GUID, userProfile.ssoGuid)
-            userProfile.grMasterPersonId?.let { put(USER_PROP_GR_MASTER_PERSON_ID, it) }
-        }
-    }
-
-    private val userProfileStateFlow = oktaUserProfileProvider.userInfoFlow(refreshIfStale = false)
-        .onEach { analyticsExecutor.execute { updateVisitorIdIdentifiers(it) } }
-        .stateIn(coroutineScope, SharingStarted.Eagerly, null)
-
-    @WorkerThread
-    private fun updateVisitorIdIdentifiers(userProfile: UserInfo?) {
-        Visitor.syncIdentifiers(
-            mutableMapOf(
-                VISITOR_ID_GUID to userProfile?.ssoGuid,
-                VISITOR_ID_MASTER_PERSON_ID to userProfile?.grMasterPersonId
-            ), when {
-            userProfile != null -> VisitorID.VisitorIDAuthenticationState.VISITOR_ID_AUTHENTICATION_STATE_AUTHENTICATED
-            else -> VisitorID.VisitorIDAuthenticationState.VISITOR_ID_AUTHENTICATION_STATE_LOGGED_OUT
-        }
-        )
-    }
-    // endregion Visitor ids
-
     init {
-        oktaUserProfileProvider.userInfoFlow()
-            .map { it?.ssoGuid }
-            .distinctUntilChanged()
+        oktaUserProfileProvider.userInfoFlow(refreshIfStale = false)
             .onEach { it ->
-                firebase.setUserId(it)
-                analyticsExecutor.execute {
-                    updateVisitorIdIdentifiers(userProfileStateFlow.value)
-                    val data = buildMap {
-                        baseContextData(userProfileStateFlow.value)
-                    }
-                    data.mapValues { userProperty ->
-                        firebase.setUserProperty(userProperty.key, userProperty.value.toString())
-                    }
-                }
+                firebase.setUserProperty(USER_PROP_LOGGED_IN_STATUS, "${it != null}")
+                firebase.setUserProperty(USER_PROP_APP_NAME, VALUE_GODTOOLS)
+                firebase.setUserProperty(USER_PROP_CONTENT_LANGUAGE, LocaleCompat.toLanguageTag(Locale.getDefault()))
+                firebase.setUserId(it?.ssoGuid)
+                firebase.setUserProperty(USER_PROP_GR_MASTER_PERSON_ID, it?.grMasterPersonId)
+                firebase.setUserProperty(USER_PROP_SSO_GUID, it?.ssoGuid)
             }
             .launchIn(coroutineScope)
 
