21cf1edb3cc68abece5fafcaccc2cb04b2e69858
Daniel Frett <daniel.frett@ccci.org>
2021-01-22 17:32:20 -0500

    update the Manifest parsing tests to test some additional aspects of parsing manifests

--- library/xml-model/src/test/java/org/cru/godtools/xml/model/ManifestTest.kt
+++ library/xml-model/src/test/java/org/cru/godtools/xml/model/ManifestTest.kt
@@ -4,14 +4,16 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import java.util.Locale
 import org.cru.godtools.xml.util.getXmlParserForResource
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ManifestTest {
     @Test
-    fun verifyParseEmptyManifest() {
+    fun testParseEmptyManifest() {
         val manifest = parseManifest("manifest_empty.xml")
+        assertNull(manifest.title)
         assertEquals(0, manifest.aemImports.size)
         assertEquals(0, manifest.tractPages.size)
         assertEquals(0, manifest.resources.size)
@@ -19,16 +21,19 @@ class ManifestTest {
     }
 
     @Test
-    fun verifyParseManifestTractPages() {
-        val manifest = parseManifest("manifest_pages.xml")
+    fun testParseManifestTract() {
+        val manifest = parseManifest("manifest_tract.xml")
+        assertEquals("title", manifest.title)
         assertEquals(Manifest.Type.TRACT, manifest.type)
         assertEquals(2, manifest.tractPages.size)
+        assertEquals("page0.xml", manifest.tractPages[0].fileName)
         assertEquals(0, manifest.tractPages[0].position)
+        assertEquals(null, manifest.tractPages[1].fileName)
         assertEquals(1, manifest.tractPages[1].position)
     }
 
     @Test
-    fun verifyParseManifestWithTips() {
+    fun testParseManifestWithTips() {
         val manifest = parseManifest("manifest_tips.xml")
         assertEquals(0, manifest.tractPages.size)
         assertEquals(0, manifest.resources.size)
@@ -37,7 +42,7 @@ class ManifestTest {
     }
 
     @Test
-    fun verifyParseManifestWithInvalidTips() {
+    fun testParseManifestWithInvalidTips() {
         val manifest = parseManifest("manifest_tips_invalid.xml")
         assertEquals(0, manifest.tractPages.size)
         assertEquals(0, manifest.resources.size)
