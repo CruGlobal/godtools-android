67b80ba0baa34ad0e078ccd1931a6ac966f3b8dd
Daniel Frett <daniel.frett@ccci.org>
2021-01-26 11:04:34 -0500

    add an additional createController test

--- ui/base-tool/src/test/java/org/cru/godtools/base/tool/ui/controller/UiControllerCacheTest.kt
+++ ui/base-tool/src/test/java/org/cru/godtools/base/tool/ui/controller/UiControllerCacheTest.kt
@@ -3,26 +3,51 @@ package org.cru.godtools.base.tool.ui.controller
 import android.content.pm.ApplicationInfo
 import android.view.ViewGroup
 import com.nhaarman.mockitokotlin2.anyVararg
+import com.nhaarman.mockitokotlin2.clearInvocations
+import com.nhaarman.mockitokotlin2.doAnswer
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import org.cru.godtools.xml.model.Image
 import org.cru.godtools.xml.model.Text
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNotSame
 import org.junit.Assert.assertNull
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.RETURNS_DEEP_STUBS
 import org.mockito.Mockito.any
+import org.mockito.Mockito.verifyNoInteractions
 import timber.log.Timber
 
 class UiControllerCacheTest {
     private lateinit var parent: ViewGroup
     private lateinit var parentController: BaseController<*>
+    private lateinit var imageFactory: BaseController.Factory<BaseController<Image>>
     private lateinit var cache: UiControllerCache
 
     @Before
     fun setup() {
         parent = mock(defaultAnswer = RETURNS_DEEP_STUBS)
         parentController = mock()
-        cache = UiControllerCache(parent, parentController, emptyMap())
+        imageFactory = mock { on { create(parent, parentController) } doAnswer { mock() } }
+        cache = UiControllerCache(parent, parentController, mapOf(Image::class.java to imageFactory))
+    }
+
+    // region createController()
+    @Test
+    fun testCreateController() {
+        val controller = cache.acquire(Image::class)
+        assertNotNull(controller)
+        verify(imageFactory).create(parent, parentController)
+        verifyNoMoreInteractions(imageFactory)
+        clearInvocations(imageFactory)
+
+        val controller2 = cache.acquire(Image::class)
+        assertNotNull(controller2)
+        assertNotSame(controller, controller2)
+        verify(imageFactory).create(parent, parentController)
+        verifyNoMoreInteractions(imageFactory)
     }
 
     @Test(expected = IllegalArgumentException::class)
@@ -39,9 +64,11 @@ class UiControllerCacheTest {
         try {
             val controller = cache.acquire(Text::class)
             assertNull(controller)
+            verifyNoInteractions(imageFactory)
             verify(tree).e(any<IllegalArgumentException>(), any(), anyVararg())
         } finally {
             Timber.uproot(tree)
         }
     }
+    // endregion createController()
 }
