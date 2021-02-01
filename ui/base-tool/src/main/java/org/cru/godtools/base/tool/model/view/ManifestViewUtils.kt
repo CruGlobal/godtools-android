1cb2f7dda074f0b8bd9b5cd24189bf84c35b6d3f
Daniel Frett <daniel.frett@ccci.org>
2021-02-01 14:10:15 -0500

    update the manifest background to be set via data binding

--- ui/base-tool/src/main/java/org/cru/godtools/base/tool/model/view/ManifestViewUtils.kt
+++ ui/base-tool/src/main/java/org/cru/godtools/base/tool/model/view/ManifestViewUtils.kt
@@ -1,16 +1,7 @@
 package org.cru.godtools.base.tool.model.view
 
 import android.content.Context
-import org.cru.godtools.base.tool.widget.ScaledPicassoImageView
 import org.cru.godtools.base.ui.util.getTypeface
 import org.cru.godtools.xml.model.Manifest
-import org.cru.godtools.xml.model.backgroundImageGravity
-import org.cru.godtools.xml.model.backgroundImageScaleType
 
 fun Manifest.getTypeface(context: Context) = context.getTypeface(locale)
-
-fun ScaledPicassoImageView.bindBackgroundImage(manifest: Manifest?) = bindBackgroundImage(
-    manifest?.backgroundImage,
-    manifest.backgroundImageScaleType,
-    manifest.backgroundImageGravity
-)
--- ui/tract-renderer/src/main/java/org/cru/godtools/tract/activity/TractActivity.kt
+++ ui/tract-renderer/src/main/java/org/cru/godtools/tract/activity/TractActivity.kt
@@ -39,7 +39,6 @@ import org.cru.godtools.base.URI_SHARE_BASE
 import org.cru.godtools.base.model.Event
 import org.cru.godtools.base.tool.EXTRA_SHOW_TIPS
 import org.cru.godtools.base.tool.activity.BaseToolActivity
-import org.cru.godtools.base.tool.model.view.bindBackgroundImage
 import org.cru.godtools.tract.PARAM_LIVE_SHARE_STREAM
 import org.cru.godtools.tract.PARAM_PARALLEL_LANGUAGE
 import org.cru.godtools.tract.PARAM_PRIMARY_LANGUAGE
@@ -286,6 +285,7 @@ class TractActivity :
     override val activeDownloadProgressLiveData get() = dataModel.downloadProgress
 
     private fun setupBinding() {
+        binding.manifest = dataModel.activeManifest
         binding.activeLocale = dataModel.activeLocale
         binding.visibleLocales = dataModel.visibleLocales
     }
@@ -297,10 +297,7 @@ class TractActivity :
     }
 
     private fun setupBackground() {
-        dataModel.activeManifest.observe(this) {
-            window.decorView.setBackgroundColor(it.backgroundColor)
-            binding.backgroundImage.bindBackgroundImage(it)
-        }
+        dataModel.activeManifest.observe(this) { window.decorView.setBackgroundColor(it.backgroundColor) }
     }
 
     // region Language Toggle
