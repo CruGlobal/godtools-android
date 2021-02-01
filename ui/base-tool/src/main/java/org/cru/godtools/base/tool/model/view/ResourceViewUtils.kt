bcea7a69b6591660c45433def8448e32fe249d23
Daniel Frett <daniel.frett@ccci.org>
2021-02-01 14:12:58 -0500

    don't update layout params when binding a background image
    this should be automatically handled by the matrix scale type

--- ui/base-tool/src/main/java/org/cru/godtools/base/tool/model/view/ResourceViewUtils.kt
+++ ui/base-tool/src/main/java/org/cru/godtools/base/tool/model/view/ResourceViewUtils.kt
@@ -1,9 +1,6 @@
 package org.cru.godtools.base.tool.model.view
 
-import android.view.Gravity
 import android.view.View
-import android.widget.FrameLayout
-import android.widget.RelativeLayout
 import jp.wasabeef.picasso.transformations.CropTransformation.GravityHorizontal
 import jp.wasabeef.picasso.transformations.CropTransformation.GravityVertical
 import org.ccci.gto.android.common.picasso.view.PicassoImageView
@@ -45,48 +42,4 @@ fun ScaledPicassoImageView.bindBackgroundImage(resource: Resource?, scale: Image
         }
     )
     toggleBatchUpdates(false)
-
-    // update layout params
-    view.layoutParams = view.layoutParams.apply {
-        when (this) {
-            is FrameLayout.LayoutParams -> {
-                val horizontal = when {
-                    gravity.isStart -> Gravity.START
-                    gravity.isEnd -> Gravity.END
-                    gravity.isCenterX -> Gravity.CENTER_HORIZONTAL
-                    else -> Gravity.NO_GRAVITY
-                }
-                val vertical = when {
-                    gravity.isTop -> Gravity.TOP
-                    gravity.isBottom -> Gravity.BOTTOM
-                    gravity.isCenterY -> Gravity.CENTER_VERTICAL
-                    else -> Gravity.NO_GRAVITY
-                }
-                this.gravity = horizontal.or(vertical)
-            }
-            is RelativeLayout.LayoutParams -> {
-                // set default layout for background image first
-                addRule(RelativeLayout.ALIGN_PARENT_START, 0)
-                addRule(RelativeLayout.ALIGN_PARENT_END, 0)
-                addRule(RelativeLayout.CENTER_HORIZONTAL, 0)
-                addRule(RelativeLayout.ALIGN_PARENT_TOP, 0)
-                addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 0)
-                addRule(RelativeLayout.CENTER_VERTICAL, 0)
-
-                // update gravity (X-Axis)
-                when {
-                    gravity.isStart -> addRule(RelativeLayout.ALIGN_PARENT_START)
-                    gravity.isEnd -> addRule(RelativeLayout.ALIGN_PARENT_END)
-                    gravity.isCenterX -> addRule(RelativeLayout.CENTER_HORIZONTAL)
-                }
-
-                // update gravity (Y-Axis)
-                when {
-                    gravity.isTop -> addRule(RelativeLayout.ALIGN_PARENT_TOP)
-                    gravity.isBottom -> addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
-                    gravity.isCenterY -> addRule(RelativeLayout.CENTER_VERTICAL)
-                }
-            }
-        }
-    }
 }
