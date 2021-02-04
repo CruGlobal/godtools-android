6719c32c7ddfafa28b49ddfa67ec807e97079acb
Daniel Frett <daniel.frett@ccci.org>
2021-02-04 17:35:21 -0500

    set the default text size for lesson content

--- library/xml-model/src/main/java/org/cru/godtools/xml/model/lesson/LessonPage.kt
+++ library/xml-model/src/main/java/org/cru/godtools/xml/model/lesson/LessonPage.kt
@@ -3,23 +3,27 @@ package org.cru.godtools.xml.model.lesson
 import androidx.annotation.VisibleForTesting
 import androidx.annotation.WorkerThread
 import org.ccci.gto.android.common.util.xmlpull.skipTag
+import org.cru.godtools.xml.R
 import org.cru.godtools.xml.XMLNS_LESSON
 import org.cru.godtools.xml.model.BaseModel
 import org.cru.godtools.xml.model.Content
 import org.cru.godtools.xml.model.Manifest
 import org.cru.godtools.xml.model.Parent
+import org.cru.godtools.xml.model.Styles
 import org.cru.godtools.xml.model.parseContent
 import org.xmlpull.v1.XmlPullParser
 
 private const val XML_PAGE = "page"
 private const val XML_CONTENT = "content"
 
-class LessonPage : BaseModel, Parent {
+class LessonPage : BaseModel, Parent, Styles {
     val id get() = fileName ?: "${manifest.code}-$position"
     val position: Int
 
     @VisibleForTesting
     internal val fileName: String?
+
+    override val textSize get() = R.dimen.text_size_lesson
 
     override val content: List<Content>
 
