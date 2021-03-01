2490fab9c7a94174397e31e588b3d49027ad0316
Daniel Frett <daniel.frett@ccci.org>
2021-03-01 15:27:04 -0500

    configure the animator on initialization

--- ui/base-tool/src/main/java/org/cru/godtools/base/tool/databinding/AccordionSectionVisibilityAdapters.kt
+++ ui/base-tool/src/main/java/org/cru/godtools/base/tool/databinding/AccordionSectionVisibilityAdapters.kt
@@ -10,7 +10,10 @@ private const val VISIBLE_IF = "accordionSectionVisibleIf"
 
 @BindingAdapter(VISIBLE_IF)
 fun View.animatedVisibleIf(visible: Boolean) {
-    val animator = HeightAndAlphaVisibilityAnimator.of(this, interpolator = FastOutSlowInInterpolator()) { visibleIf(visible) }
+    val animator = HeightAndAlphaVisibilityAnimator.of(this, isVisible = visible) {
+        interpolator = FastOutSlowInInterpolator()
+        visibleIf(visible)
+    }
     if (visible) {
         animator.show()
     } else {
