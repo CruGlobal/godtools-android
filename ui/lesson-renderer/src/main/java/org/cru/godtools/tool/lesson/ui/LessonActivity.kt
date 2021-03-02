3d713fe943397f1839f3d41850c083a73a2b73b4
Daniel Frett <daniel.frett@ccci.org>
2021-03-02 15:42:42 -0500

    update the lesson activity to listen for content events on pages

--- ui/lesson-renderer/src/main/java/org/cru/godtools/tool/lesson/ui/LessonActivity.kt
+++ ui/lesson-renderer/src/main/java/org/cru/godtools/tool/lesson/ui/LessonActivity.kt
@@ -1,11 +1,17 @@
 package org.cru.godtools.tool.lesson.ui
 
+import android.os.Bundle
+import androidx.annotation.MainThread
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+import org.ccci.gto.android.common.eventbus.lifecycle.register
+import org.cru.godtools.base.model.Event
 import org.cru.godtools.base.tool.activity.BaseSingleToolActivity
 import org.cru.godtools.tool.lesson.R
 import org.cru.godtools.tool.lesson.databinding.LessonActivityBinding
 import org.cru.godtools.xml.model.Manifest
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 @AndroidEntryPoint
 class LessonActivity : BaseSingleToolActivity<LessonActivityBinding>(
@@ -14,9 +20,20 @@ class LessonActivity : BaseSingleToolActivity<LessonActivityBinding>(
     supportedType = Manifest.Type.LESSON
 ) {
     // region Lifecycle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        eventBus.register(this, this)
+    }
+
     override fun onContentChanged() {
         super.onContentChanged()
         binding.setupPages()
+    }
+
+    @MainThread
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onContentEvent(event: Event) {
+        checkForPageEvent(event)
     }
     // endregion Lifecycle
 
@@ -38,6 +55,13 @@ class LessonActivity : BaseSingleToolActivity<LessonActivityBinding>(
 
     private fun LessonActivityBinding.setupPages() {
         pages.adapter = lessonPageAdapter
+    }
+
+    private fun checkForPageEvent(event: Event) {
+        val page = activeManifestLiveData.value?.lessonPages?.firstOrNull { it.listeners.contains(event.id) }
+        if (page != null) {
+            binding.pages.currentItem = page.position
+        }
     }
     // endregion Pages
     // endregion UI
