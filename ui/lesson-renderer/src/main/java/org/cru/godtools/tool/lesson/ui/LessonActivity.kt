a690605836a77f4a6df21e0a549d113b9ea8194e
Daniel Frett <daniel.frett@ccci.org>
2021-03-08 10:18:04 -0500

    update LessonActivity to hide hidden pages until an event triggers them

--- ui/lesson-renderer/src/main/java/org/cru/godtools/tool/lesson/ui/LessonActivity.kt
+++ ui/lesson-renderer/src/main/java/org/cru/godtools/tool/lesson/ui/LessonActivity.kt
@@ -1,17 +1,29 @@
 package org.cru.godtools.tool.lesson.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.annotation.MainThread
+import androidx.lifecycle.distinctUntilChanged
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.SCROLL_STATE_IDLE
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import org.ccci.gto.android.common.androidx.lifecycle.SetLiveData
+import org.ccci.gto.android.common.androidx.lifecycle.combineWith
+import org.ccci.gto.android.common.androidx.viewpager2.widget.whileMaintainingVisibleCurrentItem
 import org.ccci.gto.android.common.eventbus.lifecycle.register
 import org.cru.godtools.base.model.Event
 import org.cru.godtools.base.tool.activity.BaseSingleToolActivity
+import org.cru.godtools.base.tool.activity.BaseSingleToolActivityDataModel
+import org.cru.godtools.base.tool.service.ManifestManager
+import org.cru.godtools.download.manager.GodToolsDownloadManager
 import org.cru.godtools.tool.lesson.R
 import org.cru.godtools.tool.lesson.databinding.LessonActivityBinding
 import org.cru.godtools.xml.model.Manifest
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
+import org.keynote.godtools.android.db.GodToolsDao
 
 @AndroidEntryPoint
 class LessonActivity : BaseSingleToolActivity<LessonActivityBinding>(
@@ -19,6 +31,8 @@ class LessonActivity : BaseSingleToolActivity<LessonActivityBinding>(
     requireTool = true,
     supportedType = Manifest.Type.LESSON
 ) {
+    override val dataModel: LessonActivityDataModel by viewModels()
+
     // region Lifecycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,20 +63,45 @@ class LessonActivity : BaseSingleToolActivity<LessonActivityBinding>(
     // region Pages
     @Inject
     lateinit var lessonPageAdapterFactory: LessonPageAdapter.Factory
-    private val lessonPageAdapter by lazy {
-        lessonPageAdapterFactory.create(this).also { activeManifestLiveData.observe(this, it) }
-    }
+    private val lessonPageAdapter by lazy { lessonPageAdapterFactory.create(this) }
 
     private fun LessonActivityBinding.setupPages() {
         pages.adapter = lessonPageAdapter
+        dataModel.pages.observe(this@LessonActivity) { lessonPages ->
+            pages.whileMaintainingVisibleCurrentItem { lessonPageAdapter.pages = lessonPages }
+        }
+        pages.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrollStateChanged(state: Int) {
+                if (state == SCROLL_STATE_IDLE) {
+                    val currentItemId = dataModel.pages.value?.getOrNull(pages.currentItem)?.id
+                    dataModel.visiblePages.removeAll { it != currentItemId }
+                }
+            }
+        })
     }
 
     private fun checkForPageEvent(event: Event) {
-        val page = activeManifestLiveData.value?.lessonPages?.firstOrNull { it.listeners.contains(event.id) }
+        val page = dataModel.manifest.value?.lessonPages?.firstOrNull { it.listeners.contains(event.id) }
         if (page != null) {
-            binding.pages.currentItem = page.position
+            dataModel.visiblePages += page.id
+            dataModel.pages.value?.indexOfFirst { it.id == page.id }?.takeIf { it >= 0 }?.let {
+                binding.pages.currentItem = it
+            }
         }
     }
     // endregion Pages
     // endregion UI
+}
+
+@HiltViewModel
+class LessonActivityDataModel @Inject constructor(
+    manifestManager: ManifestManager,
+    dao: GodToolsDao,
+    downloadManager: GodToolsDownloadManager
+) : BaseSingleToolActivityDataModel(manifestManager, dao, downloadManager) {
+    val visiblePages = SetLiveData<String>(synchronous = true)
+
+    val pages = manifest.combineWith(visiblePages) { manifest, visible ->
+        manifest?.lessonPages.orEmpty().filter { !it.isHidden || it.id in visible }
+    }.distinctUntilChanged()
 }
