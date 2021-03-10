381fa51f0fad115809a99c7498ff1a253e2e9ede
Daniel Frett <daniel.frett@ccci.org>
2021-03-10 09:54:55 -0500

    these events only go to AppsFlyer, so there is no need to define a firebaseEventName for them

--- ui/base-tool/src/main/java/org/cru/godtools/base/tool/analytics/model/OpenToolsAnalyticsActionEvent.kt
+++ ui/base-tool/src/main/java/org/cru/godtools/base/tool/analytics/model/OpenToolsAnalyticsActionEvent.kt
@@ -3,10 +3,6 @@ package org.cru.godtools.base.tool.analytics.model
 import org.cru.godtools.analytics.model.AnalyticsActionEvent
 import org.cru.godtools.analytics.model.AnalyticsSystem
 
-object ToolOpenedAnalyticsActionEvent : AnalyticsActionEvent("tool-opened", system = AnalyticsSystem.APPSFLYER) {
-    override val firebaseEventName = "tool_opened"
-}
+object ToolOpenedAnalyticsActionEvent : AnalyticsActionEvent("tool-opened", system = AnalyticsSystem.APPSFLYER)
 object FirstToolOpenedAnalyticsActionEvent :
-    AnalyticsActionEvent("first-tool-opened", system = AnalyticsSystem.APPSFLYER) {
-    override val firebaseEventName = "first_tool_opened"
-}
+    AnalyticsActionEvent("first-tool-opened", system = AnalyticsSystem.APPSFLYER)
