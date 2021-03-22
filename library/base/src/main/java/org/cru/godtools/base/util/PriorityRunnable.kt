b11a75ce5e84a607ef8e7a4a45817dfb9b6bba02
Daniel Frett <daniel.frett@ccci.org>
2021-03-22 13:38:30 -0400

    utilize kotlin comparator utils to generate the PriorityRunnable Comparator

--- library/base/src/main/java/org/cru/godtools/base/util/PriorityRunnable.kt
+++ library/base/src/main/java/org/cru/godtools/base/util/PriorityRunnable.kt
@@ -1,7 +1,5 @@
 package org.cru.godtools.base.util
 
-import java.util.Comparator
-
 private const val PRIORITY_DEFAULT = 0
 
 interface PriorityRunnable : Runnable, Comparable<PriorityRunnable> {
@@ -10,10 +8,6 @@ interface PriorityRunnable : Runnable, Comparable<PriorityRunnable> {
 
     companion object {
         @JvmField
-        val COMPARATOR = Comparator { r1: Runnable?, r2: Runnable? ->
-            val p1 = (r1 as? PriorityRunnable)?.priority ?: PRIORITY_DEFAULT
-            val p2 = (r2 as? PriorityRunnable)?.priority ?: PRIORITY_DEFAULT
-            p1.compareTo(p2)
-        }
+        val COMPARATOR = compareBy<Runnable?> { (it as? PriorityRunnable)?.priority ?: PRIORITY_DEFAULT }
     }
 }
