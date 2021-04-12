4657856a974e6ed7eee7fc7ec6f8c46e43ffff7e
Daniel Frett <daniel.frett@ccci.org>
2021-04-12 13:57:18 -0400

    support parsing fixed height spacers

--- library/xml-model/src/main/java/org/cru/godtools/xml/model/Spacer.kt
+++ library/xml-model/src/main/java/org/cru/godtools/xml/model/Spacer.kt
@@ -4,14 +4,40 @@ import androidx.annotation.WorkerThread
 import org.ccci.gto.android.common.util.xmlpull.skipTag
 import org.xmlpull.v1.XmlPullParser
 
+private const val XML_HEIGHT = "height"
+private const val XML_MODE = "mode"
+private const val XML_MODE_AUTO = "auto"
+private const val XML_MODE_FIXED = "fixed"
+
 class Spacer : Content {
     companion object {
         internal const val XML_SPACER = "spacer"
     }
 
+    enum class Mode {
+        AUTO, FIXED;
+
+        companion object {
+            val DEFAULT = AUTO
+
+            internal fun parseOrNull(value: String?) = when (value) {
+                XML_MODE_AUTO -> AUTO
+                XML_MODE_FIXED -> FIXED
+                else -> null
+            }
+        }
+    }
+
+    val mode: Mode
+    val height: Int
+
     @WorkerThread
     constructor(parent: Base, parser: XmlPullParser) : super(parent, parser) {
         parser.require(XmlPullParser.START_TAG, XMLNS_CONTENT, XML_SPACER)
+
+        mode = Mode.parseOrNull(parser.getAttributeValue(null, XML_MODE)) ?: Mode.DEFAULT
+        height = parser.getAttributeValue(null, XML_HEIGHT)?.toIntOrNull()?.coerceAtLeast(0) ?: 0
+
         parser.skipTag()
     }
 }
--- library/xml-model/src/test/java/org/cru/godtools/xml/model/SpacerTest.kt
+++ library/xml-model/src/test/java/org/cru/godtools/xml/model/SpacerTest.kt
@@ -2,6 +2,7 @@ package org.cru.godtools.xml.model
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.cru.godtools.xml.util.getXmlParserForResource
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -9,8 +10,17 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class SpacerTest {
     @Test
-    fun testParseSpacer() {
+    fun `parseSpacer - Defaults`() {
         val spacer = Spacer(Manifest(), getXmlParserForResource("spacer.xml"))
         assertNotNull(spacer)
+        assertEquals(Spacer.Mode.AUTO, spacer.mode)
+        assertEquals(0, spacer.height)
+    }
+
+    @Test
+    fun `parseSpacer - Fixed Height`() {
+        val spacer = Spacer(Manifest(), getXmlParserForResource("spacer_fixed.xml"))
+        assertEquals(Spacer.Mode.FIXED, spacer.mode)
+        assertEquals(123, spacer.height)
     }
 }
