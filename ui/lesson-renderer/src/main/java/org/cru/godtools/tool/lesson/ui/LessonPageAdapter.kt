1a962fe533f9a9cb070c7bf9cb2f1ee6cd85e926
Daniel Frett <daniel.frett@ccci.org>
2021-04-21 10:11:29 -0400

    use a constrained lifecycle owner for lesson pages
    this prevents any non-visible pages from being resumed

--- ui/lesson-renderer/src/main/java/org/cru/godtools/tool/lesson/ui/LessonPageAdapter.kt
+++ ui/lesson-renderer/src/main/java/org/cru/godtools/tool/lesson/ui/LessonPageAdapter.kt
@@ -3,10 +3,14 @@ package org.cru.godtools.tool.lesson.ui
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.ObservableField
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.RecyclerView
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import org.ccci.gto.android.common.androidx.viewpager2.adapter.PrimaryItemChangeObserver
+import org.ccci.gto.android.common.androidx.viewpager2.adapter.onUpdatePrimaryItem
 import org.ccci.gto.android.common.recyclerview.adapter.SimpleDataBindingAdapter
 import org.ccci.gto.android.common.support.v4.util.IdUtils
 import org.cru.godtools.tool.lesson.databinding.LessonPageBinding
@@ -43,21 +47,44 @@ class LessonPageAdapter @AssistedInject internal constructor(
             notifyDataSetChanged()
         }
 
+    private var primaryItemObserver: PrimaryItemChangeObserver<*>? = null
+
     override fun getItemCount() = pages.size
     private fun getItem(position: Int) = pages[position]
     override fun getItemId(position: Int) = IdUtils.convertId(getItem(position).id)
 
     // region Lifecycle
-    override fun onCreateViewDataBinding(parent: ViewGroup, viewType: Int) =
-        LessonPageBinding.inflate(LayoutInflater.from(parent.context), parent, false).apply {
-            callbacks = _callbacks
-            bindController(controllerFactory)
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        primaryItemObserver = onUpdatePrimaryItem(recyclerView) { primary, previous ->
+            previous?.binding?.controller?.lifecycleOwner?.apply { maxState = minOf(maxState, Lifecycle.State.STARTED) }
+            primary?.binding?.controller?.lifecycleOwner?.apply { maxState = maxOf(maxState, Lifecycle.State.RESUMED) }
         }
+    }
+
+    override fun onCreateViewDataBinding(parent: ViewGroup, viewType: Int) =
+        LessonPageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+    override fun onViewDataBindingCreated(binding: LessonPageBinding, viewType: Int) {
+        binding.callbacks = _callbacks
+        binding.bindController(controllerFactory)
+    }
 
     override fun onBindViewDataBinding(binding: LessonPageBinding, position: Int) {
-        binding.controller?.model = getItem(position)
+        binding.controller?.apply {
+            model = getItem(position)
+            lifecycleOwner?.apply { maxState = maxOf(maxState, Lifecycle.State.STARTED) }
+        }
         binding.isFirstPage = position == 0
         binding.isLastPage = position == pages.size - 1
+    }
+
+    override fun onViewDataBindingRecycled(binding: LessonPageBinding) {
+        binding.controller?.lifecycleOwner?.apply { maxState = minOf(maxState, Lifecycle.State.CREATED) }
+    }
+
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        primaryItemObserver?.unregister()
+        primaryItemObserver = null
     }
     // endregion Lifecycle
 }
