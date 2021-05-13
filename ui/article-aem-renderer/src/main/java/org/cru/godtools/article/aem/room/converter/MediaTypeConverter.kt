5c299cf3fba0302a54dee20ebbdec43d6db3695c
Daniel Frett <daniel.frett@ccci.org>
2021-05-13 16:18:36 -0400

    Fix broken APIs

--- ui/article-aem-renderer/src/main/java/org/cru/godtools/article/aem/room/converter/MediaTypeConverter.kt
+++ ui/article-aem-renderer/src/main/java/org/cru/godtools/article/aem/room/converter/MediaTypeConverter.kt
@@ -2,11 +2,12 @@ package org.cru.godtools.article.aem.room.converter
 
 import androidx.room.TypeConverter
 import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 
 object MediaTypeConverter {
     @JvmStatic
     @TypeConverter
-    fun toMediaType(raw: String?) = raw?.let { MediaType.parse(it) }
+    fun toMediaType(raw: String?) = raw?.toMediaTypeOrNull()
 
     @JvmStatic
     @TypeConverter
