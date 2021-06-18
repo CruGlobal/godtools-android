a1ca14825b7bada5a8fdb40e7b1264cc5ff1c8dc
Daniel Frett <daniel.frett@ccci.org>
2021-06-18 09:00:35 -0400

    add some text style unit tests

--- ui/base-tool/src/test/java/org/cru/godtools/base/tool/databinding/TextViewAdaptersTest.kt
+++ ui/base-tool/src/test/java/org/cru/godtools/base/tool/databinding/TextViewAdaptersTest.kt
@@ -3,7 +3,9 @@ package org.cru.godtools.base.tool.databinding
 import android.app.Activity
 import android.graphics.Color
 import android.graphics.Paint.UNDERLINE_TEXT_FLAG
+import android.graphics.Typeface.BOLD
 import android.graphics.Typeface.BOLD_ITALIC
+import android.graphics.Typeface.ITALIC
 import android.graphics.Typeface.NORMAL
 import android.view.Gravity
 import android.widget.TextView
@@ -11,6 +13,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.cru.godtools.xml.R
 import org.cru.godtools.xml.model.Manifest
 import org.cru.godtools.xml.model.Text
+import org.cru.godtools.xml.model.defaultTextColor
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
@@ -31,7 +34,6 @@ class TextViewAdaptersTest {
     fun setup() {
         activityController = Robolectric.buildActivity(Activity::class.java)
         activity = activityController.get()
-
         view = TextView(activity)
     }
 
@@ -65,5 +67,40 @@ class TextViewAdaptersTest {
         assertEquals(1.5f * baseTextSize, view.textSize, 0.001f)
         assertEquals(NORMAL, view.typeface?.style ?: NORMAL)
         assertFalse(view.paintFlags.hasFlag(UNDERLINE_TEXT_FLAG))
+    }
+
+    @Test
+    fun verifyBindTextNodeTextColor() {
+        with(Text(Manifest())) {
+            view.bindTextNode(this, null, null)
+            assertEquals(defaultTextColor, view.textColors.defaultColor)
+        }
+
+        view.bindTextNode(Text(Manifest(), textColor = Color.GREEN), null, Color.RED)
+        assertEquals(Color.GREEN, view.textColors.defaultColor)
+
+        view.bindTextNode(null, null, Color.BLUE)
+        assertEquals(Color.BLUE, view.textColors.defaultColor)
+
+        view.bindTextNode(null, null, null)
+        assertEquals((null as Text?).defaultTextColor, view.textColors.defaultColor)
+    }
+
+    @Test
+    fun verifyBindTextNodeTextStyles() {
+        view.bindTextNode(Text(Manifest()), null, null)
+        assertEquals(NORMAL, view.typeface?.style ?: NORMAL)
+
+        view.bindTextNode(Text(Manifest(), textStyles = setOf(Text.Style.UNDERLINE)), null, null)
+        assertEquals(NORMAL, view.typeface?.style ?: NORMAL)
+
+        view.bindTextNode(Text(Manifest(), textStyles = setOf(Text.Style.BOLD)), null, null)
+        assertEquals(BOLD, view.typeface.style)
+
+        view.bindTextNode(Text(Manifest(), textStyles = setOf(Text.Style.ITALIC)), null, null)
+        assertEquals(ITALIC, view.typeface.style)
+
+        view.bindTextNode(Text(Manifest(), textStyles = setOf(Text.Style.BOLD, Text.Style.ITALIC)), null, null)
+        assertEquals(BOLD_ITALIC, view.typeface.style)
     }
 }
