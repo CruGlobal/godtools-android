7be711a9c4a560b697f8381e5163f2679118efac
Daniel Frett <daniel.frett@ccci.org>
2021-06-25 15:12:49 -0400

    consolidate ManifestParser caching into ManifestManager

--- ui/base-tool/src/main/java/org/cru/godtools/base/tool/BaseToolRendererModule.kt
+++ ui/base-tool/src/main/java/org/cru/godtools/base/tool/BaseToolRendererModule.kt
@@ -9,7 +9,11 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.IntoSet
 import javax.inject.Named
+import kotlinx.coroutines.runBlocking
 import org.ccci.gto.android.common.androidx.lifecycle.net.isConnectedLiveData
+import org.cru.godtools.base.ToolFileManager
+import org.cru.godtools.tool.service.ManifestParser
+import org.cru.godtools.tool.xml.AndroidXmlPullParserFactory
 import org.greenrobot.eventbus.meta.SubscriberInfoIndex
 
 @Module
@@ -21,6 +25,12 @@ object BaseToolRendererModule {
     @Reusable
     @Named(IS_CONNECTED_LIVE_DATA)
     fun isConnectedLiveData(@ApplicationContext context: Context) = context.isConnectedLiveData()
+
+    @Provides
+    @Reusable
+    fun manifestParser(fileManager: ToolFileManager) = ManifestParser(object : AndroidXmlPullParserFactory() {
+        override fun openFile(fileName: String) = runBlocking { fileManager.getInputStream(fileName).buffered() }
+    })
 
     @IntoSet
     @Provides
