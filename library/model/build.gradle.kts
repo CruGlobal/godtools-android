0b3527cad83db270a9e8eef2d4706750513fa16f
Daniel Frett <daniel.frett@ccci.org>
2021-07-20 11:23:47 -0400

    re-enable the NullSafeMutableLiveData lint check once lifecycle 2.4.0 is released

--- library/model/build.gradle.kts
+++ library/model/build.gradle.kts
@@ -1,9 +1,3 @@
-android {
-    // HACK: disable the NullSafeMutableLiveData check because it throws an error until lifecycle 2.4.0-alpha02
-    //       relevant bug: https://issuetracker.google.com/issues/183696616
-    lint.disable("NullSafeMutableLiveData")
-}
-
 dependencies {
     implementation(project(":library:base"))
 
