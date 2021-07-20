672a3305f8c749f5380340f12fa5cf7633ef0f04
Daniel Frett <daniel.frett@ccci.org>
2021-07-20 14:31:13 -0400

    convert the settings.gradle file to be a Kotlin build script

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+rootProject.name = "godtools"
+
+include("library:analytics")
+include("library:api")
+include("library:base")
+include("library:db")
+include("library:download-manager")
+include("library:initial-content")
+include("library:model")
+include("library:sync")
+
+include("ui:article-aem-renderer")
+include("ui:article-renderer")
+include("ui:base")
+include("ui:base-tool")
+include("ui:lesson-renderer")
+include("ui:shortcuts")
+include("ui:tract-renderer")
+include("ui:tutorial-renderer")
+
+include("app")
+
+include("feature:bundledcontent")
