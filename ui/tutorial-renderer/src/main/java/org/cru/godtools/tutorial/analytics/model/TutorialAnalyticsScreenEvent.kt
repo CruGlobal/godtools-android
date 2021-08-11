695379f7d99b320b0924ab0fad35efa02200611e
Daniel Frett <daniel.frett@ccci.org>
2021-08-11 12:49:40 -0400

    GT-1236 Send the first and last page of the onboarding tutorial to appsflyer

--- ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/analytics/model/TutorialAnalyticsScreenEvent.kt
+++ ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/analytics/model/TutorialAnalyticsScreenEvent.kt
@@ -2,10 +2,18 @@ package org.cru.godtools.tutorial.analytics.model
 
 import java.util.Locale
 import org.cru.godtools.analytics.model.AnalyticsScreenEvent
+import org.cru.godtools.analytics.model.AnalyticsSystem
+import org.cru.godtools.tutorial.Page
 import org.cru.godtools.tutorial.PageSet
 
-class TutorialAnalyticsScreenEvent(private val tutorial: PageSet, page: Int, locale: Locale?) :
+class TutorialAnalyticsScreenEvent(private val tutorial: PageSet, private val page: Int, locale: Locale?) :
     AnalyticsScreenEvent("${tutorial.analyticsBaseScreenName}-${page + 1}", locale) {
+    override fun isForSystem(system: AnalyticsSystem) = when (tutorial.pages[page]) {
+        Page.ONBOARDING_WELCOME,
+        Page.ONBOARDING_FINAL -> system == AnalyticsSystem.APPSFLYER || super.isForSystem(system)
+        else -> super.isForSystem(system)
+    }
+
     override val appSection get() = when (tutorial) {
         PageSet.ONBOARDING -> APP_SECTION_ONBOARDING
         PageSet.TRAINING -> APP_SECTION_TUTORIAL
