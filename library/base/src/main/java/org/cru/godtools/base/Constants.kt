d9daa3e03cced11af7206d4aed8e60bf1fd7135d
Daniel Frett <daniel.frett@ccci.org>
2021-08-13 11:47:10 -0400

    create a constant for the knowgod.com domain

--- library/base/src/main/java/org/cru/godtools/base/Constants.kt
+++ library/base/src/main/java/org/cru/godtools/base/Constants.kt
@@ -7,4 +7,6 @@ const val EXTRA_TOOL = "tool"
 const val EXTRA_LANGUAGE = "language"
 const val EXTRA_LANGUAGES = "languages"
 
-val URI_SHARE_BASE: Uri = Uri.parse("https://knowgod.com/")
+const val HOST_KNOWGOD_COM = "knowgod.com"
+
+val URI_SHARE_BASE: Uri = Uri.parse("https://$HOST_KNOWGOD_COM/")
--- ui/tract-renderer/src/test/java/org/cru/godtools/tract/util/DeepLinkUtilsTest.kt
+++ ui/tract-renderer/src/test/java/org/cru/godtools/tract/util/DeepLinkUtilsTest.kt
@@ -1,34 +1,23 @@
 package org.cru.godtools.tract.util
 
-import android.app.Activity
-import android.content.Context
 import android.net.Uri
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
 
 @RunWith(AndroidJUnit4::class)
 class DeepLinkUtilsTest {
-    lateinit var context: Context
-
-    @Before
-    fun setup() {
-        context = Robolectric.buildActivity(Activity::class.java).get()
-    }
-
     @Test
     fun verifyIsTractDeepLink() {
-        assertFalse(Uri.parse("wss://knowgod.com/en/kgp").isTractDeepLink(context))
-        assertFalse(Uri.parse("https://example.com/en/kgp").isTractDeepLink(context))
-        assertFalse(Uri.parse("https://knowgod.com/en").isTractDeepLink(context))
-        assertTrue(Uri.parse("http://knowgod.com/en/kgp").isTractDeepLink(context))
-        assertTrue(Uri.parse("https://knowgod.com/en/kgp").isTractDeepLink(context))
-        assertTrue(Uri.parse("https://www.knowgod.com/en/kgp").isTractDeepLink(context))
-        assertTrue(Uri.parse("https://knowgod.com/en/fourlaws").isTractDeepLink(context))
-        assertTrue(Uri.parse("https://knowgod.com/en/kgp/2").isTractDeepLink(context))
+        assertFalse(Uri.parse("wss://knowgod.com/en/kgp").isTractDeepLink())
+        assertFalse(Uri.parse("https://example.com/en/kgp").isTractDeepLink())
+        assertFalse(Uri.parse("https://knowgod.com/en").isTractDeepLink())
+        assertTrue(Uri.parse("http://knowgod.com/en/kgp").isTractDeepLink())
+        assertTrue(Uri.parse("https://knowgod.com/en/kgp").isTractDeepLink())
+        assertFalse(Uri.parse("https://www.knowgod.com/en/kgp").isTractDeepLink())
+        assertTrue(Uri.parse("https://knowgod.com/en/fourlaws").isTractDeepLink())
+        assertTrue(Uri.parse("https://knowgod.com/en/kgp/2").isTractDeepLink())
     }
 }
