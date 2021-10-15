99a4e8abe29481b14685999dbfce47d70804b6d2
Daniel Frett <daniel.frett@ccci.org>
2021-10-15 16:32:19 -0400

    move logic that loads languages, translations, and manifests to the base data model

--- ui/base-tool/src/main/java/org/cru/godtools/base/tool/activity/BaseMultiLanguageToolActivityDataModel.kt
+++ ui/base-tool/src/main/java/org/cru/godtools/base/tool/activity/BaseMultiLanguageToolActivityDataModel.kt
@@ -1,8 +1,82 @@
 package org.cru.godtools.base.tool.activity
 
+import androidx.annotation.VisibleForTesting
+import androidx.annotation.VisibleForTesting.PROTECTED
+import androidx.collection.LruCache
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.distinctUntilChanged
+import androidx.lifecycle.map
+import androidx.lifecycle.switchMap
+import dagger.hilt.android.lifecycle.HiltViewModel
+import java.util.Locale
+import javax.inject.Inject
+import org.ccci.gto.android.common.androidx.lifecycle.ImmutableLiveData
+import org.ccci.gto.android.common.androidx.lifecycle.combineWith
+import org.ccci.gto.android.common.androidx.lifecycle.emptyLiveData
+import org.ccci.gto.android.common.androidx.lifecycle.switchFold
+import org.ccci.gto.android.common.androidx.lifecycle.withInitialValue
+import org.ccci.gto.android.common.db.Expression
+import org.ccci.gto.android.common.db.Query
+import org.ccci.gto.android.common.db.getAsLiveData
+import org.cru.godtools.base.tool.service.ManifestManager
+import org.cru.godtools.model.Language
+import org.cru.godtools.model.Translation
+import org.cru.godtools.model.TranslationKey
+import org.cru.godtools.tool.model.Manifest
+import org.keynote.godtools.android.db.Contract.LanguageTable
+import org.keynote.godtools.android.db.GodToolsDao
 
-open class BaseMultiLanguageToolActivityDataModel : ViewModel() {
+@HiltViewModel
+open class BaseMultiLanguageToolActivityDataModel @Inject constructor(
+    dao: GodToolsDao,
+    manifestManager: ManifestManager,
+) : ViewModel() {
     val toolCode = MutableLiveData<String?>()
+    val primaryLocales = MutableLiveData<List<Locale>>(emptyList())
+    val parallelLocales = MutableLiveData<List<Locale>>(emptyList())
+
+    // region Resolved Data
+    val locales = primaryLocales.combineWith(parallelLocales) { primary, parallel -> primary + parallel }
+    protected val distinctToolCode = toolCode.distinctUntilChanged()
+    private val distinctLocales = locales.distinctUntilChanged()
+
+    val languages = distinctLocales.switchMap {
+        Query.select<Language>()
+            .where(LanguageTable.FIELD_CODE.`in`(*Expression.constants(*it.toTypedArray())))
+            .getAsLiveData(dao)
+    }.map { it.associateBy { it.code } }
+
+    @VisibleForTesting(otherwise = PROTECTED)
+    val translations =
+        distinctLocales.switchFold(ImmutableLiveData(emptyList<Pair<Locale, Translation?>>())) { acc, locale ->
+            distinctToolCode.switchMap { translationCache.get(it, locale).withInitialValue(null) }
+                .distinctUntilChanged()
+                .combineWith(acc.distinctUntilChanged()) { it, translations -> translations + Pair(locale, it) }
+        }.map { it.toMap() }
+
+    @VisibleForTesting(otherwise = PROTECTED)
+    val manifests =
+        distinctLocales.switchFold(ImmutableLiveData(emptyList<Pair<Locale, Manifest?>>())) { acc, locale ->
+            distinctToolCode.switchMap { manifestCache.get(it, locale).withInitialValue(null) }
+                .distinctUntilChanged()
+                .combineWith(acc.distinctUntilChanged()) { it, manifests -> manifests + Pair(locale, it) }
+        }.map { it.toMap() }
+
+    protected val translationCache = object : LruCache<TranslationKey, LiveData<Translation?>>(10) {
+        override fun create(key: TranslationKey) =
+            dao.getLatestTranslationLiveData(key.tool, key.locale, trackAccess = true).distinctUntilChanged()
+    }
+
+    protected val manifestCache = object : LruCache<TranslationKey, LiveData<Manifest?>>(10) {
+        override fun create(key: TranslationKey): LiveData<Manifest?> {
+            val tool = key.tool ?: return emptyLiveData()
+            val locale = key.locale ?: return emptyLiveData()
+            return manifestManager.getLatestPublishedManifestLiveData(tool, locale).distinctUntilChanged()
+        }
+    }
+    // endregion Resolved Data
 }
+
+private fun <T> LruCache<TranslationKey, T>.get(tool: String?, locale: Locale?) = get(TranslationKey(tool, locale))!!
--- ui/tract-renderer/src/main/java/org/cru/godtools/tract/activity/TractActivityDataModel.kt
+++ ui/tract-renderer/src/main/java/org/cru/godtools/tract/activity/TractActivityDataModel.kt
@@ -1,36 +1,26 @@
 package org.cru.godtools.tract.activity
 
-import androidx.annotation.VisibleForTesting
 import androidx.collection.LruCache
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.map
-import androidx.lifecycle.switchMap
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.util.Locale
 import javax.inject.Inject
 import javax.inject.Named
-import org.ccci.gto.android.common.androidx.lifecycle.ImmutableLiveData
 import org.ccci.gto.android.common.androidx.lifecycle.combineWith
 import org.ccci.gto.android.common.androidx.lifecycle.emptyLiveData
 import org.ccci.gto.android.common.androidx.lifecycle.switchCombineWith
-import org.ccci.gto.android.common.androidx.lifecycle.switchFold
 import org.ccci.gto.android.common.androidx.lifecycle.withInitialValue
-import org.ccci.gto.android.common.db.Expression
-import org.ccci.gto.android.common.db.Query
-import org.ccci.gto.android.common.db.getAsLiveData
 import org.cru.godtools.base.tool.BaseToolRendererModule.Companion.IS_CONNECTED_LIVE_DATA
 import org.cru.godtools.base.tool.activity.BaseMultiLanguageToolActivityDataModel
 import org.cru.godtools.base.tool.activity.BaseToolActivity.LoadingState
 import org.cru.godtools.base.tool.service.ManifestManager
 import org.cru.godtools.download.manager.GodToolsDownloadManager
-import org.cru.godtools.model.Language
-import org.cru.godtools.model.Translation
 import org.cru.godtools.model.TranslationKey
 import org.cru.godtools.tool.model.Manifest
-import org.keynote.godtools.android.db.Contract.LanguageTable
 import org.keynote.godtools.android.db.GodToolsDao
 
 private const val STATE_ACTIVE_LOCALE = "activeLocale"
@@ -38,14 +28,13 @@ private const val STATE_LIVE_SHARE_TUTORIAL_SHOWN = "liveShareTutorialShown"
 
 @HiltViewModel
 class TractActivityDataModel @Inject constructor(
-    private val dao: GodToolsDao,
-    private val downloadManager: GodToolsDownloadManager,
-    private val manifestManager: ManifestManager,
+    dao: GodToolsDao,
+    downloadManager: GodToolsDownloadManager,
+    manifestManager: ManifestManager,
     @Named(IS_CONNECTED_LIVE_DATA) private val isConnected: LiveData<Boolean>,
     private val savedState: SavedStateHandle
-) : BaseMultiLanguageToolActivityDataModel() {
+) : BaseMultiLanguageToolActivityDataModel(dao, manifestManager) {
     val isInitialSyncFinished = MutableLiveData(false)
-    private val distinctToolCode = toolCode.distinctUntilChanged()
 
     // region Active Tool
     val activeLocale = savedState.getLiveData<String?>(STATE_ACTIVE_LOCALE)
@@ -72,30 +61,6 @@ class TractActivityDataModel @Inject constructor(
     // endregion Active Tool
 
     // region Language Switcher
-    val primaryLocales = MutableLiveData<List<Locale>>(emptyList())
-    val parallelLocales = MutableLiveData<List<Locale>>(emptyList())
-    val locales = primaryLocales.combineWith(parallelLocales) { primary, parallel -> primary + parallel }
-    private val distinctLocales = locales.distinctUntilChanged()
-
-    val languages = distinctLocales.switchMap {
-        Query.select<Language>()
-            .where(LanguageTable.FIELD_CODE.`in`(*Expression.constants(*it.toTypedArray())))
-            .getAsLiveData(dao)
-    }.map { it.associateBy { it.code } }
-    @VisibleForTesting
-    internal val manifests =
-        distinctLocales.switchFold(ImmutableLiveData(emptyList<Pair<Locale, Manifest?>>())) { acc, locale ->
-            distinctToolCode.switchMap { manifestCache.get(it, locale).withInitialValue(null) }
-                .distinctUntilChanged()
-                .combineWith(acc.distinctUntilChanged()) { it, manifests -> manifests + Pair(locale, it) }
-        }.map { it.toMap() }
-    @VisibleForTesting
-    internal val translations =
-        distinctLocales.switchFold(ImmutableLiveData(emptyList<Pair<Locale, Translation?>>())) { acc, locale ->
-            distinctToolCode.switchMap { translationCache.get(it, locale).withInitialValue(null) }
-                .distinctUntilChanged()
-                .combineWith(acc.distinctUntilChanged()) { it, translations -> translations + Pair(locale, it) }
-        }.map { it.toMap() }
     val loadingState = locales
         .combineWith(manifests, translations, isConnected, isInitialSyncFinished) { l, m, t, connected, syncFinished ->
             l.associateWith {
@@ -146,18 +111,6 @@ class TractActivityDataModel @Inject constructor(
         set(value) {
             savedState[STATE_LIVE_SHARE_TUTORIAL_SHOWN] = value
         }
-
-    private val manifestCache = object : LruCache<TranslationKey, LiveData<Manifest?>>(10) {
-        override fun create(key: TranslationKey): LiveData<Manifest?> {
-            val tool = key.tool ?: return emptyLiveData()
-            val locale = key.locale ?: return emptyLiveData()
-            return manifestManager.getLatestPublishedManifestLiveData(tool, locale).distinctUntilChanged()
-        }
-    }
-    private val translationCache = object : LruCache<TranslationKey, LiveData<Translation?>>(10) {
-        override fun create(key: TranslationKey) =
-            dao.getLatestTranslationLiveData(key.tool, key.locale, trackAccess = true).distinctUntilChanged()
-    }
 }
 
 private fun <T> LruCache<TranslationKey, T>.get(tool: String?, locale: Locale?) = get(TranslationKey(tool, locale))!!
