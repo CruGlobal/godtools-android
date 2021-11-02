f7e097e0610aeec04be964f694fbb076fe51a0e9
Daniel Frett <daniel.frett@ccci.org>
2021-11-02 16:56:24 -0400

    remove unnecessary LayoutAdapters

--- ui/base/src/main/kotlin/org/cru/godtools/base/ui/databinding/LayoutAdapters.kt
+++
@@ -1,9 +0,0 @@
-package org.cru.godtools.base.ui.databinding
-
-import android.view.View
-import androidx.databinding.BindingAdapter
-
-@BindingAdapter("android:layout_height")
-internal fun View.setLayoutDimensions(h: Int) {
-    layoutParams = layoutParams.apply { height = h }
-}
