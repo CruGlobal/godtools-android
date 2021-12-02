96f5a8f3ff2e374f5fd91295e3f84b0c40faadda
Daniel Frett <daniel.frett@ccci.org>
2021-12-02 16:45:17 -0500

    support parsing the hidden flag from the API for tools

--- library/model/src/test/kotlin/org/cru/godtools/model/ToolTest.kt
+++ library/model/src/test/kotlin/org/cru/godtools/model/ToolTest.kt
@@ -5,20 +5,26 @@ import org.cru.godtools.model.jsonapi.ToolTypeConverter
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.hasSize
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class ToolTest {
-    @Test
-    fun testToolParsing() {
-        val raw = this::class.java.getResourceAsStream("tool.json")!!.reader().use { it.readText() }
-        val tool = JsonApiConverter.Builder()
+    // region jsonapi parsing
+    private val jsonApiConverter by lazy {
+        JsonApiConverter.Builder()
             .addClasses(Tool::class.java)
             .addClasses(Attachment::class.java, Translation::class.java)
             .addConverters(ToolTypeConverter)
             .build()
-            .fromJson(raw, Tool::class.java).dataSingle!!
+    }
+
+    @Test
+    fun testJsonApiParsing() {
+        val tool = parseJson("tool.json")
 
         assertEquals(1, tool.id)
+        assertFalse(tool.isHidden)
         assertEquals("kgp-us", tool.code)
         assertEquals("Know God Personally", tool.name)
         assertEquals("conversation_starter", tool.category)
@@ -31,6 +37,17 @@ class ToolTest {
         assertThat(tool.attachments, hasSize(3))
         assertThat(tool.latestTranslations, hasSize(2))
     }
+
+    @Test
+    fun `testJsonApiParsing - isHidden`() {
+        assertTrue(parseJson("tool_hidden_true.json").isHidden)
+        assertFalse(parseJson("tool_hidden_null.json").isHidden)
+        assertFalse(parseJson("tool_hidden_invalid.json").isHidden)
+    }
+
+    private fun parseJson(file: String) = this::class.java.getResourceAsStream(file)!!.reader()
+        .use { jsonApiConverter.fromJson(it.readText(), Tool::class.java).dataSingle!! }
+    // endregion jsonapi parsing
 
     @Test
     fun testTotalShares() {
