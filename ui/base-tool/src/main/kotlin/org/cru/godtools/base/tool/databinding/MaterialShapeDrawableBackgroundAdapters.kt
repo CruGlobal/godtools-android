b89455ce5a5667de58bfcf55c98b05e03d1910aa
Daniel Frett <daniel.frett@ccci.org>
2022-01-26 16:08:42 -0500

    use a MaterialShapeDrawable for flat option backgrounds

---
+++ ui/base-tool/src/main/kotlin/org/cru/godtools/base/tool/databinding/MaterialShapeDrawableBackgroundAdapters.kt
@@ -0,0 +1,20 @@
+package org.cru.godtools.base.tool.databinding
+
+import android.content.res.ColorStateList
+import android.view.View
+import androidx.annotation.ColorInt
+import androidx.databinding.BindingAdapter
+import com.google.android.material.shape.MaterialShapeDrawable
+
+@BindingAdapter("backgroundMaterialShapeColor")
+internal fun View.materialShapeBackgroundColor(@ColorInt color: Int?) {
+    backgroundMaterialShapeDrawable.fillColor = color?.let { ColorStateList.valueOf(it) }
+}
+
+@BindingAdapter("backgroundMaterialShapeCornerSize")
+internal fun View.materialShapeBackgroundCornerSize(size: Float) {
+    backgroundMaterialShapeDrawable.setCornerSize(size)
+}
+
+private val View.backgroundMaterialShapeDrawable
+    get() = background as? MaterialShapeDrawable ?: MaterialShapeDrawable().also { background = it }
