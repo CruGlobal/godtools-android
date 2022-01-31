e3dc0764942acbeadff72ffd539297470d4d61ea
Gyasi Story <gyasi.story@cru.org>
2022-01-31 14:36:59 -0500

    GT-1259 Create Spotlight and Category Fragment

---
+++ app/src/main/java/org/cru/godtools/ui/tools/ToolsCategoryFragment.kt
@@ -0,0 +1,7 @@
+package org.cru.godtools.ui.tools
+
+import org.cru.godtools.R
+import org.cru.godtools.base.ui.fragment.BaseFragment
+import org.cru.godtools.databinding.ToolsCategoryFragmentBinding
+
+class ToolsCategoryFragment: BaseFragment<ToolsCategoryFragmentBinding>(R.layout.tools_category_fragment)
---
+++ app/src/main/java/org/cru/godtools/ui/tools/ToolsSpotlightFragment.kt
@@ -0,0 +1,7 @@
+package org.cru.godtools.ui.tools
+
+import org.cru.godtools.R
+import org.cru.godtools.base.ui.fragment.BaseFragment
+import org.cru.godtools.databinding.ToolsSpotlightFragmentBinding
+
+class ToolsSpotlightFragment: BaseFragment<ToolsSpotlightFragmentBinding>(R.layout.tools_spotlight_fragment)
