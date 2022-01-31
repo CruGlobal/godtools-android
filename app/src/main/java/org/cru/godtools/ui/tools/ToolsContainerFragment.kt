af954d1a8ccf34d91f1e575fb462c34a38ab7def
Gyasi Story <gyasi.story@cru.org>
2022-01-31 14:25:36 -0500

    GT-1259 add DataModel logic to Fragment

--- app/src/main/java/org/cru/godtools/ui/tools/ToolsContainerFragment.kt
+++ app/src/main/java/org/cru/godtools/ui/tools/ToolsContainerFragment.kt
@@ -1,6 +1,7 @@
 package org.cru.godtools.ui.tools
 
 import android.os.Bundle
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import org.cru.godtools.R
 import org.cru.godtools.databinding.ToolsContainerFragmentBinding
@@ -9,8 +10,10 @@ import org.cru.godtools.fragment.BasePlatformFragment
 @AndroidEntryPoint
 class ToolsContainerFragment(): BasePlatformFragment<ToolsContainerFragmentBinding>(R.layout.tools_container_fragment) {
 
+    private val dataModel: ToolsContainerFragmentDataModel by viewModels()
     override fun onBindingCreated(binding: ToolsContainerFragmentBinding, savedInstanceState: Bundle?) {
         super.onBindingCreated(binding, savedInstanceState)
         binding.refresh.setupSwipeRefresh()
+        binding.hasSpotLight = dataModel.hasSpotlight
     }
 }
