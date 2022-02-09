f17df812da4a804bab61205b59c8394b36cb57b4
Gyasi Story <gyasistory@gmail.com>
2022-02-09 08:11:20 -0500

    GT-1259 remove where variable

--- app/src/main/java/org/cru/godtools/ui/dashboard/tools/ToolsCategoryDataModel.kt
+++ app/src/main/java/org/cru/godtools/ui/dashboard/tools/ToolsCategoryDataModel.kt
@@ -1,5 +1,6 @@
 package org.cru.godtools.ui.dashboard.tools
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,21 +11,21 @@ import org.ccci.gto.android.common.androidx.lifecycle.combineWith
 import org.ccci.gto.android.common.db.Expression.Companion.constants
 import org.ccci.gto.android.common.db.Query
 import org.ccci.gto.android.common.db.getAsLiveData
+import org.cru.godtools.base.ui.util.getCategory
 import org.cru.godtools.model.Tool
 import org.keynote.godtools.android.db.Contract
 import org.keynote.godtools.android.db.GodToolsDao
+import java.util.*
 
 @HiltViewModel
-class ToolsCategoryDataModel @Inject constructor(dao: GodToolsDao) : ViewModel() {
+class ToolsCategoryDataModel @Inject constructor(dao: GodToolsDao, context: Context) : ViewModel() {
 
     val selectedCategory = MutableLiveData<String?>(null)
 
-    private val where =
+    private val allTools = Query.select<Tool>().where(
         Contract.ToolTable.FIELD_TYPE.`in`(*constants(Tool.Type.TRACT, Tool.Type.ARTICLE, Tool.Type.CYOA))
             .and(Contract.ToolTable.FIELD_HIDDEN.ne(true))
-    private val allTools = Query.select<Tool>()
-        .where(where)
-        .getAsLiveData(dao)
+    ).getAsLiveData(dao)
 
     val categories = allTools.map {
         it.mapNotNull { tool ->
