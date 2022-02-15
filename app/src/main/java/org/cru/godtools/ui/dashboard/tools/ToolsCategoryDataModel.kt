f4331b16340d2a79e109ed0441dedd5d2abe32cc
Gyasi Story <gyasistory@gmail.com>
2022-02-15 17:13:44 -0500

    GT-1259 Simplify Category list view

--- app/src/main/java/org/cru/godtools/ui/dashboard/tools/ToolsCategoryDataModel.kt
+++ app/src/main/java/org/cru/godtools/ui/dashboard/tools/ToolsCategoryDataModel.kt
@@ -3,17 +3,14 @@ package org.cru.godtools.ui.dashboard.tools
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
-import androidx.lifecycle.switchMap
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import org.ccci.gto.android.common.androidx.lifecycle.combineWith
 import org.ccci.gto.android.common.androidx.lifecycle.orEmpty
 import org.ccci.gto.android.common.db.Expression.Companion.constants
 import org.ccci.gto.android.common.db.Query
-import org.ccci.gto.android.common.db.findLiveData
 import org.ccci.gto.android.common.db.getAsLiveData
 import org.cru.godtools.base.Settings
-import org.cru.godtools.model.Language
 import org.cru.godtools.model.Tool
 import org.keynote.godtools.android.db.Contract.ToolTable
 import org.keynote.godtools.android.db.GodToolsDao
