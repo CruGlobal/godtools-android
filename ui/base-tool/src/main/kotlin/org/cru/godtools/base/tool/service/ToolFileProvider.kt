2c5826e96b2ebe903e5465d890323d7ed20d28b7
Daniel Frett <daniel.frett@ccci.org>
2022-02-25 17:05:35 -0500

    attempt to resolve the mime-type from a custom displayName

--- ui/base-tool/src/main/kotlin/org/cru/godtools/base/tool/service/ToolFileProvider.kt
+++ ui/base-tool/src/main/kotlin/org/cru/godtools/base/tool/service/ToolFileProvider.kt
@@ -1,5 +1,15 @@
 package org.cru.godtools.base.tool.service
 
+import android.net.Uri
+import android.webkit.MimeTypeMap
 import androidx.core.content.FileProvider
 
-class ToolFileProvider : FileProvider()
+private const val DISPLAYNAME_FIELD = "displayName"
+
+class ToolFileProvider : FileProvider() {
+    override fun getType(uri: Uri) = uri.getQueryParameter(DISPLAYNAME_FIELD)
+        ?.substringAfterLast('.', missingDelimiterValue = "")
+        ?.takeIf { it.isNotEmpty() }
+        ?.let { MimeTypeMap.getSingleton().getMimeTypeFromExtension(it) }
+        ?: super.getType(uri)
+}
