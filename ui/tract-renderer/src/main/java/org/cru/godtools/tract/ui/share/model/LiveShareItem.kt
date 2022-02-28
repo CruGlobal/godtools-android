4250c8b53cf42a6e46dde122c0e72e09d44fda9b
Daniel Frett <daniel.frett@ccci.org>
2022-02-28 16:11:55 -0500

    no need to store the layout as a field backed property

--- ui/tract-renderer/src/main/java/org/cru/godtools/tract/ui/share/model/LiveShareItem.kt
+++ ui/tract-renderer/src/main/java/org/cru/godtools/tract/ui/share/model/LiveShareItem.kt
@@ -1,7 +1,6 @@
 package org.cru.godtools.tract.ui.share.model
 
 import android.app.Activity
-import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 import org.cru.godtools.base.tool.ui.share.model.ShareItem
 import org.cru.godtools.tract.R
@@ -9,8 +8,7 @@ import org.cru.godtools.tract.activity.TractActivity
 
 @Parcelize
 class LiveShareItem : ShareItem {
-    @IgnoredOnParcel
-    override val actionLayout = R.layout.tract_share_item_live_share
+    override val actionLayout get() = R.layout.tract_share_item_live_share
 
     override fun triggerAction(activity: Activity) {
         (activity as? TractActivity)?.shareLiveShareLink()
