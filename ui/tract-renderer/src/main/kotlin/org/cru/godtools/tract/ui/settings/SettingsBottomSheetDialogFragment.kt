112eecb348ae47ee96684ae7fc3f4a8a596bff86
Daniel Frett <daniel.frett@ccci.org>
2022-03-29 13:54:43 -0400

    display tool shareables in the tract settings dialog

--- ui/tract-renderer/src/main/kotlin/org/cru/godtools/tract/ui/settings/SettingsBottomSheetDialogFragment.kt
+++ ui/tract-renderer/src/main/kotlin/org/cru/godtools/tract/ui/settings/SettingsBottomSheetDialogFragment.kt
@@ -15,7 +15,9 @@ import org.ccci.gto.android.common.androidx.lifecycle.toggleValue
 import org.ccci.gto.android.common.material.bottomsheet.BindingBottomSheetDialogFragment
 import org.cru.godtools.base.tool.activity.BaseToolActivity
 import org.cru.godtools.base.tool.activity.MultiLanguageToolActivityDataModel
+import org.cru.godtools.base.tool.ui.shareable.ShareableImageBottomSheetDialogFragment
 import org.cru.godtools.base.ui.languages.LanguagesDropdownAdapter
+import org.cru.godtools.tool.model.shareable.ShareableImage
 import org.cru.godtools.tract.R
 import org.cru.godtools.tract.activity.TractActivity
 import org.cru.godtools.tract.databinding.TractSettingsSheetBinding
@@ -39,6 +41,7 @@ class SettingsBottomSheetDialogFragment :
         binding.primaryLanguage = primaryLanguage
         binding.parallelLanguage = parallelLanguage
         setupLanguageViews(binding)
+        setupShareables(binding)
     }
     // endregion Lifecycle
 
@@ -86,6 +89,12 @@ class SettingsBottomSheetDialogFragment :
             setOnItemClickListener { _, _, pos, _ -> updateParallelLanguage(adapter.getItem(pos)?.code) }
         }
     }
+
+    private fun setupShareables(binding: TractSettingsSheetBinding) {
+        val adapter = ShareablesAdapter(viewLifecycleOwner, this)
+        activityDataModel.activeManifest.observe(viewLifecycleOwner) { adapter.shareables = it?.shareables }
+        binding.shareables.adapter = adapter
+    }
     // endregion UI
 
     private fun updatePrimaryLanguage(locale: Locale) = with(activityDataModel) {
@@ -108,6 +117,18 @@ class SettingsBottomSheetDialogFragment :
 
     override fun shareScreen() {
         (activity as? TractActivity)?.shareLiveShareLink()
+        dismissAllowingStateLoss()
+    }
+
+    override fun shareShareable(shareable: ShareableImage?) {
+        val manifest = shareable?.manifest
+        val tool = manifest?.code
+        val locale = manifest?.locale
+        val id = shareable?.id
+        if (tool != null && locale != null && id != null) {
+            activity?.supportFragmentManager
+                ?.let { ShareableImageBottomSheetDialogFragment(tool, locale, id).show(it, null) }
+        }
         dismissAllowingStateLoss()
     }
 
