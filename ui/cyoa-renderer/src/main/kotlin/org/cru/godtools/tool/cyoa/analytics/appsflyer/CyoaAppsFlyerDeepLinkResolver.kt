43876440120cd483da8b9db458fb8dbf405b6b14
Daniel Frett <daniel.frett@ccci.org>
2022-03-30 11:23:49 -0400

    update CyoaAppsFlyerDeepLinkResolver to leverage DeepLinkListener

--- ui/cyoa-renderer/src/main/kotlin/org/cru/godtools/tool/cyoa/analytics/appsflyer/CyoaAppsFlyerDeepLinkResolver.kt
+++ ui/cyoa-renderer/src/main/kotlin/org/cru/godtools/tool/cyoa/analytics/appsflyer/CyoaAppsFlyerDeepLinkResolver.kt
@@ -1,15 +1,13 @@
 package org.cru.godtools.tool.cyoa.analytics.appsflyer
 
 import android.content.Context
-import android.net.Uri
 import java.util.Locale
-import org.cru.godtools.analytics.appsflyer.AppsFlyerAnalyticsService.Companion.AF_DEEP_LINK_VALUE
 import org.cru.godtools.analytics.appsflyer.AppsFlyerDeepLinkResolver
 import org.cru.godtools.base.ui.createCyoaActivityIntent
 
 internal object CyoaAppsFlyerDeepLinkResolver : AppsFlyerDeepLinkResolver {
-    override fun resolve(context: Context, uri: Uri?, data: Map<String, String?>) = data[AF_DEEP_LINK_VALUE]
-        ?.takeIf { it.startsWith("tool|cyoa|") }
+    override fun resolve(context: Context, deepLinkValue: String) = deepLinkValue
+        .takeIf { it.startsWith("tool|cyoa|") }
         ?.split("|")?.takeIf { it.size >= 4 }
         ?.let { context.createCyoaActivityIntent(it[2], Locale.forLanguageTag(it[3])) }
 }
