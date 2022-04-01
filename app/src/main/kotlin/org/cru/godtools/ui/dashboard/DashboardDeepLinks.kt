22cfb805c1afd7772ca175408b29e6ab7fb31896
Daniel Frett <daniel.frett@ccci.org>
2022-04-01 16:23:43 -0400

    extract some deep link parsing logic for reuse

--- app/src/main/kotlin/org/cru/godtools/ui/dashboard/DashboardDeepLinks.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/dashboard/DashboardDeepLinks.kt
@@ -2,6 +2,14 @@ package org.cru.godtools.ui.dashboard
 
 import android.net.Uri
 import org.cru.godtools.base.HOST_GODTOOLSAPP_COM
+import org.cru.godtools.base.ui.dashboard.Page
 
 internal fun Uri.isDashboardLessonsDeepLink() =
     (scheme == "http" || scheme == "https") && host.equals(HOST_GODTOOLSAPP_COM, true) && path == "/lessons"
+
+internal fun findPageByUriPathSegment(segment: String?) = when (segment) {
+    "lessons" -> Page.LESSONS
+    "tools" -> Page.ALL_TOOLS
+    "home" -> Page.FAVORITE_TOOLS
+    else -> Page.FAVORITE_TOOLS
+}
