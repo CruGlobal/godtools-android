3b11024b79a159ef2a1f1b0322df7513a0b114af
Daniel Frett <daniel.frett@ccci.org>
2022-04-01 16:25:29 -0400

    use SavedStateHandle delegates for tracking the selectedPage

--- app/src/main/kotlin/org/cru/godtools/ui/dashboard/DashboardSavedState.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/dashboard/DashboardSavedState.kt
@@ -2,15 +2,14 @@ package org.cru.godtools.ui.dashboard
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import org.ccci.gto.android.common.androidx.lifecycle.delegate
+import org.ccci.gto.android.common.androidx.lifecycle.livedata
 import org.cru.godtools.base.ui.dashboard.Page
 
 private const val ATTR_SELECTED_PAGE = "selectedPage"
 private val DEFAULT_PAGE = Page.FAVORITE_TOOLS
 
-internal class DashboardSavedState(private val savedState: SavedStateHandle) : ViewModel() {
-    var selectedPage: Page
-        get() = savedState[ATTR_SELECTED_PAGE] ?: DEFAULT_PAGE
-        set(value) { savedState[ATTR_SELECTED_PAGE] = value }
-
-    val selectedPageLiveData get() = savedState.getLiveData(ATTR_SELECTED_PAGE, DEFAULT_PAGE)
+internal class DashboardSavedState(savedState: SavedStateHandle) : ViewModel() {
+    var selectedPage by savedState.delegate(ATTR_SELECTED_PAGE, DEFAULT_PAGE)
+    val selectedPageLiveData by savedState.livedata(ATTR_SELECTED_PAGE, DEFAULT_PAGE)
 }
