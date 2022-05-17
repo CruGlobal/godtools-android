0ea7fdf4da2a6f0ece43db4ab6bbdaddcf235ce1
Daniel Frett <daniel.frett@ccci.org>
2022-05-17 17:08:35 -0400

    LoadingStateTest doesn't need robolectric

--- ui/base-tool/src/test/kotlin/org/cru/godtools/base/tool/activity/LoadingStateTest.kt
+++ ui/base-tool/src/test/kotlin/org/cru/godtools/base/tool/activity/LoadingStateTest.kt
@@ -1,6 +1,5 @@
 package org.cru.godtools.base.tool.activity
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import java.util.EnumSet
 import org.cru.godtools.base.tool.activity.BaseToolActivity.LoadingState
 import org.cru.godtools.base.tool.activity.BaseToolActivity.LoadingState.INVALID_TYPE
@@ -12,9 +11,7 @@ import org.cru.godtools.model.Translation
 import org.cru.godtools.tool.model.Manifest
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class LoadingStateTest {
     @Test
     fun verifyDetermineToolState() {
