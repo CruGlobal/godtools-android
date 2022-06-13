b7097f56edf9f672d267e52ba27aa66a21ab7492
Jason Hammar <jason@jbear.org>
2022-06-13 13:46:33 -0700

    Cleaning up the code a bit

--- ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/TipsLayout.kt
+++ ui/tutorial-renderer/src/main/java/org/cru/godtools/tutorial/TipsLayout.kt
@@ -1,29 +1,19 @@
 package org.cru.godtools.tutorial
-import androidx.compose.foundation.Image
+
+//import org.cru.godtools.R
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
-import com.airbnb.lottie.compose.animateLottieCompositionAsState
-import com.airbnb.lottie.compose.rememberLottieComposition
-//import org.cru.godtools.R
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.*
 import org.cru.godtools.base.ui.theme.GodToolsTheme
-import java.nio.file.Files.size
 
 @Preview(showBackground = true)
 @Composable
