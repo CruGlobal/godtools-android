d15d78a4195e86fef68324e7f8f171ed68f7e777
Daniel Frett <daniel.frett@ccci.org>
2022-06-15 13:17:07 -0400

    add a Composable for rendering a square tool card

--- app/src/main/kotlin/org/cru/godtools/ui/tools/ToolCardLayouts.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/tools/ToolCardLayouts.kt
@@ -1,20 +1,29 @@
 package org.cru.godtools.ui.tools
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults.elevatedCardElevation
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalMinimumTouchTargetEnforcement
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
@@ -26,6 +35,7 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -35,6 +45,7 @@ import coil.compose.AsyncImage
 import org.ccci.gto.android.common.androidx.compose.foundation.text.minLinesHeight
 import org.cru.godtools.R
 import org.cru.godtools.base.ui.theme.GRAY_E6
+import org.cru.godtools.base.ui.util.getCategory
 import org.cru.godtools.base.ui.util.getFontFamilyOrNull
 import org.cru.godtools.model.Language
 import org.cru.godtools.model.Tool
@@ -85,6 +96,74 @@ fun LessonToolCard(
 }
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun SquareToolCard(
+    toolCode: String,
+    modifier: Modifier = Modifier,
+    onOpenTool: (Tool?, Translation?, Translation?) -> Unit = { _, _, _ -> },
+    onOpenToolDetails: (String) -> Unit = {},
+    onClick: (Tool?, Translation?, Translation?) -> Unit = onOpenTool
+) {
+    val viewModel = toolViewModel(toolCode)
+    val tool by viewModel.tool.collectAsState()
+    val firstTranslation by viewModel.firstTranslation.collectAsState()
+    val secondTranslation = viewModel.secondTranslation.collectAsState()
+    val secondLanguage = viewModel.secondLanguage.collectAsState()
+    val parallelLanguage by viewModel.parallelLanguage.collectAsState()
+
+    ElevatedCard(
+        elevation = toolCardElevation,
+        onClick = { onClick(tool, firstTranslation, secondTranslation.value) },
+        modifier = modifier.width(189.dp)
+    ) {
+        ToolBanner(viewModel, modifier = Modifier.aspectRatio(189f / 128f))
+        Column(modifier = Modifier.padding(16.dp)) {
+            ToolName(viewModel, lines = 2, modifier = Modifier.fillMaxWidth())
+            ToolCategory(viewModel, modifier = Modifier.fillMaxWidth())
+            if (parallelLanguage != null) {
+                Row(
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                        .fillMaxWidth()
+                        .alpha(if (secondTranslation.value != null) 1f else 0f)
+                ) {
+                    AvailableInLanguage(language = secondLanguage, translation = secondTranslation)
+                }
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                val contentPadding = PaddingValues(horizontal = 4.dp, vertical = 8.dp)
+                val minHeight = 30.dp
+
+                CompositionLocalProvider(LocalMinimumTouchTargetEnforcement provides false) {
+                    OutlinedButton(
+                        onClick = { onOpenToolDetails(toolCode) },
+                        contentPadding = contentPadding,
+                        modifier = Modifier
+                            .weight(1f)
+                            .defaultMinSize(minHeight = minHeight)
+                    ) {
+                        Text(stringResource(R.string.action_tools_about), style = MaterialTheme.typography.labelMedium)
+                    }
+                    Button(
+                        onClick = { onOpenTool(tool, firstTranslation, secondTranslation.value) },
+                        contentPadding = contentPadding,
+                        modifier = Modifier
+                            .weight(1f)
+                            .defaultMinSize(minHeight = minHeight)
+                    ) {
+                        Text(stringResource(R.string.action_tools_open), style = MaterialTheme.typography.labelMedium)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
 private fun ToolBanner(viewModel: ToolsAdapterViewModel.ToolViewModel, modifier: Modifier = Modifier) = AsyncImage(
     model = viewModel.bannerFile.collectAsState().value,
     contentDescription = null,
@@ -115,6 +194,21 @@ private fun ToolName(viewModel: ToolsAdapterViewModel.ToolViewModel, modifier: M
         maxLines = lines,
         overflow = TextOverflow.Ellipsis,
         modifier = modifier.minLinesHeight(minLines = lines, textStyle = style)
+    )
+}
+
+@Composable
+private fun ToolCategory(viewModel: ToolsAdapterViewModel.ToolViewModel, modifier: Modifier = Modifier) {
+    val tool by viewModel.tool.collectAsState()
+    val firstTranslation by viewModel.firstTranslation.collectAsState()
+
+    val context = LocalContext.current
+    val locale by remember { derivedStateOf { firstTranslation?.languageCode } }
+
+    Text(
+        tool.getCategory(context, locale),
+        style = MaterialTheme.typography.bodySmall,
+        modifier = modifier
     )
 }
 
