edb40e164882752097a3d78d5740d2f73775a2cd
Daniel Frett <daniel.frett@ccci.org>
2022-06-17 17:59:36 -0400

    adjust the line height for titleMedium

--- ui/base/src/main/kotlin/org/cru/godtools/base/ui/theme/GodToolsTheme.kt
+++ ui/base/src/main/kotlin/org/cru/godtools/base/ui/theme/GodToolsTheme.kt
@@ -3,6 +3,7 @@ package org.cru.godtools.base.ui.theme
 import android.net.Uri
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.material3.contentColorFor
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -12,6 +13,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.platform.UriHandler
+import androidx.compose.ui.unit.sp
 import org.cru.godtools.base.ui.util.openUrl
 
 private val GT_BLUE = Color(red = 0x3B, green = 0xA4, blue = 0xDB)
@@ -30,9 +32,20 @@ private val GodToolsLightColorScheme = lightColorScheme(
     surfaceTint = Color.White
 )
 
+private val GodToolsTypography = with(Typography()) {
+    copy(
+        titleMedium = titleMedium.copy(
+            lineHeight = 22.sp
+        )
+    )
+}
+
 @Composable
 fun GodToolsTheme(content: @Composable () -> Unit) {
-    MaterialTheme(colorScheme = GodToolsLightColorScheme) {
+    MaterialTheme(
+        colorScheme = GodToolsLightColorScheme,
+        typography = GodToolsTypography
+    ) {
         CompositionLocalUtils {
             CompositionLocalProvider(
                 LocalContentColor provides contentColorFor(MaterialTheme.colorScheme.background),
