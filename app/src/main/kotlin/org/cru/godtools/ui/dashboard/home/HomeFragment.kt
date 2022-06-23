50d507f584b9ee780f966766c04fef8c0bcdef64
Daniel Frett <daniel.frett@ccci.org>
2022-06-23 16:56:28 -0400

    track HomeFragment views in analytics

--- app/src/main/kotlin/org/cru/godtools/ui/dashboard/home/HomeFragment.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/dashboard/home/HomeFragment.kt
@@ -5,16 +5,25 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.annotation.CallSuper
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 import org.ccci.gto.android.common.androidx.fragment.app.findListener
 import org.cru.godtools.R
+import org.cru.godtools.analytics.firebase.model.ACTION_IAM_HOME
+import org.cru.godtools.analytics.firebase.model.FirebaseIamActionEvent
+import org.cru.godtools.analytics.model.AnalyticsScreenEvent
+import org.cru.godtools.analytics.model.AnalyticsScreenEvent.Companion.SCREEN_HOME
 import org.cru.godtools.base.ui.dashboard.Page
 import org.cru.godtools.base.ui.fragment.BaseFragment
 import org.cru.godtools.databinding.ComposeLayoutBinding
 import org.cru.godtools.ui.dashboard.DashboardActivity
 import org.cru.godtools.ui.tools.ToolsAdapterCallbacks
+import org.greenrobot.eventbus.EventBus
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<ComposeLayoutBinding>(R.layout.compose_layout) {
+    @Inject
+    internal lateinit var eventBus: EventBus
+
     override fun onCreateBinding(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
         ComposeLayoutBinding.inflate(inflater, container, false)
 
@@ -29,5 +38,15 @@ class HomeFragment : BaseFragment<ComposeLayoutBinding>(R.layout.compose_layout)
                 onViewAllTools = { findListener<DashboardActivity>()?.showPage(Page.ALL_TOOLS) }
             )
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        trackInAnalytics()
+    }
+
+    private fun trackInAnalytics() {
+        eventBus.post(AnalyticsScreenEvent(SCREEN_HOME))
+        eventBus.post(FirebaseIamActionEvent(ACTION_IAM_HOME))
     }
 }
