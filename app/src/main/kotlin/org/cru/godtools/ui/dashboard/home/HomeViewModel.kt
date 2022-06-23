da7289f634750099ea975434c3ee7c932fff397b
Daniel Frett <daniel.frett@ccci.org>
2022-06-23 16:45:14 -0400

    display the features tutorial banner on the home layout

--- app/src/main/kotlin/org/cru/godtools/ui/dashboard/home/HomeViewModel.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/dashboard/home/HomeViewModel.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import org.ccci.gto.android.common.db.Query
 import org.ccci.gto.android.common.db.getAsFlow
+import org.cru.godtools.base.Settings
 import org.cru.godtools.model.Tool
 import org.cru.godtools.sync.GodToolsSyncService
 import org.keynote.godtools.android.db.Contract.ToolTable
@@ -20,9 +21,14 @@ import org.keynote.godtools.android.db.repository.ToolsRepository
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     dao: GodToolsDao,
+    settings: Settings,
     private val syncService: GodToolsSyncService,
     toolsRepository: ToolsRepository
 ) : ViewModel() {
+    val showTutorialFeaturesBanner = settings.isFeatureDiscoveredFlow(Settings.FEATURE_TUTORIAL_FEATURES)
+        .map { !it }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), false)
+
     val spotlightLessons = Query.select<Tool>()
         .where(ToolTable.FIELD_TYPE.eq(Tool.Type.LESSON) and ToolTable.FIELD_SPOTLIGHT.eq(true))
         .orderBy(ToolTable.COLUMN_DEFAULT_ORDER)
