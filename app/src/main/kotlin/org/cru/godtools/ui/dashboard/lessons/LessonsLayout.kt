fe71d628ca2dad8017736dd04c700e5d0a232a1a
Daniel Frett <daniel.frett@ccci.org>
2022-06-24 10:56:45 -0400

    use the LessonsFragment for the dashboard

--- app/src/main/kotlin/org/cru/godtools/ui/dashboard/lessons/LessonsLayout.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/dashboard/lessons/LessonsLayout.kt
@@ -1,18 +1,14 @@
 package org.cru.godtools.ui.dashboard.lessons
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -58,19 +54,14 @@ fun LessonsLayout(
 
 @Composable
 @Preview(showBackground = true)
-fun LessonsHeader() = GodToolsTheme {
-    CompositionLocalProvider(
-        LocalContentColor provides contentColorFor(MaterialTheme.colorScheme.background)
-    ) {
-        Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-            Text(
-                stringResource(R.string.dashboard_lessons_header_title),
-                style = MaterialTheme.typography.titleLarge
-            )
-            Text(
-                stringResource(R.string.dashboard_lessons_header_description),
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
-    }
+fun LessonsHeader() = Column {
+    Text(
+        stringResource(R.string.dashboard_lessons_header_title),
+        style = MaterialTheme.typography.titleLarge
+    )
+    Text(
+        stringResource(R.string.dashboard_lessons_header_description),
+        style = MaterialTheme.typography.bodyMedium,
+        modifier = Modifier.padding(top = 4.dp)
+    )
 }
