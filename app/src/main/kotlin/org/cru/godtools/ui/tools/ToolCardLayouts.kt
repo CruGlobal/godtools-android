1017cf1ff32185a430719511fedf77ade3dd6b40
Daniel Frett <daniel.frett@ccci.org>
2022-06-30 18:42:15 -0400

    create a ToolCardActions composable that renders the open and tool details buttons

--- app/src/main/kotlin/org/cru/godtools/ui/tools/ToolCardLayouts.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/tools/ToolCardLayouts.kt
@@ -4,24 +4,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults.elevatedCardElevation
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LocalMinimumTouchTargetEnforcement
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
@@ -31,7 +24,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -39,7 +31,6 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import org.ccci.gto.android.common.androidx.compose.foundation.text.minLinesHeight
-import org.cru.godtools.R
 import org.cru.godtools.base.ui.theme.GRAY_E6
 import org.cru.godtools.base.ui.util.ProvideLayoutDirectionFromLocale
 import org.cru.godtools.base.ui.util.getCategory
@@ -200,40 +191,12 @@ fun SquareToolCard(
                     }
                 }
 
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(4.dp),
+                ToolCardActions(
+                    viewModel,
+                    onOpenTool = onOpenTool,
+                    onOpenToolDetails = onOpenToolDetails,
                     modifier = Modifier.padding(top = 8.dp)
-                ) {
-                    val contentPadding = PaddingValues(horizontal = 4.dp, vertical = 8.dp)
-                    val minHeight = 30.dp
-
-                    CompositionLocalProvider(LocalMinimumTouchTargetEnforcement provides false) {
-                        OutlinedButton(
-                            onClick = { onOpenToolDetails(toolCode) },
-                            contentPadding = contentPadding,
-                            modifier = Modifier
-                                .weight(1f)
-                                .defaultMinSize(minHeight = minHeight)
-                        ) {
-                            Text(
-                                stringResource(R.string.action_tools_about),
-                                style = MaterialTheme.typography.labelMedium
-                            )
-                        }
-                        Button(
-                            onClick = { onOpenTool(tool, firstTranslation, secondTranslation) },
-                            contentPadding = contentPadding,
-                            modifier = Modifier
-                                .weight(1f)
-                                .defaultMinSize(minHeight = minHeight)
-                        ) {
-                            Text(
-                                stringResource(R.string.action_tools_open),
-                                style = MaterialTheme.typography.labelMedium
-                            )
-                        }
-                    }
-                }
+                )
             }
         }
     }
