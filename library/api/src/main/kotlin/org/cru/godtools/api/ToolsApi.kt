c15c063bd1c37328a799a2aa9b94166333dfc62a
Daniel Frett <daniel.frett@ccci.org>
2022-07-06 11:49:16 -0400

    remove unused get tool API call

--- library/api/src/main/kotlin/org/cru/godtools/api/ToolsApi.kt
+++ library/api/src/main/kotlin/org/cru/godtools/api/ToolsApi.kt
@@ -3,10 +3,8 @@ package org.cru.godtools.api
 import org.ccci.gto.android.common.jsonapi.model.JsonApiObject
 import org.ccci.gto.android.common.jsonapi.retrofit2.JsonApiParams
 import org.cru.godtools.model.Tool
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.QueryMap
 
 private const val PATH_RESOURCES = "resources"
@@ -15,7 +13,4 @@ private const val PARAM_FILTER_SYSTEM = "filter[system]"
 interface ToolsApi {
     @GET("$PATH_RESOURCES?$PARAM_FILTER_SYSTEM=${BuildConfig.MOBILE_CONTENT_SYSTEM}")
     suspend fun list(@QueryMap params: JsonApiParams): Response<JsonApiObject<Tool>>
-
-    @GET("$PATH_RESOURCES/{id}")
-    fun get(@Path("id") id: Long, @QueryMap params: JsonApiParams): Call<JsonApiObject<Tool>>
 }
