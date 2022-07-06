1035171ecd72098584539f08ef3f7eeb109a882d
Daniel Frett <daniel.frett@ccci.org>
2022-07-06 16:32:52 -0400

    remove unused callback

--- app/src/main/kotlin/org/cru/godtools/ui/tools/ToolsAdapterCallbacks.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/tools/ToolsAdapterCallbacks.kt
@@ -4,7 +4,6 @@ import org.cru.godtools.model.Tool
 import org.cru.godtools.model.Translation
 
 interface ToolsAdapterCallbacks {
-    fun onToolClicked(tool: Tool?, primary: Translation?) = onToolClicked(tool, primary, null)
     fun onToolClicked(tool: Tool?, primary: Translation?, parallel: Translation?) = openTool(tool, primary, parallel)
 
     fun openTool(tool: Tool?, primary: Translation?) = openTool(tool, primary, null)
