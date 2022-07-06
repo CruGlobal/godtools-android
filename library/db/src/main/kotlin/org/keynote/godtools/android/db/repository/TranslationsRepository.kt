e79c36cc416f7c01a9a77993bec49b56c07b60b3
Daniel Frett <daniel.frett@ccci.org>
2022-07-06 10:56:39 -0400

    cache the latest translation flows loaded from the database

--- library/db/src/main/kotlin/org/keynote/godtools/android/db/repository/TranslationsRepository.kt
+++ library/db/src/main/kotlin/org/keynote/godtools/android/db/repository/TranslationsRepository.kt
@@ -5,9 +5,13 @@ import java.util.Locale
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.withContext
+import org.ccci.gto.android.common.androidx.collection.WeakLruCache
+import org.ccci.gto.android.common.androidx.collection.getOrPut
 import org.ccci.gto.android.common.db.Query
 import org.ccci.gto.android.common.db.get
 import org.ccci.gto.android.common.db.getAsFlow
@@ -36,6 +40,9 @@ class TranslationsRepository @Inject constructor(private val dao: GodToolsDao) {
         }
     }
 
+    private val latestTranslationFlowCache =
+        WeakLruCache<Triple<String, Locale, Boolean>, Flow<Translation?>>(maxSize = 20)
+
     fun getLatestTranslationFlow(
         code: String?,
         locale: Locale?,
@@ -49,7 +56,15 @@ class TranslationsRepository @Inject constructor(private val dao: GodToolsDao) {
             @Suppress("DeferredResultUnused")
             dao.updateAsync(obj, where, TranslationTable.COLUMN_LAST_ACCESSED)
         }
-        return getLatestTranslationQuery(code, locale, isDownloaded).getAsFlow(dao).map { it.firstOrNull() }
+        return latestTranslationFlowCache.getOrPut(Triple(code, locale, isDownloaded)) { (code, locale, isDownloaded) ->
+            getLatestTranslationQuery(code, locale, isDownloaded)
+                .getAsFlow(dao).map { it.firstOrNull() }
+                .shareIn(
+                    dao.coroutineScope,
+                    SharingStarted.WhileSubscribed(replayExpirationMillis = REPLAY_EXPIRATION),
+                    replay = 1
+                )
+        }
     }
 
     fun getLatestTranslationLiveData(
