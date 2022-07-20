3e5d1d927e7faa1c6274e09de41b435cc4f97e4a
Daniel Frett <daniel.frett@ccci.org>
2022-07-20 16:40:34 -0400

    centralize the Banner display logic so we can reuse it

---
+++ app/src/main/kotlin/org/cru/godtools/ui/banner/Banners.kt
@@ -0,0 +1,33 @@
+package org.cru.godtools.ui.banner
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.with
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.cru.godtools.widget.BannerType
+
+@Composable
+@OptIn(ExperimentalAnimationApi::class)
+internal fun Banners(
+    banner: () -> BannerType?,
+    modifier: Modifier = Modifier
+) = Box(modifier = modifier.heightIn(min = 1.dp)) {
+    AnimatedContent(
+        targetState = banner(),
+        transitionSpec = {
+            slideInVertically(initialOffsetY = { -it }) with slideOutVertically(targetOffsetY = { -it })
+        }
+    ) {
+        when (it) {
+            BannerType.TOOL_LIST_FAVORITES -> FavoriteToolsBanner()
+            BannerType.TUTORIAL_FEATURES -> TutorialFeaturesBanner()
+            else -> Unit
+        }
+    }
+}
