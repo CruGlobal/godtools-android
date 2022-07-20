6e858819ddb82718ffcacf84b59191f32ea96d09
Daniel Frett <daniel.frett@ccci.org>
2022-07-20 12:24:43 -0400

    add icon support to banners

--- app/src/main/kotlin/org/cru/godtools/ui/banner/Banner+Preview.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/banner/Banner+Preview.kt
@@ -1,8 +1,11 @@
 package org.cru.godtools.ui.banner
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import org.cru.godtools.R
 
 @Composable
 @Preview(showBackground = true)
@@ -10,7 +13,7 @@ private fun SingleLineBanner() = Banner("Single Line", primaryButton = "Confirm"
 
 @Composable
 @Preview(showBackground = true)
-private fun MultiLineBanner() = Banner(
+private fun DefaultBanner() = Banner(
     LoremIpsum(20).values.first().replace("\n", " "),
     primaryButton = "Confirm",
     secondaryButton = "Dismiss",
@@ -18,8 +21,38 @@ private fun MultiLineBanner() = Banner(
 
 @Composable
 @Preview(showBackground = true)
-private fun ActionsOnSeparateLinesBanner() = Banner(
+private fun BannerWithActionsOnSeparateLines() = Banner(
     "Short Message",
     primaryButton = "Confirm Primary Action",
     secondaryButton = LoremIpsum(7).values.first(),
+)
+
+@Composable
+@Preview(showBackground = true)
+private fun BannerIcon() = Banner(
+    LoremIpsum(20).values.first().replace("\n", " "),
+    primaryButton = "Confirm",
+    secondaryButton = "Dismiss",
+    icon = painterResource(R.drawable.ic_favorite_24dp),
+    iconTint = Color.Cyan
+)
+
+@Composable
+@Preview(showBackground = true)
+private fun BannerIconShortText() = Banner(
+    "Single Line",
+    primaryButton = "Confirm",
+    secondaryButton = "Dismiss",
+    icon = painterResource(R.drawable.ic_favorite_24dp),
+    iconTint = Color.Cyan
+)
+
+@Composable
+@Preview(showBackground = true)
+private fun BannerIconShortTextLongActions() = Banner(
+    "Single Line",
+    primaryButton = "Confirm Primary Action",
+    secondaryButton = "Dismiss Secondary Action",
+    icon = painterResource(R.drawable.ic_favorite_24dp),
+    iconTint = Color.Cyan
 )
--- app/src/main/kotlin/org/cru/godtools/ui/banner/Banner.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/banner/Banner.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.LocalMinimumTouchTargetEnforcement
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,9 +16,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.constrain
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.offset
 
@@ -28,10 +34,13 @@ internal fun Banner(
     primaryAction: () -> Unit = {},
     secondaryButton: String? = null,
     secondaryAction: () -> Unit = {},
+    icon: Painter? = null,
+    iconTint: Color = if (icon != null) LocalContentColor.current else Color.Unspecified,
     modifier: Modifier = Modifier,
 ) = Surface(modifier = modifier.fillMaxWidth()) {
     Column {
         CompositionLocalProvider(LocalMinimumTouchTargetEnforcement provides false) {
+            val iconNode = "icon"
             val textNode = "text"
             val primaryActionNode = "primaryAction"
             val secondaryActionNode = "secondaryAction"
@@ -52,26 +61,36 @@ internal fun Banner(
                             .heightIn(min = 36.dp)
                     ) { Text(secondaryButton) }
                 }
+                if (icon != null) {
+                    Icon(icon, contentDescription = null, tint = iconTint, modifier = Modifier.layoutId(iconNode))
+                }
             }) { measurables, constraints ->
                 require(constraints.hasBoundedWidth) { "Banner requires a bounded width" }
 
                 val textMargin = 16.dp.roundToPx()
                 val actionMargin = 8.dp.roundToPx()
+                val iconSize = 40.dp.roundToPx()
 
-                val textPlaceable = measurables.first { it.layoutId == textNode }
-                    .measure(constraints.offset(horizontal = -2 * textMargin))
+                val iconPlaceable = measurables.firstOrNull { it.layoutId == iconNode }
+                    ?.measure(constraints.constrain(Constraints.fixed(iconSize, iconSize)))
+                val textPlaceable = measurables.first { it.layoutId == textNode }.measure(
+                    constraints
+                        .offset(horizontal = iconPlaceable?.let { 0 - textMargin - it.width } ?: 0)
+                        .offset(horizontal = -2 * textMargin)
+                )
                 val primaryActionPlaceable = measurables.first { it.layoutId == primaryActionNode }
                     .measure(constraints.offset(horizontal = -2 * actionMargin))
                 val secondaryActionPlaceable = measurables.firstOrNull { it.layoutId == secondaryActionNode }
                     ?.measure(constraints.offset(horizontal = -2 * actionMargin))
 
                 val bannerWidth = constraints.maxWidth
-                val actionsWidth =
-                    primaryActionPlaceable.width + (secondaryActionPlaceable?.let { it.width + actionMargin } ?: 0)
 
                 when {
                     // single line layout
-                    textMargin + textPlaceable.width + 36.dp.roundToPx() + actionsWidth + actionMargin < bannerWidth -> {
+                    iconPlaceable == null &&
+                        textMargin + textPlaceable.width + 36.dp.roundToPx() +
+                        primaryActionPlaceable.width + actionMargin +
+                        (secondaryActionPlaceable?.let { it.width + actionMargin } ?: 0) < bannerWidth -> {
                         val bannerHeight = maxOf(
                             textPlaceable.height,
                             primaryActionPlaceable.height,
@@ -97,10 +116,20 @@ internal fun Banner(
                     }
                     // default layout
                     else -> {
-                        val textPosition = IntOffset(textMargin, textMargin)
+                        val iconPosition = when (iconPlaceable) {
+                            null -> IntOffset.Zero
+                            else -> IntOffset(textMargin, textMargin)
+                        }
+                        val textPosition = when (iconPlaceable) {
+                            null -> IntOffset(textMargin, textMargin)
+                            else -> IntOffset(iconPosition.x + iconPlaceable.width + textMargin, textMargin)
+                        }
                         val primaryActionPosition = IntOffset(
                             bannerWidth - actionMargin - primaryActionPlaceable.width,
-                            textPosition.y + textPlaceable.height + 12.dp.roundToPx()
+                            maxOf(
+                                iconPlaceable?.let { iconPosition.y + it.height } ?: 0,
+                                textPosition.y + textPlaceable.height,
+                            ) + 12.dp.roundToPx()
                         )
                         val secondaryActionPosition = when (secondaryActionPlaceable) {
                             null -> IntOffset.Zero
@@ -118,12 +147,14 @@ internal fun Banner(
                         }
 
                         val bannerHeight = maxOf(
+                            iconPlaceable?.let { iconPosition.y + it.height + textMargin } ?: 0,
                             textPosition.y + textPlaceable.height + textMargin,
                             primaryActionPosition.y + primaryActionPlaceable.height + actionMargin,
-                            secondaryActionPosition.y + (secondaryActionPlaceable?.height ?: 0) + actionMargin,
+                            secondaryActionPlaceable?.let { secondaryActionPosition.y + it.height + actionMargin } ?: 0,
                         )
 
                         layout(bannerWidth, bannerHeight) {
+                            iconPlaceable?.placeRelative(iconPosition)
                             textPlaceable.placeRelative(textPosition)
                             primaryActionPlaceable.placeRelative(primaryActionPosition)
                             secondaryActionPlaceable?.placeRelative(secondaryActionPosition)
