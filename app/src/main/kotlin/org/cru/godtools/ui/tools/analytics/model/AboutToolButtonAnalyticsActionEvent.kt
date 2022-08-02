6a722e959c50655ecac5f2786a240c0d26f1219c
Daniel Frett <daniel.frett@ccci.org>
2022-08-02 16:14:05 -0400

    remove old open tool analytics event object

--- app/src/main/kotlin/org/cru/godtools/ui/tools/analytics/model/AboutToolButtonAnalyticsActionEvent.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/tools/analytics/model/AboutToolButtonAnalyticsActionEvent.kt
@@ -3,6 +3,5 @@ package org.cru.godtools.ui.tools.analytics.model
 import org.cru.godtools.analytics.model.AnalyticsActionEvent
 import org.cru.godtools.analytics.model.AnalyticsSystem
 
-object ToolOpenTapAnalyticsActionEvent : AnalyticsActionEvent("tool_open_tap", system = AnalyticsSystem.FIREBASE)
 object AboutToolButtonAnalyticsActionEvent :
     AnalyticsActionEvent("about_tool_open_button", system = AnalyticsSystem.FIREBASE)
