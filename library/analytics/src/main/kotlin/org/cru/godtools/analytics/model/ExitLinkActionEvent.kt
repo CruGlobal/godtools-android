f6754eba6b5f5a189c5048c728dd5c2e4dd1ce05
Daniel Frett <daniel.frett@ccci.org>
2022-08-19 11:25:50 -0400

    support specifying the ExitLinkActionEvent url as a string

--- library/analytics/src/main/kotlin/org/cru/godtools/analytics/model/ExitLinkActionEvent.kt
+++ library/analytics/src/main/kotlin/org/cru/godtools/analytics/model/ExitLinkActionEvent.kt
@@ -1,19 +1,22 @@
 package org.cru.godtools.analytics.model
 
 import android.net.Uri
-import android.os.Bundle
+import androidx.core.os.bundleOf
 import java.util.Locale
 
 private const val ACTION_EXIT_LINK = "exit_link_engaged"
 private const val FIREBASE_PARAM_EXIT_LINK = "cru_mobileexitlink"
 
-class ExitLinkActionEvent(private val tool: String?, private val link: Uri, locale: Locale? = null) :
-    AnalyticsActionEvent(ACTION_EXIT_LINK, locale = locale) {
+class ExitLinkActionEvent(
+    private val tool: String?,
+    private val link: String,
+    locale: Locale? = null
+) : AnalyticsActionEvent(ACTION_EXIT_LINK, locale = locale) {
+    constructor(tool: String?, link: Uri, locale: Locale? = null) : this(tool, link.toString(), locale)
+
     override fun isForSystem(system: AnalyticsSystem) = system == AnalyticsSystem.FIREBASE
 
     override val appSection get() = tool
 
-    override val firebaseParams get() = Bundle().apply {
-        putString(FIREBASE_PARAM_EXIT_LINK, link.toString())
-    }
+    override val firebaseParams get() = bundleOf(FIREBASE_PARAM_EXIT_LINK to link)
 }
