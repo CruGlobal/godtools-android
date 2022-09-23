64223786cc37eb5d284fba31a456f2c9514975c5
Daniel Frett <daniel.frett@cru.org>
2022-09-23 16:59:55 -0400

    load the GlobalActivity from the repository

--- app/src/main/kotlin/org/cru/godtools/ui/profile/GlobalActivityFragment.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/profile/GlobalActivityFragment.kt
@@ -3,16 +3,18 @@ package org.cru.godtools.ui.profile
 import android.os.Bundle
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-import org.ccci.gto.android.common.db.findLiveData
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import org.ccci.gto.android.common.sync.swiperefreshlayout.widget.SwipeRefreshSyncHelper
 import org.cru.godtools.R
 import org.cru.godtools.databinding.ProfilePageGlobalActivityFragmentBinding
+import org.cru.godtools.db.repository.GlobalActivityRepository
 import org.cru.godtools.fragment.BasePlatformFragment
 import org.cru.godtools.model.GlobalActivityAnalytics
-import org.keynote.godtools.android.db.GodToolsDao
 
 @AndroidEntryPoint
 class GlobalActivityFragment :
@@ -30,6 +32,7 @@ class GlobalActivityFragment :
 }
 
 @HiltViewModel
-class GlobalActivityFragmentViewModel @Inject constructor(dao: GodToolsDao) : ViewModel() {
-    val globalActivity = dao.findLiveData<GlobalActivityAnalytics>(1)
+class GlobalActivityFragmentViewModel @Inject constructor(repository: GlobalActivityRepository) : ViewModel() {
+    val globalActivity = repository.getGlobalActivityFlow()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), GlobalActivityAnalytics())
 }
