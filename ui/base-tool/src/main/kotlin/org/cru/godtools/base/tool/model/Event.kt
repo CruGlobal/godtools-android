ea0caf0d6c183f26c2f92955b3ef075e613f628e
Daniel Frett <daniel.frett@cru.org>
2022-10-07 12:32:52 -0400

    initialize the event builder with an optional manifest

--- ui/base-tool/src/main/kotlin/org/cru/godtools/base/tool/model/Event.kt
+++ ui/base-tool/src/main/kotlin/org/cru/godtools/base/tool/model/Event.kt
@@ -2,6 +2,7 @@ package org.cru.godtools.base.tool.model
 
 import java.util.Locale
 import org.cru.godtools.tool.model.EventId
+import org.cru.godtools.tool.model.Manifest
 
 class Event private constructor(builder: Builder) {
     val id = builder.id
@@ -9,10 +10,10 @@ class Event private constructor(builder: Builder) {
     val locale = builder.locale
     val fields = builder.fields.toMap()
 
-    class Builder {
+    class Builder(manifest: Manifest? = null) {
         internal lateinit var id: EventId
-        internal var tool: String? = null
-        internal var locale: Locale? = null
+        internal var tool: String? = manifest?.code
+        internal var locale: Locale? = manifest?.locale
         internal val fields = mutableMapOf<String, String>()
 
         fun id(id: EventId) = apply { this.id = id }
