5d65a93f6aa2d44ed701be537d85604f5002b18f
Daniel Frett <daniel.frett@cru.org>
2022-10-10 11:59:31 -0400

    update AccountListRegistrationService to utilize the AccountManager to get account info

--- app/src/main/kotlin/org/cru/godtools/service/AccountListRegistrationService.kt
+++ app/src/main/kotlin/org/cru/godtools/service/AccountListRegistrationService.kt
@@ -1,43 +1,35 @@
 package org.cru.godtools.service
 
-import com.okta.authfoundation.claims.email
-import com.okta.authfoundation.claims.familyName
-import com.okta.authfoundation.claims.givenName
-import com.okta.authfoundation.client.dto.OidcUserInfo
 import dagger.Lazy
 import java.io.IOException
 import javax.inject.Inject
-import javax.inject.Named
 import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import org.ccci.gto.android.common.okta.authfoundation.client.dto.oktaUserId
-import org.ccci.gto.android.common.okta.authfoundation.client.dto.ssoGuid
+import org.cru.godtools.account.GodToolsAccountManager
+import org.cru.godtools.account.model.AccountInfo
 import org.cru.godtools.api.BuildConfig.CAMPAIGN_FORMS_ID
 import org.cru.godtools.api.CampaignFormsApi
-import org.cru.godtools.base.DAGGER_OKTA_USER_INFO_FLOW
 import org.cru.godtools.base.Settings
 import timber.log.Timber
 
 @Singleton
 class AccountListRegistrationService @Inject internal constructor(
     private val settings: Settings,
-    @Named(DAGGER_OKTA_USER_INFO_FLOW)
-    oktaUserInfoFlow: SharedFlow<OidcUserInfo?>,
+    accountManager: GodToolsAccountManager,
     private val campaignFormsApi: Lazy<CampaignFormsApi>
 ) {
     private val coroutineScope = CoroutineScope(Dispatchers.Default)
 
     init {
-        oktaUserInfoFlow
+        accountManager.accountInfoFlow()
             .onEach { it?.let { registerUser(it) } }
             .launchIn(coroutineScope)
     }
 
-    private suspend fun registerUser(userInfo: OidcUserInfo) {
+    private suspend fun registerUser(userInfo: AccountInfo) {
         val oktaId = userInfo.oktaUserId
         val guid = userInfo.ssoGuid
         val email = userInfo.email ?: return
@@ -49,8 +41,7 @@ class AccountListRegistrationService @Inject internal constructor(
                 .signup(CAMPAIGN_FORMS_ID, email, userInfo.givenName, userInfo.familyName)
             if (response.isSuccessful) settings.recordAddedToCampaign(oktaId = oktaId, guid = guid)
         } catch (e: IOException) {
-            Timber.tag("AccountListRegService")
-                .d(e, "error registering user in account list")
+            Timber.tag("AccountListRegService").d(e, "error registering user in account list")
         }
 
         // TODO: if this failed due to no connectivity, we should retry when we have an internet connection
