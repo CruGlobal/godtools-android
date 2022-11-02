f121126ac21957a4589fe5fe45fc29eeac78ae88
Daniel Frett <daniel.frett@cru.org>
2022-11-02 13:54:21 -0400

    expose a UserActivity flow

--- library/user-data/src/main/kotlin/org/cru/godtools/user/activity/UserActivityManager.kt
+++ library/user-data/src/main/kotlin/org/cru/godtools/user/activity/UserActivityManager.kt
@@ -5,9 +5,14 @@ import javax.inject.Inject
 import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
 import org.cru.godtools.db.repository.UserCountersRepository
 import org.cru.godtools.model.UserCounter
+import org.cru.godtools.shared.user.activity.model.UserActivity
 import org.cru.godtools.sync.GodToolsSyncService
 
 @Singleton
@@ -25,10 +30,20 @@ class UserActivityManager internal constructor(
     // region Counters
     fun isValidCounterName(name: String) = UserCounter.VALID_NAME.matches(name)
 
+    private val countersFlow = userCountersRepository.getCountersFlow()
+        .map { it.associateBy({ it.id }, { it.count }) }
+        .shareIn(coroutineScope, SharingStarted.WhileSubscribed(), 1)
+        .distinctUntilChanged()
+
     suspend fun updateCounter(name: String, change: Int = 1) {
         require(isValidCounterName(name)) { "Invalid counter name: $name" }
         userCountersRepository.updateCounter(name, change)
         coroutineScope.launch { syncService.get().syncDirtyUserCounters() }
     }
     // endregion Counters
+
+    val userActivityFlow = countersFlow
+        .map { UserActivity(it) }
+        .shareIn(coroutineScope, SharingStarted.WhileSubscribed(), 1)
+        .distinctUntilChanged()
 }
