cfc130fa25cd7a1f062d90415a3740aaf5c66533
Daniel Frett <daniel.frett@cru.org>
2022-11-03 15:04:03 -0400

    increment the link_shares user counter when sharing a link from a tool

--- ui/base-tool/src/main/kotlin/org/cru/godtools/base/tool/analytics/model/ShareActionEvent.kt
+++ ui/base-tool/src/main/kotlin/org/cru/godtools/base/tool/analytics/model/ShareActionEvent.kt
@@ -2,9 +2,15 @@ package org.cru.godtools.base.tool.analytics.model
 
 import org.cru.godtools.analytics.model.AnalyticsActionEvent
 import org.cru.godtools.analytics.model.AnalyticsSystem
+import org.cru.godtools.shared.user.activity.UserCounterNames
 
 private const val ACTION_SHARE = "share_icon_engaged"
 
 object ShareActionEvent : AnalyticsActionEvent(action = ACTION_SHARE) {
-    override fun isForSystem(system: AnalyticsSystem) = system == AnalyticsSystem.FIREBASE
+    override fun isForSystem(system: AnalyticsSystem) = when (system) {
+        AnalyticsSystem.FIREBASE, AnalyticsSystem.USER -> true
+        else -> false
+    }
+
+    override val userCounterName = UserCounterNames.LINK_SHARED
 }
