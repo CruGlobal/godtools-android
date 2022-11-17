fbbbb204d3f1d40f5083bb4b87ecf6c60ccd0ec3
Daniel Frett <daniel.frett@cru.org>
2022-11-17 16:57:44 -0500

    track article opens in the user counters

--- ui/article-aem-renderer/src/main/kotlin/org/cru/godtools/article/aem/analytics/model/ArticleAnalyticsScreenEvent.kt
+++ ui/article-aem-renderer/src/main/kotlin/org/cru/godtools/article/aem/analytics/model/ArticleAnalyticsScreenEvent.kt
@@ -1,8 +1,10 @@
 package org.cru.godtools.article.aem.analytics.model
 
 import java.util.Locale
+import org.cru.godtools.analytics.model.AnalyticsSystem
 import org.cru.godtools.article.aem.model.Article
 import org.cru.godtools.base.tool.analytics.model.ToolAnalyticsScreenEvent
+import org.cru.godtools.shared.user.activity.UserCounterNames
 
 private const val SCREEN_ARTICLE_PREFIX = "Article : "
 
@@ -10,5 +12,12 @@ private const val APP_SUB_SECTION_ARTICLE = "article"
 
 class ArticleAnalyticsScreenEvent(article: Article, tool: String?, locale: Locale?) :
     ToolAnalyticsScreenEvent("$SCREEN_ARTICLE_PREFIX${article.title}", tool, locale) {
+    override fun isForSystem(system: AnalyticsSystem) = when (system) {
+        AnalyticsSystem.USER -> true
+        else -> super.isForSystem(system)
+    }
+
     override val appSubSection get() = APP_SUB_SECTION_ARTICLE
+
+    override val userCounterName = UserCounterNames.ARTICLE_OPEN(article.uri)
 }
