43480465ccc8c3ecadf0c65abc7efda54f76ca92
Daniel Frett <daniel.frett@cru.org>
2022-11-18 09:49:16 -0500

    add unit tests for the userActivityFlow

--- library/user-data/src/test/kotlin/org/cru/godtools/user/activity/UserActivityManagerTest.kt
+++ library/user-data/src/test/kotlin/org/cru/godtools/user/activity/UserActivityManagerTest.kt
@@ -1,5 +1,6 @@
 package org.cru.godtools.user.activity
 
+import app.cash.turbine.test
 import io.mockk.Called
 import io.mockk.coEvery
 import io.mockk.coVerifySequence
@@ -7,8 +8,12 @@ import io.mockk.every
 import io.mockk.excludeRecords
 import io.mockk.mockk
 import io.mockk.verifyAll
+import java.util.Locale
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.TestScope
@@ -18,6 +23,8 @@ import org.cru.godtools.db.repository.TrainingTipsRepository
 import org.cru.godtools.db.repository.UserCountersRepository
 import org.cru.godtools.model.TrainingTip
 import org.cru.godtools.model.UserCounter
+import org.cru.godtools.shared.user.activity.UserCounterNames
+import org.cru.godtools.shared.user.activity.model.Badge.BadgeType
 import org.cru.godtools.sync.GodToolsSyncService
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -71,4 +78,29 @@ class UserActivityManagerTest {
         }
     }
     // endregion updateCounter()
+
+    // region userActivityFlow
+    @Test
+    fun `Property userActivityFlow`() = testScope.runTest {
+        manager.userActivityFlow.test {
+            assertNotNull(awaitItem()) {
+                assertEquals(0, it.sessions)
+                assertTrue(it.badges.none { it.isEarned })
+            }
+
+            countersFlow.value = listOf(UserCounter(UserCounterNames.SESSION).apply { apiCount = 5 })
+            assertNotNull(awaitItem()) {
+                assertEquals(5, it.sessions)
+                assertTrue(it.badges.none { it.isEarned })
+            }
+
+            completedTipsFlow.value = List(30) { TrainingTip("tool", Locale.ENGLISH, "tipId$it", true) }.toSet()
+            assertNotNull(awaitItem()) {
+                assertEquals(5, it.sessions)
+                assertTrue(it.badges.filter { it.type == BadgeType.TIPS_COMPLETED }.all { it.isEarned })
+                assertTrue(it.badges.filterNot { it.type == BadgeType.TIPS_COMPLETED }.none { it.isEarned })
+            }
+        }
+    }
+    // endregion userActivityFlow
 }
