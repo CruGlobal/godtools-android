9dd44ff176a06722966dd79d85359593b91cffbc
Daniel Frett <daniel.frett@cru.org>
2022-12-06 14:52:13 -0500

    add support for parsing tool details sections

--- library/model/src/test/kotlin/org/cru/godtools/model/TranslationTest.kt
+++ library/model/src/test/kotlin/org/cru/godtools/model/TranslationTest.kt
@@ -25,6 +25,9 @@ class TranslationTest {
         assertEquals("Renderer Testing", translation.name)
         assertEquals("Renderer Testing Description", translation.description)
         assertEquals("Renderer Testing Tagline", translation.tagline)
+        assertEquals("Renderer Test Bible References", translation.toolDetailsBibleReferences)
+        assertEquals("Renderer Test Conversation Starters", translation.toolDetailsConversationStarters)
+        assertEquals("Renderer Test Outline", translation.toolDetailsOutline)
         assertEquals(
             "d13997976f4f9cd9ff6852d7d47afbada4f0f81e315e3061f996dd269391c2dd.xml",
             translation.manifestFileName
