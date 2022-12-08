cad0936219708c13e158332bd7145645a7477513
Daniel Frett <daniel.frett@cru.org>
2022-12-08 17:32:41 -0500

    use the common RoomDatabaseRule that gto-support provides

--- library/db/src/test/kotlin/org/cru/godtools/db/room/repository/GlobalActivityRoomRepositoryIT.kt
+++ library/db/src/test/kotlin/org/cru/godtools/db/room/repository/GlobalActivityRoomRepositoryIT.kt
@@ -1,14 +1,15 @@
 package org.cru.godtools.db.room.repository
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.ccci.gto.android.common.androidx.room.RoomDatabaseRule
 import org.cru.godtools.db.repository.GlobalActivityRepositoryIT
-import org.cru.godtools.db.room.GodToolsRoomDatabaseRule
+import org.cru.godtools.db.room.GodToolsRoomDatabase
 import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class GlobalActivityRoomRepositoryIT : GlobalActivityRepositoryIT() {
     @get:Rule
-    internal val dbRule = GodToolsRoomDatabaseRule()
+    internal val dbRule = RoomDatabaseRule(GodToolsRoomDatabase::class.java)
     override val repository get() = dbRule.db.globalActivityRepository
 }
