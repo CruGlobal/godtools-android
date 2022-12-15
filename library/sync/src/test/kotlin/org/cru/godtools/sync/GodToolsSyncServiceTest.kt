77b5c0e6c2e8eded091947d7ef3da1459e69ba54
Daniel Frett <daniel.frett@cru.org>
2022-12-15 14:37:26 -0500

    log unhandled exceptions from sync tasks

--- library/sync/src/test/kotlin/org/cru/godtools/sync/GodToolsSyncServiceTest.kt
+++ library/sync/src/test/kotlin/org/cru/godtools/sync/GodToolsSyncServiceTest.kt
@@ -2,9 +2,13 @@ package org.cru.godtools.sync
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.work.WorkManager
+import io.mockk.Called
 import io.mockk.coEvery
-import io.mockk.coVerify
+import io.mockk.coVerifyAll
+import io.mockk.every
+import io.mockk.excludeRecords
 import io.mockk.mockk
+import java.io.IOException
 import javax.inject.Provider
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestScope
@@ -12,8 +16,12 @@ import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.cru.godtools.sync.task.BaseSyncTasks
 import org.cru.godtools.sync.task.ToolSyncTasks
+import org.cru.godtools.sync.work.scheduleSyncToolsWork
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import timber.log.Timber
 
 @RunWith(AndroidJUnit4::class)
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -23,15 +31,68 @@ class GodToolsSyncServiceTest {
     private val syncTasks = mapOf<Class<out BaseSyncTasks>, Provider<BaseSyncTasks>>(
         ToolSyncTasks::class.java to Provider { toolsSyncTasks }
     )
+    private val timber: Timber.Tree = mockk(relaxed = true)
     private val workManager: WorkManager = mockk()
     private val testScope = TestScope()
 
     private val syncService =
         GodToolsSyncService(mockk(relaxUnitFun = true), { workManager }, syncTasks, UnconfinedTestDispatcher())
 
+    @Before
+    fun setup() {
+        Timber.plant(timber)
+        excludeRecords { Timber.tag(any()) }
+    }
+
+    @After
+    fun cleanup() {
+        Timber.uproot(timber)
+    }
+
     @Test
-    fun testSyncTools() = testScope.runTest {
+    fun `syncTools()`() = testScope.runTest {
         syncService.syncTools(false)
-        coVerify { toolsSyncTasks.syncTools(any()) }
+        coVerifyAll {
+            toolsSyncTasks.syncTools(false)
+            workManager wasNot Called
+        }
+    }
+
+    @Test
+    fun `syncTools() - Failure`() = testScope.runTest {
+        coEvery { toolsSyncTasks.syncTools(any()) } returns false
+        every { workManager.scheduleSyncToolsWork() } returns mockk()
+
+        syncService.syncTools(false)
+        coVerifyAll {
+            toolsSyncTasks.syncTools(false)
+            workManager.scheduleSyncToolsWork()
+        }
+    }
+
+    @Test
+    fun `syncTools() - Failure - IOException`() = testScope.runTest {
+        coEvery { toolsSyncTasks.syncTools(any()) } throws IOException()
+        every { workManager.scheduleSyncToolsWork() } returns mockk()
+
+        syncService.syncTools(false)
+        coVerifyAll {
+            toolsSyncTasks.syncTools(false)
+            workManager.scheduleSyncToolsWork()
+        }
+    }
+
+    @Test
+    fun `syncTools() - Failure - Other Exception`() = testScope.runTest {
+        val e = Exception()
+        coEvery { toolsSyncTasks.syncTools(any()) } throws e
+        every { workManager.scheduleSyncToolsWork() } returns mockk()
+
+        syncService.syncTools(false)
+        coVerifyAll {
+            toolsSyncTasks.syncTools(false)
+            timber.e(e, any())
+            workManager.scheduleSyncToolsWork()
+        }
     }
 }
