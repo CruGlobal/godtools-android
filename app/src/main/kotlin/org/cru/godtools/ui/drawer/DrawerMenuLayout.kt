785cb78563775764b5c5e1c174409c41d376c657
Daniel Frett <daniel.frett@cru.org>
2023-03-15 10:05:15 -0600

    Support submitting app store reviews from the app

--- app/src/main/kotlin/org/cru/godtools/ui/drawer/DrawerMenuLayout.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/drawer/DrawerMenuLayout.kt
@@ -1,5 +1,6 @@
 package org.cru.godtools.ui.drawer
 
+import android.app.Activity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -31,6 +32,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
@@ -39,6 +41,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.android.play.core.ktx.launchReview
+import com.google.android.play.core.ktx.requestReview
+import com.google.android.play.core.review.ReviewManagerFactory
+import kotlinx.coroutines.launch
 import org.ccci.gto.android.common.androidx.compose.foundation.layout.padding
 import org.ccci.gto.android.common.androidx.compose.material3.ui.navigationdrawer.NavigationDrawerHeadline
 import org.cru.godtools.BuildConfig
@@ -60,6 +66,7 @@ fun DrawerContentLayout(
     onItemSelected: (id: Int) -> Unit = {},
     dismissDrawer: () -> Unit = {},
 ) = ModalDrawerSheet {
+    val scope = rememberCoroutineScope()
     CompositionLocalProvider(LocalTextStyle provides MaterialTheme.typography.labelLarge) {
         Column(
             modifier = Modifier
@@ -176,8 +183,12 @@ fun DrawerContentLayout(
                 label = { Text(stringResource(R.string.menu_review)) },
                 selected = false,
                 onClick = {
-                    /* TODO */
-                    dismissDrawer()
+                    scope.launch {
+                        val reviewManager = ReviewManagerFactory.create(context)
+                        val reviewInfo = reviewManager.requestReview()
+                        if (context is Activity) reviewManager.launchReview(context, reviewInfo)
+                        dismissDrawer()
+                    }
                 }
             )
             NavigationDrawerItem(
