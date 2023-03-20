6c06ce61998f5208e99821d4ab13da5a776d6f9d
Daniel Frett <daniel.frett@cru.org>
2023-03-20 17:00:42 -0600

    create a GodToolsTheme object that will contain all the theme specific values

--- ui/base/src/main/kotlin/org/cru/godtools/base/ui/theme/GodToolsTheme.kt
+++ ui/base/src/main/kotlin/org/cru/godtools/base/ui/theme/GodToolsTheme.kt
@@ -18,71 +18,73 @@ import org.cru.godtools.ui.BuildConfig
 
 const val DisabledAlpha = 0.38f
 
-private val GT_BLUE = Color(red = 0x3B, green = 0xA4, blue = 0xDB)
-val GT_RED = Color(red = 0xE5, green = 0x5B, blue = 0x36)
-val GRAY_E6 = Color(red = 0xE6, green = 0xE6, blue = 0xE6)
+object GodToolsTheme {
+    private val GT_BLUE = Color(red = 0x3B, green = 0xA4, blue = 0xDB)
+    val GT_RED = Color(red = 0xE5, green = 0x5B, blue = 0x36)
+    val GRAY_E6 = Color(red = 0xE6, green = 0xE6, blue = 0xE6)
 
-private val GodToolsLightColorScheme = lightColorScheme(
-    primary = GT_BLUE,
-    onBackground = Color(90, 90, 90),
-    onSurface = Color(90, 90, 90),
-    // HACK: We are currently disabling surface tint to avoid using tonal elevation on surfaces.
-    //       When we transition to using tonal elevation we can revert this back to the default value
-    surfaceTint = Color.White,
-    // The following colors were automatically generated using the material 3 theme builder:
-    // https://m3.material.io/theme-builder#/custom
-    // Primary: #3ba4db
-    // Secondary: #3ba4db
-    // Neutral: #8f9193
-    primaryContainer = Color(0xffc7e7ff),
-    onPrimaryContainer = Color(0xff001e2e),
-    secondary = Color(0xff00658e),
-    onSecondary = Color(0xffffffff),
-    secondaryContainer = Color(0xffc7e7ff),
-    onSecondaryContainer = Color(0xff001e2e),
-    background = Color(0xfffcfcff),
-    surface = Color(0xfffcfcff),
-    surfaceVariant = Color(0xffdde3ea),
-    onSurfaceVariant = Color(0xff41484d),
-    outline = Color(0xff71787e),
-    // HACK: This sets the surfaceTint to what should be the primary color.
-    // surfaceTint = Color(0xff00658e),
-)
-
-private val GodToolsDarkColorScheme = darkColorScheme(
-    primary = Color(0xff83cfff),
-    onPrimary = Color(0xff00344c),
-    primaryContainer = Color(0xff004c6c),
-    onPrimaryContainer = Color(0xffc7e7ff),
-    secondary = Color(0xff83cfff),
-    onSecondary = Color(0xff00344c),
-    secondaryContainer = Color(0xff004c6c),
-    onSecondaryContainer = Color(0xffc7e7ff),
-    background = Color(0xff191c1e),
-    onBackground = Color(0xffe2e2e5),
-    surface = Color(0xff191c1e),
-    onSurface = Color(0xffe2e2e5),
-    surfaceVariant = Color(0xff41484d),
-    onSurfaceVariant = Color(0xffc1c7ce),
-    outline = Color(0xff8b9198)
-)
-
-private val GodToolsTypography = Typography().run {
-    copy(
-        titleMedium = titleMedium.copy(
-            lineHeight = 22.sp
-        )
+    internal val lightColorScheme = lightColorScheme(
+        primary = GT_BLUE,
+        onBackground = Color(90, 90, 90),
+        onSurface = Color(90, 90, 90),
+        // HACK: We are currently disabling surface tint to avoid using tonal elevation on surfaces.
+        //       When we transition to using tonal elevation we can revert this back to the default value
+        surfaceTint = Color.White,
+        // The following colors were automatically generated using the material 3 theme builder:
+        // https://m3.material.io/theme-builder#/custom
+        // Primary: #3ba4db
+        // Secondary: #3ba4db
+        // Neutral: #8f9193
+        primaryContainer = Color(0xffc7e7ff),
+        onPrimaryContainer = Color(0xff001e2e),
+        secondary = Color(0xff00658e),
+        onSecondary = Color(0xffffffff),
+        secondaryContainer = Color(0xffc7e7ff),
+        onSecondaryContainer = Color(0xff001e2e),
+        background = Color(0xfffcfcff),
+        surface = Color(0xfffcfcff),
+        surfaceVariant = Color(0xffdde3ea),
+        onSurfaceVariant = Color(0xff41484d),
+        outline = Color(0xff71787e),
+        // HACK: This sets the surfaceTint to what should be the primary color.
+        // surfaceTint = Color(0xff00658e),
     )
+
+    internal val darkColorScheme = darkColorScheme(
+        primary = Color(0xff83cfff),
+        onPrimary = Color(0xff00344c),
+        primaryContainer = Color(0xff004c6c),
+        onPrimaryContainer = Color(0xffc7e7ff),
+        secondary = Color(0xff83cfff),
+        onSecondary = Color(0xff00344c),
+        secondaryContainer = Color(0xff004c6c),
+        onSecondaryContainer = Color(0xffc7e7ff),
+        background = Color(0xff191c1e),
+        onBackground = Color(0xffe2e2e5),
+        surface = Color(0xff191c1e),
+        onSurface = Color(0xffe2e2e5),
+        surfaceVariant = Color(0xff41484d),
+        onSurfaceVariant = Color(0xffc1c7ce),
+        outline = Color(0xff8b9198)
+    )
+
+    internal val typography = Typography().run {
+        copy(
+            titleMedium = titleMedium.copy(
+                lineHeight = 22.sp
+            )
+        )
+    }
 }
 
 @Composable
 fun GodToolsTheme(content: @Composable () -> Unit) {
     MaterialTheme(
         colorScheme = when {
-            isSystemInDarkTheme() && BuildConfig.DEBUG -> GodToolsDarkColorScheme
-            else -> GodToolsLightColorScheme
+            isSystemInDarkTheme() && BuildConfig.DEBUG -> GodToolsTheme.darkColorScheme
+            else -> GodToolsTheme.lightColorScheme
         },
-        typography = GodToolsTypography
+        typography = GodToolsTheme.typography
     ) {
         CompositionLocals {
             CompositionLocalProvider(
