aab2eab6ee8b6ece0c8f58e0f0ad9989c5d5214f
Daniel Frett <daniel.frett@cru.org>
2023-04-10 10:56:06 -0600

    update the SyncRepository to utilize the new ToolsRepository.deleteIfNotFavorite() method

--- library/sync/src/main/kotlin/org/cru/godtools/sync/repository/SyncRepository.kt
+++ library/sync/src/main/kotlin/org/cru/godtools/sync/repository/SyncRepository.kt
@@ -2,7 +2,6 @@ package org.cru.godtools.sync.repository
 
 import androidx.annotation.VisibleForTesting
 import androidx.collection.LongSparseArray
-import androidx.collection.forEach
 import androidx.collection.valueIterator
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -27,21 +26,17 @@ internal class SyncRepository @Inject constructor(
     private val toolsRepository: ToolsRepository,
 ) {
     // region Tools
-    fun storeTools(tools: List<Tool>, existingTools: LongSparseArray<Tool>?, includes: Includes) {
+    fun storeTools(tools: List<Tool>, existingTools: MutableSet<String>?, includes: Includes) {
         tools.forEach {
             storeTool(it, existingTools, includes)
-            existingTools?.remove(it.id)
+            existingTools?.remove(it.code)
         }
 
         // prune any existing tools that weren't synced and aren't already added to the device
-        existingTools?.forEach { _, tool ->
-            if (tool.isAdded) return@forEach
-
-            toolsRepository.deleteBlocking(tool)
-        }
+        existingTools?.forEach { toolsRepository.deleteIfNotFavoriteBlocking(it) }
     }
 
-    private fun storeTool(tool: Tool, existingTools: LongSparseArray<Tool>?, includes: Includes) {
+    private fun storeTool(tool: Tool, existingTools: MutableSet<String>?, includes: Includes) {
         // don't store the tool if it's not valid
         if (!tool.isValid) return
 
@@ -70,13 +65,13 @@ internal class SyncRepository @Inject constructor(
         if (includes.include(Tool.JSON_METATOOL)) {
             tool.metatool?.let {
                 storeTool(it, existingTools, includes.descendant(Tool.JSON_METATOOL))
-                existingTools?.remove(it.id)
+                existingTools?.remove(it.code)
             }
         }
         if (includes.include(Tool.JSON_DEFAULT_VARIANT)) {
             tool.defaultVariant?.let {
                 storeTool(it, existingTools, includes.descendant(Tool.JSON_DEFAULT_VARIANT))
-                existingTools?.remove(it.id)
+                existingTools?.remove(it.code)
             }
         }
     }
