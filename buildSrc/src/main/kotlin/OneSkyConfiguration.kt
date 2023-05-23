d48e99fb1978ea970e500c4a7b59c1ca830a1a5e
Daniel Frett <daniel.frett@cru.org>
2023-05-22 18:50:13 -0600

    account for our "hacked" locales in OneSky

--- buildSrc/src/main/kotlin/OneSkyConfiguration.kt
+++ buildSrc/src/main/kotlin/OneSkyConfiguration.kt
@@ -9,6 +9,13 @@ import org.gradle.kotlin.dsl.extra
 private const val PROP_API_KEY = "ONESKY_API_KEY"
 private const val PROP_API_SECRET = "ONESKY_API_SECRET"
 
+private val ONESKY_LANGUAGE_CODE_OVERRIDES = mapOf(
+    "bn-rIN" to "kha",
+    "ku-rTR" to "ckb",
+    "lv-rLV" to "lv",
+    "nl-rCW" to "pap",
+)
+
 fun Project.onesky(configuration: OneSkyPluginExtension.() -> Unit) {
     extra.set(DEPRECATE_STRINGS_FLAG, true)
     apply(plugin = "co.brainly.onesky")
@@ -26,6 +33,24 @@ fun Project.onesky(configuration: OneSkyPluginExtension.() -> Unit) {
         // handle legacy locales for Hebrew & Indonesian
         tasks.named("downloadTranslations", DownloadTranslationsTask::class.java) {
             doLast {
+                // HACK: handle manually overridden language codes.
+                //       For certain language codes that OneSky doesn't support we hijack other languages and
+                //       replace the language code. It appears that the plugin uses the OneSky project languages API
+                //       which doesn't correctly return the updated codes. So we need to manually correct that here.
+                ONESKY_LANGUAGE_CODE_OVERRIDES.forEach { (orig, hack) ->
+                    val valuesOrig = file(sourcePath).resolve("values-$orig")
+                    val valuesHack = file(sourcePath).resolve("values-$hack")
+
+                    sourceStringFiles.forEach { file ->
+                        // move Locale(orig) to Locale(hack)
+                        valuesOrig.resolve(file).takeIf { it.exists() }?.let {
+                            it.copyTo(valuesHack.resolve(file), overwrite = true)
+                            it.delete()
+                        }
+                    }
+                }
+
+                // duplicate some legacy locales to both the old and new directories
                 val valuesHe = file(sourcePath).resolve("values-he")
                 val valuesIw = file(sourcePath).resolve("values-iw")
                 val valuesIn = file(sourcePath).resolve("values-in")
