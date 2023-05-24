54670c0a62d89a5065d7ef1553e8f7877c7f1d25
Daniel Frett <daniel.frett@cru.org>
2023-05-24 10:22:47 -0600

    OptIn to the delicate isClosedForReceive val

--- ui/article-aem-renderer/src/main/kotlin/org/cru/godtools/article/aem/service/AemArticleManager.kt
+++ ui/article-aem-renderer/src/main/kotlin/org/cru/godtools/article/aem/service/AemArticleManager.kt
@@ -14,6 +14,7 @@ import java.util.Date
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.ObsoleteCoroutinesApi
@@ -254,7 +255,7 @@ class AemArticleManager @VisibleForTesting internal constructor(
         // endregion Stale AemImports Refresh
 
         // region Cleanup
-        @OptIn(ObsoleteCoroutinesApi::class)
+        @OptIn(DelicateCoroutinesApi::class, ObsoleteCoroutinesApi::class)
         private val cleanupActor = coroutineScope.actor<Unit>(capacity = Channel.CONFLATED) {
             withTimeoutOrNull(CLEANUP_DELAY_INITIAL) { channel.receiveCatching() }
             while (!channel.isClosedForReceive) {
