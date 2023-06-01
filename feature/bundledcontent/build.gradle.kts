108d6a7d99f0f721d58713e2511cf7d91cf553df
Daniel Frett <daniel.frett@cru.org>
2023-06-01 15:11:41 -0400

    there are no unit tests for the bundledcontent feature, so let's always disable them

--- feature/bundledcontent/build.gradle.kts
+++ feature/bundledcontent/build.gradle.kts
@@ -2,12 +2,16 @@ plugins {
     id("com.android.dynamic-feature")
     kotlin("android")
     kotlin("kapt")
+    id("org.jetbrains.kotlinx.kover")
 }
 
 android {
     namespace = "org.cru.godtools.feature.bundledcontent"
     baseConfiguration(project)
 }
+
+androidComponents.beforeVariants { it.enableUnitTest = false }
+kover { disable() }
 
 dependencies {
     implementation(project(":library:initial-content"))
