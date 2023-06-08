ffbba7d3d2de9a66c5e5cb7ed34c2a5094b6b6ba
Daniel Frett <daniel.frett@cru.org>
2023-06-08 12:21:35 -0600

    reduce the API surface area for GodToolsDao

--- library/db/src/main/kotlin/org/keynote/godtools/android/db/GodToolsDao.kt
+++ library/db/src/main/kotlin/org/keynote/godtools/android/db/GodToolsDao.kt
@@ -1,19 +1,14 @@
 package org.keynote.godtools.android.db
 
-import androidx.annotation.RestrictTo
 import javax.inject.Inject
 import javax.inject.Singleton
 import org.ccci.gto.android.common.db.AbstractDao
 import org.ccci.gto.android.common.db.CoroutinesAsyncDao
-import org.ccci.gto.android.common.db.CoroutinesFlowDao
-import org.ccci.gto.android.common.db.LiveDataDao
 import org.cru.godtools.model.TranslationFile
 import org.keynote.godtools.android.db.Contract.TranslationFileTable
 
 @Singleton
-class GodToolsDao @Inject internal constructor(
-    database: GodToolsDatabase
-) : AbstractDao(database), CoroutinesAsyncDao, CoroutinesFlowDao, LiveDataDao {
+class GodToolsDao @Inject internal constructor(database: GodToolsDatabase) : AbstractDao(database), CoroutinesAsyncDao {
     init {
         registerType(
             TranslationFile::class.java,
@@ -28,9 +23,4 @@ class GodToolsDao @Inject internal constructor(
         is TranslationFile -> getPrimaryKeyWhere(TranslationFile::class.java, obj.translationId, obj.filename)
         else -> super.getPrimaryKeyWhere(obj)
     }
-
-    @get:RestrictTo(RestrictTo.Scope.LIBRARY)
-    override val coroutineDispatcher get() = super<CoroutinesAsyncDao>.coroutineDispatcher
-    @get:RestrictTo(RestrictTo.Scope.LIBRARY)
-    override val coroutineScope get() = super<CoroutinesAsyncDao>.coroutineScope
 }
