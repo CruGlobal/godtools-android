5688bc2b0c79cf8db46a08fdfebf3cad63844305
Daniel Frett <daniel.frett@cru.org>
2023-06-22 15:01:52 -0600

    add unit tests for triggerSync

---
+++ app/src/test/kotlin/org/cru/godtools/ui/dashboard/DashboardViewModelTest.kt
@@ -0,0 +1,93 @@
+package org.cru.godtools.ui.dashboard
+
+import androidx.lifecycle.SavedStateHandle
+import app.cash.turbine.test
+import io.mockk.Called
+import io.mockk.clearMocks
+import io.mockk.coEvery
+import io.mockk.coVerifyAll
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.cru.godtools.sync.GodToolsSyncService
+import org.junit.After
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+@Suppress("DeferredResultUnused")
+@OptIn(ExperimentalCoroutinesApi::class)
+class DashboardViewModelTest {
+    private val syncService: GodToolsSyncService = mockk {
+        every { syncFollowupsAsync() } returns CompletableDeferred()
+        every { syncToolSharesAsync() } returns CompletableDeferred()
+        coEvery { syncTools(any()) } returns true
+    }
+    private val testScope = TestScope()
+
+    private lateinit var viewModel: DashboardViewModel
+
+    @Before
+    fun createViewModel() {
+        Dispatchers.setMain(UnconfinedTestDispatcher(testScope.testScheduler))
+
+        viewModel = DashboardViewModel(
+            syncService = syncService,
+            savedState = SavedStateHandle()
+        )
+    }
+
+    @After
+    fun reset() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `triggerSync() - Initial sync`() = testScope.runTest {
+        coVerifyAll {
+            syncService.syncFollowupsAsync()
+            syncService.syncToolSharesAsync()
+            syncService.syncTools(false)
+        }
+    }
+
+    @Test
+    fun `triggerSync() - isSyncRunning is true while running`() = testScope.runTest {
+        // clear initial sync calls
+        clearMocks(syncService, answers = false, recordedCalls = true, childMocks = false, exclusionRules = false)
+        verify { syncService wasNot Called }
+
+        val semaphore = Semaphore(1, 1)
+        coEvery { syncService.syncTools(any()) } coAnswers { semaphore.acquire(); true }
+
+        viewModel.isSyncRunning.test {
+            runCurrent()
+            assertFalse(expectMostRecentItem())
+
+            viewModel.triggerSync()
+            runCurrent()
+            assertTrue(expectMostRecentItem())
+
+            semaphore.release()
+            runCurrent()
+            assertFalse(expectMostRecentItem())
+        }
+
+        coVerifyAll {
+            syncService.syncFollowupsAsync()
+            syncService.syncToolSharesAsync()
+            syncService.syncTools(any())
+        }
+    }
+}
