2455bee80aa026b7cc326d6e4851854323ff1d70
Daniel Frett <daniel.frett@cru.org>
2023-06-27 17:02:22 -0600

    record the Language Settings analytics event from the compose layout

--- app/src/main/kotlin/org/cru/godtools/ui/languages/LanguageSettingsActivity.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/languages/LanguageSettingsActivity.kt
@@ -5,12 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
-import org.cru.godtools.analytics.model.AnalyticsScreenEvent
 import org.cru.godtools.base.Settings.Companion.FEATURE_LANGUAGE_SETTINGS
 import org.cru.godtools.base.ui.activity.BaseActivity
 import org.cru.godtools.base.ui.startAppLanguageActivity
 import org.cru.godtools.base.ui.theme.GodToolsTheme
-import org.cru.godtools.shared.analytics.AnalyticsScreenNames
 import org.cru.godtools.ui.drawer.DrawerMenuLayout
 import org.cru.godtools.ui.languages.downloadable.startDownloadableLanguagesActivity
 
@@ -46,7 +44,6 @@ class LanguageSettingsActivity : BaseActivity() {
     override fun onResume() {
         super.onResume()
         settings.setFeatureDiscovered(FEATURE_LANGUAGE_SETTINGS)
-        eventBus.post(AnalyticsScreenEvent(AnalyticsScreenNames.SETTINGS_LANGUAGES))
     }
     // endregion Lifecycle
 }
