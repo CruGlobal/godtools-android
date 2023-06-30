d6fffd665b94e77604b89c9df4170f1d24c915cb
Daniel Frett <daniel.frett@cru.org>
2023-06-30 09:35:51 -0600

    avoid using `resources.getIdentifier()` when displaying the tool category name

--- ui/base/src/test/kotlin/org/cru/godtools/base/ui/util/ModelUtilsRobolectricTest.kt
+++ ui/base/src/test/kotlin/org/cru/godtools/base/ui/util/ModelUtilsRobolectricTest.kt
@@ -1,45 +1,63 @@
 package org.cru.godtools.base.ui.util
 
-import android.app.Activity
 import android.content.Context
+import android.content.res.Resources
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import java.util.Locale
 import org.cru.godtools.model.Tool
+import org.cru.godtools.ui.BuildConfig
+import org.cru.godtools.ui.R
 import org.junit.Assert.assertEquals
-import org.junit.Before
+import org.junit.Assume.assumeFalse
+import org.junit.Assume.assumeTrue
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
 
 @RunWith(AndroidJUnit4::class)
 class ModelUtilsRobolectricTest {
-    private lateinit var context: Context
-    private val tool = Tool().apply {
+    private val context: Context get() = ApplicationProvider.getApplicationContext()
+    private val tool = Tool("tool") {
         category = "gospel"
     }
     private val toolNull: Tool? = null
 
-    @Before
-    fun setup() {
-        context = Robolectric.buildActivity(Activity::class.java).get()
-    }
-
     @Test
-    fun testToolGetCategoryNullTool() {
+    fun `getCategory() - Null Tool`() {
         assertEquals("", toolNull.getCategory(context, null))
         assertEquals("", toolNull.getCategory(context, Locale.FRENCH))
     }
 
     @Test
-    fun testToolGetCategoryValidCategory() {
+    fun `getCategory() - Valid Category`() {
         assertEquals("Gospel Invitation", tool.getCategory(context, null))
         assertEquals("Invitation à Évangile", tool.getCategory(context, Locale.FRENCH))
     }
 
+    @Test(expected = Resources.NotFoundException::class)
+    fun `getCategory(unknown) - Debug`() {
+        assumeTrue(BuildConfig.DEBUG)
+        tool.category = "unknown"
+        tool.getCategory(context)
+    }
+
     @Test
-    fun testToolGetCategoryUnknownCategory() {
+    fun `getCategory(unknown) - Release`() {
+        assumeFalse(BuildConfig.DEBUG)
         tool.category = "unknown"
         assertEquals("unknown", tool.getCategory(context, null))
         assertEquals("unknown", tool.getCategory(context, Locale.FRENCH))
+    }
+
+    @Test
+    fun `getToolCategoryName()`() {
+        assertEquals(context.getString(R.string.tool_category_gospel), getToolCategoryName("gospel", context))
+        assertEquals(context.getString(R.string.tool_category_articles), getToolCategoryName("articles", context))
+        assertEquals(
+            context.getString(R.string.tool_category_conversation_starter),
+            getToolCategoryName("conversation_starter", context)
+        )
+        assertEquals(context.getString(R.string.tool_category_growth), getToolCategoryName("growth", context))
+        assertEquals(context.getString(R.string.tool_category_training), getToolCategoryName("training", context))
     }
 }
