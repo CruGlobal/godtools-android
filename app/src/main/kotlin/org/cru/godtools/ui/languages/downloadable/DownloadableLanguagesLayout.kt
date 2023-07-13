3175e0e679d239945a0f6d281fd26316177c42e7
Daniel Frett <daniel.frett@cru.org>
2023-07-13 12:31:03 -0600

    use the GodToolsTheme property to set search bar colors

--- app/src/main/kotlin/org/cru/godtools/ui/languages/downloadable/DownloadableLanguagesLayout.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/languages/downloadable/DownloadableLanguagesLayout.kt
@@ -16,10 +16,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -76,12 +74,7 @@ fun DownloadableLanguagesLayout(
                 onSearch = updateSearchQuery,
                 active = false,
                 onActiveChange = {},
-                colors = when {
-                    GodToolsTheme.isLightColorSchemeActive -> SearchBarDefaults.colors(
-                        containerColor = MaterialTheme.colorScheme.surfaceVariant
-                    )
-                    else -> SearchBarDefaults.colors()
-                },
+                colors = GodToolsTheme.searchBarColors,
                 leadingIcon = {
                     IconButton(
                         onClick = { onEvent(DownloadableLanguagesEvent.NavigateUp) },
