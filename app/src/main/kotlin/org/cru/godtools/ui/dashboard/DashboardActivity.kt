a1a3449ca3e3fd96e3681825dbe83d26f62bc404
Daniel Frett <daniel.frett@cru.org>
2023-07-14 16:01:55 -0600

    remove language settings app bar action

--- app/src/main/kotlin/org/cru/godtools/ui/dashboard/DashboardActivity.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/dashboard/DashboardActivity.kt
@@ -2,25 +2,17 @@ package org.cru.godtools.ui.dashboard
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.View
 import androidx.activity.viewModels
-import androidx.core.view.GravityCompat
 import androidx.lifecycle.asLiveData
-import com.getkeepsafe.taptargetview.TapTarget
-import com.getkeepsafe.taptargetview.TapTargetView
 import dagger.Lazy
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Locale
 import javax.inject.Inject
 import kotlinx.coroutines.flow.map
-import org.ccci.gto.android.common.androidx.lifecycle.observe
 import org.ccci.gto.android.common.compat.content.getSerializableExtraCompat
-import org.cru.godtools.R
 import org.cru.godtools.activity.BasePlatformActivity
 import org.cru.godtools.analytics.LaunchTrackingViewModel
 import org.cru.godtools.base.EXTRA_PAGE
-import org.cru.godtools.base.Settings.Companion.FEATURE_LANGUAGE_SETTINGS
 import org.cru.godtools.base.Settings.Companion.FEATURE_TUTORIAL_ONBOARDING
 import org.cru.godtools.base.tool.service.ManifestManager
 import org.cru.godtools.base.ui.dashboard.Page
@@ -29,7 +21,6 @@ import org.cru.godtools.databinding.ActivityDashboardBinding
 import org.cru.godtools.model.Tool
 import org.cru.godtools.tutorial.PageSet
 import org.cru.godtools.tutorial.startTutorialActivity
-import org.cru.godtools.ui.languages.startLanguageSettingsActivity
 import org.cru.godtools.ui.tooldetails.startToolDetailsActivity
 import org.cru.godtools.util.openToolActivity
 
@@ -45,11 +36,6 @@ class DashboardActivity : BasePlatformActivity<ActivityDashboardBinding>() {
         super.onCreate(savedInstanceState)
         if (savedInstanceState == null) intent?.let { processIntent(it) }
         triggerOnboardingIfNecessary()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu) = super.onCreateOptionsMenu(menu).also {
-        menuInflater.inflate(R.menu.activity_main, menu)
-        menu.observeSelectedPageChanges()
     }
 
     override fun onBindingChanged() {
@@ -115,13 +101,6 @@ class DashboardActivity : BasePlatformActivity<ActivityDashboardBinding>() {
 
     override fun inflateBinding() = ActivityDashboardBinding.inflate(layoutInflater)
 
-    private fun Menu.observeSelectedPageChanges() {
-        findItem(R.id.action_switch_language)?.let { item ->
-            viewModel.currentPage.asLiveData()
-                .observe(this@DashboardActivity, item) { item.isVisible = it != Page.LESSONS }
-        }
-    }
-
     // region ToolsAdapterCallbacks
     @Inject
     internal lateinit var lazyManifestManager: Lazy<ManifestManager>
@@ -136,75 +115,6 @@ class DashboardActivity : BasePlatformActivity<ActivityDashboardBinding>() {
     }
     // endregion ToolsAdapterCallbacks
     // endregion UI
-
-    // region Feature Discovery
-    private var featureDiscovery: TapTargetView? = null
-    override fun isFeatureDiscoveryVisible() = super.isFeatureDiscoveryVisible() || featureDiscovery != null
-
-    override fun canShowFeatureDiscovery(feature: String) = when (feature) {
-        FEATURE_LANGUAGE_SETTINGS -> !binding.drawerLayout.isDrawerOpen(GravityCompat.START)
-        else -> super.canShowFeatureDiscovery(feature)
-    }
-
-    override fun showNextFeatureDiscovery() = when {
-        !settings.isFeatureDiscovered(FEATURE_LANGUAGE_SETTINGS) &&
-            canShowFeatureDiscovery(FEATURE_LANGUAGE_SETTINGS) ->
-            dispatchDelayedFeatureDiscovery(FEATURE_LANGUAGE_SETTINGS, false, 15000)
-        else -> super.showNextFeatureDiscovery()
-    }
-
-    override fun onShowFeatureDiscovery(feature: String, force: Boolean) = when (feature) {
-        FEATURE_LANGUAGE_SETTINGS -> showLanguageSettingsFeatureDiscovery(force)
-        else -> super.onShowFeatureDiscovery(feature, force)
-    }
-
-    // region Language Settings
-    private fun showLanguageSettingsFeatureDiscovery(force: Boolean) {
-        if (toolbar.findViewById<View>(R.id.action_switch_language) != null) {
-            // purge any pending feature discovery triggers since we are showing feature discovery now
-            purgeQueuedFeatureDiscovery(FEATURE_LANGUAGE_SETTINGS)
-
-            // show language settings feature discovery
-            val target = TapTarget.forToolbarMenuItem(
-                toolbar,
-                R.id.action_switch_language,
-                getString(R.string.feature_discovery_title_language_settings),
-                getString(R.string.feature_discovery_desc_language_settings)
-            )
-            featureDiscovery = TapTargetView.showFor(this, target, LanguageSettingsFeatureDiscoveryListener())
-            featureDiscoveryActive = FEATURE_LANGUAGE_SETTINGS
-        } else {
-            // TODO: we currently don't (can't?) distinguish between when the menu item doesn't exist and when
-            //       the menu item just hasn't been drawn yet.
-
-            // the toolbar action isn't available yet.
-            // re-attempt this feature discovery on the next frame iteration.
-            dispatchDelayedFeatureDiscovery(FEATURE_LANGUAGE_SETTINGS, force, 17)
-        }
-    }
-
-    private inner class LanguageSettingsFeatureDiscoveryListener : TapTargetView.Listener() {
-        override fun onTargetClick(view: TapTargetView) {
-            super.onTargetClick(view)
-            startLanguageSettingsActivity()
-        }
-
-        override fun onOuterCircleClick(view: TapTargetView) {
-            onTargetCancel(view)
-        }
-
-        override fun onTargetDismissed(view: TapTargetView, userInitiated: Boolean) {
-            super.onTargetDismissed(view, userInitiated)
-            if (userInitiated) {
-                settings.setFeatureDiscovered(FEATURE_LANGUAGE_SETTINGS)
-                featureDiscoveryActive = null
-                showNextFeatureDiscovery()
-            }
-            if (view === featureDiscovery) featureDiscovery = null
-        }
-    }
-    // endregion Language Settings
-    // endregion Feature Discovery
 
     override fun onSupportNavigateUp() = when {
         onBackPressedDispatcher.hasEnabledCallbacks() -> {
