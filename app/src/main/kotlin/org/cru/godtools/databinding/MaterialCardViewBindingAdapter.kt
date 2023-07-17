488fda5fd24756e185f55b8b07eb7d10c2560d9e
Daniel Frett <daniel.frett@cru.org>
2023-07-17 11:52:24 -0600

    remove unused databinding adapters

--- app/src/main/kotlin/org/cru/godtools/databinding/MaterialCardViewBindingAdapter.kt
+++
@@ -1,7 +0,0 @@
-package org.cru.godtools.databinding
-
-import androidx.databinding.BindingAdapter
-import com.google.android.material.card.MaterialCardView
-
-@BindingAdapter("strokeWidth")
-fun MaterialCardView.setStrokeWidth(width: Float) = setStrokeWidth(width.toInt())
