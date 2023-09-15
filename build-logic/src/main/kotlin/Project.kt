11a6cf9eb003c1bd30127905638fa9e47d3fc6bd
Daniel Frett <daniel.frett@cru.org>
2023-09-15 10:22:43 -0600

    update createEventBusIndex to register the correct kapt processor

--- build-logic/src/main/kotlin/Project.kt
+++ build-logic/src/main/kotlin/Project.kt
@@ -2,10 +2,13 @@ import com.android.build.api.variant.AndroidComponentsExtension
 import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 
 internal fun Project.androidComponents(configure: Action<AndroidComponentsExtension<*, *, *>>): Unit =
     extensions.configure("androidComponents", configure)
+internal fun Project.kapt(configure: KaptExtension.() -> Unit) = extensions.configure(configure)
 
 internal val Project.libs get() = project.extensions.getByType<VersionCatalogsExtension>().named("libs")
 internal val Project.androidComponents
