9492ef3240cd37fe6d8019bc3fc3c3a2dd5367fe
Daniel Frett <daniel.frett@cru.org>
2023-09-19 16:26:51 -0600

    Remove version 1 of the GodToolsRoomDatabase
    this version was never published

--- library/db/src/main/kotlin/org/cru/godtools/db/room/GodToolsRoomDatabase.kt
+++ library/db/src/main/kotlin/org/cru/godtools/db/room/GodToolsRoomDatabase.kt
@@ -58,7 +58,6 @@ import org.cru.godtools.db.room.repository.UserRoomRepository
         LastSyncTimeEntity::class,
     ],
     autoMigrations = [
-        AutoMigration(from = 1, to = 2),
         AutoMigration(from = 2, to = 3),
         AutoMigration(from = 3, to = 4),
         AutoMigration(from = 4, to = 5),
@@ -113,7 +112,6 @@ internal abstract class GodToolsRoomDatabase : RoomDatabase() {
  * Version history
  *
  * v6.0.1
- * 1: 2022-09-22
  * 2: 2022-09-23
  * 3: 2022-10-12
  * 4: 2022-11-03
