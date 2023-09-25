96a2e26ecafe2361885d2568afe9dec2c9a1e378
Daniel Frett <daniel.frett@cru.org>
2023-09-25 16:45:47 -0600

    remove several unused functions from BaseSyncTasks

--- library/sync/src/main/kotlin/org/cru/godtools/sync/task/BaseSyncTasks.kt
+++ library/sync/src/main/kotlin/org/cru/godtools/sync/task/BaseSyncTasks.kt
@@ -1,20 +1,8 @@
 package org.cru.godtools.sync.task
 
-import android.content.ContentResolver
-import android.os.Bundle
 import androidx.annotation.RestrictTo
 import androidx.annotation.WorkerThread
-import androidx.collection.LongSparseArray
-import org.cru.godtools.model.Base
 
 @WorkerThread
 @RestrictTo(RestrictTo.Scope.LIBRARY)
-abstract class BaseSyncTasks internal constructor() {
-    companion object {
-        fun isForced(extras: Bundle) = extras.getBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, false)
-
-        fun <E : Base> index(items: Collection<E>) = LongSparseArray<E>().apply {
-            for (item in items) put(item.id, item)
-        }
-    }
-}
+abstract class BaseSyncTasks internal constructor()
