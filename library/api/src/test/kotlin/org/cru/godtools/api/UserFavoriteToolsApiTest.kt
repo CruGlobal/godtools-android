fbc6546af94fe2ca2eacf07487eddde146367cab
Daniel Frett <daniel.frett@cru.org>
2023-09-27 16:42:08 -0600

    add an API interface to add/remove favorite tools

---
+++ library/api/src/test/kotlin/org/cru/godtools/api/UserFavoriteToolsApiTest.kt
@@ -0,0 +1,46 @@
+package org.cru.godtools.api
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlinx.coroutines.test.runTest
+import net.javacrumbs.jsonunit.assertj.assertThatJson
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.ccci.gto.android.common.jsonapi.retrofit2.JsonApiConverterFactory
+import org.cru.godtools.api.UserFavoriteToolsApi.Companion.PATH_FAVORITE_TOOLS
+import org.cru.godtools.model.Tool
+import org.junit.Rule
+import retrofit2.Retrofit
+
+private const val JSON_RESPONSE_FAVORITES = "{data:[{id:5,type:\"resource\"}]}"
+
+class UserFavoriteToolsApiTest {
+    @get:Rule
+    val server = MockWebServer()
+
+    private var api = Retrofit.Builder()
+        .baseUrl(server.url("/"))
+        .addConverterFactory(JsonApiConverterFactory(Tool::class.java))
+        .build()
+        .create(UserFavoriteToolsApi::class.java)
+
+    @Test
+    fun `removeFavoriteTools()`() = runTest {
+        server.enqueue(MockResponse().setBody(JSON_RESPONSE_FAVORITES))
+
+        val resp = api.removeFavoriteTools(tools = listOf(Tool("en") { id = 1 })).body()!!.data.single()
+        assertNotNull(resp) { assertEquals(5, it.id) }
+
+        val request = server.takeRequest()
+        assertEquals("DELETE", request.method)
+        assertEquals("/$PATH_FAVORITE_TOOLS", request.path)
+        assertThatJson(request.body.readUtf8()) {
+            isObject
+            node("data").isArray.hasSize(1)
+            node("data[0].id").isEqualTo(1)
+            node("data[0].attributes").isAbsent()
+            node("data[0].relationships").isAbsent()
+        }
+    }
+}
