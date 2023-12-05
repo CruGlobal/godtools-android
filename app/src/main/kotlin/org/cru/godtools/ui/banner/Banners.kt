6b18e7acbbbaae4fb6b5b1981a8d90f8999d5a5a
Daniel Frett <daniel.frett@cru.org>
2023-12-04 18:27:34 -0700

    replace deprecated with with togetherWith

--- app/src/main/kotlin/org/cru/godtools/ui/banner/Banners.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/banner/Banners.kt
@@ -1,10 +1,9 @@
 package org.cru.godtools.ui.banner
 
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.animation.with
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.runtime.Composable
@@ -12,12 +11,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-@OptIn(ExperimentalAnimationApi::class)
 internal fun Banners(banner: () -> BannerType?, modifier: Modifier = Modifier) = Box(modifier.heightIn(min = 1.dp)) {
     AnimatedContent(
         targetState = banner(),
         transitionSpec = {
-            slideInVertically(initialOffsetY = { -it }) with slideOutVertically(targetOffsetY = { -it })
+            slideInVertically(initialOffsetY = { -it }) togetherWith slideOutVertically(targetOffsetY = { -it })
         },
         label = "Banner Visibility",
     ) {
