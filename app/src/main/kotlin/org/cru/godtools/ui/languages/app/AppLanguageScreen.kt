b124b381b1ef226e70f21290b1124da773a8f8c4
Daniel Frett <daniel.frett@cru.org>
2024-01-12 15:44:11 -0700

    create an AppLanguagePresenter for the AppLanguage UI

---
+++ app/src/main/kotlin/org/cru/godtools/ui/languages/app/AppLanguageScreen.kt
@@ -0,0 +1,23 @@
+package org.cru.godtools.ui.languages.app
+
+import com.slack.circuit.runtime.CircuitUiEvent
+import com.slack.circuit.runtime.CircuitUiState
+import com.slack.circuit.runtime.screen.Screen
+import java.util.Locale
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data object AppLanguageScreen : Screen {
+    data class State(
+        val languages: List<Locale> = emptyList(),
+        val selectedLanguage: Locale? = null,
+        val eventSink: (Event) -> Unit = {}
+    ) : CircuitUiState
+
+    sealed interface Event : CircuitUiEvent {
+        data object NavigateBack : Event
+        data class SelectLanguage(val language: Locale) : Event
+        data class ConfirmLanguage(val language: Locale) : Event
+        data object DismissConfirmDialog : Event
+    }
+}
