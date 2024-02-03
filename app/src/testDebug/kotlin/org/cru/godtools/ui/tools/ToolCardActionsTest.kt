9d7e1e6fec10fbf6529684e714491de7b60ea0d6
Daniel Frett <daniel.frett@cru.org>
2024-02-02 18:15:11 -0700

    add a unit test for recomposing ToolCardActions() with a new eventSink

--- app/src/testDebug/kotlin/org/cru/godtools/ui/tools/ToolCardActionsTest.kt
+++ app/src/testDebug/kotlin/org/cru/godtools/ui/tools/ToolCardActionsTest.kt
@@ -1,13 +1,16 @@
 package org.cru.godtools.ui.tools
 
 import android.app.Application
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.slack.circuit.test.TestEventSink
+import kotlin.test.Test
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.runTest
 import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 
@@ -20,7 +23,7 @@ class ToolCardActionsTest {
     private val events = TestEventSink<ToolCard.Event>()
 
     @Test
-    fun `ToolCardActions() - Open Tool`() {
+    fun `Button - Open Tool`() {
         val state = ToolCard.State(eventSink = events)
         composeTestRule.setContent { ToolCardActions(state) }
 
@@ -29,11 +32,29 @@ class ToolCardActionsTest {
     }
 
     @Test
-    fun `ToolCardActions() - Tool Details`() {
+    fun `Button - Tool Details`() {
         val state = ToolCard.State(eventSink = events)
         composeTestRule.setContent { ToolCardActions(state) }
 
         composeTestRule.onNodeWithText("Details", substring = true, ignoreCase = true).performClick()
         events.assertEvent(ToolCard.Event.OpenToolDetails)
+    }
+
+    @Test
+    fun `Recompose - eventSink updates`() = runTest {
+        val stateFlow = MutableStateFlow(ToolCard.State())
+        composeTestRule.setContent { ToolCardActions(stateFlow.collectAsState().value) }
+
+        composeTestRule.onNodeWithText("Open", substring = true, ignoreCase = true).performClick()
+        composeTestRule.onNodeWithText("Details", substring = true, ignoreCase = true).performClick()
+        events.assertNoEvents()
+
+        stateFlow.value = ToolCard.State(eventSink = events)
+        composeTestRule.onNodeWithText("Open", substring = true, ignoreCase = true).performClick()
+        composeTestRule.onNodeWithText("Details", substring = true, ignoreCase = true).performClick()
+        events.assertEvents(
+            ToolCard.Event.OpenTool,
+            ToolCard.Event.OpenToolDetails
+        )
     }
 }
