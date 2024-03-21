c1508976d2b04bf2a06e34bf18d889310e9e5e1f
Daniel Frett <daniel.frett@cru.org>
2024-03-21 16:37:11 -0600

    exclude redundant accessibility tests

--- app/src/testDebug/kotlin/org/cru/godtools/ui/BasePaparazziTest.kt
+++ app/src/testDebug/kotlin/org/cru/godtools/ui/BasePaparazziTest.kt
@@ -17,15 +17,18 @@ import app.cash.paparazzi.Paparazzi
 import app.cash.paparazzi.accessibility.AccessibilityRenderExtension
 import com.android.ide.common.rendering.api.SessionParams.RenderingMode
 import com.android.resources.NightMode
+import kotlin.test.BeforeTest
 import org.cru.godtools.base.ui.compose.LocalEventBus
 import org.cru.godtools.base.ui.theme.GodToolsTheme
 import org.greenrobot.eventbus.EventBus
+import org.junit.Assume.assumeFalse
 import org.junit.Rule
 
 abstract class BasePaparazziTest(
-    nightMode: NightMode = NightMode.NOTNIGHT,
-    accessibilityMode: AccessibilityMode = AccessibilityMode.NO_ACCESSIBILITY,
+    private val nightMode: NightMode = NightMode.NOTNIGHT,
+    private val accessibilityMode: AccessibilityMode = AccessibilityMode.NO_ACCESSIBILITY,
     renderingMode: RenderingMode = RenderingMode.NORMAL,
+    private val excludeRedundantTests: Boolean = true,
     private val eventBus: EventBus = EventBus(),
 ) {
     enum class AccessibilityMode { ACCESSIBILITY, NO_ACCESSIBILITY }
@@ -51,6 +54,14 @@ abstract class BasePaparazziTest(
             if (accessibilityMode == AccessibilityMode.ACCESSIBILITY) add(AccessibilityRenderExtension())
         },
     )
+
+    @BeforeTest
+    fun excludeRedundantTests() {
+        if (excludeRedundantTests) {
+            // don't run accessibility mode for night mode, we already test it for Not Night
+            assumeFalse(accessibilityMode == AccessibilityMode.ACCESSIBILITY && nightMode == NightMode.NIGHT)
+        }
+    }
 
     protected fun centerInSnapshot(content: @Composable BoxScope.() -> Unit) {
         paparazzi.snapshot {
