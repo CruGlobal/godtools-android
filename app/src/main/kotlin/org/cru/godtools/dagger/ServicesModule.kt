044cad67c079e72ce700f10ce16a86d68e7b4b01
Daniel Frett <daniel.frett@cru.org>
2024-03-28 11:51:47 -0600

    switch to Kermit for godtools-shared logging

--- app/src/main/kotlin/org/cru/godtools/dagger/ServicesModule.kt
+++ app/src/main/kotlin/org/cru/godtools/dagger/ServicesModule.kt
@@ -11,14 +11,12 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.IntoSet
-import io.github.aakira.napier.Napier
 import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import org.ccci.gto.android.common.dagger.eager.EagerModule
 import org.ccci.gto.android.common.dagger.eager.EagerSingleton
 import org.ccci.gto.android.common.dagger.splitinstall.SplitInstallModule
-import org.ccci.gto.android.common.napier.TimberAntilog
 import org.cru.godtools.service.AccountListRegistrationService
 
 @Module(
@@ -42,12 +40,6 @@ abstract class ServicesModule {
         @Provides
         @Singleton
         fun picasso() = Picasso.get()
-
-        @Provides
-        @IntoSet
-        @Singleton
-        @EagerSingleton(threadMode = EagerSingleton.ThreadMode.MAIN)
-        fun napierConfig(): Any = Napier.apply { base(TimberAntilog) }
 
         @Provides
         @Reusable
