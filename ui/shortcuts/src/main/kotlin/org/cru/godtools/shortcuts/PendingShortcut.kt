2d2478ddd5d799e7d4f946b827265f96c1d59d3c
Daniel Frett <daniel.frett@cru.org>
2024-04-10 15:41:18 -0600

    make the tool and shortcut for a PendingShortcut internal

--- ui/shortcuts/src/main/kotlin/org/cru/godtools/shortcuts/PendingShortcut.kt
+++ ui/shortcuts/src/main/kotlin/org/cru/godtools/shortcuts/PendingShortcut.kt
@@ -3,9 +3,9 @@ package org.cru.godtools.shortcuts
 import androidx.core.content.pm.ShortcutInfoCompat
 import kotlinx.coroutines.sync.Mutex
 
-class PendingShortcut internal constructor(val tool: String) {
+class PendingShortcut internal constructor(internal val tool: String) {
     internal val mutex = Mutex()
 
     @Volatile
-    var shortcut: ShortcutInfoCompat? = null
+    internal var shortcut: ShortcutInfoCompat? = null
 }
