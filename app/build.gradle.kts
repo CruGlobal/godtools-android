f0c29395e7924b69d9655dfd054b08b2e37bbe17
Daniel Frett <daniel.frett@cru.org>
2025-03-28 11:44:25 -0600

    prepare for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
     defaultConfig {
         applicationId = "org.keynote.godtools.android"
-        versionName = "6.3.3-SNAPSHOT"
+        versionName = "6.3.3"
         versionCode = grgit.log(mapOf("includes" to listOf("HEAD"))).size + 4029265
 
         proguardFile(getDefaultProguardFile("proguard-android-optimize.txt"))
