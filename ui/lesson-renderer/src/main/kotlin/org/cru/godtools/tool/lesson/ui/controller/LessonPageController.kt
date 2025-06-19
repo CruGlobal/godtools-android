6b4e5f04589504dbdf9b38bab5b780875b8e7ef6
Daniel Frett <daniel.frett@cru.org>
2025-06-19 16:37:28 -0600

    update LessonPageController to render the content stack using Compose

--- ui/lesson-renderer/src/main/kotlin/org/cru/godtools/tool/lesson/ui/controller/LessonPageController.kt
+++ ui/lesson-renderer/src/main/kotlin/org/cru/godtools/tool/lesson/ui/controller/LessonPageController.kt
@@ -4,12 +4,16 @@ import androidx.lifecycle.LifecycleOwner
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import javax.inject.Named
 import kotlinx.coroutines.Job
+import okio.FileSystem
 import org.ccci.gto.android.common.androidx.lifecycle.onPause
 import org.ccci.gto.android.common.androidx.lifecycle.onResume
-import org.cru.godtools.base.tool.ui.controller.ParentController
-import org.cru.godtools.base.tool.ui.controller.cache.UiControllerCache
+import org.cru.godtools.base.tool.BaseToolRendererModule.Companion.TOOL_RESOURCE_FILE_SYSTEM
+import org.cru.godtools.base.tool.ui.controller.BaseController
+import org.cru.godtools.shared.renderer.content.RenderContentStack
 import org.cru.godtools.shared.renderer.state.State
+import org.cru.godtools.shared.renderer.util.ProvideRendererServices
 import org.cru.godtools.shared.tool.parser.model.AnalyticsEvent.Trigger
 import org.cru.godtools.shared.tool.parser.model.lesson.LessonPage
 import org.cru.godtools.tool.lesson.databinding.LessonPageBinding
@@ -19,9 +23,10 @@ class LessonPageController @AssistedInject constructor(
     @Assisted private val binding: LessonPageBinding,
     @Assisted override val lifecycleOwner: LifecycleOwner,
     @Assisted override val toolState: State,
-    cacheFactory: UiControllerCache.Factory,
-    eventBus: EventBus
-) : ParentController<LessonPage>(LessonPage::class, binding.root, cacheFactory = cacheFactory, eventBus = eventBus) {
+    eventBus: EventBus,
+    @Named(TOOL_RESOURCE_FILE_SYSTEM)
+    private val resourceFileSystem: FileSystem,
+) : BaseController<LessonPage>(LessonPage::class, binding.root, eventBus = eventBus) {
     @AssistedFactory
     interface Factory {
         fun create(binding: LessonPageBinding, lifecycleOwner: LifecycleOwner, toolState: State): LessonPageController
@@ -44,9 +49,15 @@ class LessonPageController @AssistedInject constructor(
     override fun onBind() {
         super.onBind()
         binding.model = model
+        binding.compose.setContent {
+            ProvideRendererServices(resourceFileSystem) {
+                RenderContentStack(
+                    model?.content.orEmpty(),
+                    state = toolState,
+                )
+            }
+        }
     }
-
-    override val childContainer get() = binding.content
 }
 
 fun LessonPageBinding.bindController(
