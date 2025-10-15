fc1116d22fd712bf6ebac503aaa8a0bb9760aeb3
Daniel Frett <daniel.frett@cru.org>
2025-10-15 11:11:27 -0600

    remove some dead logic from lesson-renderer

--- ui/lesson-renderer/src/main/kotlin/org/cru/godtools/tool/lesson/ui/feedback/LessonFeedbackDialogOverlay.kt
+++ ui/lesson-renderer/src/main/kotlin/org/cru/godtools/tool/lesson/ui/feedback/LessonFeedbackDialogOverlay.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
