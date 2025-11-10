0dc1183b4d148f1eca4c98193b779d300da1d9d9
Tim Johnson <timjohnson0914@gmail.com>
2025-11-10 16:23:28 -0500

    Revert change to edit sharesheet message displayed for articles

--- ui/base-tool/src/main/kotlin/org/cru/godtools/base/tool/activity/BaseArticleActivity.kt
+++ ui/base-tool/src/main/kotlin/org/cru/godtools/base/tool/activity/BaseArticleActivity.kt
@@ -13,6 +13,5 @@ abstract class BaseArticleActivity<B : ViewDataBinding> protected constructor(
     protected constructor(@LayoutRes contentLayoutId: Int) : this(contentLayoutId, true)
 
     @get:StringRes
-    override val shareLinkMessageWithQrCodeRes get() = R.string.share_tool_message_article
-//    override val shareLinkMessageRes get() = R.string.share_tool_message_article
+    override val shareLinkMessageRes get() = R.string.share_tool_message_article
 }
