4d0c5e461d5c4b062b0cab8b583a41331cc44981
Tim Johnson <timjohnson0914@gmail.com>
2025-11-11 11:08:25 -0500

    Move customAction logic to defaultShaeItem; Add conditional for android 14

--- ui/base-tool/src/main/kotlin/org/cru/godtools/base/tool/ui/share/model/DefaultShareItem.kt
+++ ui/base-tool/src/main/kotlin/org/cru/godtools/base/tool/ui/share/model/DefaultShareItem.kt
@@ -1,15 +1,45 @@
 package org.cru.godtools.base.tool.ui.share.model
 
 import android.app.Activity
+import android.app.ActivityOptions
+import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.drawable.Icon
+import android.net.Uri
+import android.os.Build
+import android.service.chooser.ChooserAction
+import android.util.Log
 import kotlinx.parcelize.Parcelize
 import org.ccci.gto.android.common.base.Ordered
+import org.cru.godtools.tool.R
 
 @Parcelize
 class DefaultShareItem(override val shareIntent: Intent) : ShareItem {
     override val order get() = Ordered.HIGHEST_PRECEDENCE
 
     override fun triggerAction(activity: Activity) {
-        activity.startActivity(Intent.createChooser(shareIntent, null))
+        val chooserIntent = Intent.createChooser(shareIntent, null)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            val options = ActivityOptions.makeBasic().apply {
+            setPendingIntentCreatorBackgroundActivityStartMode(
+                ActivityOptions.MODE_BACKGROUND_ACTIVITY_START_ALLOWED)
+            }
+            val qrCodeCustomAction = arrayOf(
+                ChooserAction.Builder(
+                    Icon.createWithResource(activity, R.drawable.ic_checkmark),
+                    "Share QR Code",
+                    PendingIntent.getActivity(
+                        activity,
+                        42,
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=godtools"))
+                            .addCategory(Intent.CATEGORY_BROWSABLE),
+                        PendingIntent.FLAG_IMMUTABLE,
+                        options.toBundle()
+                    )
+                ).build()
+            )
+            chooserIntent.putExtra(Intent.EXTRA_CHOOSER_CUSTOM_ACTIONS, qrCodeCustomAction)
+        }
+        activity.startActivity(chooserIntent)
     }
 }
