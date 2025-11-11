88a1c4b86eb324e360829e76a8b6e9dc8aa5461b
Daniel Frett <daniel.frett@cru.org>
2025-11-11 16:33:02 -0700

    render tip page contents using the compose renderer

--- ui/tips-renderer/src/main/kotlin/org/cru/godtools/tool/tips/ui/controller/TipPageController.kt
+++ ui/tips-renderer/src/main/kotlin/org/cru/godtools/tool/tips/ui/controller/TipPageController.kt
@@ -1,12 +1,21 @@
 package org.cru.godtools.tool.tips.ui.controller
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LifecycleOwner
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
-import org.cru.godtools.base.tool.ui.controller.ParentController
-import org.cru.godtools.base.tool.ui.controller.cache.UiControllerCache
+import javax.inject.Named
+import okio.FileSystem
+import org.cru.godtools.base.tool.BaseToolRendererModule.Companion.TOOL_RESOURCE_FILE_SYSTEM
+import org.cru.godtools.base.tool.ui.controller.BaseController
+import org.cru.godtools.base.ui.theme.GodToolsTheme
+import org.cru.godtools.shared.renderer.content.RenderContentStack
 import org.cru.godtools.shared.renderer.state.State
+import org.cru.godtools.shared.renderer.tips.TipsRepository
+import org.cru.godtools.shared.renderer.util.ProvideRendererServices
 import org.cru.godtools.shared.tool.parser.model.tips.TipPage
 import org.cru.godtools.tool.tips.databinding.ToolTipPageBinding
 import org.greenrobot.eventbus.EventBus
@@ -16,8 +25,10 @@ class TipPageController @AssistedInject internal constructor(
     @Assisted override val lifecycleOwner: LifecycleOwner,
     @Assisted override val toolState: State,
     eventBus: EventBus,
-    cacheFactory: UiControllerCache.Factory
-) : ParentController<TipPage>(TipPage::class, binding.root, cacheFactory = cacheFactory, eventBus = eventBus) {
+    @param:Named(TOOL_RESOURCE_FILE_SYSTEM)
+    private val resourceFileSystem: FileSystem,
+    private val tipsRepository: TipsRepository,
+) : BaseController<TipPage>(TipPage::class, binding.root, eventBus = eventBus) {
     @AssistedFactory
     interface Factory {
         fun create(binding: ToolTipPageBinding, lifecycleOwner: LifecycleOwner, toolState: State): TipPageController
@@ -28,11 +39,20 @@ class TipPageController @AssistedInject internal constructor(
         binding.controller = this
     }
 
-    override val childContainer get() = binding.content
-
     override fun onBind() {
         super.onBind()
         binding.page = model
+        binding.compose.setContent {
+            GodToolsTheme(darkTheme = false) {
+                ProvideRendererServices(resourceFileSystem, tipsRepository) {
+                    RenderContentStack(
+                        model?.content.orEmpty(),
+                        state = toolState,
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+                }
+            }
+        }
     }
 }
 
