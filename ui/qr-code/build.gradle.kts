6a0d32f430caa9af75e76d29d2d96e9d275b27a2
Tim Johnson <timjohnson0914@gmail.com>
2025-12-05 16:14:56 -0500

    Add test dependencies for screenshot tests

--- ui/qr-code/build.gradle.kts
+++ ui/qr-code/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("godtools.library-conventions")
+    alias(libs.plugins.paparazzi)
 }
 
 android {
@@ -14,10 +15,14 @@ tasks.withType<Test> {
 
 dependencies {
     implementation(project(":ui:base"))
-
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
-
     implementation(libs.timber)
     implementation(libs.zxing)
+
+    testImplementation(libs.androidx.lifecycle.runtime.testing)
+    testImplementation(libs.junit)
+    testImplementation(libs.testparameterinjector)
+    testImplementation(libs.paparazzi)
+    testImplementation(testFixtures(project(":ui:base")))
 }
