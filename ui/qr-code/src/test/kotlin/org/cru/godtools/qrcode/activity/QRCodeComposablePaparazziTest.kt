389ef4edbb5c9bb739f613e6c83d520270e19664
Tim Johnson <timjohnson0914@gmail.com>
2025-12-05 16:16:01 -0500

    Create test folder and generate paoparazzi screenshots

---
+++ ui/qr-code/src/test/kotlin/org/cru/godtools/qrcode/activity/QRCodeComposablePaparazziTest.kt
@@ -0,0 +1,26 @@
+package org.cru.godtools.qrcode.activity
+
+import androidx.compose.ui.Modifier
+import app.cash.paparazzi.DeviceConfig
+import com.android.resources.NightMode
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import org.cru.godtools.base.ui.BasePaparazziTest
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+class QRCodeComposablePaparazziTest(
+    @TestParameter(valuesProvider = DeviceConfigProvider::class) deviceConfig: DeviceConfig,
+    @TestParameter nightMode: NightMode,
+    @TestParameter accessibilityMode: AccessibilityMode,
+) : BasePaparazziTest(deviceConfig = deviceConfig, nightMode = nightMode, accessibilityMode = accessibilityMode) {
+
+    @Test
+    fun `QRCode Composable`() = snapshot {
+        QRCodeScreen(
+            url = "https://example.com",
+            modifier = Modifier
+        )
+    }
+}
