da56620f29c7df50439a7ff5b4bc9c365666667f
Tim Johnson <timjohnson0914@gmail.com>
2026-03-06 13:50:08 -0500

    expose connectivity state in downloadable languages UiState

--- app/src/main/kotlin/org/cru/godtools/ui/languages/downloadable/DownloadableLanguagesScreen.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/languages/downloadable/DownloadableLanguagesScreen.kt
@@ -1,6 +1,7 @@
 package org.cru.godtools.ui.languages.downloadable
 
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
@@ -16,14 +17,13 @@ object DownloadableLanguagesScreen : Screen {
     data class UiState(
         val query: MutableState<String> = mutableStateOf(""),
         val languages: ImmutableList<UiLanguage> = persistentListOf(),
-        val showOfflineDialog: Boolean = false,
+        val isConnected: State<Boolean> = mutableStateOf(true),
         val eventSink: (UiEvent) -> Unit = {},
     ) : CircuitUiState {
         data class UiLanguage(val language: Language, val downloadedTools: Int = 0, val totalTools: Int = 0)
 
         sealed interface UiEvent : CircuitUiEvent {
             data object NavigateUp : UiEvent
-            data object DismissOfflineDialog : UiEvent
             data class PinLanguage(val locale: Locale) : UiEvent
             data class UnpinLanguage(val locale: Locale) : UiEvent
         }
