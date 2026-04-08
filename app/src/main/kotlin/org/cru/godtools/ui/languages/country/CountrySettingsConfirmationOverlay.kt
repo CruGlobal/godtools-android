5042437827bdb52e8d5a497991ccfc8ddffecb57
Tim Johnson <timjohnson0914@gmail.com>
2026-04-08 10:20:50 -0400

    Make country displayName nullable for better null handling

--- app/src/main/kotlin/org/cru/godtools/ui/languages/country/CountrySettingsConfirmationOverlay.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/languages/country/CountrySettingsConfirmationOverlay.kt
@@ -17,7 +17,7 @@ import com.slack.circuit.overlay.OverlayNavigator
 import org.ccci.gto.android.common.androidx.compose.ui.text.res.annotatedStringResource
 import org.cru.godtools.R
 
-class CountrySettingsConfirmationOverlay(val countryName: String) : Overlay<Boolean> {
+class CountrySettingsConfirmationOverlay(val countryName: String?) : Overlay<Boolean> {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content(navigator: OverlayNavigator<Boolean>) {
