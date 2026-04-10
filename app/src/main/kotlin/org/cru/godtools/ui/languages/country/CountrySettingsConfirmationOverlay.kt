28122df01a9800a77a85c156f834095db04c8dbe
Tim Johnson <timjohnson0914@gmail.com>
2026-04-10 10:48:11 -0400

    Lint fix for PR

--- app/src/main/kotlin/org/cru/godtools/ui/languages/country/CountrySettingsConfirmationOverlay.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/languages/country/CountrySettingsConfirmationOverlay.kt
@@ -1,7 +1,6 @@
 package org.cru.godtools.ui.languages.country
 
 import androidx.compose.material3.AlertDialog
-
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
