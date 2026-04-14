3dffbc3fa964aff446ef8329e3b0abe7d176e024
Tim Johnson <timjohnson0914@gmail.com>
2026-04-14 17:02:29 -0400

    Clean up listItem modifier clickable block

--- app/src/main/kotlin/org/cru/godtools/ui/languages/country/CountrySettingsLayout.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/languages/country/CountrySettingsLayout.kt
@@ -164,18 +164,11 @@ internal fun CountrySettingsLayout(state: UiState, modifier: Modifier = Modifier
                     },
                     modifier = Modifier.clickable {
                         coroutineScope.launch {
-                            if (state.countryCode == country.isoCode) {
-                                eventSink(UiEvent.SelectCountry(country.isoCode))
-                            } else {
-                                val confirmCountrySelection = overlayHost.show(
-                                    CountrySettingsConfirmationOverlay(
-                                        country.displayName ?: country.isoCode,
-                                    ),
-                                )
-                                if (confirmCountrySelection) {
-                                    eventSink(UiEvent.SelectCountry(country.isoCode))
-                                }
+                            if (country.isoCode != state.countryCode) {
+                                val overlay = CountrySettingsConfirmationOverlay(country.displayName ?: country.isoCode)
+                                if (!overlayHost.show(overlay)) return@launch
                             }
+                            eventSink(UiEvent.SelectCountry(country.isoCode))
                         }
                     },
                 )
