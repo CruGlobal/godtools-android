d525bcb1f0d4ed8941cb7e3bf3e52a3cc7b735a1
Tim Johnson <timjohnson0914@gmail.com>
2026-04-21 10:29:55 -0400

    Add pop result to app language screen to track result

--- ui/base/src/main/kotlin/org/cru/godtools/base/ui/circuit/screen/AppLanguageScreen.kt
+++ ui/base/src/main/kotlin/org/cru/godtools/base/ui/circuit/screen/AppLanguageScreen.kt
@@ -1,7 +1,13 @@
 package org.cru.godtools.base.ui.circuit.screen
 
+import com.slack.circuit.runtime.screen.PopResult
 import com.slack.circuit.runtime.screen.Screen
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data object AppLanguageScreen : Screen
+data object AppLanguageScreen : Screen {
+    sealed interface Result : PopResult {
+        @Parcelize data object SetLanguage : Result
+        @Parcelize data object DismissAppLanguage : Result
+    }
+}
