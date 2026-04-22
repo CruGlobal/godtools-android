c31fb6102d1e49fd8c55194722b27c0f84d6d944
Tim Johnson <timjohnson0914@gmail.com>
2026-04-22 17:26:26 -0400

    Add answering navigators and Next event for language/country flow in presenter

--- app/src/main/kotlin/org/cru/godtools/ui/onboarding/OnboardingPresenter.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/onboarding/OnboardingPresenter.kt
@@ -2,7 +2,13 @@ package org.cru.godtools.ui.onboarding
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import com.slack.circuit.codegen.annotations.CircuitInject
+import com.slack.circuit.foundation.rememberAnsweringNavigator
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.Navigator
@@ -16,6 +22,7 @@ import org.cru.godtools.base.Settings.Companion.FEATURE_TUTORIAL_ONBOARDING
 import org.cru.godtools.base.ui.circuit.screen.AppLanguageScreen
 import org.cru.godtools.shared.analytics.TutorialAnalyticsActionNames
 import org.cru.godtools.tutorial.analytics.model.TutorialAnalyticsActionEvent
+import org.cru.godtools.ui.languages.country.CountrySettingsScreen
 import org.cru.godtools.ui.onboarding.OnboardingPresenter.UiState
 import org.greenrobot.eventbus.EventBus
 
@@ -24,21 +31,50 @@ class OnboardingPresenter @AssistedInject constructor(
     private val settings: Settings,
     @Assisted private val navigator: Navigator,
 ) : Presenter<UiState> {
-    data class UiState(val eventSink: (UiEvent) -> Unit = {}) : CircuitUiState
+    data class UiState(
+        val currentPage: Int = 0,
+        val userScrollEnabled: Boolean = false,
+        val eventSink: (UiEvent) -> Unit = {}
+    ) : CircuitUiState
 
     sealed interface UiEvent : CircuitUiEvent {
         data object ChangeLanguage : UiEvent
+        data object Next : UiEvent
         data object Skip : UiEvent
         data object Finish : UiEvent
     }
 
     @Composable
     override fun present(): UiState {
+        var languageWasSet by rememberSaveable { mutableStateOf(false) }
+        var currentPage by rememberSaveable { mutableIntStateOf(0) }
+        val countrySettingsNavigator = rememberAnsweringNavigator<CountrySettingsScreen.Result>(navigator) {
+            currentPage = 1
+        }
+        val languageButtonNavigator = rememberAnsweringNavigator<AppLanguageScreen.Result>(navigator) { result ->
+            if (result is AppLanguageScreen.Result.LanguageSelected) {
+                languageWasSet = true
+            }
+        }
+        val nextButtonNavigator = rememberAnsweringNavigator<AppLanguageScreen.Result>(navigator) { result ->
+            if (result is AppLanguageScreen.Result.LanguageSelected) {
+                languageWasSet = true
+                countrySettingsNavigator.goTo(CountrySettingsScreen)
+            }
+        }
         LaunchedEffect(Unit) { settings.setFeatureDiscovered(FEATURE_TUTORIAL_ONBOARDING) }
 
-        return UiState { event ->
+        val eventSink: (UiEvent) -> Unit = { event ->
             when (event) {
-                UiEvent.ChangeLanguage -> navigator.goTo(AppLanguageScreen)
+                UiEvent.ChangeLanguage -> languageButtonNavigator.goTo(AppLanguageScreen)
+
+                UiEvent.Next -> {
+                    if (languageWasSet) {
+                        countrySettingsNavigator.goTo(CountrySettingsScreen)
+                    } else {
+                        nextButtonNavigator.goTo(AppLanguageScreen)
+                    }
+                }
 
                 UiEvent.Skip -> {
                     eventBus.post(TutorialAnalyticsActionEvent(TutorialAnalyticsActionNames.ONBOARDING_SKIP))
@@ -51,6 +87,12 @@ class OnboardingPresenter @AssistedInject constructor(
                 }
             }
         }
+
+        return UiState(
+            currentPage = currentPage,
+            userScrollEnabled = currentPage > 0,
+            eventSink = eventSink
+        )
     }
 
     @AssistedFactory
