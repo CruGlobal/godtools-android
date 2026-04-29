9c8be9a7ac8efdb00cd48c98b4464cf10da36054
Tim Johnson <timjohnson0914@gmail.com>
2026-04-29 17:19:42 -0400

    Fix import to match new file structure

--- app/src/main/kotlin/org/cru/godtools/ui/onboarding/OnboardingPresenter.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/onboarding/OnboardingPresenter.kt
@@ -27,7 +27,7 @@ import org.cru.godtools.base.Settings.Companion.FEATURE_TUTORIAL_ONBOARDING
 import org.cru.godtools.base.ui.circuit.screen.AppLanguageScreen
 import org.cru.godtools.shared.analytics.TutorialAnalyticsActionNames
 import org.cru.godtools.tutorial.analytics.model.TutorialAnalyticsActionEvent
-import org.cru.godtools.ui.languages.country.CountrySettingsScreen
+import org.cru.godtools.ui.settings.country.CountrySettingsScreen
 import org.cru.godtools.ui.onboarding.OnboardingPresenter.UiState
 import org.greenrobot.eventbus.EventBus
 
