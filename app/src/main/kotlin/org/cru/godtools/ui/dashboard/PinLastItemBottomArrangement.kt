6cb5543e766dc3cdf27b0f2cdebed14f29d8532a
Tim Johnson <timjohnson0914@gmail.com>
2026-05-22 15:44:09 -0400

    Remove unnecessary key from remember and params

--- app/src/main/kotlin/org/cru/godtools/ui/dashboard/PinLastItemBottomArrangement.kt
+++ app/src/main/kotlin/org/cru/godtools/ui/dashboard/PinLastItemBottomArrangement.kt
@@ -6,7 +6,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.Density
 
 @Composable
-internal fun rememberPinLastItemBottomArrangement(items: Int): Arrangement.Vertical = remember(items) {
+internal fun rememberPinLastItemBottomArrangement(): Arrangement.Vertical = remember {
     object : Arrangement.Vertical {
         override fun Density.arrange(totalSize: Int, sizes: IntArray, outPositions: IntArray) {
             var currentOffset = 0
